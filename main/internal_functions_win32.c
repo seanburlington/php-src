eda203bee31eb163c5bb89e35937031a3ccf83f4
andi <andi>
1999-08-09 18:29:40 +0000

    Go beta 2 (final)

--- main/internal_functions_win32.c
+++ main/internal_functions_win32.c
@@ -18,7 +18,7 @@
  */
 
 
-/* $Id: internal_functions_win32.c,v 1.7 1999/07/19 16:08:43 andi Exp $ */
+/* $Id: internal_functions_win32.c,v 1.8 1999/08/09 18:29:40 andi Exp $ */
 
 
 #include "php.h"
@@ -47,6 +47,7 @@
 #include "ext/COM/php3_COM.h"
 #include "ext/standard/reg.h"
 #include "ext/pcre/php_pcre.h"
+#include "ext/odbc/php3_odbc.h"
 
 /* SNMP has to be moved to ext */
 /* #include "dl/snmp/php3_snmp.h" */
@@ -74,7 +75,8 @@ zend_module_entry *php3_builtin_modules[] = {
 	phpext_standard_ptr,
 	COM_module_ptr,
 	phpext_regex_ptr,
-	phpext_pcre_ptr
+	phpext_pcre_ptr,
+	phpext_odbc_ptr
 };
 
 	

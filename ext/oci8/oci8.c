680b25af644e3c00bbf6a23d9ae217a237637660
thies <thies>
1999-08-18 12:51:51 +0000

    ocifetchinto fix

--- ext/oci8/oci8.c
+++ ext/oci8/oci8.c
@@ -22,7 +22,7 @@
 
 #define OCI_USE_EMALLOC 0		/* set this to 1 if you want to use the php memory manager! */
 
-/* $Id: oci8.c,v 1.18 1999/08/17 13:54:24 thies Exp $ */
+/* $Id: oci8.c,v 1.19 1999/08/18 12:51:51 thies Exp $ */
 
 /* TODO list:
  *
@@ -3041,6 +3041,8 @@ PHP_FUNCTION(ocifetchinto)
 			RETURN_FALSE;
 		}
 	}
+
+	zend_hash_internal_pointer_reset(array->value.ht);
 
 #if PHP_API_VERSION < 19990421
 	element = emalloc(sizeof(pval));

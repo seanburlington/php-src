5ac695868c34943679eea5eb6fd7d8328f15c517
riffraff <riffraff>
1999-09-05 00:55:48 +0000

    Appended second argument of addslashes(), Changed php_addslashes() to add \000 instead of just \0

--- ext/standard/php3_string.h
+++ ext/standard/php3_string.h
@@ -29,7 +29,7 @@
  */
 
 
-/* $Id: php3_string.h,v 1.11 1999/09/04 13:18:56 zeev Exp $ */
+/* $Id: php3_string.h,v 1.12 1999/09/05 00:55:48 riffraff Exp $ */
 
 /* Synced with php3 revision 1.43 1999-06-16 [ssb] */
 
@@ -89,6 +89,7 @@ extern PHPAPI char *_php3_strtoupper(char *s);
 extern PHPAPI char *_php3_strtolower(char *s);
 extern PHPAPI char *_php3_strtr(char *string, int len, char *str_from, char *str_to, int trlen);
 extern PHPAPI char *php_addslashes(char *string, int length, int *new_length, int freeit);
+extern PHPAPI char *php_addslashes_ex(char *string, int length, int *new_length, int freeit, char *what, int wlength);
 extern PHPAPI void php_stripslashes(char *string, int *len);
 extern PHPAPI void _php3_dirname(char *str, int len);
 extern PHPAPI char *php3i_stristr(unsigned char *s, unsigned char *t);

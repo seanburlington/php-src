e291331891dd3814fb152bd09c5c9d7c35ac33a7
andrey <andrey>
1999-09-11 20:36:47 +0000

    Use OnUpdateBool for Boolean entries.

--- main/main.c
+++ main/main.c
@@ -19,7 +19,7 @@
 */
 
 
-/* $Id: main.c,v 1.119 1999/09/11 16:32:08 zeev Exp $ */
+/* $Id: main.c,v 1.120 1999/09/11 20:36:47 andrey Exp $ */
 
 
 #include <stdio.h>
@@ -229,7 +229,7 @@ PHP_INI_BEGIN()
 	STD_PHP_INI_BOOLEAN("gpc_globals",			"1",								PHP_INI_ALL,		OnUpdateBool,				gpc_globals,	php_core_globals,	core_globals)
 	STD_PHP_INI_ENTRY("gpc_order",				"GPC",			PHP_INI_ALL,		OnUpdateStringUnempty,	gpc_order,		php_core_globals,	core_globals)
 	STD_PHP_INI_ENTRY("arg_separator",			"&",			PHP_INI_ALL,		OnUpdateStringUnempty,	arg_separator,	php_core_globals,	core_globals)
-	STD_PHP_INI_BOOLEAN("ignore_user_abort",	"1",			PHP_INI_ALL,		OnUpdateInt,			ignore_user_abort,		php_core_globals,	core_globals)
+	STD_PHP_INI_BOOLEAN("ignore_user_abort",	"1",			PHP_INI_ALL,		OnUpdateBool,			ignore_user_abort,		php_core_globals,	core_globals)
 PHP_INI_END()
 
 

fe99834eb401958bdc25e1238cdf0405dfe69b06
askalski <askalski>
1999-09-20 15:40:22 +0000

    php3 compatibility added

--- ext/ftp/php_ftp.h
+++ ext/ftp/php_ftp.h
@@ -1,4 +1,4 @@
-/* $Id: php_ftp.h,v 1.1 1999/09/20 14:10:25 askalski Exp $ */
+/* $Id: php_ftp.h,v 1.2 1999/09/20 15:40:22 askalski Exp $ */
 
 #ifndef	_INCLUDED_FTP_H
 #define	_INCLUDED_FTP_H
@@ -13,7 +13,11 @@
 extern php3_module_entry php3_ftp_module_entry;
 #define php3_ftp_module_ptr &php3_ftp_module_entry
 
-extern PHP_MINIT_FUNCTION(ftp);
+#ifdef ZEND_VERSION
+PHP_MINIT_FUNCTION(ftp);
+#else
+int PHP_MINIT_FUNCTION(INIT_FUNC_ARGS);
+#endif
 
 PHP_FUNCTION(ftp_connect);
 PHP_FUNCTION(ftp_login);
@@ -23,7 +27,7 @@ PHP_FUNCTION(ftp_chdir);
 PHP_FUNCTION(ftp_mkdir);
 PHP_FUNCTION(ftp_rmdir);
 PHP_FUNCTION(ftp_nlist);
-PHP_FUNCTION(ftp_listraw);
+PHP_FUNCTION(ftp_rawlist);
 PHP_FUNCTION(ftp_systype);
 PHP_FUNCTION(ftp_get);
 PHP_FUNCTION(ftp_put);

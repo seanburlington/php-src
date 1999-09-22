7d716971b96e0f2b1a9798bf495d183056964fe1
askalski <askalski>
1999-09-22 16:07:41 +0000

    Added optional port argument to ftp_connect()

--- ext/ftp/php_ftp.h
+++ ext/ftp/php_ftp.h
@@ -1,4 +1,34 @@
-/* $Id: php_ftp.h,v 1.2 1999/09/20 15:40:22 askalski Exp $ */
+/*
+   +----------------------------------------------------------------------+
+   | PHP HTML Embedded Scripting Language Version 3.0                     |
+   +----------------------------------------------------------------------+
+   | Copyright (c) 1997-1999 PHP Development Team (See Credits file)      |
+   +----------------------------------------------------------------------+
+   | This program is free software; you can redistribute it and/or modify |
+   | it under the terms of one of the following licenses:                 |
+   |                                                                      |
+   |  A) the GNU General Public License as published by the Free Software |
+   |     Foundation; either version 2 of the License, or (at your option) |
+   |     any later version.                                               |
+   |                                                                      |
+   |  B) the PHP License as published by the PHP Development Team and     |
+   |     included in the distribution in the file: LICENSE                |
+   |                                                                      |
+   | This program is distributed in the hope that it will be useful,      |
+   | but WITHOUT ANY WARRANTY; without even the implied warranty of       |
+   | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        |
+   | GNU General Public License for more details.                         |
+   |                                                                      |
+   | You should have received a copy of both licenses referred to here.   |
+   | If you did not, or have any questions about PHP licensing, please    |
+   | contact core@php.net.                                                |
+   +----------------------------------------------------------------------+
+   | Authors:                                                             |
+   |          Andrew Skalski      <askalski@chek.com>                     |
+   +----------------------------------------------------------------------+
+ */
+
+/* $Id: php_ftp.h,v 1.3 1999/09/22 16:07:41 askalski Exp $ */
 
 #ifndef	_INCLUDED_FTP_H
 #define	_INCLUDED_FTP_H

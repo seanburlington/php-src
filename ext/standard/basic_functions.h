a24784568ba32ac791eb1d5d430744e79b4be530
andi <andi>
1999-10-16 10:14:45 +0000

    - Get windows version to compile again. No biggy if array_map is not #if 0'd   out even if it doesn't work.

--- ext/standard/basic_functions.h
+++ ext/standard/basic_functions.h
@@ -29,7 +29,7 @@
  */
 
 
-/* $Id: basic_functions.h,v 1.23 1999/10/15 22:04:59 andrei Exp $ */
+/* $Id: basic_functions.h,v 1.24 1999/10/16 10:14:46 andi Exp $ */
 
 #ifndef _BASIC_FUNCTIONS_H
 #define _BASIC_FUNCTIONS_H
@@ -60,6 +60,7 @@ PHP_FUNCTION(usort);
 PHP_FUNCTION(uasort);
 PHP_FUNCTION(uksort);
 PHP_FUNCTION(array_walk);
+PHP_FUNCTION(array_map);
 PHP_FUNCTION(count);
 PHP_FUNCTION(flush);
 PHP_FUNCTION(end);

22430a40fb8d8ae2a876fe20a8408c7517afbf9d
thies <thies>
1999-11-14 15:34:50 +0000

    @- Fixed strtr() not to modify arg1. (Thies) (PHP strtr) no longer modifies arg1.

--- ext/standard/string.c
+++ ext/standard/string.c
@@ -18,7 +18,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: string.c,v 1.53 1999/10/27 22:06:05 andrei Exp $ */
+/* $Id: string.c,v 1.54 1999/11/14 15:34:50 thies Exp $ */
 
 /* Synced with php3 revision 1.193 1999-06-16 [ssb] */
 
@@ -1008,12 +1008,13 @@ PHP_FUNCTION(strtr)
 	convert_to_string_ex(from);
 	convert_to_string_ex(to);
 
-	RETVAL_STRING(php_strtr((*str)->value.str.val,
-							(*str)->value.str.len,
-							(*from)->value.str.val,
-							(*to)->value.str.val,
-							MIN((*from)->value.str.len,(*to)->value.str.len)),
-				  1);
+	*return_value=**str;
+	zval_copy_ctor(return_value);                                                                                        
+	php_strtr(return_value->value.str.val,
+			  return_value->value.str.len,
+			  (*from)->value.str.val,
+			  (*to)->value.str.val,
+			  MIN((*from)->value.str.len,(*to)->value.str.len));
 }
 /* }}} */
 

2907816bc06fe0a6a04c5907e53e6620ad10188f
thies <thies>
1999-11-19 14:11:15 +0000

    @- Fixed mem-overwrite in XML_Parse_Into_Struct. (Thies) (PHP xml_parse_into_struct) buffer was sometimes short by 1.

--- ext/xml/xml.c
+++ ext/xml/xml.c
@@ -17,7 +17,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: xml.c,v 1.28 1999/10/20 14:58:03 thies Exp $ */
+/* $Id: xml.c,v 1.29 1999/11/19 14:11:15 thies Exp $ */
 #define IS_EXT_MODULE
 
 #if COMPILE_DL
@@ -819,7 +819,7 @@ void _xml_characterDataHandler(void *userData, const XML_Char *s, int len)
 					/* check if the current tag already has a value - if yes append to that! */
 					if (zend_hash_find((*parser->ctag)->value.ht,"value",sizeof("value"),(void **) &myval) == SUCCESS) {
 						int newlen = (*myval)->value.str.len + decoded_len;
-						(*myval)->value.str.val = erealloc((*myval)->value.str.val,newlen);
+						(*myval)->value.str.val = erealloc((*myval)->value.str.val,newlen+1);
 						strcpy((*myval)->value.str.val + (*myval)->value.str.len,decoded_value);
 						(*myval)->value.str.len += decoded_len;
 						efree(decoded_value);

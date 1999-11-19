a9accef1d5efa4284ed4a13c9447b62896d9812e
andrei <andrei>
1999-11-19 22:28:06 +0000

    Fixed a bug that would replace only the first occurrence and result in a parse error for the rest when using /e modifier.

--- ext/pcre/php_pcre.c
+++ ext/pcre/php_pcre.c
@@ -16,7 +16,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: php_pcre.c,v 1.26 1999/10/28 16:41:17 andrei Exp $ */
+/* $Id: php_pcre.c,v 1.27 1999/11/19 22:28:06 andrei Exp $ */
 
 /*
 	TODO:
@@ -675,7 +675,7 @@ char *_php_pcre_replace(char *regex, char *subject, char *replace)
 			
 			/* If evaluating, do it and add the return string's length */
 			if (eval) {
-				eval_result_len = _preg_do_eval(replace, piece, offsets,
+				eval_result_len = _preg_do_eval(replace, subject, offsets,
 												count, &eval_result);
 				new_len += eval_result_len;
 			} else { /* do regular substitution */

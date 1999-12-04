11b9719b474abdaf495942c4207b934dadca19fd
thies <thies>
1999-12-04 12:06:32 +0000

    fixed tiny leak

--- ext/oci8/oci8.c
+++ ext/oci8/oci8.c
@@ -22,7 +22,7 @@
 
 #define OCI_USE_EMALLOC 0		/* set this to 1 if you want to use the php memory manager! */
 
-/* $Id: oci8.c,v 1.39 1999/12/01 07:34:06 thies Exp $ */
+/* $Id: oci8.c,v 1.40 1999/12/04 12:06:32 thies Exp $ */
 
 /* TODO list:
  *
@@ -2229,6 +2229,7 @@ PHP_FUNCTION(ocidefinebyname)
 		efree(define);
 		define = tmp_define;
 	} else {
+		efree(define);
 		RETURN_FALSE;
 	}
 

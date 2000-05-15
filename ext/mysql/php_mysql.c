5696c1a7893b0e3d0b46f1ef88a44262109d8b59
andrei <andrei>
2000-05-15 13:46:37 +0000

    @- NULL values are now preserved in the return value of mysql_fetch_array() @  and mysql_fetch_object(). (Andrei)

--- ext/mysql/php_mysql.c
+++ ext/mysql/php_mysql.c
@@ -16,7 +16,7 @@
    +----------------------------------------------------------------------+
 */
  
-/* $Id: php_mysql.c,v 1.42 2000/05/02 00:44:24 sas Exp $ */
+/* $Id: php_mysql.c,v 1.43 2000/05/15 13:46:37 andrei Exp $ */
 
 
 /* TODO:
@@ -1317,8 +1317,11 @@ static void php_mysql_fetch_hash(INTERNAL_FUNCTION_PARAMETERS, int result_type)
 				add_assoc_stringl(return_value, mysql_field->name, data, data_len, should_copy);
 			}
 		} else {
-			/* NULL field, don't set it */
-			/* add_get_index_stringl(return_value, i, empty_string, 0, (void **) &pval_ptr); */
+			/* NULL value. */
+			if (result_type & MYSQL_NUM)
+				add_index_unset(return_value, i);
+			else
+				add_assoc_unset(return_value, mysql_field->name);
 		}
 	}
 }

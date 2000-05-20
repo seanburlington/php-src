036bef3bad7fb03c7e83b3e4c1f135bdd3a54c94
sterling <sterling>
2000-05-20 00:13:05 +0000

    Proto fixes.
    #Harmut -- I love your web site, I was able to see that these functions
    #didn't have prototypes just by checking out the SWF extension area!

--- ext/swf/swf.c
+++ ext/swf/swf.c
@@ -16,7 +16,7 @@
    +----------------------------------------------------------------------+
 */
 
-/* $Id: swf.c,v 1.11 2000/05/18 15:34:36 zeev Exp $ */
+/* $Id: swf.c,v 1.12 2000/05/20 00:13:05 sterling Exp $ */
 
 
 #include "php.h"
@@ -287,7 +287,7 @@ PHP_FUNCTION(swf_modifyobject)
 }
 /* }}} */
 
-/* {{{ swf_removeobject(int depth)
+/* {{{ proto void swf_removeobject(int depth)
    Removes the object at the specified depth */
 PHP_FUNCTION(swf_removeobject)
 {
@@ -990,7 +990,7 @@ PHP_FUNCTION(swf_viewport)
 }
 /* }}} */
 
-/* {{{ void swf_ortho2(double xmin, double xmax, double ymin, double ymax)
+/* {{{ proto void swf_ortho2(double xmin, double xmax, double ymin, double ymax)
    Defines a 2-D orthographic mapping of user coordinates onto the current viewport */ 
 PHP_FUNCTION(swf_ortho2)
 {

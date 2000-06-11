ae7dc437dff441e6ecec6a89e209038019af385b
andi <andi>
2000-06-11 16:34:53 +0000

    - Make shockwave work with virtual_dir (a bit ugly). - swf_openfile doesn't return any values. This seems to mean that you can - only have one file open at a time.

--- ext/swf/swf.c
+++ ext/swf/swf.c
@@ -16,7 +16,7 @@
    +----------------------------------------------------------------------+
 */
 
-/* $Id: swf.c,v 1.18 2000/06/11 07:51:10 sterling Exp $ */
+/* $Id: swf.c,v 1.19 2000/06/11 16:34:53 andi Exp $ */
 
 
 #include "php.h"
@@ -168,10 +168,19 @@ PHP_FUNCTION(swf_openfile)
 	convert_to_double_ex(b);
 	
 	na = Z_STRVAL_PP(name);
+
+#ifdef VIRTUAL_DIR
+	if (virtual_filepath(na, &na)) {
+		return;
+	}
+#endif
 	
 	swf_openfile((strcasecmp("php://stdout", na)==0) ? fileno(stdout) : na,
 			 (float)Z_DVAL_PP(sizeX), (float)Z_DVAL_PP(sizeY),
       		 	 (float)Z_DVAL_PP(frameRate), (float)Z_DVAL_PP(r), (float)Z_DVAL_PP(g), (float)Z_DVAL_PP(b));
+#ifdef VIRTUAL_DIR
+	free(na);
+#endif
 }
 /* }}} */
 

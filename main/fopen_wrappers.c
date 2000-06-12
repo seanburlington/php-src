855e6e5579563ee7bbb85b4895736844b4eca4bc
zeev <zeev>
2000-06-12 20:22:16 +0000

    Avoid using E_CORE_* errorlevels in any place which is not in the global startup sequence

--- main/fopen_wrappers.c
+++ main/fopen_wrappers.c
@@ -16,7 +16,7 @@
    |          Jim Winstead <jimw@php.net>                                 |
    +----------------------------------------------------------------------+
  */
-/* $Id: fopen_wrappers.c,v 1.62 2000/06/09 13:50:39 andrei Exp $ */
+/* $Id: fopen_wrappers.c,v 1.63 2000/06/12 20:22:17 zeev Exp $ */
 
 #include "php.h"
 #include "php_globals.h"
@@ -361,7 +361,7 @@ PHPAPI FILE *php_fopen_primary_script(void)
 		fp = NULL;
 	}
 	if (!fp) {
-		php_error(E_CORE_ERROR, "Unable to open %s", fn);
+		php_error(E_ERROR, "Unable to open %s", fn);
 		STR_FREE(SG(request_info).path_translated);	/* for same reason as above */
 		return NULL;
 	}

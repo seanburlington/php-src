b4bb110c5ac60d76a7911272ebd77cfa1845745a
david <david>
2000-07-24 10:31:43 +0000

    licence versions -> 2.02 in pear files

--- pear/PEAR.php
+++ pear/PEAR.php
@@ -5,10 +5,10 @@
 // +----------------------------------------------------------------------+
 // | Copyright (c) 1997, 1998, 1999, 2000 The PHP Group                   |
 // +----------------------------------------------------------------------+
-// | This source file is subject to version 2.0 of the PHP license,       |
+// | This source file is subject to version 2.02 of the PHP license,      |
 // | that is bundled with this package in the file LICENSE, and is        |
 // | available at through the world-wide-web at                           |
-// | http://www.php.net/license/2_0.txt.                                  |
+// | http://www.php.net/license/2_02.txt.                                 |
 // | If you did not receive a copy of the PHP license and are unable to   |
 // | obtain it through the world-wide-web, please send a note to          |
 // | license@php.net so we can mail you a copy immediately.               |
@@ -17,7 +17,7 @@
 // |          Stig Bakken <ssb@fast.no>                                   |
 // +----------------------------------------------------------------------+
 //
-// $Id: PEAR.php,v 1.1 2000/07/24 08:05:51 ssb Exp $
+// $Id: PEAR.php,v 1.2 2000/07/24 10:31:43 david Exp $
 //
 
 define('PEAR_ERROR_RETURN', 0);

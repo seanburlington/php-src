0b3fca7548426191c1b9485ce714504bf28c11fb
sterling <sterling>
2000-08-15 16:50:14 +0000

    This fixes to problems:
    - Apache not having the output go to stdout in some cases.
    - Output being displayed before the header is displayed.
    
    And causes the following:
    
    - *Slightly* slower cURL transfers.

--- ext/curl/php_curl.h
+++ ext/curl/php_curl.h
@@ -16,7 +16,7 @@
    +----------------------------------------------------------------------+
 */
 
-/* $Id: php_curl.h,v 1.4 2000/08/13 19:32:09 sterling Exp $ */
+/* $Id: php_curl.h,v 1.5 2000/08/15 16:50:14 sterling Exp $ */
 
 #ifndef _PHP_CURL_H
 #define _PHP_CURL_H
@@ -32,6 +32,7 @@ extern zend_module_entry curl_module_entry;
 #define curl_module_ptr &curl_module_entry
 
 PHP_MINIT_FUNCTION(curl);
+PHP_MSHUTDOWN_FUNCTION(curl);
 PHP_MINFO_FUNCTION(curl);
 PHP_FUNCTION(curl_version);
 PHP_FUNCTION(curl_init);
@@ -87,6 +88,7 @@ PHP_FUNCTION(curl_close);
 #define C_LAST 45
 
 typedef struct {
+	int use_file = 0;
 	int le_curl;
 } php_curl_globals;
 

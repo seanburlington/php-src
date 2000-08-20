ad0740b4a220814f0a84376bfa9c7bbb7e3645e7
zeev <zeev>
2000-08-20 22:18:19 +0000

    Fix build

--- sapi/pi3web/pi3web_sapi.c
+++ sapi/pi3web/pi3web_sapi.c
@@ -20,7 +20,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: pi3web_sapi.c,v 1.7 2000/08/20 14:29:00 sas Exp $ */
+/* $Id: pi3web_sapi.c,v 1.8 2000/08/20 22:18:19 zeev Exp $ */
 
 #if WIN32|WINNT
 #  include <windows.h>
@@ -80,7 +80,7 @@ static void php_info_pi3web(ZEND_MODULE_INFO_FUNC_ARGS)
 	PUTS("<table border=5 width=600>\n");
 	PUTS("<tr><th colspan=2 bgcolor=\"" PHP_HEADER_COLOR "\">Pi3Web Server Information</th></tr>\n");
 	php_info_print_table_header(2, "Information Field", "Value");
-	php_info_print_table_row(2, "Pi3Web SAPI module version", "$Id: pi3web_sapi.c,v 1.7 2000/08/20 14:29:00 sas Exp $");
+	php_info_print_table_row(2, "Pi3Web SAPI module version", "$Id: pi3web_sapi.c,v 1.8 2000/08/20 22:18:19 zeev Exp $");
 	php_info_print_table_row(2, "Server Name Stamp", HTTPCore_getServerStamp());
 	snprintf(variable_buf, 511, "%d", HTTPCore_debugEnabled());
 	php_info_print_table_row(2, "Debug Enabled", variable_buf);
@@ -306,8 +306,8 @@ static sapi_module_struct sapi_module = {
 
 static void init_request_info(sapi_globals_struct *sapi_globals, LPCONTROL_BLOCK lpCB)
 {
-	char *path_end = strrchr(lpCB->lpszFileName, PHP_SEPARATOR);
-	if ( path_end ) *path_end = PHP_SEPARATOR;
+	char *path_end = strrchr(lpCB->lpszFileName, PHP_DIR_SEPARATOR);
+	if ( path_end ) *path_end = PHP_DIR_SEPARATOR;
 
 	SG(server_context) = lpCB;
 	SG(request_info).request_method  = lpCB->lpszMethod;

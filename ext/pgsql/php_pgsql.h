10da940d19b5858505a043e7a1c1a969ccde94b8
derick <derick>
2000-09-11 15:34:14 +0000

    - Added functions pg_putline and pg_endcopy (thanks to Dirk Elmendorf   <delmendo@rackspace.com>) @ Added functions pg_putline and pg_endcopy (Dirk Elmendorf)

--- ext/pgsql/php_pgsql.h
+++ ext/pgsql/php_pgsql.h
@@ -17,7 +17,7 @@
    +----------------------------------------------------------------------+
  */
  
-/* $Id: php_pgsql.h,v 1.18 2000/07/29 04:24:11 jah Exp $ */
+/* $Id: php_pgsql.h,v 1.19 2000/09/11 15:34:14 derick Exp $ */
 
 #ifndef PHP_PGSQL_H
 #define PHP_PGSQL_H
@@ -85,6 +85,8 @@ PHP_FUNCTION(pg_lowrite);
 PHP_FUNCTION(pg_loreadall);
 PHP_FUNCTION(pg_loimport);
 PHP_FUNCTION(pg_loexport);
+PHP_FUNCTION(pg_putline);
+PHP_FUNCTION(pg_endcopy);
 #if HAVE_PQCLIENTENCODING
 PHP_FUNCTION(pg_clientencoding);
 PHP_FUNCTION(pg_setclientencoding);

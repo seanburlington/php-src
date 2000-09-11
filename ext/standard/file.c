2c1eec6dc5f888fc34032ade5b0c059380ea05d0
sas <sas>
2000-09-11 16:25:36 +0000

    Kill a warning

--- ext/standard/file.c
+++ ext/standard/file.c
@@ -20,7 +20,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: file.c,v 1.112 2000/09/09 12:02:31 zeev Exp $ */
+/* $Id: file.c,v 1.113 2000/09/11 16:25:36 sas Exp $ */
 
 /* Synced with php 3.0 revision 1.218 1999-06-16 [ssb] */
 
@@ -1493,11 +1493,11 @@ static size_t php_passthru_fd(int socketd, FILE *fp, int issock)
 		fd = fileno(fp);
 		fstat(fd, &sbuf);
 	
-		if(sbuf.st_size > sizeof(buf)) {
+		if (sbuf.st_size > sizeof(buf)) {
 			off = ftell(fp);
 			len = sbuf.st_size - off;
 			p = mmap(0, len, PROT_READ, MAP_PRIVATE, fd, off);
-			if(p!=MAP_FAILED) {
+			if (p != (void *) MAP_FAILED) {
 				PHPWRITE(p, len);
 				munmap(p, len);
 				bcount += len;

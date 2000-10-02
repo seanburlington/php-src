fd96739e9f3b0fa87dde6879f392daa3c21f29ef
andi <andi>
2000-10-02 18:13:53 +0000

    - Now really fix it.

--- ext/standard/url.c
+++ ext/standard/url.c
@@ -15,7 +15,7 @@
    | Author: Jim Winstead (jimw@php.net)                                  |
    +----------------------------------------------------------------------+
  */
-/* $Id: url.c,v 1.31 2000/10/02 17:51:30 andi Exp $ */
+/* $Id: url.c,v 1.32 2000/10/02 18:13:53 andi Exp $ */
 
 #include <stdlib.h>
 #include <string.h>
@@ -59,7 +59,7 @@ void free_url(php_url * theurl)
 php_url *url_parse(char *str)
 {
 	regex_t re;
-	regmatch_t subs[10];
+	regmatch_t subs[11];
 	int err;
 	int length = strlen(str);
 	char *result;
@@ -117,7 +117,7 @@ php_url *url_parse(char *str)
 		regfree(&re);			/* free the old regex */
 		
 		if ((cerr=regcomp(&re, "^(([^@:]+)(:([^@:]+))?@)?((\\[([^]]+)\\])|([^:@]+))(:([^:@]+))?", REG_EXTENDED))
-			|| (err=regexec(&re, result, 10, subs, 0))) {
+			|| (err=regexec(&re, result, 11, subs, 0))) {
 			STR_FREE(ret->scheme);
 			STR_FREE(ret->path);
 			STR_FREE(ret->query);

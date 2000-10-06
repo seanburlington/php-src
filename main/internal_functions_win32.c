d1c38422f8fc67f514f9a1d5baa4a29abc998765
andi <andi>
2000-10-06 00:03:02 +0000

    - Add calendar to default Win32 build.

--- main/internal_functions_win32.c
+++ main/internal_functions_win32.c
@@ -18,7 +18,7 @@
  */
 
 
-/* $Id: internal_functions_win32.c,v 1.32 2000/10/05 23:25:41 andi Exp $ */
+/* $Id: internal_functions_win32.c,v 1.33 2000/10/06 00:03:02 andi Exp $ */
 
 
 #include "php.h"
@@ -49,6 +49,7 @@
 #include "ext/standard/php_output.h"
 #include "ext/standard/php_array.h"
 #include "ext/standard/php_assert.h"
+#include "ext/calendar/php_calendar.h"
 #include "ext/com/php_COM.h"
 #include "ext/ftp/php_ftp.h"
 #include "ext/standard/reg.h"
@@ -67,6 +68,7 @@ zend_module_entry *php_builtin_extensions[] = {
 	phpext_bcmath_ptr,
 #endif
 	phpext_standard_ptr,
+	phpext_calendar_ptr,
 	COM_module_ptr,
 	phpext_ftp_ptr,
 	phpext_pcre_ptr,

ac2b25cb60c325489ccacae55541f466550124a7
stas <stas>
2000-11-06 18:19:05 +0000

    And now the right fix - should add it still

--- ext/standard/var.c
+++ ext/standard/var.c
@@ -18,7 +18,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: var.c,v 1.78 2000/11/06 18:09:51 stas Exp $ */
+/* $Id: var.c,v 1.79 2000/11/06 18:19:05 stas Exp $ */
 
 
 /* {{{ includes 
@@ -178,7 +178,7 @@ void php_var_serialize(pval *buf, pval **struc, HashTable *var_hash)
 	HashTable *myht;
 	BLS_FETCH();
 
-	if(var_hash != NULL && (*struc)->is_ref && php_add_var_hash(var_hash,*struc,(void *)&var_already) == FAILURE) {
+	if(var_hash != NULL && php_add_var_hash(var_hash,*struc,(void *)&var_already) == FAILURE && (*struc)->is_ref) {
 		slen = sprintf(s,"R:%ld;",*var_already);
 		STR_CAT(buf, s, slen);
 		return;

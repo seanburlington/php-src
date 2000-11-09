28c2497e965307b4573d661b4b4f4f6a4fa7821c
steinm <steinm>
2000-11-09 13:36:53 +0000

    - Some more comments - Initial support to just include anchors into documents

--- ext/hyperwave/php_hyperwave.h
+++ ext/hyperwave/php_hyperwave.h
@@ -16,7 +16,7 @@
    +----------------------------------------------------------------------+
 */
 
-/* $Id: php_hyperwave.h,v 1.10 2000/10/27 09:29:39 steinm Exp $ */
+/* $Id: php_hyperwave.h,v 1.11 2000/11/09 13:36:53 steinm Exp $ */
 
 #ifndef PHP_HYPERWAVE_H
 #define PHP_HYPERWAVE_H
@@ -142,6 +142,7 @@ PHP_FUNCTION(hw_objrec2array);
 PHP_FUNCTION(hw_array2objrec);
 PHP_FUNCTION(hw_connection_info);
 PHP_FUNCTION(hw_getsrcbydestobj);
+PHP_FUNCTION(hw_insertanchors);
 PHP_FUNCTION(hw_getrellink);
 PHP_FUNCTION(hw_dummy);
 PHP_FUNCTION(hw_who);

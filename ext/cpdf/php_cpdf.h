0c7f84c583e5184b574d7ccb1644bb2d3063195d
ohrn <ohrn>
2001-03-18 18:32:58 +0000

    Solved compile failure due to clash bewteen IMAP and ClibPDF headers.

--- ext/cpdf/php_cpdf.h
+++ ext/cpdf/php_cpdf.h
@@ -26,12 +26,19 @@
    | Authors: Uwe Steinmann                                               |
    +----------------------------------------------------------------------+
  */
-/* $Id: php_cpdf.h,v 1.6 2000/07/02 23:46:38 sas Exp $ */
+/* $Id: php_cpdf.h,v 1.7 2001/03/18 18:32:58 ohrn Exp $ */
 
 #ifndef PHP_CPDF_H
 #define PHP_CPDF_H
 
 #if HAVE_CPDFLIB
+
+/* The macro T is defined in the IMAP headers and clashes with a function
+   declaration here. Get rid of it. */
+
+#ifdef T
+#undef T
+#endif
 
 #include <cpdflib.h>
 

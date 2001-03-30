492706a05856ecf08a4830ccccfd82b296369efe
ohrn <ohrn>
2001-03-30 20:36:18 +0000

    Implemented cpdf_set_viewer_preferences (previously a stub). Added new functions: cpdf_set_font_directories and cpdf_set_font_map_file.

--- ext/cpdf/php_cpdf.h
+++ ext/cpdf/php_cpdf.h
@@ -26,7 +26,7 @@
    | Authors: Uwe Steinmann                                               |
    +----------------------------------------------------------------------+
  */
-/* $Id: php_cpdf.h,v 1.7 2001/03/18 18:32:58 ohrn Exp $ */
+/* $Id: php_cpdf.h,v 1.8 2001/03/30 20:36:18 ohrn Exp $ */
 
 #ifndef PHP_CPDF_H
 #define PHP_CPDF_H
@@ -67,6 +67,8 @@ PHP_FUNCTION(cpdf_show_xy);
 PHP_FUNCTION(cpdf_continue_text);
 PHP_FUNCTION(cpdf_text);
 PHP_FUNCTION(cpdf_set_font);
+PHP_FUNCTION(cpdf_set_font_directories);
+PHP_FUNCTION(cpdf_set_font_map_file);
 PHP_FUNCTION(cpdf_set_leading);
 PHP_FUNCTION(cpdf_set_text_rendering);
 PHP_FUNCTION(cpdf_set_horiz_scaling);

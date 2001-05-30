7f4df191679017f1338dcf4696030a1d8df3b55c
dickmeiss <dickmeiss>
2001-05-30 11:58:24 +0000

    Fixed bug where yaz_connect could fail in cases where it shouldn't.

--- ext/yaz/php_yaz.c
+++ ext/yaz/php_yaz.c
@@ -16,7 +16,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: php_yaz.c,v 1.17 2001/05/24 10:07:28 ssb Exp $ */
+/* $Id: php_yaz.c,v 1.18 2001/05/30 11:58:24 dickmeiss Exp $ */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -996,14 +996,14 @@ static int do_event (int *id, int timeout)
 		}
 		else if (p->state == PHP_YAZ_STATE_CONNECTING)
 		{
-			if (FD_ISSET (fd, &input))
+			if (FD_ISSET (fd, &output))
+			{
+				send_init(p);
+			}
+			else if (FD_ISSET (fd, &input))
 			{
 				do_close(p);
 				p->error = PHP_YAZ_ERROR_CONNECT;
-			}
-			else if (FD_ISSET (fd, &output))
-			{
-				send_init(p);
 			}
 		}
 		else if (p->state == PHP_YAZ_STATE_ESTABLISHED)

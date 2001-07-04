601d1c775515f96fd144645b3558a5483cd52d57
chregu <chregu>
2001-07-04 11:24:49 +0000

    stupid missing semicolon

--- pear/Config.php
+++ pear/Config.php
@@ -16,7 +16,7 @@
 // |          Christian Stocker <chregu@phant.ch>                         |
 // +----------------------------------------------------------------------+
 //
-// $Id: Config.php,v 1.2 2001/07/03 09:34:35 chregu Exp $
+// $Id: Config.php,v 1.3 2001/07/04 11:24:49 chregu Exp $
 
 require_once( "PEAR.php") ;
 
@@ -36,7 +36,7 @@ require_once( "PEAR.php") ;
 *
 * @author      Alexander Merz <alexander.merz@t-online.de>
 * @access      public
-* @version     $Id: Config.php,v 1.2 2001/07/03 09:34:35 chregu Exp $
+* @version     $Id: Config.php,v 1.3 2001/07/04 11:24:49 chregu Exp $
 * @package     Config
 */
 
@@ -280,7 +280,7 @@ class Config  {
         if( empty( $datasrc ) ) {
             $datasrc = $this -> datasrc ;
         }
-        $this->container->data = $this->data
+        $this->container->data = $this->data;
         $this->container->writeInput($datasrc,$preserve);
     }
 

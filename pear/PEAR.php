6513f4b0c6f7fb14fa899c89f979e16703dd8178
mj <mj>
2001-07-07 15:54:27 +0000

    * reverted "DIRECTORY_SEPARATOR" patch

--- pear/PEAR.php
+++ pear/PEAR.php
@@ -17,7 +17,7 @@
 // |          Stig Bakken <ssb@fast.no>                                   |
 // +----------------------------------------------------------------------+
 //
-// $Id: PEAR.php,v 1.6 2001/07/07 12:45:20 mj Exp $
+// $Id: PEAR.php,v 1.7 2001/07/07 15:54:27 mj Exp $
 //
 
 define('PEAR_ERROR_RETURN', 1);
@@ -25,7 +25,6 @@ define('PEAR_ERROR_PRINT', 2);
 define('PEAR_ERROR_TRIGGER', 4);
 define('PEAR_ERROR_DIE', 8);
 define('PEAR_ERROR_CALLBACK', 16);
-define('DIRECTORY_SEPARATOR', '/');
 
 if (substr(PHP_OS, 0, 3) == 'WIN') {
     define('OS_WINDOWS', true);

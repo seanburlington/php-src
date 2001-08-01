247e69cabcfdce9f07358a6222ca3b45d1914662
andrei <andrei>
2001-08-01 13:48:13 +0000

    - Added a few new math functions from Jesus Castagnetto. - Converted to Z_* macros. @- Added a few new math functions. (Jesus)

--- ext/standard/php_math.h
+++ ext/standard/php_math.h
@@ -17,7 +17,7 @@
    +----------------------------------------------------------------------+
 */
 
-/* $Id: php_math.h,v 1.6 2001/02/26 06:07:23 andi Exp $ */
+/* $Id: php_math.h,v 1.7 2001/08/01 13:48:14 andrei Exp $ */
 
 #ifndef PHP_MATH_H
 #define PHP_MATH_H
@@ -54,6 +54,20 @@ PHP_FUNCTION(base_convert);
 PHP_FUNCTION(number_format);
 PHP_FUNCTION(deg2rad);
 PHP_FUNCTION(rad2deg);
+
+PHP_FUNCTION(exp2);
+PHP_FUNCTION(exp10);
+PHP_FUNCTION(log2);
+PHP_FUNCTION(cbrt);
+PHP_FUNCTION(hypot);
+PHP_FUNCTION(expm1);
+PHP_FUNCTION(log1p);
+PHP_FUNCTION(sinh);
+PHP_FUNCTION(cosh);
+PHP_FUNCTION(tanh);
+PHP_FUNCTION(asinh);
+PHP_FUNCTION(acosh);
+PHP_FUNCTION(atanh);
 
 #include <math.h>
 

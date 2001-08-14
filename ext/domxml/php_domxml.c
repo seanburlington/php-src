91c321cbffed476dec22a249bb58dbd2a8bed71c
joey <joey>
2001-08-14 00:10:39 +0000

    Remove unused variable and fix small mental error.

--- ext/domxml/php_domxml.c
+++ ext/domxml/php_domxml.c
@@ -16,7 +16,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: php_domxml.c,v 1.59 2001/08/13 18:47:51 andi Exp $ */
+/* $Id: php_domxml.c,v 1.60 2001/08/14 00:10:39 joey Exp $ */
 
 
 #ifdef HAVE_CONFIG_H
@@ -448,7 +448,6 @@ static void php_xpath_set_object(zval *wrapper, void *obj, int rsrc_type)
 static zval *php_xpathobject_new(xmlXPathObjectPtr obj, int *found TSRMLS_DC) 
 {
 	zval *wrapper;
-	int rsrc_type;
 
 	*found = 0;
 	if (!obj) {
@@ -2198,7 +2197,7 @@ PHP_FUNCTION(domxml_node_text_concat)
 	convert_to_string(content);
 
 	if(Z_STRLEN_P(content))
-		xmlTextConcat(nodep, Z_STRVAL_P(content), Z_STRVAL_P(content));
+		xmlTextConcat(nodep, Z_STRVAL_P(content), Z_STRLEN_P(content));
 
 	RETURN_TRUE;
 }

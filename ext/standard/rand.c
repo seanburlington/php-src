255b9f2174684271862c376ff69622e1a4f0a5ed
sterling <sterling>
2001-09-06 06:41:16 +0000

    fix ms compile warnings

--- ext/standard/rand.c
+++ ext/standard/rand.c
@@ -19,7 +19,7 @@
    | Based on code from: Shawn Cokus <Cokus@math.washington.edu>          |
    +----------------------------------------------------------------------+
  */
-/* $Id: rand.c,v 1.46 2001/09/06 00:40:02 sterling Exp $ */
+/* $Id: rand.c,v 1.47 2001/09/06 06:41:16 sterling Exp $ */
 
 #include <stdlib.h>
 
@@ -245,7 +245,7 @@ PHP_FUNCTION(mt_srand)
  * -RL
  */    
 #define RAND_RANGE(__n, __min, __max) \
-	(__n) = (__min) + (int)((double)(__max) - (__min) + 1.0) * ((__n) / (PHP_RAND_MAX + 1.0))
+	(__n) = (__min) + (long) ((double) ((__max) - (__min) + 1.0)) * ((__n) / (PHP_RAND_MAX + 1.0))
 
 /* {{{ proto int rand([int min, int max])
    Returns a random number */

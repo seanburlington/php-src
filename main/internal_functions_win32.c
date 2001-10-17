efedbcd20a5a1207b7ad6a4846a4121520aefc59
phanto <phanto>
2001-10-17 12:50:44 +0000

    rename com module entry

--- main/internal_functions_win32.c
+++ main/internal_functions_win32.c
@@ -18,7 +18,7 @@
  */
 
 
-/* $Id: internal_functions_win32.c,v 1.47 2001/09/25 17:08:02 dbeu Exp $ */
+/* $Id: internal_functions_win32.c,v 1.48 2001/10/17 12:50:44 phanto Exp $ */
 
 /* {{{ includes
  */
@@ -51,7 +51,6 @@
 #include "ext/standard/php_assert.h"
 #include "ext/calendar/php_calendar.h"
 #include "ext/com/php_COM.h"
-#include "ext/com/php_VARIANT.h"
 #include "ext/ftp/php_ftp.h"
 #include "ext/standard/reg.h"
 #include "ext/pcre/php_pcre.h"
@@ -71,7 +70,7 @@ zend_module_entry *php_builtin_extensions[] = {
 	phpext_bcmath_ptr,
 #endif
 	phpext_calendar_ptr,
-	COM_module_ptr,
+	phpext_com_ptr,
 	phpext_ftp_ptr,
 #if defined(MBSTR_ENC_TRANS)
 	phpext_mbstring_ptr,

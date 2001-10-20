f5547531c168e432a7482aa0123415c89deea9a3
rasmus <rasmus>
2001-10-20 22:01:56 +0000

    # Butt-ugly looking code here, but I need regular expressions to do this. Re-instated safe-mode realm mangling as it was in php 2 and 3 albeit in a somewhat more robust way. @ Re-instated safe-mode realm mangling (Rasmus)

--- ext/pcre/php_pcre.h
+++ ext/pcre/php_pcre.h
@@ -16,7 +16,7 @@
    +----------------------------------------------------------------------+
  */
  
-/* $Id: php_pcre.h,v 1.27 2001/07/30 15:41:11 andrei Exp $ */
+/* $Id: php_pcre.h,v 1.28 2001/10/20 22:01:56 rasmus Exp $ */
 
 #ifndef PHP_PCRE_H
 #define PHP_PCRE_H
@@ -40,6 +40,9 @@ PHP_FUNCTION(preg_replace_callback);
 PHP_FUNCTION(preg_split);
 PHP_FUNCTION(preg_quote);
 PHP_FUNCTION(preg_grep);
+
+char *php_pcre_replace(char *regex,   int regex_len, char *subject, int subject_len,
+                       zval *replace_val, int is_callable_replace, int *result_len, int limit TSRMLS_DC);
 
 extern zend_module_entry pcre_module_entry;
 #define pcre_module_ptr &pcre_module_entry

9495f2cadb3f8e17a487acee352da1d030e2e5ef
derick <derick>
2001-11-08 23:38:37 +0000

    - oops :)

--- ext/pdf/pdf.c
+++ ext/pdf/pdf.c
@@ -17,7 +17,7 @@
    +----------------------------------------------------------------------+
 */
 
-/* $Id: pdf.c,v 1.103 2001/11/08 23:37:33 derick Exp $ */
+/* $Id: pdf.c,v 1.104 2001/11/08 23:38:37 derick Exp $ */
 
 /* pdflib 2.02 ... 3.0x is subject to the ALADDIN FREE PUBLIC LICENSE.
    Copyright (C) 1997-1999 Thomas Merz. 2000-2001 PDFlib GmbH */
@@ -53,7 +53,7 @@ static int le_gd;
 /* }}} */
 
 #if HAVE_PDFLIB || HAVE_PDFLIB3
-#warning boo
+
 #include "php_pdf.h"
 
 static int le_pdf;
@@ -347,7 +347,7 @@ PHP_MINFO_FUNCTION(pdf)
 #else
 	php_info_print_table_row(2, "PDFlib GmbH Version", tmp );
 #endif
-	php_info_print_table_row(2, "Revision", "$Revision: 1.103 $" );
+	php_info_print_table_row(2, "Revision", "$Revision: 1.104 $" );
 	php_info_print_table_end();
 
 }

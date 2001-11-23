feb09a5250813641142b43db014b090ba7334b33
derick <derick>
2001-11-23 11:46:01 +0000

    - Reverse slight mistake (patch by Markus Fischer)

--- ext/mysql/php_mysql.c
+++ ext/mysql/php_mysql.c
@@ -16,7 +16,7 @@
    +----------------------------------------------------------------------+
 */
  
-/* $Id: php_mysql.c,v 1.108 2001/11/22 07:58:37 derick Exp $ */
+/* $Id: php_mysql.c,v 1.109 2001/11/23 11:46:01 derick Exp $ */
 
 
 /* TODO:
@@ -543,7 +543,7 @@ static void php_mysql_do_connect(INTERNAL_FUNCTION_PARAMETERS, int persistent)
 		list_entry *le;
 		
 		/* try to find if we already have this link in our persistent list */
-		if (new_link || zend_hash_find(&EG(persistent_list), hashed_details, hashed_details_length+1, (void **) &le)==FAILURE) {  /* we don't */
+		if (zend_hash_find(&EG(persistent_list), hashed_details, hashed_details_length+1, (void **) &le)==FAILURE) {  /* we don't */
 			list_entry new_le;
 
 			if (MySG(max_links)!=-1 && MySG(num_links)>=MySG(max_links)) {

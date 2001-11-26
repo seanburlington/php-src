50d48bfa5a6dba8db39b465ddd5513597517ac28
sterling <sterling>
2001-11-26 18:57:44 +0000

    Z_TYPE(filehandle)?? ok.. whatever... ;)

--- main/fopen_wrappers.c
+++ main/fopen_wrappers.c
@@ -16,7 +16,7 @@
    |          Jim Winstead <jimw@php.net>                                 |
    +----------------------------------------------------------------------+
  */
-/* $Id: fopen_wrappers.c,v 1.137 2001/09/25 21:58:33 jeroen Exp $ */
+/* $Id: fopen_wrappers.c,v 1.138 2001/11/26 18:57:44 sterling Exp $ */
 
 /* {{{ includes
  */
@@ -360,7 +360,7 @@ PHPAPI int php_fopen_primary_script(zend_file_handle *file_handle TSRMLS_DC)
 	file_handle->filename = SG(request_info).path_translated;
 	file_handle->free_filename = 0;
 	file_handle->handle.fp = fp;
-	Z_TYPE_P(file_handle) = ZEND_HANDLE_FP;
+	file_handle->type = ZEND_HANDLE_FP;
 
 	return SUCCESS;
 }

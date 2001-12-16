b73cc3f1eb48f8bd07290c5cb142084aa1685bfe
sesser <sesser>
2001-12-16 23:21:52 +0000

    fixed: fix for my broken patch.

--- main/SAPI.c
+++ main/SAPI.c
@@ -18,7 +18,7 @@
    +----------------------------------------------------------------------+
 */
 
-/* $Id: SAPI.c,v 1.127 2001/12/16 21:47:37 sesser Exp $ */
+/* $Id: SAPI.c,v 1.128 2001/12/16 23:21:52 sesser Exp $ */
 
 #include <ctype.h>
 #include <sys/stat.h>
@@ -521,9 +521,7 @@ SAPI_API int sapi_add_header_ex(char *header_line, uint header_line_len, zend_bo
 					efree(result);
 					efree(Z_STRVAL_P(repl_temp));
 					efree(repl_temp);
-				} else {
-					efree(header_line);
-				}
+				} 
 #else
 				if(PG(safe_mode)) {
 					myuid = php_getuid();
@@ -534,10 +532,11 @@ SAPI_API int sapi_add_header_ex(char *header_line, uint header_line_len, zend_bo
 					sapi_header.header = newheader;
 					sapi_header.header_len = newlen;
 					efree(result);
-				} else {
-					efree(header_line);
-				}
+				} 
 #endif
+			}
+			if (sapi_header.header==header_line) {
+				*colon_offset = ':';
 			}
 		}
 	}

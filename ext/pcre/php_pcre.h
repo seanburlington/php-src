4e9266948842e1723bc928bc527c015208c3aaf3
andrei <andrei>
2002-02-12 03:15:26 +0000

    Fix a long-standing infelicity that resulted in extra regex information not being passed to PCRE functions.

--- ext/pcre/php_pcre.h
+++ ext/pcre/php_pcre.h
@@ -16,7 +16,7 @@
    +----------------------------------------------------------------------+
  */
  
-/* $Id: php_pcre.h,v 1.30 2002/02/12 03:00:05 andrei Exp $ */
+/* $Id: php_pcre.h,v 1.31 2002/02/12 03:15:27 andrei Exp $ */
 
 #ifndef PHP_PCRE_H
 #define PHP_PCRE_H
@@ -42,7 +42,7 @@ PHP_FUNCTION(preg_quote);
 PHP_FUNCTION(preg_grep);
 
 PHPAPI char *php_pcre_replace(char *regex,   int regex_len, char *subject, int subject_len, zval *replace_val, int is_callable_replace, int *result_len, int limit TSRMLS_DC);
-PHPAPI pcre* pcre_get_compiled_regex(char *regex, pcre_extra *extra, int *options);
+PHPAPI pcre* pcre_get_compiled_regex(char *regex, pcre_extra **extra, int *options);
 
 extern zend_module_entry pcre_module_entry;
 #define pcre_module_ptr &pcre_module_entry

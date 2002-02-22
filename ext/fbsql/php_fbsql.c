7d7fe1b59e319b98d2a00b42c4cbd3e898e99bf3
fmk <fmk>
2002-02-22 19:03:11 +0000

    Return true on update success.

--- ext/fbsql/php_fbsql.c
+++ ext/fbsql/php_fbsql.c
@@ -16,7 +16,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: php_fbsql.c,v 1.74 2002/02/06 22:31:23 fmk Exp $ */
+/* $Id: php_fbsql.c,v 1.75 2002/02/22 19:03:11 fmk Exp $ */
 
 /* TODO:
  *
@@ -1858,7 +1858,7 @@ static void phpfbQuery(INTERNAL_FUNCTION_PARAMETERS, char* sql, PHPFBLink* link)
 			}
 			ZEND_REGISTER_RESOURCE(return_value, result, le_result);
 		}
-		else if (tp[0] == 'I' || tp[0] == 'I')
+		else if (tp[0] == 'I' || tp[0] == 'U')
 		{
 			if (tp[0] == 'I') link->insert_id = fbcmdRowIndex(md);
 			if (sR == 1 && md) fbcmdRelease(md);

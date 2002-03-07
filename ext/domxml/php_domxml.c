eb5ebf6c824f11c9067d7e98653e8147f3df633c
jtate <jtate>
2002-03-07 23:52:57 +0000

    Commented out a few lines that were causing a segfault in the unlink code. This fixes bug #14522.  I've tested that it does not cause a segfault under RH 7.2, and retains all functionality.  I recommend that this patch be considered for the PHP_4_2_0 tree as well, as having a function that core dumps every time is a bad thing(TM).

--- ext/domxml/php_domxml.c
+++ ext/domxml/php_domxml.c
@@ -16,7 +16,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: php_domxml.c,v 1.119 2002/03/07 16:34:13 jtate Exp $ */
+/* $Id: php_domxml.c,v 1.120 2002/03/07 23:52:57 jtate Exp $ */
 
 /* TODO
  * - Support Notation Nodes
@@ -1802,8 +1802,12 @@ PHP_FUNCTION(domxml_node_unlink_node)
 	DOMXML_PARAM_NONE(nodep, id, le_domxmlnodep);
 
 	xmlUnlinkNode(nodep);
-	xmlFreeNode(nodep);
-	zval_dtor(id);				/* This is not enough because the children won't be deleted */
+	/*	This causes a Segmentation Fault for some reason.  Removing 
+		it allows the user to re-add the node at some other time, in
+		addition to fixing the segfault.  Node will be freed at
+		shutdown. */
+	/*xmlFreeNode(nodep);
+	zval_dtor(id);*/			/* This is not enough because the children won't be deleted */
 }
 /* }}} */
 

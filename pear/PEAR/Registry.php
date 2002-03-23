c7c089f809ad39551307997ffe253538d5766915
ssb <ssb>
2002-03-23 10:18:32 +0000

    * _lock() calls _assertStateDir()

--- pear/PEAR/Registry.php
+++ pear/PEAR/Registry.php
@@ -16,7 +16,7 @@
 // | Author: Stig Bakken <ssb@fast.no>                                    |
 // +----------------------------------------------------------------------+
 //
-// $Id: Registry.php,v 1.17 2002/03/22 08:32:40 ssb Exp $
+// $Id: Registry.php,v 1.18 2002/03/23 10:18:32 ssb Exp $
 
 require_once "System.php";
 require_once "PEAR.php";
@@ -209,6 +209,9 @@ class PEAR_Registry extends PEAR
         if ($mode != LOCK_UN && is_resource($this->lock_fp)) {
             // XXX does not check type of lock (LOCK_SH/LOCK_EX)
             return true;
+        }
+        if (PEAR::isError($err = $this->_assertStateDir())) {
+            return $err;
         }
         $this->lock_fp = @fopen($this->lockfile, 'w');
         if (!is_resource($this->lock_fp)) {

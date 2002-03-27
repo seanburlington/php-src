f03bd46b01c6fab154a3f498e9b62b27c1b49ff1
hholzgra <hholzgra>
2002-03-27 13:51:55 +0000

    moved 'static' prototypes to prevent warnings

--- ext/mime_magic/mime_magic.c
+++ ext/mime_magic/mime_magic.c
@@ -15,7 +15,7 @@
   | Author: Hartmut Holzgraefe  <hartmut@six.de>                         |
   +----------------------------------------------------------------------+
 
-  $Id: mime_magic.c,v 1.3 2002/03/27 11:56:42 hholzgra Exp $ 
+  $Id: mime_magic.c,v 1.4 2002/03/27 13:51:55 hholzgra Exp $ 
 
   This module contains a lot of stuff taken from Apache mod_mime_magic,
   so the lincense section is a little bit longer than usual:
@@ -162,6 +162,37 @@
 #include <unistd.h>
 
 #define MODNAME "mime_magic"
+
+static int apprentice(void);
+static int ascmagic(unsigned char *, int);
+static int is_tar(unsigned char *, int);
+static int softmagic(unsigned char *, int);
+static void tryit(unsigned char *, int, int);
+
+static int getvalue(struct magic *, char **);
+static int hextoint(int);
+static char *getstr(char *, char *, int, int *);
+static int parse(char *, int);
+
+static int match(unsigned char *, int);
+static int mget(union VALUETYPE *, unsigned char *,
+		struct magic *, int);
+static int mcheck(union VALUETYPE *, struct magic *);
+static void mprint(union VALUETYPE *, struct magic *);
+static int mconvert(union VALUETYPE *, struct magic *);
+static int magic_rsl_get(char **, char **);
+static int magic_process(char *);
+
+static long from_oct(int, char *);
+static int fsmagic(const char *fn);
+
+
+#if HAVE_ZLIB
+static int zmagic(unsigned char *, int);
+#endif
+
+static magic_req_rec *magic_set_config(void);
+static void magic_free_config(magic_req_rec *);
 
 ZEND_DECLARE_MODULE_GLOBALS(mime_magic)
 

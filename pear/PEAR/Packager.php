b1534d239bc412e47dfdc115231eee40b9f06617
cox <cox>
2002-04-10 15:10:49 +0000

    Do not show the "tag it!" message when verbose=0 # Forgot to commit that before

--- pear/PEAR/Packager.php
+++ pear/PEAR/Packager.php
@@ -17,7 +17,7 @@
 // |          Tomas V.V.Cox <cox@idecnet.com>                             |
 // +----------------------------------------------------------------------+
 //
-// $Id: Packager.php,v 1.36 2002/04/09 08:19:53 ssb Exp $
+// $Id: Packager.php,v 1.37 2002/04/10 15:10:49 cox Exp $
 
 require_once 'PEAR/Common.php';
 
@@ -137,8 +137,8 @@ class PEAR_Packager extends PEAR_Common
         $this->log(1, "Package $dest_package done");
         $cvsversion = preg_replace('/[^a-z0-9]/i', '_', $pkgversion);
         $cvstag = "RELEASE_$cvsversion";
-        $this->log(0, "Tag the released code with `pear cvstag $pkgfile'");
-        $this->log(0, "(or set the CVS tag $cvstag by hand)");
+        $this->log(1, "Tag the released code with `pear cvstag $pkgfile'");
+        $this->log(1, "(or set the CVS tag $cvstag by hand)");
         return $dest_package;
     }
 

9eb3422b9d098bbe146d9e7b173df02767900a30
edink <edink>
2002-04-25 17:20:05 +0000

    MFB

--- ext/oracle/oracle.c
+++ ext/oracle/oracle.c
@@ -20,7 +20,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: oracle.c,v 1.78 2002/02/28 08:26:31 sebastian Exp $ */
+/* $Id: oracle.c,v 1.79 2002/04/25 17:20:05 edink Exp $ */
 
 /* comment out the next line if you're on Oracle 7.x and don't have the olog 
    call. */
@@ -113,6 +113,8 @@ PHP_RSHUTDOWN_FUNCTION(oracle);
 PHP_MINFO_FUNCTION(oracle);
 /* }}} */
 
+static unsigned char second_args_force_ref[] = { 2, BYREF_NONE, BYREF_FORCE };
+
 /* {{{ oracle_functions[]
  */
 function_entry oracle_functions[] = {
@@ -126,7 +128,7 @@ function_entry oracle_functions[] = {
 	PHP_FE(ora_errorcode,							NULL)
 	PHP_FE(ora_exec,								NULL)
 	PHP_FE(ora_fetch,								NULL)
-   	PHP_FE(ora_fetch_into,							second_arg_force_ref)
+   	PHP_FE(ora_fetch_into,							second_args_force_ref)
 	PHP_FE(ora_columntype,							NULL)
 	PHP_FE(ora_columnname,							NULL)
 	PHP_FE(ora_columnsize,							NULL)

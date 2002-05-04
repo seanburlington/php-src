7f8e23ca899045ce1479dc6b468594e39f2f44d3
sas <sas>
2002-05-04 17:41:11 +0000

    inheader set but unused

--- ext/standard/iptc.c
+++ ext/standard/iptc.c
@@ -16,7 +16,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: iptc.c,v 1.37 2002/02/28 08:26:46 sebastian Exp $ */
+/* $Id: iptc.c,v 1.38 2002/05/04 17:41:11 sas Exp $ */
 
 /*
  * Functions to parse & compse IPTC data.
@@ -304,7 +304,7 @@ PHP_FUNCTION(iptcembed)
    Parse binary IPTC-data into associative array */
 PHP_FUNCTION(iptcparse)
 {
-	unsigned int length, inx, len, inheader, tagsfound;
+	unsigned int length, inx, len, tagsfound;
 	unsigned char *buffer;
 	unsigned char recnum, dataset;
 	unsigned char key[ 16 ];
@@ -319,7 +319,6 @@ PHP_FUNCTION(iptcparse)
 	length = Z_STRLEN_PP(str);
 	buffer = Z_STRVAL_PP(str);
 
-	inheader = 0; /* have we already found the IPTC-Header??? */
 	tagsfound = 0; /* number of tags already found */
 
 	while (inx < length) { /* find 1st tag */

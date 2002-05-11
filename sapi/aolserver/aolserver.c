0fcc3b2e1b6e2c256706f903ef53cc4fd2252d6c
zeev <zeev>
2002-05-11 02:58:43 +0000

    Add missing element

--- sapi/aolserver/aolserver.c
+++ sapi/aolserver/aolserver.c
@@ -22,7 +22,7 @@
  * - CGI/1.1 conformance
  */
 
-/* $Id: aolserver.c,v 1.67 2002/02/28 12:10:36 yohgaki Exp $ */
+/* $Id: aolserver.c,v 1.68 2002/05/11 02:58:43 zeev Exp $ */
 
 /* conflict between PHP and AOLserver headers */
 #define Debug php_Debug
@@ -205,7 +205,7 @@ static void php_info_aolserver(ZEND_MODULE_INFO_FUNC_ARGS)
 	int i;
 	
 	php_info_print_table_start();
-	php_info_print_table_row(2, "SAPI module version", "$Id: aolserver.c,v 1.67 2002/02/28 12:10:36 yohgaki Exp $");
+	php_info_print_table_row(2, "SAPI module version", "$Id: aolserver.c,v 1.68 2002/05/11 02:58:43 zeev Exp $");
 	php_info_print_table_row(2, "Build date", Ns_InfoBuildDate());
 	php_info_print_table_row(2, "Config file path", Ns_InfoConfigFile());
 	php_info_print_table_row(2, "Error Log path", Ns_InfoErrorLog());
@@ -388,6 +388,8 @@ static sapi_module_struct aolserver_sapi_module = {
 
 	php_ns_sapi_register_variables,
 	NULL,									/* Log message */
+
+	NULL,									/* php.ini path override */
 
 	NULL,									/* Block interruptions */
 	NULL,									/* Unblock interruptions */

470b7d06cf877bd05d3818050c0ae6c65d0a85e2
ssb <ssb>
2002-05-21 01:40:35 +0000

    * rename shell-test shortcut to st (was stest)

--- pear/PEAR/Command/Registry.php
+++ pear/PEAR/Command/Registry.php
@@ -17,7 +17,7 @@
 // |                                                                      |
 // +----------------------------------------------------------------------+
 //
-// $Id: Registry.php,v 1.12 2002/05/21 01:38:50 ssb Exp $
+// $Id: Registry.php,v 1.13 2002/05/21 01:40:35 ssb Exp $
 
 require_once 'PEAR/Command/Common.php';
 require_once 'PEAR/Registry.php';
@@ -40,7 +40,7 @@ lists the files in that package.
         'shell-test' => array(
             'summary' => 'Shell Script Test',
             'function' => 'doShellTest',
-            'shortcut' => 'stest',
+            'shortcut' => 'st',
             'options' => array(),
             'doc' => '<package> [[relation] version]
 Tests if a package is installed in the system. Will exit(1) if it is not.

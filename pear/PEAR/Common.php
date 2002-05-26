4c76d5db2e65a8c3db7f7d38aa0239d6e922a7bf
ssb <ssb>
2002-05-26 23:33:01 +0000

    * try being smart about de-indenting release notes from the xml

--- pear/PEAR/Common.php
+++ pear/PEAR/Common.php
@@ -17,7 +17,7 @@
 // |          Tomas V.V.Cox <cox@idecnet.com>                             |
 // +----------------------------------------------------------------------+
 //
-// $Id: Common.php,v 1.63 2002/05/26 12:51:32 ssb Exp $
+// $Id: Common.php,v 1.64 2002/05/26 23:33:01 ssb Exp $
 
 require_once 'PEAR.php';
 require_once 'Archive/Tar.php';
@@ -488,6 +488,19 @@ class PEAR_Common extends PEAR
                 }
                 break;
             case 'notes':
+                // try to "de-indent" release notes in case someone
+                // has been over-indenting their xml ;-)
+                $data = preg_replace('/^[\r\n]+/', '', $this->cdata);
+                $indent_len = strspn($data, " \t");
+                $indent = substr($data, 0, $indent_len);
+                $newdata = '';
+                foreach (explode("\n", $data) as $line) {
+                    print "indent_len=$indent_len\n";
+                    if (substr($line, 0, $indent_len) == $indent) {
+                        $newdata .= substr($line, $indent_len) . "\n";
+                    }
+                }
+                $data = $newdata;
                 if ($this->in_changelog) {
                     $this->current_release['release_notes'] = $data;
                 } else {

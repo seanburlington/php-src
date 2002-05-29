635e9ee133b467f6fb3e34e063ca28771d93df11
ssb <ssb>
2002-05-29 03:36:45 +0000

    * make PEAR_Common::log work with the new UI API

--- pear/PEAR/Common.php
+++ pear/PEAR/Common.php
@@ -17,7 +17,7 @@
 // |          Tomas V.V.Cox <cox@idecnet.com>                             |
 // +----------------------------------------------------------------------+
 //
-// $Id: Common.php,v 1.67.2.1 2002/05/28 02:14:09 ssb Exp $
+// $Id: Common.php,v 1.67.2.2 2002/05/29 03:36:45 ssb Exp $
 
 require_once 'PEAR.php';
 require_once 'Archive/Tar.php';
@@ -130,6 +130,7 @@ class PEAR_Common extends PEAR
     {
         parent::PEAR();
         $this->config = &PEAR_Config::singleton();
+        $this->debug = $this->config->get('verbose');
     }
 
     // }}}
@@ -210,7 +211,7 @@ class PEAR_Common extends PEAR
         if ($this->debug >= $level) {
             if (is_object($this->ui)) {
                 // XXX convert to new Frontend API?
-                $this->ui->displayLine($msg);
+                $this->ui->outputData($msg, 'unknown');
             } else {
                 print "$msg\n";
             }

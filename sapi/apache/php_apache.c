958beb6aa71902e929face7c027ba1b7fbeebb3d
sas <sas>
2002-06-01 17:38:23 +0000

    Fix build

--- sapi/apache/php_apache.c
+++ sapi/apache/php_apache.c
@@ -17,7 +17,7 @@
    |          David Sklar <sklar@student.net>                             |
    +----------------------------------------------------------------------+
  */
-/* $Id: php_apache.c,v 1.64 2002/06/01 10:27:51 andi Exp $ */
+/* $Id: php_apache.c,v 1.65 2002/06/01 17:38:23 sas Exp $ */
 
 #include "php_apache_http.h"
 
@@ -183,7 +183,7 @@ PHP_MINFO_FUNCTION(apache)
 	php_info_print_table_row(1, "Apache for Windows 95/NT");
 	php_info_print_table_end();
 	php_info_print_table_start();
-#elif
+#elif defined(NETWARE)
 	php_info_print_table_row(1, "Apache for NetWare");
 	php_info_print_table_end();
 	php_info_print_table_start();

027fb085d3f781ab2b893c994c0a38f5e053be15
ssb <ssb>
2002-06-06 10:35:16 +0000

    * when building, look for "package-version" subdirectory, needed for   building stuff straight from tarballs

--- pear/PEAR/Builder.php
+++ pear/PEAR/Builder.php
@@ -16,7 +16,7 @@
 // | Authors: Stig Sæther Bakken <ssb@fast.no>                            |
 // +----------------------------------------------------------------------+
 //
-// $Id: Builder.php,v 1.2 2002/06/02 13:07:18 ssb Exp $
+// $Id: Builder.php,v 1.3 2002/06/06 10:35:16 ssb Exp $
 
 require_once 'PEAR/Common.php';
 
@@ -92,7 +92,13 @@ class PEAR_Builder extends PEAR_Common
         if (!@chdir($dir)) {
             return $this->raiseError("could not chdir to $dir");
         }
+        $vdir = "$info[package]-$info[version]";
+        if (is_dir($vdir)) {
+            chdir($vdir);
+        }
         $dir = getcwd();
+        $this->log(2, "building in $dir");
+        $this->_runCommand("find $dir -print", $callback);
         $this->current_callback = $callback;
         $err = $this->_runCommand("phpize", array(&$this, 'phpizeCallback'));
         if (PEAR::isError($err)) {

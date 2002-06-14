bced774402f631b53514fce161c3f828d5636a05
alan_k <alan_k>
2002-06-14 14:55:28 +0000

    Fixed XML RPC sending of args - lets try and get the right tag :)

--- pear/PEAR/Remote.php
+++ pear/PEAR/Remote.php
@@ -16,7 +16,7 @@
 // | Author: Stig Bakken <ssb@fast.no>                                    |
 // +----------------------------------------------------------------------+
 //
-// $Id: Remote.php,v 1.24 2002/05/26 12:52:16 ssb Exp $
+// $Id: Remote.php,v 1.25 2002/06/14 14:55:28 alan_k Exp $
 
 require_once 'PEAR.php';
 require_once 'PEAR/Config.php';
@@ -63,7 +63,9 @@ class PEAR_Remote extends PEAR
         $server_host = $this->config->get('master_server');
         $username = $this->config->get('username');
         $password = $this->config->get('password');
-        $f = new XML_RPC_Message($method, $this->_encode($args));
+        $eargs = array();
+        foreach($args as $arg) $eargs[] = $this->_encode($arg);
+        $f = new XML_RPC_Message($method, $eargs);
         $c = new XML_RPC_Client('/xmlrpc.php', $server_host, 80);
         if ($username && $password) {
             $c->setCredentials($username, $password);

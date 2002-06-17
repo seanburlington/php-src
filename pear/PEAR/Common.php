74bf9d7ede59566072e4f77b6d4176950da55e58
alan_k <alan_k>
2002-06-17 10:52:21 +0000

    Fixing for the coding standard :)

--- pear/PEAR/Common.php
+++ pear/PEAR/Common.php
@@ -17,7 +17,7 @@
 // |          Tomas V.V.Cox <cox@idecnet.com>                             |
 // +----------------------------------------------------------------------+
 //
-// $Id: Common.php,v 1.73 2002/06/17 04:49:14 alan_k Exp $
+// $Id: Common.php,v 1.74 2002/06/17 10:52:21 alan_k Exp $
 
 require_once 'PEAR.php';
 require_once 'Archive/Tar.php';
@@ -743,8 +743,9 @@ class PEAR_Common extends PEAR
     function infoFromString($data)
     {
         require_once('PEAR/Dependency.php');
-        if ($error = PEAR_Dependency::checkExtension('xml'))
+        if ($error = PEAR_Dependency::checkExtension('xml')) {
             return $this->raiseError($error);
+        }
         $xp = @xml_parser_create();
         if (!$xp) {
             return $this->raiseError('Unable to create XML parser');

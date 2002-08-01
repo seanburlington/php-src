d73041218256fbba75665ee365dbac495c23b45d
sniper <sniper>
2002-08-01 16:39:52 +0000

    Reset index when doing array_pop()

--- ext/standard/array.c
+++ ext/standard/array.c
@@ -21,7 +21,7 @@
    +----------------------------------------------------------------------+
 */
 
-/* $Id: array.c,v 1.177 2002/08/01 12:07:15 sniper Exp $ */
+/* $Id: array.c,v 1.178 2002/08/01 16:39:52 sniper Exp $ */
 
 #include "php.h"
 #include "php_ini.h"
@@ -1695,7 +1695,7 @@ static void _phpi_pop(INTERNAL_FUNCTION_PARAMETERS, int off_the_end)
 	zend_hash_del_key_or_index(Z_ARRVAL_PP(stack), key, key_len, index, (key) ? HASH_DEL_KEY : HASH_DEL_INDEX);
 	
 	/* If we did a shift... re-index like it did before */
-	if(!off_the_end) {
+	if (!off_the_end) {
 		HANDLE_BLOCK_INTERRUPTIONS();
 		{
 			int k = 0;
@@ -1709,6 +1709,9 @@ static void _phpi_pop(INTERNAL_FUNCTION_PARAMETERS, int off_the_end)
 			zend_hash_rehash(Z_ARRVAL_PP(stack));
 		}
 		HANDLE_UNBLOCK_INTERRUPTIONS();
+	} else {
+		Z_ARRVAL_PP(stack)->nNextFreeElement--;
+		zend_hash_rehash(Z_ARRVAL_PP(stack));
 	}
 }
 /* }}} */

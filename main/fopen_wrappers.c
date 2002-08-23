76b8474818fb82599b08f1f3b48b49831a58268e
zeev <zeev>
2002-08-23 08:00:48 +0000

    Fix a few warnings (MFH)

--- main/fopen_wrappers.c
+++ main/fopen_wrappers.c
@@ -16,7 +16,7 @@
    |          Jim Winstead <jimw@php.net>                                 |
    +----------------------------------------------------------------------+
  */
-/* $Id: fopen_wrappers.c,v 1.142.2.2 2002/05/11 20:02:21 rasmus Exp $ */
+/* $Id: fopen_wrappers.c,v 1.142.2.3 2002/08/23 08:00:49 zeev Exp $ */
 
 /* {{{ includes
  */
@@ -137,7 +137,7 @@ int php_shutdown_fopen_wrappers(TSRMLS_D)
 	
 	When open_basedir is NULL, always return 0
 */
-PHPAPI int php_check_specific_open_basedir(char *basedir, char *path TSRMLS_DC)
+PHPAPI int php_check_specific_open_basedir(const char *basedir, const char *path TSRMLS_DC)
 {
 	char resolved_name[MAXPATHLEN];
 	char resolved_basedir[MAXPATHLEN];
@@ -184,7 +184,7 @@ PHPAPI int php_check_specific_open_basedir(char *basedir, char *path TSRMLS_DC)
 
 /* {{{ php_check_open_basedir
  */
-PHPAPI int php_check_open_basedir(char *path TSRMLS_DC)
+PHPAPI int php_check_open_basedir(const char *path TSRMLS_DC)
 {
 	/* Only check when open_basedir is available */
 	if (PG(open_basedir) && *PG(open_basedir)) {

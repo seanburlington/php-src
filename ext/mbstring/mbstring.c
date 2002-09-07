32dcf3df1fc160bae89fa8e48633c0eae31aee0c
yohgaki <yohgaki>
2002-09-07 14:07:05 +0000

    Fixed array var handling. Patch by Moriyoshi Koizumi <readjust@deneb.freemail.ne.jp>

--- ext/mbstring/mbstring.c
+++ ext/mbstring/mbstring.c
@@ -17,7 +17,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: mbstring.c,v 1.96 2002/09/07 09:23:39 yohgaki Exp $ */
+/* $Id: mbstring.c,v 1.97 2002/09/07 14:07:05 yohgaki Exp $ */
 
 /*
  * PHP4 Multibyte String module "mbstring" (currently only for Japanese)
@@ -1271,15 +1271,16 @@ php_mbstr_encoding_handler(zval *arg, char *res, char *separator TSRMLS_DC)
 	strtok_buf = NULL;
 	var = php_strtok_r(res, separator, &strtok_buf);
 	while (var)  {
-		len_list[n] = php_url_decode(var, strlen(var));
 		val = strchr(var, '=');
 		val_list[n] = var;
 		n++;
 		if (val) { /* have a value */
+			php_url_decode(var, val-var);
 			*val++ = '\0';
 			val_list[n] = val;
 			len_list[n] = php_url_decode(val, strlen(val));
 		} else {
+			php_url_decode(var, strlen(var));
 			val_list[n] = "";
 			len_list[n] = 0;
 		}

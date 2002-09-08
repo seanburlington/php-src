e102184b59b480382f2c76dfff9cdfcf42261d65
shane <shane>
2002-09-08 20:52:03 +0000

    someone missed code requiring ssl

--- ext/standard/ftp_fopen_wrapper.c
+++ ext/standard/ftp_fopen_wrapper.c
@@ -17,7 +17,7 @@
    |          Hartmut Holzgraefe <hholzgra@php.net>                       |
    +----------------------------------------------------------------------+
  */
-/* $Id: ftp_fopen_wrapper.c,v 1.30 2002/09/08 19:35:42 sesser Exp $ */
+/* $Id: ftp_fopen_wrapper.c,v 1.31 2002/09/08 20:52:03 shane Exp $ */
 
 #include "php.h"
 #include "php_globals.h"
@@ -413,12 +413,14 @@ php_stream * php_stream_url_wrap_ftp(php_stream_wrapper *wrapper, char *path, ch
 	php_stream_context_set(datastream, context);
 	php_stream_notify_progress_init(context, 0, file_size);
 
+#if HAVE_OPENSSL_EXT
 	if (use_ssl_on_data && php_stream_sock_ssl_activate_with_method(datastream, 1, SSLv23_method()) == FAILURE)	{
 		php_stream_wrapper_log_error(wrapper, options TSRMLS_CC, "Unable to activate SSL mode");
 		php_stream_close(datastream);
 		datastream = NULL;
 		goto errexit;
 	}
+#endif
 
 	php_url_free(resource);
 	return datastream;

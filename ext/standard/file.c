ca69cd8e7667bbc825fda19f75ec7fd20fd9dfb1
wez <wez>
2002-09-24 02:46:38 +0000

    Fix EOL markers for file() so that Mac EOL work correctly.

--- ext/standard/file.c
+++ ext/standard/file.c
@@ -21,7 +21,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: file.c,v 1.256 2002/09/23 18:12:38 wez Exp $ */
+/* $Id: file.c,v 1.257 2002/09/24 02:46:38 wez Exp $ */
 
 /* Synced with php 3.0 revision 1.218 1999-06-16 [ssb] */
 
@@ -468,6 +468,7 @@ PHP_FUNCTION(file)
 	char *slashed, *target_buf;
 	register int i = 0;
 	int target_len, len;
+	char eol_marker = '\n';
 	zend_bool use_include_path = 0;
 	zend_bool reached_eof = 0;
 	php_stream *stream;
@@ -507,9 +508,14 @@ PHP_FUNCTION(file)
 				reached_eof = 1;
 			}
 		}
+		
+		/* mini-hack because I don't feel like re-writing this whole function */
+		if (stream->flags & PHP_STREAM_FLAG_EOL_MAC)
+			eol_marker = '\r';
+		
 		if (!reached_eof) {
 			target_len += strlen(target_buf+target_len);
-			if (target_buf[target_len-1] != '\n') {
+			if (target_buf[target_len-1] != eol_marker) {
 				continue;
 			}
 		}

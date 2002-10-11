5ba86db5f264d8bd21e0d2b9240787f5b0d750b2
sebastian <sebastian>
2002-10-11 04:44:14 +0000

    Fix ZTS build.

--- ext/standard/formatted_print.c
+++ ext/standard/formatted_print.c
@@ -16,7 +16,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: formatted_print.c,v 1.54 2002/10/10 16:29:35 iliaa Exp $ */
+/* $Id: formatted_print.c,v 1.55 2002/10/11 04:44:14 sebastian Exp $ */
 
 #include <math.h>				/* modf() */
 #include "php.h"
@@ -278,7 +278,8 @@ php_sprintf_appenddouble(char **buffer, int *pos,
 						 int width, char padding,
 						 int alignment, int precision,
 						 int adjust, char fmt,
-						 int always_sign)
+						 int always_sign
+						 TSRMLS_DC)
 {
 	char numbuf[NUM_BUF_SIZE];
 	char *cvt;
@@ -608,7 +609,8 @@ php_formatted_print(int ht, int *len, int use_array TSRMLS_DC)
 											 Z_DVAL_PP(args[argnum]),
 											 width, padding, alignment,
 											 precision, adjusting,
-											 format[inpos], always_sign);
+											 format[inpos], always_sign
+											 TSRMLS_CC);
 					break;
 					
 				case 'c':

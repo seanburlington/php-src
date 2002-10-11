0499c6a72119411ea1e0cdf1cdcb540bbf8c9b9a
moriyoshi <moriyoshi>
2002-10-11 07:50:47 +0000

    Added ICONV_IMPL and ICONV_VERSION constants. @- Added ICONV_IMPL and ICONV_VERSION constants to iconv extension to @  indicate which iconv implementation is used. (Moriyoshi)

--- ext/iconv/iconv.c
+++ ext/iconv/iconv.c
@@ -17,7 +17,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: iconv.c,v 1.48 2002/09/07 21:08:47 yohgaki Exp $ */
+/* $Id: iconv.c,v 1.49 2002/10/11 07:50:47 moriyoshi Exp $ */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -106,8 +106,25 @@ static void php_iconv_init_globals(zend_iconv_globals *iconv_globals)
 
 PHP_MINIT_FUNCTION(miconv)
 {
+	char *impl_name = "unknown";
+	char *version = "";
+
 	ZEND_INIT_MODULE_GLOBALS(iconv, php_iconv_init_globals, NULL);
 	REGISTER_INI_ENTRIES();
+
+#if HAVE_LIBICONV
+	{
+		static char buf[16];
+		impl_name = "libiconv"; 
+		snprintf( buf, sizeof(buf), "%d.%d", ((_libiconv_version >> 8) & 0x0f),
+		                                     (_libiconv_version & 0x0f) ); 
+		version = buf;
+	}
+#endif
+
+	REGISTER_STRING_CONSTANT( "ICONV_IMPL", ICONV_IMPL, CONST_CS | CONST_PERSISTENT );
+	REGISTER_STRING_CONSTANT( "ICONV_VERSION", version, CONST_CS | CONST_PERSISTENT );
+
 	return SUCCESS;
 }
 

921b7c6d413532c6ccacd6178f323a968a339427
sterling <sterling>
2002-10-21 22:35:59 +0000

    no need to double fetch

--- ext/curl/curl.c
+++ ext/curl/curl.c
@@ -16,7 +16,7 @@
    +----------------------------------------------------------------------+
 */
 
-/* $Id: curl.c,v 1.119 2002/10/02 16:44:48 derick Exp $ */
+/* $Id: curl.c,v 1.120 2002/10/21 22:35:59 sterling Exp $ */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -304,7 +304,6 @@ static size_t curl_write(char *data, size_t size, size_t nmemb, void *ctx)
 	php_curl       *ch     = (php_curl *) ctx;
 	php_curl_write *t      = ch->handlers->write;
 	size_t          length = size * nmemb;
-	TSRMLS_FETCH();
 
 	switch (t->method) {
 	case PHP_CURL_STDOUT:
@@ -1125,6 +1124,6 @@ static void _php_curl_close(zend_rsrc_list_entry *rsrc TSRMLS_DC)
  * tab-width: 4
  * c-basic-offset: 4
  * End:
- * vim600: noet sw=4 ts=4 fdm=marker
- * vim<600: noet sw=4 ts=4
+ * vim600: fdm=marker
+ * vim: noet sw=4 ts=4
  */

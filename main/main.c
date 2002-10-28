c57b29d135151f9bbb132d4c36488a9cf247df42
iliaa <iliaa>
2002-10-28 03:45:20 +0000

    A better fix for bug #20110.

--- main/main.c
+++ main/main.c
@@ -18,7 +18,7 @@
    +----------------------------------------------------------------------+
 */
 
-/* $Id: main.c,v 1.507 2002/10/28 02:46:09 sterling Exp $ */
+/* $Id: main.c,v 1.508 2002/10/28 03:45:20 iliaa Exp $ */
 
 /* {{{ includes
  */
@@ -702,7 +702,7 @@ static FILE *php_fopen_wrapper_for_zend(const char *filename, char **opened_path
 {
 	TSRMLS_FETCH();
 
-	return php_stream_open_wrapper_as_file((char *)filename, "rb", ENFORCE_SAFE_MODE|USE_PATH|IGNORE_URL_WIN|REPORT_ERRORS, opened_path);
+	return php_stream_open_wrapper_as_file((char *)filename, "rb", ENFORCE_SAFE_MODE|USE_PATH|IGNORE_URL_WIN|REPORT_ERRORS|STREAM_OPEN_FOR_INCLUDE, opened_path);
 }
 /* }}} */
 

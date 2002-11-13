3c462d8e2489da073db78a9169746a8e2c8cb12a
moriyoshi <moriyoshi>
2002-11-13 08:13:34 +0000

    Fixed mb_encode_mimeheader() to work with languages other than Japanese and English

--- ext/mbstring/mbstring.c
+++ ext/mbstring/mbstring.c
@@ -17,7 +17,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: mbstring.c,v 1.140 2002/11/13 07:40:11 moriyoshi Exp $ */
+/* $Id: mbstring.c,v 1.141 2002/11/13 08:13:34 moriyoshi Exp $ */
 
 /*
  * PHP4 Multibyte String module "mbstring" (currently only for Japanese)
@@ -2784,17 +2784,10 @@ PHP_FUNCTION(mb_encode_mimeheader)
 			RETURN_FALSE;
 		}
 	} else {
-		switch (MBSTRG(current_language)) {
-		case mbfl_no_language_japanese:
-			charset = mbfl_no_encoding_2022jp;
-			break;
-		case mbfl_no_language_english:
-			charset = mbfl_no_encoding_8859_1;
-			transenc = mbfl_no_encoding_qprint;
-			break;
-		default:
-			charset = mbfl_no_encoding_utf8;
-			break;
+		const mbfl_language *lang = mbfl_no2language(MBSTRG(current_language));
+		if (lang != NULL) {
+			charset = lang->mail_charset;
+			transenc = lang->mail_header_encoding;
 		}
 	}
 

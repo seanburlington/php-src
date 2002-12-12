2ae8f33e4984c752335f6c246d37af5142926099
helly <helly>
2002-12-12 14:16:15 +0000

    MFH

--- ext/openssl/openssl.c
+++ ext/openssl/openssl.c
@@ -18,7 +18,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: openssl.c,v 1.52.2.3 2002/12/12 13:46:28 wez Exp $ */
+/* $Id: openssl.c,v 1.52.2.4 2002/12/12 14:16:15 helly Exp $ */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -255,7 +255,7 @@ static time_t asn1_time_to_time_t(ASN1_UTCTIME * timestr TSRMLS_DC)
 	long gmadjust = 0;
 
 	if (timestr->length < 13) {
-		php_error_docref(NULL TSRMLS_CC, E_WARNING, "%extension author too lazy to parse %s correctly", timestr->data);
+		php_error_docref(NULL TSRMLS_CC, E_WARNING, "extension author too lazy to parse %s correctly", timestr->data);
 		return (time_t)-1;
 	}
 

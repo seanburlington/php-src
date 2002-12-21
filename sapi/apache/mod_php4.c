2f33899dd2179edfbc64a697dacdc32686577fb7
andrei <andrei>
2002-12-21 20:12:07 +0000

    MFB.

--- sapi/apache/mod_php4.c
+++ sapi/apache/mod_php4.c
@@ -17,7 +17,7 @@
    | PHP 4.0 patches by Zeev Suraski <zeev@zend.com>                      |
    +----------------------------------------------------------------------+
  */
-/* $Id: mod_php4.c,v 1.148 2002/12/01 03:28:21 sas Exp $ */
+/* $Id: mod_php4.c,v 1.149 2002/12/21 20:12:08 andrei Exp $ */
 
 #include "php_apache_http.h"
 
@@ -448,7 +448,7 @@ static void init_request_info(TSRMLS_D)
 		authorization = table_get(r->headers_in, "Authorization");
 	}
 	if (authorization
-		&& !auth_type(r)
+		&& (!PG(safe_mode) || (PG(safe_mode) && !auth_type(r)))
 		&& !strcasecmp(getword(r->pool, &authorization, ' '), "Basic")) {
 		tmp = uudecode(r->pool, authorization);
 		SG(request_info).auth_user = getword_nulls_nc(r->pool, &tmp, ':');

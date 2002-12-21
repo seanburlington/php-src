114aa1cee36c7e7820664768906fbe4ec341be34
moriyoshi <moriyoshi>
2002-12-21 21:52:41 +0000

    MFH: Made auth information inaccessible under safe mode

--- sapi/apache2filter/sapi_apache2.c
+++ sapi/apache2filter/sapi_apache2.c
@@ -18,7 +18,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: sapi_apache2.c,v 1.91.2.1 2002/12/09 18:45:37 iliaa Exp $ */
+/* $Id: sapi_apache2.c,v 1.91.2.2 2002/12/21 21:52:41 moriyoshi Exp $ */
 
 #include <fcntl.h>
 
@@ -369,8 +369,13 @@ static void php_apache_request_ctor(ap_filter_t *f, php_struct *ctx TSRMLS_DC)
 	apr_table_unset(f->r->headers_out, "Expires");
 	apr_table_unset(f->r->headers_out, "ETag");
 	apr_table_unset(f->r->headers_in, "Connection");
-	auth = apr_table_get(f->r->headers_in, "Authorization");
-	php_handle_auth_data(auth TSRMLS_CC);
+	if (!PG(safe_mode)) {
+		auth = apr_table_get(f->r->headers_in, "Authorization");
+		php_handle_auth_data(auth TSRMLS_CC);
+	} else {
+		SG(request_info).auth_user = NULL;
+		SG(request_info).auth_password = NULL;
+	}
 
 	php_request_startup(TSRMLS_C);
 }

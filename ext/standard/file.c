92a002e56b12711d5d03e10c22d3a85bd733777a
wez <wez>
2002-12-26 22:35:00 +0000

    Remove cruft that opened a stream that was never closed in get_meta_tags(). (probably my fault). Noticed by Philip Olson

--- ext/standard/file.c
+++ ext/standard/file.c
@@ -21,7 +21,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: file.c,v 1.282 2002/12/05 20:01:18 iliaa Exp $ */
+/* $Id: file.c,v 1.283 2002/12/26 22:35:00 wez Exp $ */
 
 /* Synced with php 3.0 revision 1.218 1999-06-16 [ssb] */
 
@@ -273,14 +273,9 @@ PHP_FUNCTION(get_meta_tags)
 		return;
 	}
 
-	php_stream_open_wrapper(filename, "rb", 0, NULL);
-
-	
 	md.stream = php_stream_open_wrapper(filename, "rb",
 			(use_include_path ? USE_PATH : 0) | ENFORCE_SAFE_MODE | REPORT_ERRORS,
 			NULL);
-	
-
 
 	if (!md.stream)	{
 		RETURN_FALSE;

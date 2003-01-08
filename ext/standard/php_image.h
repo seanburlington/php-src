7e0ba2392e0b02c9a804b97b4cd97470112a3a36
iliaa <iliaa>
2003-01-08 20:36:03 +0000

    Added support for WBMP images.

--- ext/standard/php_image.h
+++ ext/standard/php_image.h
@@ -17,7 +17,7 @@
    +----------------------------------------------------------------------+
 */
 
-/* $Id: php_image.h,v 1.20 2002/12/31 16:07:51 sebastian Exp $ */
+/* $Id: php_image.h,v 1.21 2003/01/08 20:36:03 iliaa Exp $ */
 
 #ifndef PHP_IMAGE_H
 #define PHP_IMAGE_H
@@ -46,6 +46,7 @@ typedef enum
   IMAGE_FILETYPE_JB2,
   IMAGE_FILETYPE_SWC,
   IMAGE_FILETYPE_IFF,
+  IMAGE_FILETYPE_WBMP,
 /* WHEN EXTENDING: PLEASE ALSO REGISTER IN image.c:PHP_MINIT_FUNCTION(imagetypes) */
 } image_filetype;
 /* }}} */

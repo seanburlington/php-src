3e5db2bb5527a93fa827be2ec6c18022f8ba09bc
sniper <sniper>
2003-01-24 10:13:16 +0000

    MFH: typofix

--- ext/imap/php_imap.c
+++ ext/imap/php_imap.c
@@ -26,7 +26,7 @@
    | PHP 4.0 updates:  Zeev Suraski <zeev@zend.com>                       |
    +----------------------------------------------------------------------+
  */
-/* $Id: php_imap.c,v 1.142.2.7 2003/01/23 16:30:07 sniper Exp $ */
+/* $Id: php_imap.c,v 1.142.2.8 2003/01/24 10:13:16 sniper Exp $ */
 
 #define IMAP41
 
@@ -420,7 +420,7 @@ PHP_MINIT_FUNCTION(imap)
 #ifndef PHP_WIN32
 	auth_link(&auth_log);		/* link in the log authenticator */
 	auth_link(&auth_md5);       /* link in the cram-md5 authenticator */ 
-#ifdef HAVE_IMAP_KRB && defined(HAVE_IMAP_AUTH_GSS)
+#if HAVE_IMAP_KRB && defined(HAVE_IMAP_AUTH_GSS)
 	auth_link(&auth_gss);		/* link in the gss authenticator */
 #endif
 

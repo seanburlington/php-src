d92a416f0f21a90da7b7fcf2e05d7870fac7ae1b
sniper <sniper>
2003-02-03 12:25:47 +0000

    MFH: Fixed bug: #22011 (-n must ignore all ini files)

--- main/php_ini.c
+++ main/php_ini.c
@@ -16,7 +16,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: php_ini.c,v 1.106.2.4 2003/01/28 01:51:25 iliaa Exp $ */
+/* $Id: php_ini.c,v 1.106.2.5 2003/02/03 12:25:47 sniper Exp $ */
 
 /* Check CWD for php.ini */
 #define INI_CHECK_CWD
@@ -400,7 +400,7 @@ int php_init_config()
 
 	/* If the config_file_scan_dir is set at compile-time, go and scan this directory and
 	 * parse any .ini files found in this directory. */
-	if(strlen(PHP_CONFIG_FILE_SCAN_DIR)) {
+	if (!sapi_module.php_ini_ignore && strlen(PHP_CONFIG_FILE_SCAN_DIR)) {
  		struct dirent **namelist;
  		int ndir, i;
  

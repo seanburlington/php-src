1e86efc52dacef995a7cd6eb4446ef182f3da53d
k.schroeder <k.schroeder>
2003-02-09 21:42:31 +0000

    fix for #17928

--- ext/standard/datetime.c
+++ ext/standard/datetime.c
@@ -19,7 +19,7 @@
  */
 
 
-/* $Id: datetime.c,v 1.96.2.5 2003/02/09 12:28:31 k.schroeder Exp $ */
+/* $Id: datetime.c,v 1.96.2.6 2003/02/09 21:42:31 k.schroeder Exp $ */
 
 
 #include "php.h"
@@ -343,7 +343,11 @@ php_date(INTERNAL_FUNCTION_PARAMETERS, int gm)
 #if HAVE_TM_ZONE
 				size += strlen(ta->tm_zone);
 #elif HAVE_TZNAME
-				size += strlen(tname[0]);
+				if (ta->tm_isdst > 0 ) {
+					size += strlen(tname[1]);
+				} else {
+					size += strlen(tname[0]);
+				}
 #endif
 				break;
 			case 'Z':		/* timezone offset in seconds */

4d23c7fa82b8d55b9722730124bc7f8424f34b42
sniper <sniper>
2003-02-13 07:16:55 +0000

    - MFH: improved jpeg 2000 and wbmp support for getimagesize()

--- ext/standard/php_image.h
+++ ext/standard/php_image.h
@@ -17,7 +17,7 @@
    +----------------------------------------------------------------------+
 */
 
-/* $Id: php_image.h,v 1.19.4.2 2003/02/03 13:08:19 sniper Exp $ */
+/* $Id: php_image.h,v 1.19.4.3 2003/02/13 07:16:55 sniper Exp $ */
 
 #ifndef PHP_IMAGE_H
 #define PHP_IMAGE_H
@@ -45,7 +45,10 @@ typedef enum
   IMAGE_FILETYPE_JPX,
   IMAGE_FILETYPE_JB2,
   IMAGE_FILETYPE_SWC,
-  IMAGE_FILETYPE_IFF
+  IMAGE_FILETYPE_IFF,
+  IMAGE_FILETYPE_WBMP,
+  /* IMAGE_FILETYPE_JPEG2000 is a userland alias for IMAGE_FILETYPE_JPC */
+  IMAGE_FILETYPE_XBM
 /* WHEN EXTENDING: PLEASE ALSO REGISTER IN image.c:PHP_MINIT_FUNCTION(imagetypes) */
 } image_filetype;
 /* }}} */

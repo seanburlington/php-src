f1e904cbb03edc2082af44f41d5c4736f2e3bc87
sniper <sniper>
2003-02-21 10:42:01 +0000

    fixed headers and cleaned up a bit

--- ext/snmp/snmp.c
+++ ext/snmp/snmp.c
@@ -1,24 +1,25 @@
 /*
-+----------------------------------------------------------------------+
-| PHP Version 4                                                        |
-+----------------------------------------------------------------------+
-| Copyright (c) 1997-2003 The PHP Group                                |
-+----------------------------------------------------------------------+
-| This source file is subject to version 2.02 of the PHP license,      |
-| that is bundled with this package in the file LICENSE, and is        |
-| available at through the world-wide-web at                           |
-| http://www.php.net/license/2_02.txt.                                 |
-| If you did not receive a copy of the PHP license and are unable to   |
-| obtain it through the world-wide-web, please send a note to          |
-| license@php.net so we can mail you a copy immediately.               |
-+----------------------------------------------------------------------+
-| Authors: Rasmus Lerdorf <rasmus@php.net>                             |
-|          Mike Jackson <mhjack@tscnet.com>                            |
-|          Steven Lawrance <slawrance@technologist.com>                |
-|          Harrie Hazewinkel <harrie@lisanza.net>                      |
-+----------------------------------------------------------------------+
+  +----------------------------------------------------------------------+
+  | PHP Version 4                                                        |
+  +----------------------------------------------------------------------+
+  | Copyright (c) 1997-2003 The PHP Group                                |
+  +----------------------------------------------------------------------+
+  | This source file is subject to version 2.02 of the PHP license,      |
+  | that is bundled with this package in the file LICENSE, and is        |
+  | available at through the world-wide-web at                           |
+  | http://www.php.net/license/2_02.txt.                                 |
+  | If you did not receive a copy of the PHP license and are unable to   |
+  | obtain it through the world-wide-web, please send a note to          |
+  | license@php.net so we can mail you a copy immediately.               |
+  +----------------------------------------------------------------------+
+  | Authors: Rasmus Lerdorf <rasmus@php.net>                             |
+  |          Mike Jackson <mhjack@tscnet.com>                            |
+  |          Steven Lawrance <slawrance@technologist.com>                |
+  |          Harrie Hazewinkel <harrie@lisanza.net>                      |
+  +----------------------------------------------------------------------+
 */
-/* $Id: snmp.c,v 1.70.2.4 2003/01/24 09:53:16 sniper Exp $ */
+
+/* $Id: snmp.c,v 1.70.2.5 2003/02/21 10:42:01 sniper Exp $ */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"

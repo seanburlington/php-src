6df9247fb519244d30e9f174d1de3efb2af00551
iliaa <iliaa>
2003-02-24 03:15:53 +0000

    MFH

--- ext/standard/file.c
+++ ext/standard/file.c
@@ -21,7 +21,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: file.c,v 1.279.2.10 2003/02/22 02:43:58 wez Exp $ */
+/* $Id: file.c,v 1.279.2.11 2003/02/24 03:15:53 iliaa Exp $ */
 
 /* Synced with php 3.0 revision 1.218 1999-06-16 [ssb] */
 
@@ -2210,6 +2210,21 @@ PHP_FUNCTION(fgetcsv)
 
 			/* 2A. handle enclosure delimited field */
 			while (*bptr) {
+				/* we need to determine if the enclosure is 'real' or is it escaped */
+				if (*(bptr - 1) == '\\') {
+					int escape_cnt = 0;
+					char *bptr_p = bptr - 2;
+				
+					while (bptr_p > buf && *bptr_p == '\\') {
+						escape_cnt++;
+						bptr_p--;
+					}
+					if (!(escape_cnt % 2)) {
+						goto normal_char;
+						continue;
+					}
+				}
+			
 				if (*bptr == enclosure) {
 					/* handle the enclosure */
 					if ( *(bptr+1) == enclosure) {
@@ -2223,6 +2238,7 @@ PHP_FUNCTION(fgetcsv)
 						break;	/* .. from handling this field - resumes at 3. */
 					}
 				} else {
+normal_char:
 				/* normal character */
 					*tptr++ = *bptr++;
 

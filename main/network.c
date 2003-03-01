5ae383f2e4cdbdfbf4f36062055ffb7fe418e1dc
moriyoshi <moriyoshi>
2003-03-01 18:47:18 +0000

    Reverted my previous commit

--- main/network.c
+++ main/network.c
@@ -17,7 +17,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: network.c,v 1.96 2003/03/01 15:49:06 moriyoshi Exp $ */
+/* $Id: network.c,v 1.97 2003/03/01 18:47:18 moriyoshi Exp $ */
 
 /*#define DEBUG_MAIN_NETWORK 1*/
 
@@ -509,7 +509,7 @@ static void populate_name(
 
 				break;
 
-#if HAVE_IPV6 && HAVE_INET_NTOP
+#if HAVE_IPV6
 			case AF_INET6:
 				buf = (char*)inet_ntop(sa->sa_family, &((struct sockaddr_in6*)sa)->sin6_addr, (char *)&abuf, sizeof(abuf));
 				if (buf) {

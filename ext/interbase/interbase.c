b00fc3cd5a25ad63c545cc09b1bcd20d8a4348df
daniela <daniela>
2003-03-13 22:02:10 +0000

    Numeric/decimal datatype overflow on Win32 Fixed bug #20641

--- ext/interbase/interbase.c
+++ ext/interbase/interbase.c
@@ -17,7 +17,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: interbase.c,v 1.91.2.1 2002/12/31 16:34:46 sebastian Exp $ */
+/* $Id: interbase.c,v 1.91.2.2 2003/03/13 22:02:10 daniela Exp $ */
 
 
 /* TODO: Arrays, roles?
@@ -609,7 +609,7 @@ PHP_MINFO_FUNCTION(ibase)
 
 	php_info_print_table_start();
 	php_info_print_table_row(2, "Interbase Support", "enabled");    
-	php_info_print_table_row(2, "Revision", "$Revision: 1.91.2.1 $");
+	php_info_print_table_row(2, "Revision", "$Revision: 1.91.2.2 $");
 #ifdef COMPILE_DL_INTERBASE
 	php_info_print_table_row(2, "Dynamic Module", "yes");
 #endif
@@ -1810,8 +1810,8 @@ static int _php_ibase_var_pval(pval *val, void *data, int type, int len, int sca
 				sprintf(dt, "%%0.%df", -scale);
 				val->value.str.len = sprintf (string_data, dt, number/f );
 			} else {
-				val->value.str.len = sprintf (string_data, "%Ld",
-					(ISC_INT64) (*((ISC_INT64 *)data)));
+				val->value.str.len = sprintf (string_data, "%.0f",
+								(double)(ISC_INT64) (*((ISC_INT64 *)data)));
 			}
 
 			val->value.str.val = estrdup(string_data);

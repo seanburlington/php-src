a960199e6885fade8651684eb65f353eb0746ad5
iliaa <iliaa>
2003-03-17 22:25:55 +0000

    Win32 build fix.

--- main/streams/streams.c
+++ main/streams/streams.c
@@ -19,7 +19,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: streams.c,v 1.9 2003/03/17 18:32:15 iliaa Exp $ */
+/* $Id: streams.c,v 1.10 2003/03/17 22:25:55 iliaa Exp $ */
 
 #define _GNU_SOURCE
 #include "php.h"
@@ -1168,7 +1168,11 @@ PHPAPI size_t _php_stream_copy_to_stream(php_stream *src, php_stream *dest, size
 	if (php_stream_stat(src, &ssbuf) == 0) {
 		/* in the event that the source file is 0 bytes, return 1 to indicate success
 		 * because opening the file to write had already created a copy */
-		if (ssbuf.sb.st_size == 0 && !S_ISFIFO(ssbuf.sb.st_mode)) {
+		if (ssbuf.sb.st_size == 0
+#ifdef S_ISFIFO
+		 && !S_ISFIFO(ssbuf.sb.st_mode)
+#endif
+		) {
 			return 1;
 		}
 	}

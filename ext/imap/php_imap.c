cba97973f7b67ce728e5081f52eebd3f4d80a6cd
moriyoshi <moriyoshi>
2003-03-31 20:37:15 +0000

    MFH(r-1.161): fixed proto to sync with the documentation

--- ext/imap/php_imap.c
+++ ext/imap/php_imap.c
@@ -26,7 +26,7 @@
    | PHP 4.0 updates:  Zeev Suraski <zeev@zend.com>                       |
    +----------------------------------------------------------------------+
  */
-/* $Id: php_imap.c,v 1.142.2.11 2003/02/11 22:57:50 iliaa Exp $ */
+/* $Id: php_imap.c,v 1.142.2.12 2003/03/31 20:37:15 moriyoshi Exp $ */
 
 #define IMAP41
 
@@ -1993,8 +1993,8 @@ PHP_FUNCTION(imap_rfc822_parse_adrlist)
 }
 /* }}} */
 
-/* {{{ proto string imap_utf8(string string)
-   Convert a string to UTF-8 */
+/* {{{ proto string imap_utf8(string mime_encoded_text)
+   Convert a mime-encoded text to UTF-8 */
 PHP_FUNCTION(imap_utf8)
 {
 	zval **str;

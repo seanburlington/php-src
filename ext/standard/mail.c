9236ae847d4b15f91a6aa90e1cf84cf4cc23c2ce
pollita <pollita>
2003-04-01 06:23:22 +0000

    MFH(r1.71) Bug #22962

--- ext/standard/mail.c
+++ ext/standard/mail.c
@@ -16,7 +16,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: mail.c,v 1.66.2.3 2003/02/24 19:41:40 iliaa Exp $ */
+/* $Id: mail.c,v 1.66.2.4 2003/04/01 06:23:22 pollita Exp $ */
 
 #include <stdlib.h>
 #include <ctype.h>
@@ -212,10 +212,10 @@ PHPAPI int php_mail(char *to, char *subject, char *message, char *headers, char 
 			return 0;
 		}
 #endif
-		fprintf(sendmail, "To: %s\n", to);
-		fprintf(sendmail, "Subject: %s\n", subject);
+		fprintf(sendmail, "To: %s\r\n", to);
+		fprintf(sendmail, "Subject: %s\r\n", subject);
 		if (headers != NULL) {
-			fprintf(sendmail, "%s\n", headers);
+			fprintf(sendmail, "%s\r\n", headers);
 		}
 		fprintf(sendmail, "\n%s\n", message);
 		ret = pclose(sendmail);

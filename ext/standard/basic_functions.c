e26920d54765dafe22f77dc990b7fa6a39837941
rasmus <rasmus>
2003-04-02 23:09:39 +0000

    Fix unregister_tick_function crash

--- ext/standard/basic_functions.c
+++ ext/standard/basic_functions.c
@@ -17,7 +17,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: basic_functions.c,v 1.543.2.10 2003/04/01 10:03:19 sas Exp $ */
+/* $Id: basic_functions.c,v 1.543.2.11 2003/04/02 23:09:39 rasmus Exp $ */
 
 #include "php.h"
 #include "php_streams.h"
@@ -2672,9 +2672,11 @@ PHP_FUNCTION(unregister_tick_function)
 	zval **function;
 	user_tick_function_entry tick_fe;
 
-	if (ZEND_NUM_ARGS() != 1 || zend_get_parameters_ex(ZEND_NUM_ARGS(), &function)) {
+	if (ZEND_NUM_ARGS() != 1 || zend_get_parameters_ex(1, &function)) {
 		WRONG_PARAM_COUNT;
 	}
+
+	if(!BG(user_tick_functions)) return;
 
 	if (Z_TYPE_PP(function) != IS_ARRAY) {
 		convert_to_string_ex(function);

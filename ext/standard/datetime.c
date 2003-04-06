c514ccbadf630bf1d58beb55eaf59c4be5a4abb2
sniper <sniper>
2003-04-06 22:37:58 +0000

    MFH: Fixed bug #23071 (when DST in effect, date("T") crashed PHP)

--- ext/standard/datetime.c
+++ ext/standard/datetime.c
@@ -19,7 +19,7 @@
  */
 
 
-/* $Id: datetime.c,v 1.96.2.6 2003/02/09 21:42:31 k.schroeder Exp $ */
+/* $Id: datetime.c,v 1.96.2.7 2003/04/06 22:37:58 sniper Exp $ */
 
 
 #include "php.h"
@@ -540,7 +540,7 @@ php_date(INTERNAL_FUNCTION_PARAMETERS, int gm)
 #if HAVE_TM_ZONE
 				strcat(Z_STRVAL_P(return_value), ta->tm_zone);
 #elif HAVE_TZNAME
-				strcat(Z_STRVAL_P(return_value), tname[0]);
+				strcat(Z_STRVAL_P(return_value), ta->tm_isdst ? tname[1] : tname[0]);
 #endif
 				break;
 			case 'B':	/* Swatch Beat a.k.a. Internet Time */

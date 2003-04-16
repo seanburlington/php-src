943077afa8e20d1ee5cdd14a4f1c944d521b95f3
iliaa <iliaa>
2003-04-16 23:31:22 +0000

    Fixed bug #23232 (safe_mode does not honor PHP_AUTH_* in apache2)

--- sapi/apache2filter/sapi_apache2.c
+++ sapi/apache2filter/sapi_apache2.c
@@ -18,7 +18,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: sapi_apache2.c,v 1.108 2003/04/10 11:28:54 stas Exp $ */
+/* $Id: sapi_apache2.c,v 1.109 2003/04/16 23:31:22 iliaa Exp $ */
 
 #include <fcntl.h>
 
@@ -396,7 +396,7 @@ static void php_apache_request_ctor(ap_filter_t *f, php_struct *ctx TSRMLS_DC)
 	apr_table_unset(f->r->headers_out, "Expires");
 	apr_table_unset(f->r->headers_out, "ETag");
 	apr_table_unset(f->r->headers_in, "Connection");
-	if (!PG(safe_mode)) {
+	if (!PG(safe_mode) || (PG(safe_mode) && !ap_auth_type(r))) {
 		auth = apr_table_get(f->r->headers_in, "Authorization");
 		php_handle_auth_data(auth TSRMLS_CC);
 	} else {

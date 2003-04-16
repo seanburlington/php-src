5c0a514b4cd38fc8ad01f084865bc0721fc147e0
moriyoshi <moriyoshi>
2003-04-16 19:18:35 +0000

    compiler warning fix and possible 64bit fix

--- ext/sockets/sockets.c
+++ ext/sockets/sockets.c
@@ -19,7 +19,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: sockets.c,v 1.136 2003/04/16 17:31:31 moriyoshi Exp $ */
+/* $Id: sockets.c,v 1.137 2003/04/16 19:18:35 moriyoshi Exp $ */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -275,7 +275,8 @@ int accept_connect(php_socket *in_sock, php_socket **new_sock, struct sockaddr *
 /* {{{ php_read -- wrapper around read() so that it only reads to a \r or \n. */
 int php_read(int bsd_socket, void *buf, size_t maxlen, int flags)
 {
-	int m = 0, n = 0;
+	int m = 0;
+	size_t n = 0;
 	int no_read = 0;
 	int nonblock = 0;
 	char *t = (char *) buf;

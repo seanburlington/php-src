c688c6b9d64a64c4e03a4a88347d215f45d6e391
derick <derick>
2003-04-28 18:41:32 +0000

    - Make them available on Windows too from other files.

--- ext/standard/php_var.h
+++ ext/standard/php_var.h
@@ -16,7 +16,7 @@
    +----------------------------------------------------------------------+
 */
 
-/* $Id: php_var.h,v 1.24 2003/01/18 15:03:00 andrey Exp $ */
+/* $Id: php_var.h,v 1.25 2003/04/28 18:41:32 derick Exp $ */
 
 #ifndef PHP_VAR_H
 #define PHP_VAR_H
@@ -32,9 +32,9 @@ PHP_FUNCTION(unserialize);
 PHP_FUNCTION(memory_get_usage);
 #endif
 
-void php_var_dump(zval **struc, int level TSRMLS_DC);
-void php_var_export(zval **struc, int level TSRMLS_DC);
-void php_debug_zval_dump(zval **struc, int level TSRMLS_DC);
+PHPAPI void php_var_dump(zval **struc, int level TSRMLS_DC);
+PHPAPI void php_var_export(zval **struc, int level TSRMLS_DC);
+PHPAPI void php_debug_zval_dump(zval **struc, int level TSRMLS_DC);
 
 /* typdef HashTable php_serialize_data_t; */
 #define php_serialize_data_t HashTable

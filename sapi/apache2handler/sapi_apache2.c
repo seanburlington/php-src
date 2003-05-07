0c2310de59d9ed32eef1ff8ceb922a6493d82d6c
moriyoshi <moriyoshi>
2003-05-07 14:11:58 +0000

    Possible fix for bug #23455 (patch by rrichards@digarc.com)

--- sapi/apache2handler/sapi_apache2.c
+++ sapi/apache2handler/sapi_apache2.c
@@ -18,7 +18,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: sapi_apache2.c,v 1.15 2003/04/16 23:31:23 iliaa Exp $ */
+/* $Id: sapi_apache2.c,v 1.16 2003/05/07 14:11:58 moriyoshi Exp $ */
 
 #include <fcntl.h>
 
@@ -416,7 +416,7 @@ static void php_apache_request_ctor(request_rec *r, php_struct *ctx TSRMLS_DC)
 	r->no_local_copy = 1;
 
 	content_type = sapi_get_default_content_type(TSRMLS_C);
-	ap_set_content_type(r, apr_pstrdup(r->pool, sapi_get_default_content_type(TSRMLS_C)));
+	ap_set_content_type(r, apr_pstrdup(r->pool, content_type));
 	efree(content_type);
 
 	apr_table_unset(r->headers_out, "Content-Length");

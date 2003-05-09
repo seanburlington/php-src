68e26677137b9415585a7df21a86ec7e4e5c2bc0
edink <edink>
2003-05-09 09:13:01 +0000

    Reverting last patch (merge from sapi/apache).

--- sapi/apache_hooks/mod_php4.c
+++ sapi/apache_hooks/mod_php4.c
@@ -17,7 +17,7 @@
    | PHP 4.0 patches by Zeev Suraski <zeev@zend.com>					  |
    +----------------------------------------------------------------------+
  */
-/* $Id: mod_php4.c,v 1.10 2003/05/08 09:31:08 edink Exp $ */
+/* $Id: mod_php4.c,v 1.11 2003/05/09 09:13:01 edink Exp $ */
 
 #include "php_apache_http.h"
 
@@ -634,12 +634,6 @@ static int send_php(request_rec *r, int display_source_mode, char *filename)
 	int retval;
 	php_per_dir_config *per_dir_conf;
 	TSRMLS_FETCH();
-
-	per_dir_conf = (php_per_dir_config *) get_module_config(r->per_dir_config, &php4_module);
-	if (per_dir_conf) {
-		zend_hash_apply((HashTable *) per_dir_conf->ini_settings, (apply_func_t) php_apache_alter_ini_entries TSRMLS_CC);
-	}
-
 	if (AP(in_request)) {
 		zend_file_handle fh;
 
@@ -661,6 +655,11 @@ static int send_php(request_rec *r, int display_source_mode, char *filename)
 		/* Make sure file exists */
 		if (filename == NULL && r->finfo.st_mode == 0) {
 			return DECLINED;
+		}
+
+		per_dir_conf = (php_per_dir_config *) get_module_config(r->per_dir_config, &php4_module);
+		if (per_dir_conf) {
+			zend_hash_apply((HashTable *) per_dir_conf->ini_settings, (apply_func_t) php_apache_alter_ini_entries TSRMLS_CC);
 		}
 
 		/* If PHP parser engine has been turned off with an "engine off"

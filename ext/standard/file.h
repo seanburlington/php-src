e7549ca11bc069400d799506aac2f0e09c2d1aa3
pollita <pollita>
2003-05-21 04:04:50 +0000

    Implement default contexts. For the moment this is useless but it will be used soon for connection pooling.

--- ext/standard/file.h
+++ ext/standard/file.h
@@ -16,7 +16,7 @@
    +----------------------------------------------------------------------+
 */
 
-/* $Id: file.h,v 1.82 2003/04/08 23:24:53 pollita Exp $ */
+/* $Id: file.h,v 1.83 2003/05/21 04:04:50 pollita Exp $ */
 
 /* Synced with php 3.0 revision 1.30 1999-06-16 [ssb] */
 
@@ -78,6 +78,7 @@ PHPAPI int php_copy_file(char *src, char *dest TSRMLS_DC);
 #define PHP_FILE_IGNORE_NEW_LINES 2
 #define PHP_FILE_SKIP_EMPTY_LINES 4
 #define PHP_FILE_APPEND 8
+#define PHP_FILE_NO_DEFAULT_CONTEXT 16
 
 typedef enum _php_meta_tags_token {
 	TOK_EOF = 0,
@@ -110,6 +111,7 @@ typedef struct {
 	long default_socket_timeout;
 	char *user_agent;
 	char *user_stream_current_filename; /* for simple recursion protection */
+	php_stream_context *default_context;
 } php_file_globals;
 
 #ifdef ZTS

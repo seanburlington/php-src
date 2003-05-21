f0be4c7e37c316c93de360a6bb79a64d5f8a076f
sterling <sterling>
2003-05-21 16:52:09 +0000

    make brian happy

--- ext/standard/string.c
+++ ext/standard/string.c
@@ -18,7 +18,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: string.c,v 1.382 2003/05/16 23:30:08 pollita Exp $ */
+/* $Id: string.c,v 1.383 2003/05/21 16:52:09 sterling Exp $ */
 
 /* Synced with php 3.0 revision 1.193 1999-06-16 [ssb] */
 
@@ -836,8 +836,7 @@ PHPAPI void php_implode(zval *delim, zval *arr, zval *return_value)
 	zend_hash_internal_pointer_reset_ex(Z_ARRVAL_P(arr), &pos);
 
 	while (zend_hash_get_current_data_ex(Z_ARRVAL_P(arr), (void **) &tmp, &pos) == SUCCESS) {
-		SEPARATE_ZVAL(tmp);
-		convert_to_string(*tmp);
+		convert_to_string_ex(tmp);
 
 		smart_str_appendl(&implstr, Z_STRVAL_PP(tmp), Z_STRLEN_PP(tmp));
 		if (++i != numelems) {

bd4c727f681fc997b22c967f9c029e0d244ec5a5
sterling <sterling>
2003-05-21 17:38:17 +0000

    be stupid, but at least be smart about being stupid.

--- ext/standard/string.c
+++ ext/standard/string.c
@@ -18,7 +18,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: string.c,v 1.383 2003/05/21 16:52:09 sterling Exp $ */
+/* $Id: string.c,v 1.384 2003/05/21 17:38:17 sterling Exp $ */
 
 /* Synced with php 3.0 revision 1.193 1999-06-16 [ssb] */
 
@@ -836,8 +836,15 @@ PHPAPI void php_implode(zval *delim, zval *arr, zval *return_value)
 	zend_hash_internal_pointer_reset_ex(Z_ARRVAL_P(arr), &pos);
 
 	while (zend_hash_get_current_data_ex(Z_ARRVAL_P(arr), (void **) &tmp, &pos) == SUCCESS) {
-		convert_to_string_ex(tmp);
-
+		if ((*tmp)->type != IS_STRING) {
+			if (PZVAL_IS_REF(*tmp)) {
+				SEPARATE_ZVAL(tmp);
+				convert_to_string(*tmp);
+			} else {
+				convert_to_string_ex(tmp);
+			}
+		} 
+		
 		smart_str_appendl(&implstr, Z_STRVAL_PP(tmp), Z_STRLEN_PP(tmp));
 		if (++i != numelems) {
 			smart_str_appendl(&implstr, Z_STRVAL_P(delim), Z_STRLEN_P(delim));

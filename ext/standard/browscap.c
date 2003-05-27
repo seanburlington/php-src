36c98f28e27a6bb9e0e2fef126df1ef8cedc1338
sniper <sniper>
2003-05-27 17:24:59 +0000

    MFB: php_error_docref -> zend_error (MINIT)

--- ext/standard/browscap.c
+++ ext/standard/browscap.c
@@ -16,7 +16,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: browscap.c,v 1.74 2003/05/20 17:59:16 jay Exp $ */
+/* $Id: browscap.c,v 1.75 2003/05/27 17:24:59 sniper Exp $ */
 
 #include "php.h"
 #include "php_regex.h"
@@ -159,7 +159,7 @@ PHP_MINIT_FUNCTION(browscap)
 		fh.opened_path = NULL;
 		fh.free_filename = 0;
 		if (!fh.handle.fp) {
-			php_error_docref(NULL TSRMLS_CC, E_CORE_WARNING, "Cannot open '%s' for reading", browscap);
+			zend_error(E_CORE_WARNING, "Cannot open '%s' for reading", browscap);
 			return FAILURE;
 		}
 		fh.filename = browscap;

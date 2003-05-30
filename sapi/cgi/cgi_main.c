38a3a25c8949b64a2b044bf3680df05cba0ea14c
derick <derick>
2003-05-30 17:03:09 +0000

    - Added a "DEBUG" qualifier to 'php -v's version output, and made the CGI   version statement look the same as the CLI version. (Derick)

--- sapi/cgi/cgi_main.c
+++ sapi/cgi/cgi_main.c
@@ -20,7 +20,7 @@
    +----------------------------------------------------------------------+
 */
 
-/* $Id: cgi_main.c,v 1.190.2.33 2003/05/29 15:41:52 helly Exp $ */
+/* $Id: cgi_main.c,v 1.190.2.34 2003/05/30 17:03:10 derick Exp $ */
 
 #include "php.h"
 #include "php_globals.h"
@@ -1367,7 +1367,11 @@ consult the installation file that came with this distribution, or visit \n\
 							SG(headers_sent) = 1;
 							SG(request_info).no_headers = 1;
 						}
-						php_printf("PHP %s (%s), Copyright (c) 1997-2003 The PHP Group\n%s", PHP_VERSION, sapi_module.name, get_zend_version());
+#if ZEND_DEBUG
+						php_printf("PHP %s (%s) (built: %s %s) (DEBUG)\nCopyright (c) 1997-2003 The PHP Group\n%s", PHP_VERSION, sapi_module.name, __DATE__, __TIME__, get_zend_version());
+#else
+						php_printf("PHP %s (%s) (built: %s %s)\nCopyright (c) 1997-2003 The PHP Group\n%s", PHP_VERSION, sapi_module.name, __DATE__, __TIME__, get_zend_version());
+#endif
 						php_end_ob_buffers(1 TSRMLS_CC);
 						exit(1);
 						break;

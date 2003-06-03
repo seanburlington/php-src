7e1e0be820d40296f98ffe54daad0687af89f57e
hholzgra <hholzgra>
2003-06-03 11:07:40 +0000

    MFH

--- ext/standard/dir.c
+++ ext/standard/dir.c
@@ -16,7 +16,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: dir.c,v 1.109.2.5 2003/05/05 12:17:02 thies Exp $ */
+/* $Id: dir.c,v 1.109.2.6 2003/06/03 11:07:40 hholzgra Exp $ */
 
 /* {{{ includes/startup/misc */
 
@@ -398,11 +398,16 @@ PHP_FUNCTION(glob)
 		RETURN_FALSE;
 	}
 
+#ifndef GLOB_NOMATCH
+	// now catch the FreeBSD style of "no matches"
+	if (!globbuf.gl_pathc) {
+			array_init(return_value);
+			return;
+	}
+#endif
+
 	/* we assume that any glob pattern will match files from one directory only
 	   so checking the dirname of the first match should be sufficient */
-	if (!globbuf.gl_pathv) {
-		RETURN_FALSE;
-	}
 	strncpy(cwd, globbuf.gl_pathv[0], MAXPATHLEN);
 	if (PG(safe_mode) && (!php_checkuid(cwd, NULL, CHECKUID_CHECK_FILE_AND_DIR))) {
 		RETURN_FALSE;

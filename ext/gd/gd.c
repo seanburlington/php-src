f99b9d03a89a3986cd9b954a9d80a6b3cd22518f
iliaa <iliaa>
2003-06-04 00:20:19 +0000

    GD 2.0.15 sync

--- ext/gd/gd.c
+++ ext/gd/gd.c
@@ -18,7 +18,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: gd.c,v 1.270 2003/06/03 23:54:55 iliaa Exp $ */
+/* $Id: gd.c,v 1.271 2003/06/04 00:20:19 iliaa Exp $ */
 
 /* gd 1.2 is copyright 1994, 1995, Quest Protein Database Center,
    Cold Spring Harbor Labs. */
@@ -440,7 +440,7 @@ PHP_RSHUTDOWN_FUNCTION(gd)
 /* }}} */
 
 #if HAVE_GD_BUNDLED
-#define PHP_GD_VERSION_STRING "bundled (2.0.14 compatible)"
+#define PHP_GD_VERSION_STRING "bundled (2.0.15 compatible)"
 #elif HAVE_LIBGD20
 #define PHP_GD_VERSION_STRING "2.0 or higher"
 #elif HAVE_GDIMAGECOLORRESOLVE

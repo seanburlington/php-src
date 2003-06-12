e20dfe27072134475f7bbdf4ca4c9970e8733089
helly <helly>
2003-06-12 19:55:16 +0000

    MFH: Bugfix #2397: dba_open locking error with db3

--- ext/dba/dba.c
+++ ext/dba/dba.c
@@ -17,7 +17,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: dba.c,v 1.61.2.16 2003/05/29 14:16:25 helly Exp $ */
+/* $Id: dba.c,v 1.61.2.17 2003/06/12 19:55:16 helly Exp $ */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -642,15 +642,20 @@ static void php_dba_open(INTERNAL_FUNCTION_PARAMETERS, int persistent)
 			lock_mode = (lock_flag & DBA_LOCK_WRITER) ? LOCK_EX : 0;
 			file_mode = "r+b";
 			break;
-		case 'n':
-			modenr = DBA_TRUNC;
-			lock_mode = (lock_flag & DBA_LOCK_TRUNC) ? LOCK_EX : 0;
-			file_mode = "w+b";
-			break;
 		case 'c': 
 			modenr = DBA_CREAT; 
 			lock_mode = (lock_flag & DBA_LOCK_CREAT) ? LOCK_EX : 0;
 			file_mode = "a+b";
+			if (!lock_mode || !lock_dbf) {
+				break;
+			}
+			/* When we lock the db file it will be created before the handler
+			 * even tries to open it, hence we must change to truncate mode.
+			 */
+		case 'n':
+			modenr = DBA_TRUNC;
+			lock_mode = (lock_flag & DBA_LOCK_TRUNC) ? LOCK_EX : 0;
+			file_mode = "w+b";
 			break;
 		default:
 			modenr = 0;

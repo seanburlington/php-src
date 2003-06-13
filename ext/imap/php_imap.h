e20077610bfae8b7504abd51b9d9b6ccd7d8d82a
iliaa <iliaa>
2003-06-13 14:39:22 +0000

    Fixed bug #24161 (No timeout value for imap functions)
    Added imap_timeout() that allows the user to specify as well as retrieve
    timeout values and set default timeout values based on the
    default_socket_timeout ini setting.

--- ext/imap/php_imap.h
+++ ext/imap/php_imap.h
@@ -27,7 +27,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: php_imap.h,v 1.29 2003/06/10 20:03:30 imajes Exp $ */
+/* $Id: php_imap.h,v 1.30 2003/06/13 14:39:22 iliaa Exp $ */
 
 #ifndef PHP_IMAP_H
 #define PHP_IMAP_H
@@ -165,6 +165,7 @@ PHP_FUNCTION(imap_utf7_decode);
 PHP_FUNCTION(imap_utf7_encode);
 PHP_FUNCTION(imap_mime_header_decode);
 PHP_FUNCTION(imap_thread);
+PHP_FUNCTION(imap_timeout);
 
 #if defined(HAVE_IMAP2000) || defined(HAVE_IMAP2001)
 PHP_FUNCTION(imap_get_quota);

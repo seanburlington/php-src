21c5963550931fbbe52c7edc16392cd5c33e5d96
sterling <sterling>
2003-06-25 17:22:31 +0000

    Don't pass resource as an argument when its not being parsed.

--- ext/sqlite/sqlite.c
+++ ext/sqlite/sqlite.c
@@ -17,7 +17,7 @@
    |          Marcus Boerger <helly@php.net>                              |
    +----------------------------------------------------------------------+
 
-   $Id: sqlite.c,v 1.59 2003/06/25 16:59:24 iliaa Exp $ 
+   $Id: sqlite.c,v 1.60 2003/06/25 17:22:31 sterling Exp $ 
 */
 
 #ifdef HAVE_CONFIG_H
@@ -935,7 +935,7 @@ PHP_MINFO_FUNCTION(sqlite)
 {
 	php_info_print_table_start();
 	php_info_print_table_header(2, "SQLite support", "enabled");
-	php_info_print_table_row(2, "PECL Module version", PHP_SQLITE_MODULE_VERSION " $Id: sqlite.c,v 1.59 2003/06/25 16:59:24 iliaa Exp $");
+	php_info_print_table_row(2, "PECL Module version", PHP_SQLITE_MODULE_VERSION " $Id: sqlite.c,v 1.60 2003/06/25 17:22:31 sterling Exp $");
 	php_info_print_table_row(2, "SQLite Library", sqlite_libversion());
 	php_info_print_table_row(2, "SQLite Encoding", sqlite_libencoding());
 	php_info_print_table_end();
@@ -2385,7 +2385,7 @@ PHP_FUNCTION(sqlite_create_function)
 	zval *object = getThis();
 
 	if (object) {
-		if (FAILURE == zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "sz|l", &zdb, &funcname, &funcname_len, &zcall, &num_args)) {
+		if (FAILURE == zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "sz|l", &funcname, &funcname_len, &zcall, &num_args)) {
 			return;
 		}
 		DB_FROM_OBJECT(db, object);

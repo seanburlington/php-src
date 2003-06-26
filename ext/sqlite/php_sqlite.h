06d5e3b4bdc4e0e81344580a5b9a06f3739b592b
helly <helly>
2003-06-26 19:26:26 +0000

    Rename sqlite_fetch_string() to sqlite_fetch_single() + alias

--- ext/sqlite/php_sqlite.h
+++ ext/sqlite/php_sqlite.h
@@ -17,7 +17,7 @@
    |          Marcus Boerger <helly@php.net>                              |
    +----------------------------------------------------------------------+
 
-   $Id: php_sqlite.h,v 1.23 2003/06/25 16:59:24 iliaa Exp $ 
+   $Id: php_sqlite.h,v 1.24 2003/06/26 19:26:26 helly Exp $ 
 */
 
 #ifndef PHP_SQLITE_H
@@ -51,7 +51,7 @@ PHP_FUNCTION(sqlite_array_query);
 PHP_FUNCTION(sqlite_single_query);
 
 PHP_FUNCTION(sqlite_fetch_array);
-PHP_FUNCTION(sqlite_fetch_string);
+PHP_FUNCTION(sqlite_fetch_single);
 PHP_FUNCTION(sqlite_fetch_all);
 PHP_FUNCTION(sqlite_current);
 PHP_FUNCTION(sqlite_column);

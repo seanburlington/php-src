f7bac84adcbb32e80dac18b88f7ea1f5d66d27ec
edink <edink>
2003-06-27 14:30:26 +0000

    Fix windows build

--- ext/sqlite/sqlite.c
+++ ext/sqlite/sqlite.c
@@ -17,7 +17,7 @@
    |          Marcus Boerger <helly@php.net>                              |
    +----------------------------------------------------------------------+
 
-   $Id: sqlite.c,v 1.65 2003/06/26 21:07:07 helly Exp $ 
+   $Id: sqlite.c,v 1.66 2003/06/27 14:30:26 edink Exp $ 
 */
 
 #ifdef HAVE_CONFIG_H
@@ -34,7 +34,9 @@
 #if HAVE_TIME_H
 # include <time.h>
 #endif
+#if HAVE_UNISTD_H
 #include <unistd.h>
+#endif
 
 #include <sqlite.h>
 
@@ -936,7 +938,7 @@ PHP_MINFO_FUNCTION(sqlite)
 {
 	php_info_print_table_start();
 	php_info_print_table_header(2, "SQLite support", "enabled");
-	php_info_print_table_row(2, "PECL Module version", PHP_SQLITE_MODULE_VERSION " $Id: sqlite.c,v 1.65 2003/06/26 21:07:07 helly Exp $");
+	php_info_print_table_row(2, "PECL Module version", PHP_SQLITE_MODULE_VERSION " $Id: sqlite.c,v 1.66 2003/06/27 14:30:26 edink Exp $");
 	php_info_print_table_row(2, "SQLite Library", sqlite_libversion());
 	php_info_print_table_row(2, "SQLite Encoding", sqlite_libencoding());
 	php_info_print_table_end();

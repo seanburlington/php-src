9970452518851136928aea7c159b857a00bc3217
cox <cox>
2003-07-08 10:52:23 +0000

    Detect enable_dl off or safe_mode on PEAR::loadExtension()

--- pear/PEAR.php
+++ pear/PEAR.php
@@ -18,7 +18,7 @@
 // |          Tomas V.V.Cox <cox@idecnet.com>                             |
 // +----------------------------------------------------------------------+
 //
-// $Id: PEAR.php,v 1.63 2003/07/03 06:54:26 cox Exp $
+// $Id: PEAR.php,v 1.64 2003/07/08 10:52:23 cox Exp $
 //
 
 define('PEAR_ERROR_RETURN',     1);
@@ -225,7 +225,7 @@ class PEAR
      * @param   mixed $data   the value to test
      * @param   int   $code   if $data is an error object, return true
      *                        only if $code is a string and
-     *                        $obj->getMessage() == $code or 
+     *                        $obj->getMessage() == $code or
      *                        $code is an integer and $obj->getCode() == $code
      * @access  public
      * @return  bool    true if parameter is an error
@@ -621,6 +621,10 @@ class PEAR
     function loadExtension($ext)
     {
         if (!extension_loaded($ext)) {
+            // if either returns true dl() will produce a FATAL error, stop that
+            if ((ini_get('enable_dl') != 1) || (ini_get('safe_mode') == 1)) {
+                return false;
+            }
             if (OS_WINDOWS) {
                 $suffix = '.dll';
             } elseif (PHP_OS == 'HP-UX') {

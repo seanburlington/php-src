91ddf341f7f31cb11f13c1d6f91fe6d98d1c445d
sniper <sniper>
2003-07-10 00:17:09 +0000

    - Fixed bug #11924 (ibase_query() and ibase_execute() mangled passed   parameter variables)

--- ext/interbase/interbase.c
+++ ext/interbase/interbase.c
@@ -17,7 +17,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: interbase.c,v 1.111 2003/06/16 10:41:42 daniela Exp $ */
+/* $Id: interbase.c,v 1.112 2003/07/10 00:17:09 sniper Exp $ */
 
 
 /* TODO: Arrays, roles?
@@ -628,7 +628,7 @@ PHP_MINFO_FUNCTION(ibase)
 
 	php_info_print_table_start();
 	php_info_print_table_row(2, "Interbase Support", "enabled");
-	php_info_print_table_row(2, "Revision", "$Revision: 1.111 $");
+	php_info_print_table_row(2, "Revision", "$Revision: 1.112 $");
 #ifdef COMPILE_DL_INTERBASE
 	php_info_print_table_row(2, "Dynamic Module", "yes");
 #endif
@@ -1432,6 +1432,10 @@ static int _php_ibase_exec(ibase_result **ib_resultp, ibase_query *ib_query, int
 	int rv = FAILURE;
 	
 	IB_RESULT = NULL;
+
+	if (argc > 0 && args != NULL) {
+		SEPARATE_ZVAL(args);
+	}
 	
 	/* allocate sqlda and output buffers */
 	if (ib_query->out_sqlda) { /* output variables in select, select for update */

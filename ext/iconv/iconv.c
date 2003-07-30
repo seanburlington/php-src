1862fa217a74affa926dde3b8ac35cc682bf8f16
sniper <sniper>
2003-07-30 14:09:42 +0000

    Fix build on certain platforms which do not accept #include FOOBAR

--- ext/iconv/iconv.c
+++ ext/iconv/iconv.c
@@ -18,7 +18,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: iconv.c,v 1.92 2003/07/06 21:30:23 moriyoshi Exp $ */
+/* $Id: iconv.c,v 1.93 2003/07/30 14:09:42 sniper Exp $ */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -36,16 +36,15 @@
 #endif
 
 #include <errno.h>
-
 #include "php_iconv.h"
 
 #ifdef HAVE_ICONV
 
-#ifndef PHP_ICONV_H_PATH
-#define PHP_ICONV_H_PATH <iconv.h>
+#ifdef HAVE_GICONV_H
+#include <giconv.h>
+#else
+#include <iconv.h>
 #endif
-
-#include PHP_ICONV_H_PATH
 
 #ifdef HAVE_GLIBC_ICONV
 #include <gnu/libc-version.h>

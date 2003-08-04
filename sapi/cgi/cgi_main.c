fa91520d9386ac45d82954349576dccbc21fc3ad
sas <sas>
2003-08-04 19:05:05 +0000

    don't overwrite script_path_translated if no PATH_TRANSLATED was received

--- sapi/cgi/cgi_main.c
+++ sapi/cgi/cgi_main.c
@@ -20,7 +20,7 @@
    +----------------------------------------------------------------------+
 */
 
-/* $Id: cgi_main.c,v 1.190.2.42 2003/08/01 16:53:03 iliaa Exp $ */
+/* $Id: cgi_main.c,v 1.190.2.43 2003/08/04 19:05:05 sas Exp $ */
 
 #include "php.h"
 #include "php_globals.h"
@@ -829,7 +829,8 @@ static void init_request_info(TSRMLS_D)
 				SG(request_info).request_uri = env_script_name;
 			}
 #if !DISCARD_PATH
-			script_path_translated = env_path_translated;
+			if (env_path_translated)
+				script_path_translated = env_path_translated;
 #endif
 #if ENABLE_PATHINFO_CHECK
 		}

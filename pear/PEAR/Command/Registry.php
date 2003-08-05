141b329e416b268132262a8a444b09ddab422143
cox <cox>
2003-08-05 14:38:29 +0000

    Pass raw data to frontend (Christian Dickmann)

--- pear/PEAR/Command/Registry.php
+++ pear/PEAR/Command/Registry.php
@@ -17,7 +17,7 @@
 // |                                                                      |
 // +----------------------------------------------------------------------+
 //
-// $Id: Registry.php,v 1.34 2003/07/06 14:17:58 pajoye Exp $
+// $Id: Registry.php,v 1.35 2003/08/05 14:38:29 cox Exp $
 
 require_once 'PEAR/Command/Common.php';
 require_once 'PEAR/Registry.php';
@@ -340,6 +340,7 @@ installed package.'
             $key = ucwords(trim(str_replace('_', ' ', $key)));
             $data['data'][] = array($key, $value);
         }
+        $data['raw'] = $info;
 
         $this->ui->outputData($data, 'package-info');
     }

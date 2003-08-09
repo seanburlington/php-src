8a8c609241018dc678eb5dc9f3e88707be668874
derick <derick>
2003-08-09 11:22:40 +0000

    - MFH: Memleak fix

--- ext/zlib/zlib.c
+++ ext/zlib/zlib.c
@@ -18,7 +18,7 @@
    |          Jade Nicoletti <nicoletti@nns.ch>                           |
    +----------------------------------------------------------------------+
  */
-/* $Id: zlib.c,v 1.153.2.11 2003/07/27 18:16:47 iliaa Exp $ */
+/* $Id: zlib.c,v 1.153.2.12 2003/08/09 11:22:40 derick Exp $ */
 #define IS_EXT_MODULE
 
 #ifdef HAVE_CONFIG_H
@@ -351,13 +351,12 @@ PHP_FUNCTION(gzopen)
 	}
 	convert_to_string_ex(arg1);
 	convert_to_string_ex(arg2);
-	p = estrndup(Z_STRVAL_PP(arg2),Z_STRLEN_PP(arg2));
 	
-	stream = php_stream_gzopen(NULL, Z_STRVAL_PP(arg1), p, use_include_path|ENFORCE_SAFE_MODE|REPORT_ERRORS, NULL, NULL STREAMS_CC TSRMLS_CC);
+	stream = php_stream_gzopen(NULL, Z_STRVAL_PP(arg1), Z_STRVAL_PP(arg2), use_include_path|ENFORCE_SAFE_MODE|REPORT_ERRORS, NULL, NULL STREAMS_CC TSRMLS_CC);
+
 	if (!stream) {
 		RETURN_FALSE;
 	}
-	efree(p);
 	php_stream_to_zval(stream, return_value);
 }	
 /* }}} */

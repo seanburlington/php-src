6a65ff293800fc6605adb6f404186ea1d3918f16
iliaa <iliaa>
2003-08-11 03:22:24 +0000

    Fixed bug #21957 (serialize() mangles objects with __sleep).

--- ext/standard/var.c
+++ ext/standard/var.c
@@ -18,7 +18,7 @@
    +----------------------------------------------------------------------+
 */
 
-/* $Id: var.c,v 1.168 2003/08/05 09:19:38 stas Exp $ */
+/* $Id: var.c,v 1.169 2003/08/11 03:22:24 iliaa Exp $ */
 
 
 /* {{{ includes 
@@ -483,7 +483,7 @@ static void php_var_serialize_class(smart_str *buf, zval **struc, zval *retval_p
 						"serialize.");
 				/* we should still add element even if it's not OK,
 				   since we already wrote the length of the array before */
-				smart_str_appendl(buf,"s:0:\"\";N;", 9);
+				smart_str_appendl(buf,"N;", 2);
 				continue;
 			}
 
@@ -565,6 +565,9 @@ static void php_var_serialize_intern(smart_str *buf, zval **struc, HashTable *va
 								php_error_docref(NULL TSRMLS_CC, E_NOTICE, "__sleep should return an array only "
 												 "containing the names of instance-variables to "
 												 "serialize.");
+								/* we should still add element even if it's not OK,
+				   				since we already wrote the length of the array before */
+								smart_str_appendl(buf,"N;", 2);
 							}
 							
 							zval_ptr_dtor(&retval_ptr);

c98d719108f2f7f1f5edf51c6427c1bde0f2a5c4
moriyoshi <moriyoshi>
2003-08-13 18:22:17 +0000

    MFH(r-1.96): Slight improvement. Hope this will make some difference.

--- ext/iconv/iconv.c
+++ ext/iconv/iconv.c
@@ -17,7 +17,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: iconv.c,v 1.65.2.9 2003/07/30 21:57:02 sniper Exp $ */
+/* $Id: iconv.c,v 1.65.2.10 2003/08/13 18:22:17 moriyoshi Exp $ */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -36,11 +36,11 @@
 
 #ifdef HAVE_ICONV
 
-#ifndef PHP_ICONV_H_PATH
-#define PHP_ICONV_H_PATH <iconv.h>
-#endif
-
+#ifdef PHP_ICONV_H_PATH
 #include PHP_ICONV_H_PATH
+#else
+#include <iconv.h>
+#endif
 
 #ifdef HAVE_GLIBC_ICONV
 #include <gnu/libc-version.h>

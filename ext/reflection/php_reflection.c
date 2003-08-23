ffbfcc980a5c5500093ced13f9975e840dfe8ca7
helly <helly>
2003-08-23 15:47:23 +0000

    Show ctor/dtor information and those don't return anything

--- ext/reflection/php_reflection.c
+++ ext/reflection/php_reflection.c
@@ -18,7 +18,7 @@
    +----------------------------------------------------------------------+
 */
 
-/* $Id: php_reflection.c,v 1.25 2003/08/21 23:32:13 helly Exp $ */
+/* $Id: php_reflection.c,v 1.26 2003/08/23 15:47:23 helly Exp $ */
 #include "zend.h"
 #include "zend_API.h"
 #include "zend_default_classes.h"
@@ -356,6 +356,12 @@ static void _function_string(string *str, zend_function *fptr, char* indent TSRM
 
 	string_printf(str, fptr->common.scope ? "%sMethod [ " : "%sFunction [ ", indent);
 	string_printf(str, (fptr->type == ZEND_USER_FUNCTION) ? "<user> " : "<internal> ");
+	if (fptr->common.fn_flags & ZEND_ACC_CTOR) {
+		string_printf(str, "<ctor> ");
+	}
+	if (fptr->common.fn_flags & ZEND_ACC_DTOR) {
+		string_printf(str, "<dtor> ");
+	}
 	if (fptr->common.fn_flags & ZEND_ACC_ABSTRACT) {
 		string_printf(str, "abstract ");
 	}
@@ -380,7 +386,7 @@ static void _function_string(string *str, zend_function *fptr, char* indent TSRM
 	}
 
 	string_printf(str, fptr->common.scope ? "method " : "function ");
-	if(fptr->op_array.return_reference) {
+	if (fptr->op_array.return_reference && !(fptr->common.fn_flags & (ZEND_ACC_CTOR|ZEND_ACC_DTOR))) {
 		string_printf(str, "&");
 	}
 	string_printf(str, "%s ] {\n", fptr->common.function_name);

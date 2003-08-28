6a97b4f1aad77f3b31829cee191ba128b668325e
stas <stas>
2003-08-28 11:50:32 +0000

    MF4: -m fix for premature extension unloading

--- sapi/cli/php_cli.c
+++ sapi/cli/php_cli.c
@@ -19,7 +19,7 @@
    +----------------------------------------------------------------------+
 */
 
-/* $Id: php_cli.c,v 1.101 2003/08/24 13:10:02 helly Exp $ */
+/* $Id: php_cli.c,v 1.102 2003/08/28 11:50:32 stas Exp $ */
 
 #include "php.h"
 #include "php_globals.h"
@@ -166,6 +166,7 @@ static void print_extensions(TSRMLS_D)
 	zend_llist sorted_exts;
 
 	zend_llist_copy(&sorted_exts, &zend_extensions);
+	sorted_exts.dtor = NULL;
 	zend_llist_sort(&sorted_exts, extension_name_cmp TSRMLS_CC);
 	zend_llist_apply_with_argument(&sorted_exts, (llist_apply_with_arg_func_t) print_extension_info, NULL TSRMLS_CC);
 	zend_llist_destroy(&sorted_exts);

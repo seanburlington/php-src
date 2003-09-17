40c00b44b943f9b0e2d9824bff60a32a79c46754
abies <abies>
2003-09-17 01:25:08 +0000

    Link to fbclient.dll if available Add client lib name to dll version info

--- ext/interbase/interbase.c
+++ ext/interbase/interbase.c
@@ -18,7 +18,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: interbase.c,v 1.183 2003/09/16 20:28:27 abies Exp $ */
+/* $Id: interbase.c,v 1.184 2003/09/17 01:25:08 abies Exp $ */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -26,9 +26,9 @@
 
 #include "php.h"
 
-#define FILE_REVISION "$Revision: 1.183 $"
+#define FILE_REVISION "$Revision: 1.184 $"
 
-#if HAVE_IBASE && !defined(VERSION_INFO_ONLY)
+#if HAVE_IBASE
 
 #include "php_ini.h"
 #include "ext/standard/php_standard.h"
@@ -71,6 +71,8 @@ typedef unsigned long long ISC_UINT64;
 #define LL_LIT(lit) lit ## I64
 #ifdef FB_SQLDA
 #pragma comment(lib, "fbclient_ms.lib")
+#else
+#pragma comment(lib, "gds32_ms.lib")
 #endif
 #else
 #define LL_MASK "ll"

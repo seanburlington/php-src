b365d9fa457c7b9ef416671646d50f69829bc2ff
sas <sas>
2003-09-21 22:08:13 +0000

    Major bug fix upgrade to the bundled libfcgi
    A delta between libfcgi 2.2.2 and 2.4.1-SNAP has been applied to the
    source.  Shane's impersonation, putenv and exit->return improvements
    have been carried over.  One of Shane's changes caused the first process
    to hang after one request.  The modification has been deactived (os_win32.c).
    
    These changes together with a patch to mod_fastcgi, make Apache/mod_fastcgi
    and PHP work under Win32.  It was basically unusable before that.

--- sapi/cgi/libfcgi/include/fcgimisc.h
+++ sapi/cgi/libfcgi/include/fcgimisc.h
@@ -9,7 +9,7 @@
  * See the file "LICENSE.TERMS" for information on usage and redistribution
  * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * $Id: fcgimisc.h,v 1.1 2002/03/10 21:39:28 shane Exp $
+ * $Id: fcgimisc.h,v 1.1.4.1 2003/09/21 22:08:17 sas Exp $
  */
 
 #ifndef _FCGIMISC_H

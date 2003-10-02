7bea5743c924c52d1989ba115c10d8558b29a3fa
moriyoshi <moriyoshi>
2003-10-02 11:02:54 +0000

    Remove leftovers

--- ext/mbstring/mbstring.c
+++ ext/mbstring/mbstring.c
@@ -17,7 +17,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: mbstring.c,v 1.199 2003/09/26 14:42:14 moriyoshi Exp $ */
+/* $Id: mbstring.c,v 1.200 2003/10/02 11:02:54 moriyoshi Exp $ */
 
 /*
  * PHP4 Multibyte String module "mbstring"
@@ -977,21 +977,6 @@ PHP_MINFO_FUNCTION(mbstring)
 	php_info_print_table_start();
 	php_info_print_table_row(2, "Multibyte Support", "enabled");
 	php_info_print_table_row(2, "Multibyte string engine", "libmbfl");
-#if defined(HAVE_MBSTR_JA)
-	php_info_print_table_row(2, "Japanese support", "enabled");	
-#endif
-#if defined(HAVE_MBSTR_CN)
-	php_info_print_table_row(2, "Simplified chinese support", "enabled");	
-#endif
-#if defined(HAVE_MBSTR_TW)
-	php_info_print_table_row(2, "Traditional chinese support", "enabled");	
-#endif
-#if defined(HAVE_MBSTR_KR)
-	php_info_print_table_row(2, "Korean support", "enabled");	
-#endif
-#if defined(HAVE_MBSTR_RU)
-	php_info_print_table_row(2, "Russian support", "enabled");	
-#endif
 	if (MBSTRG(encoding_translation)) {
 		php_info_print_table_row(2, "HTTP input encoding translation", "enabled");	
 	}

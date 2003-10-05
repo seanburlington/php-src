5bac1f0215cc4ef678fcb821a32f199d35bc27d0
shane <shane>
2003-10-05 16:37:50 +0000

    remove a debug printf

--- ext/dom/xpath.c
+++ ext/dom/xpath.c
@@ -17,7 +17,7 @@
    +----------------------------------------------------------------------+
 */
 
-/* $Id: xpath.c,v 1.6 2003/10/05 13:14:21 rrichards Exp $ */
+/* $Id: xpath.c,v 1.7 2003/10/05 16:37:50 shane Exp $ */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -121,7 +121,7 @@ PHP_FUNCTION(dom_xpath_register_ns)
 	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "ss", &prefix, &prefix_len, &ns_uri, &ns_uri_len) == FAILURE) {
 		RETURN_FALSE;
 	}
-	fprintf(stderr,"register %s=%s\n",prefix, ns_uri);
+
 	if (xmlXPathRegisterNs(ctxp, prefix, ns_uri) != 0) {
 		RETURN_FALSE
 	}

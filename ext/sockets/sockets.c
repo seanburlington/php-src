16794a4bba45fb1300145a60d28ef4b371b2f4db
helly <helly>
2003-10-24 18:24:28 +0000

    Zend/ZEND_CHANGES

--- ext/sockets/sockets.c
+++ ext/sockets/sockets.c
@@ -19,7 +19,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: sockets.c,v 1.153 2003/08/31 20:45:48 iliaa Exp $ */
+/* $Id: sockets.c,v 1.154 2003/10/24 18:24:28 helly Exp $ */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -107,17 +107,6 @@ static
 		ZEND_ARG_PASS_INFO(1)
 		ZEND_ARG_PASS_INFO(0)
 		ZEND_ARG_PASS_INFO(0)
-		ZEND_ARG_PASS_INFO(1)
-		ZEND_ARG_PASS_INFO(1)
-	ZEND_END_ARG_INFO();
-
-static 
-	ZEND_BEGIN_ARG_INFO(third_through_seventh_args_force_ref, 0)
-		ZEND_ARG_PASS_INFO(0)
-		ZEND_ARG_PASS_INFO(0)
-		ZEND_ARG_PASS_INFO(1)
-		ZEND_ARG_PASS_INFO(1)
-		ZEND_ARG_PASS_INFO(1)
 		ZEND_ARG_PASS_INFO(1)
 		ZEND_ARG_PASS_INFO(1)
 	ZEND_END_ARG_INFO();

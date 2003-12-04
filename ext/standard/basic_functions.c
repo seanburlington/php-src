1cdb373a58afd555c26be9e663abd219817b7730
wez <wez>
2003-12-04 22:38:22 +0000

    Work around a bug in putenv() in the VS.Net C run time library (MSVCRT71) where it will double free a string.

--- ext/standard/basic_functions.c
+++ ext/standard/basic_functions.c
@@ -17,7 +17,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: basic_functions.c,v 1.644 2003/11/29 21:46:47 wez Exp $ */
+/* $Id: basic_functions.c,v 1.645 2003/12/04 22:38:22 wez Exp $ */
 
 #include "php.h"
 #include "php_streams.h"
@@ -1448,6 +1448,14 @@ PHP_FUNCTION(putenv)
 			}
 		}
 
+#if _MSC_VER >= 1300
+		/* VS.Net has a bug in putenv() when setting a variable that
+		 * is already set; if the SetEnvironmentVariable() API call
+		 * fails, the Crt will double free() a string.
+		 * We try to avoid this by setting our own value first */
+		SetEnvironmentVariable(pe.key, "bugbug");
+#endif
+		
 		if (putenv(pe.putenv_string) == 0) {	/* success */
 			zend_hash_add(&BG(putenv_ht), pe.key, pe.key_len+1, (void **) &pe, sizeof(putenv_entry), NULL);
 #ifdef HAVE_TZSET

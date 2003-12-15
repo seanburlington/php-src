8c8839c9a9e66a860ab74c775d2849c5805d480e
iliaa <iliaa>
2003-12-15 21:56:55 +0000

    Always set panic call handler, allows us to address critical libtidy errors from inside PHP. When memory_limit is enabled use e* memory allocation macros to control memory usage.

--- ext/tidy/tidy.c
+++ ext/tidy/tidy.c
@@ -16,7 +16,7 @@
   +----------------------------------------------------------------------+
 */
 
-/* $Id: tidy.c,v 1.21 2003/12/15 01:43:45 john Exp $ */
+/* $Id: tidy.c,v 1.22 2003/12/15 21:56:55 iliaa Exp $ */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -156,6 +156,7 @@ zend_module_entry tidy_module_entry = {
 ZEND_GET_MODULE(tidy)
 #endif
 
+#if MEMORY_LIMIT
 void *php_tidy_malloc(size_t len)
 {
 	return emalloc(len);
@@ -170,12 +171,11 @@ void php_tidy_free(void *buf)
 {
 	efree(buf);
 }
-
+#endif
 void php_tidy_panic(ctmbstr msg)
 {
 	zend_error(E_ERROR, "Could not allocate memory for tidy! (Reason: %s)", (char *)msg);
 }
-
 
 static void php_tidy_quick_repair(INTERNAL_FUNCTION_PARAMETERS, zend_bool is_file)
 {
@@ -297,13 +297,13 @@ static void tidy_object_new(zend_class_entry *class_type, zend_object_handlers *
 			break;
 
 		case is_doc:
-/* for debugging of libtidy only
+#if MEMORY_LIMIT
 			tidySetMallocCall(php_tidy_malloc);
 			tidySetReallocCall(php_tidy_realloc);
 			tidySetFreeCall(php_tidy_free);
-		
+#endif
 			tidySetPanicCall(php_tidy_panic);
-*/
+
 			intern->ptdoc = emalloc(sizeof(PHPTidyDoc));
 			intern->ptdoc->doc = tidyCreate();
 			intern->ptdoc->parsed = FALSE;
@@ -742,7 +742,7 @@ PHP_MINFO_FUNCTION(tidy)
 	php_info_print_table_start();
 	php_info_print_table_header(2, "Tidy support", "enabled");
 	php_info_print_table_row(2, "libTidy Library Version", (char *)tidyReleaseDate());
-	php_info_print_table_row(2, "Extension Version", PHP_TIDY_MODULE_VERSION " ($Id: tidy.c,v 1.21 2003/12/15 01:43:45 john Exp $)");
+	php_info_print_table_row(2, "Extension Version", PHP_TIDY_MODULE_VERSION " ($Id: tidy.c,v 1.22 2003/12/15 21:56:55 iliaa Exp $)");
 	php_info_print_table_end();
 
 	DISPLAY_INI_ENTRIES();

50f897146aa2c02a3c416cb6d1d3344ed12ed7b2
sniper <sniper>
2003-12-16 21:00:21 +0000

    Fix some compile warnings (patch by Joe Orton)

--- ext/xmlrpc/libxmlrpc/simplestring.c
+++ ext/xmlrpc/libxmlrpc/simplestring.c
@@ -31,7 +31,7 @@
 */
 
 
-static const char rcsid[] = "#(@) $Id: simplestring.c,v 1.3 2002/08/22 01:25:50 sniper Exp $";
+static const char rcsid[] = "#(@) $Id: simplestring.c,v 1.4 2003/12/16 21:00:21 sniper Exp $";
 
 
 #define SIMPLESTRING_INCR 32
@@ -45,6 +45,9 @@ static const char rcsid[] = "#(@) $Id: simplestring.c,v 1.3 2002/08/22 01:25:50 
  *   06/2000
  * HISTORY
  *   $Log: simplestring.c,v $
+ *   Revision 1.4  2003/12/16 21:00:21  sniper
+ *   Fix some compile warnings (patch by Joe Orton)
+ *
  *   Revision 1.3  2002/08/22 01:25:50  sniper
  *   kill some compile warnings
  *
@@ -79,6 +82,7 @@ static const char rcsid[] = "#(@) $Id: simplestring.c,v 1.3 2002/08/22 01:25:50 
  ******/
 
 #include <stdlib.h>
+#include <string.h>
 #include "simplestring.h"
 
 #define my_free(thing)  if(thing) {free(thing); thing = 0;}

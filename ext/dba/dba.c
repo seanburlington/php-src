b2fb257af9e9940e55b0db1b413d99e2df1dacea
helly <helly>
2003-12-18 20:25:19 +0000

    dded qdbm handler (http://qdbm.sourceforge.net).

--- ext/dba/dba.c
+++ ext/dba/dba.c
@@ -17,7 +17,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: dba.c,v 1.107 2003/12/18 20:14:15 helly Exp $ */
+/* $Id: dba.c,v 1.108 2003/12/18 20:25:20 helly Exp $ */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -48,6 +48,7 @@
 #include "php_db4.h"
 #include "php_flatfile.h"
 #include "php_inifile.h"
+#include "php_qdbm.h"
 
 /* {{{ dba_functions[]
  */
@@ -253,6 +254,9 @@ static dba_handler handler[] = {
 #if DBA_FLATFILE
 	DBA_HND(flatfile, DBA_STREAM_OPEN|DBA_LOCK_ALL|DBA_NO_APPEND) /* No lock in lib */
 #endif
+#if DBA_QDBM
+	DBA_HND(qdbm, DBA_LOCK_EXT)
+#endif
 	{ NULL, 0, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL }
 };
 
@@ -270,6 +274,8 @@ static dba_handler handler[] = {
 #define DBA_DEFAULT "ndbm"
 #elif DBA_DBM
 #define DBA_DEFAULT "dbm"
+#elif DBA_QDBM
+#define DBA_DEFAULT "qdbm"
 #else
 #define DBA_DEFAULT ""
 #endif

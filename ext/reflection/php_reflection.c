6dccd6da1bc64b71e2070a9bd10c20b2b8dadae0
helly <helly>
2003-12-22 20:03:01 +0000

    Fixed bug #26695 (Reflection API does not recognize mixed-case class hints) # The exception part

--- ext/reflection/php_reflection.c
+++ ext/reflection/php_reflection.c
@@ -19,7 +19,7 @@
    +----------------------------------------------------------------------+
 */
 
-/* $Id: php_reflection.c,v 1.77 2003/12/15 07:17:27 dmitry Exp $ */
+/* $Id: php_reflection.c,v 1.78 2003/12/22 20:03:01 helly Exp $ */
 #include "zend.h"
 #include "zend_API.h"
 #include "zend_default_classes.h"
@@ -1416,12 +1416,15 @@ ZEND_METHOD(reflection_parameter, getClass)
 		RETURN_NULL();
 	} else {
 		zend_class_entry **pce;
-		
-		if (zend_hash_find(EG(class_table), param->arg_info->class_name, param->arg_info->class_name_len + 1, (void **) &pce) == FAILURE) {
+		char *lcname = do_alloca(param->arg_info->class_name_len + 1);
+		zend_str_tolower_copy(lcname, param->arg_info->class_name, param->arg_info->class_name_len);
+		if (zend_hash_find(EG(class_table), lcname, param->arg_info->class_name_len + 1, (void **) &pce) == FAILURE) {
+			free_alloca(lcname);
 			zend_throw_exception_ex(reflection_exception_ptr, 0 TSRMLS_CC, 
 				"Class %s does not exist", param->arg_info->class_name);
 			return;
 		}
+		free_alloca(lcname);
 		reflection_class_factory(*pce, return_value TSRMLS_CC);
 	}
 }

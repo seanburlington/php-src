8a38ce1daded4ca03a0a8a1d2b2724b639e95ddb
iliaa <iliaa>
2003-12-24 16:43:23 +0000

    MFH: Fixed bug #26707 (Incorrect error for disabled functions/classes).
    # Direct MFH of the patch in PHP 5.0 cannot be used because that would
    # require the change of API. So we'll need to make do with a one time
    # (small) memory leak on startup when disable_functions and/or classes are
    # used.

--- main/main.c
+++ main/main.c
@@ -18,7 +18,7 @@
    +----------------------------------------------------------------------+
 */
 
-/* $Id: main.c,v 1.512.2.50 2003/10/09 02:59:03 iliaa Exp $ */
+/* $Id: main.c,v 1.512.2.51 2003/12/24 16:43:23 iliaa Exp $ */
 
 /* {{{ includes
  */
@@ -165,23 +165,22 @@ static PHP_INI_MH(OnChangeMemoryLimit)
  */
 static void php_disable_functions(TSRMLS_D)
 {
-	char *s = NULL;
-	char *e = INI_STR("disable_functions");
-	char p;
+	char *s = NULL, *e;
 
-	if (!*e) {
+	if (!*(INI_STR("disable_functions"))) {
 		return;
 	}
+
+	/* Intentional one time memory leak on startup */
+	e = strdup(INI_STR("disable_functions"));
 
 	while (*e) {
 		switch (*e) {
 			case ' ':
 			case ',':
 				if (s) {
-					p = *e;
 					*e = '\0';
 					zend_disable_function(s, e-s TSRMLS_CC);
-					*e = p;
 					s = NULL;
 				}
 				break;
@@ -203,23 +202,22 @@ static void php_disable_functions(TSRMLS_D)
  */
 static void php_disable_classes(TSRMLS_D)
 {
-	char *s = NULL;
-	char *e = INI_STR("disable_classes");
-	char p;
+	char *s = NULL, *e;
 
-	if (!*e) {
+	if (!*(INI_STR("disable_classes"))) {
 		return;
 	}
+
+	/* Intentional one time memory leak on startup */
+	e = strdup(INI_STR("disable_classes"));
 
 	while (*e) {
 		switch (*e) {
 			case ' ':
 			case ',':
 				if (s) {
-					p = *e;
 					*e = '\0';
 					zend_disable_class(s, e-s TSRMLS_CC);
-					*e = p;
 					s = NULL;
 				}
 				break;

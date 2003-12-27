d97e45adce0cb81a45a48fb710c410b7edf34808
helly <helly>
2003-12-27 22:20:56 +0000

    Fixed Bug #25329 sqlite_create_function with method and reference to $this

--- ext/sqlite/sqlite.c
+++ ext/sqlite/sqlite.c
@@ -17,7 +17,7 @@
    |          Marcus Boerger <helly@php.net>                              |
    +----------------------------------------------------------------------+
 
-   $Id: sqlite.c,v 1.62.2.12 2003/12/14 18:47:13 iliaa Exp $ 
+   $Id: sqlite.c,v 1.62.2.13 2003/12/27 22:20:56 helly Exp $ 
 */
 
 #ifdef HAVE_CONFIG_H
@@ -261,21 +261,19 @@ static ZEND_RSRC_DTOR_FUNC(php_sqlite_result_dtor)
 
 static int php_sqlite_forget_persistent_id_numbers(zend_rsrc_list_entry *rsrc TSRMLS_DC)
 {
-	struct php_sqlite_db *db;
+	struct php_sqlite_db *db = (struct php_sqlite_db*)rsrc->ptr;
 
+	/* prevent bad mojo if someone tries to use a previously registered function in the next request */
+	zend_hash_apply(&db->callbacks, (apply_func_t)php_sqlite_callback_invalidator TSRMLS_CC);
+	
 	if (Z_TYPE_P(rsrc) != le_sqlite_pdb) {
 		return 0;
 	}
-
-	db = (struct php_sqlite_db*)rsrc->ptr;
 
 	db->rsrc_id = FAILURE;
 
 	/* don't leave pending commits hanging around */
 	sqlite_exec(db->db, "ROLLBACK", NULL, NULL, NULL);
-	
-	/* prevent bad mojo if someone tries to use a previously registered function in the next request */
-	zend_hash_apply(&db->callbacks, (apply_func_t)php_sqlite_callback_invalidator TSRMLS_CC);
 	
 	return 0;
 }
@@ -669,7 +667,7 @@ PHP_MINFO_FUNCTION(sqlite)
 {
 	php_info_print_table_start();
 	php_info_print_table_header(2, "SQLite support", "enabled");
-	php_info_print_table_row(2, "PECL Module version", PHP_SQLITE_MODULE_VERSION " $Id: sqlite.c,v 1.62.2.12 2003/12/14 18:47:13 iliaa Exp $");
+	php_info_print_table_row(2, "PECL Module version", PHP_SQLITE_MODULE_VERSION " $Id: sqlite.c,v 1.62.2.13 2003/12/27 22:20:56 helly Exp $");
 	php_info_print_table_row(2, "SQLite Library", sqlite_libversion());
 	php_info_print_table_row(2, "SQLite Encoding", sqlite_libencoding());
 	php_info_print_table_end();

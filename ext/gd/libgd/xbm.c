6d025be1943df64563bc8e9695a59d0404a41708
iliaa <iliaa>
2003-12-28 20:11:08 +0000

    CS fixes.

--- ext/gd/libgd/xbm.c
+++ ext/gd/libgd/xbm.c
@@ -16,7 +16,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: xbm.c,v 1.4 2003/06/15 20:00:08 helly Exp $ */
+/* $Id: xbm.c,v 1.5 2003/12/28 20:11:08 iliaa Exp $ */
 
 #include <stdio.h>
 #include <math.h>
@@ -46,7 +46,7 @@ gdImagePtr gdImageCreateFromXbm(FILE * fd)
 	int ch;
 	char h[8];
 	unsigned int b;
-	
+
 	rewind(fd);
 	while (fgets(fline, MAX_XBM_LINE_SIZE, fd)) {
 		fline[MAX_XBM_LINE_SIZE-1] = '\0';
@@ -59,7 +59,7 @@ gdImagePtr gdImageCreateFromXbm(FILE * fd)
 			} else {
 				type++;
 			}
-	
+
 			if (!strcmp("width", type)) {
 				width = (unsigned int) value;
 			}
@@ -159,7 +159,7 @@ void gdCtxPrintf(gdIOCtx * out, const char *format, ...)
 	char *buf;
 	int len;
 	va_list args;
-	
+
 	va_start(args, format);
 	len = vspprintf(&buf, 0, format, args);
 	va_end(args);

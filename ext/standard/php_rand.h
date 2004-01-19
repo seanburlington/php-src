d79e26fd0bc0c6fe540c3616b0e14bfb72a164df
sniper <sniper>
2004-01-19 02:31:18 +0000

    MFH: - Fixed bug #26949 (rand(min,max) always returns min when ZTS enabled)

--- ext/standard/php_rand.h
+++ ext/standard/php_rand.h
@@ -20,7 +20,7 @@
    | Based on code from: Shawn Cokus <Cokus@math.washington.edu>          |
    +----------------------------------------------------------------------+
  */
-/* $Id: php_rand.h,v 1.19.4.4 2003/08/20 16:40:46 iliaa Exp $ */
+/* $Id: php_rand.h,v 1.19.4.5 2004/01/19 02:31:19 sniper Exp $ */
 
 #ifndef PHP_RAND_H
 #define	PHP_RAND_H
@@ -33,7 +33,8 @@
 #define RAND_MAX (1<<15)
 #endif
 
-#if defined(HAVE_LRAND48) || defined(HAVE_RANDOM)
+/* In ZTS mode we rely on rand_r() so we must use RAND_MAX. */
+#if !defined(ZTS) && (defined(HAVE_LRAND48) || defined(HAVE_RANDOM))
 #define PHP_RAND_MAX 2147483647
 #else
 #define PHP_RAND_MAX RAND_MAX

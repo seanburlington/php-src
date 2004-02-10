3f6ac8722b8f28457b8cfdcfd8254036c01e8e0b
iliaa <iliaa>
2004-02-10 17:44:30 +0000

    Fixed win32 build. Fixed building of libfcgi when PHP is built from another directory. Added missing headers needed for wait(). Removed duplicate signal.h header. Removed unneeded variables.

--- sapi/cgi/cgi_main.c
+++ sapi/cgi/cgi_main.c
@@ -20,7 +20,7 @@
    +----------------------------------------------------------------------+
 */
 
-/* $Id: cgi_main.c,v 1.246 2004/02/10 00:29:46 iliaa Exp $ */
+/* $Id: cgi_main.c,v 1.247 2004/02/10 17:44:30 iliaa Exp $ */
 
 #include "php.h"
 #include "php_globals.h"
@@ -48,6 +48,12 @@
 #if HAVE_SETLOCALE
 #include <locale.h>
 #endif
+#if HAVE_SYS_TYPES_H
+#include <sys/types.h>
+#endif
+#if HAVE_SYS_WAIT_H
+#include <sys/wait.h>
+#endif
 #include "zend.h"
 #include "zend_extensions.h"
 #include "php_ini.h"
@@ -59,10 +65,6 @@
 #include <io.h>
 #include <fcntl.h>
 #include "win32/php_registry.h"
-#endif
-
-#if HAVE_SIGNAL_H
-#include <signal.h>
 #endif
 
 #ifdef __riscos__
@@ -271,7 +273,11 @@ static void sapi_cgibin_flush(void *server_context)
 #if PHP_FASTCGI
 	if (!FCGX_IsCGI()) {
 		FCGX_Request *request = (FCGX_Request *)server_context;
-		if (!parent && (!request || FCGX_FFlush(request->out) == -1)) {
+		if (
+#ifdef PHP_WIN32
+		!parent && 
+#endif
+		(!request || FCGX_FFlush(request->out) == -1)) {
 			php_handle_aborted_connection();
 		}
 		return;
@@ -1177,7 +1183,6 @@ consult the installation file that came with this distribution, or visit \n\
 
 	if( children ) {
 		int running = 0;
-		int i;
 		pid_t pid;
 
 		/* Create a process group for ourself & children */

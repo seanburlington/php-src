e6f6638031d5dbfb8953f7ab5d2cd28b6d83afa2
abies <abies>
2004-02-22 12:10:00 +0000

    Fix 64-bit crash

--- ext/sqlite/sqlite.c
+++ ext/sqlite/sqlite.c
@@ -17,7 +17,7 @@
    |          Marcus Boerger <helly@php.net>                              |
    +----------------------------------------------------------------------+
 
-   $Id: sqlite.c,v 1.120 2004/02/15 20:42:58 iliaa Exp $ 
+   $Id: sqlite.c,v 1.121 2004/02/22 12:10:00 abies Exp $ 
 */
 
 #ifdef HAVE_CONFIG_H
@@ -1054,7 +1054,7 @@ PHP_MINFO_FUNCTION(sqlite)
 {
 	php_info_print_table_start();
 	php_info_print_table_header(2, "SQLite support", "enabled");
-	php_info_print_table_row(2, "PECL Module version", PHP_SQLITE_MODULE_VERSION " $Id: sqlite.c,v 1.120 2004/02/15 20:42:58 iliaa Exp $");
+	php_info_print_table_row(2, "PECL Module version", PHP_SQLITE_MODULE_VERSION " $Id: sqlite.c,v 1.121 2004/02/22 12:10:00 abies Exp $");
 	php_info_print_table_row(2, "SQLite Library", sqlite_libversion());
 	php_info_print_table_row(2, "SQLite Encoding", sqlite_libencoding());
 	php_info_print_table_end();
@@ -2593,7 +2593,7 @@ PHP_FUNCTION(sqlite_prev)
 PHP_FUNCTION(sqlite_escape_string)
 {
 	char *string = NULL;
-	long stringlen;
+	int stringlen;
 	char *ret;
 
 	if (FAILURE == zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "s", &string, &stringlen)) {

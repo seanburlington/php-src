81d974f25665fd8b46b3fcd7790707d0c314493e
iliaa <iliaa>
2004-02-28 22:58:56 +0000

    MFH: Remove unused variable.

--- ext/pdf/pdf.c
+++ ext/pdf/pdf.c
@@ -17,7 +17,7 @@
    +----------------------------------------------------------------------+
 */
 
-/* $Id: pdf.c,v 1.112.2.8 2004/02/28 21:00:39 iliaa Exp $ */
+/* $Id: pdf.c,v 1.112.2.9 2004/02/28 22:58:56 iliaa Exp $ */
 
 /* pdflib 2.02 ... 3.0x is subject to the ALADDIN FREE PUBLIC LICENSE.
    Copyright (C) 1997-1999 Thomas Merz. 2000-2001 PDFlib GmbH */
@@ -331,7 +331,7 @@ PHP_MINFO_FUNCTION(pdf)
 #else
 	php_info_print_table_row(2, "PDFlib GmbH Version", tmp );
 #endif
-	php_info_print_table_row(2, "Revision", "$Revision: 1.112.2.8 $" );
+	php_info_print_table_row(2, "Revision", "$Revision: 1.112.2.9 $" );
 	php_info_print_table_end();
 
 }
@@ -1952,7 +1952,7 @@ PHP_FUNCTION(pdf_open_image_file)
 PHP_FUNCTION(pdf_open_memory_image)
 {
 	zval **arg1, **arg2;
-	int i, j, color, count;
+	int i, j, color;
 	int pdf_image;
 	gdImagePtr im;
 	unsigned char *buffer, *ptr;
@@ -1970,7 +1970,6 @@ PHP_FUNCTION(pdf_open_memory_image)
 	}
 	ZEND_FETCH_RESOURCE(im, gdImagePtr, arg2, -1, "Image", le_gd);
 
-	count = 3 * im->sx * im->sy;
 	buffer = (unsigned char *) safe_emalloc(3 * im->sx, im->sy, 0);
 
 	ptr = buffer;

b4263631f71d09fa08780db99780de39b9c9d462
mj <mj>
2004-03-12 18:05:08 +0000

    * Adding new state "emeritus", which can be used for former maintainers of   packages.
    Submitted by: Daniel Convissor
    
    # The necessary updates for the website and the manual will follow in a
    # few hours or at least tomorrow morning.

--- pear/PEAR/Common.php
+++ pear/PEAR/Common.php
@@ -17,7 +17,7 @@
 // |          Tomas V.V.Cox <cox@idecnet.com>                             |
 // +----------------------------------------------------------------------+
 //
-// $Id: Common.php,v 1.119 2004/01/26 00:57:30 pajoye Exp $
+// $Id: Common.php,v 1.120 2004/03/12 18:05:08 mj Exp $
 
 require_once 'PEAR.php';
 require_once 'Archive/Tar.php';
@@ -51,7 +51,7 @@ $GLOBALS['_PEAR_Common_tempfiles'] = array();
  * Valid maintainer roles
  * @var array
  */
-$GLOBALS['_PEAR_Common_maintainer_roles'] = array('lead','developer','contributor','helper');
+$GLOBALS['_PEAR_Common_maintainer_roles'] = array('lead','developer','contributor','helper', 'emeritus');
 
 /**
  * Valid release states

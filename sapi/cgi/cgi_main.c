cdc19e81dd064a70fc6ee93b8bc72661577b1514
iliaa <iliaa>
2004-03-12 14:07:13 +0000

    MFH: Fixed bug #22127 (Invalid response code when force-cgi-redirect safety mechanism is triggered).

--- sapi/cgi/cgi_main.c
+++ sapi/cgi/cgi_main.c
@@ -20,7 +20,7 @@
    +----------------------------------------------------------------------+
 */
 
-/* $Id: cgi_main.c,v 1.190.2.58 2004/02/11 22:00:34 helly Exp $ */
+/* $Id: cgi_main.c,v 1.190.2.59 2004/03/12 14:07:14 iliaa Exp $ */
 
 #include "php.h"
 #include "php_globals.h"
@@ -1106,6 +1106,7 @@ int main(int argc, char *argv[])
 			    in case some server does something different than above */
 			&& (!redirect_status_env || !getenv(redirect_status_env))
 			) {
+			SG(sapi_headers).http_response_code = 400;
 			PUTS("<b>Security Alert!</b> The PHP CGI cannot be accessed directly.\n\n\
 <p>This PHP CGI binary was compiled with force-cgi-redirect enabled.  This\n\
 means that a page will only be served up if the REDIRECT_STATUS CGI variable is\n\

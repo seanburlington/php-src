95eb3ceba962886516ede69e506a805daf7e6cd6
iliaa <iliaa>
2004-04-01 19:07:01 +0000

    Revert patch for bug #27782.

--- ext/standard/array.c
+++ ext/standard/array.c
@@ -22,7 +22,7 @@
 */
 
 
-/* $Id: array.c,v 1.199.2.31 2004/03/30 19:09:10 iliaa Exp $ */
+/* $Id: array.c,v 1.199.2.32 2004/04/01 19:07:01 iliaa Exp $ */
 
 #include "php.h"
 #include "php_ini.h"
@@ -718,10 +718,6 @@ PHP_FUNCTION(prev)
 		RETURN_FALSE;
 	}
 	zend_hash_move_backwards(target_hash);
-	if (!target_hash->pInternalPointer) {
-		zend_hash_internal_pointer_reset(target_hash);
-		RETURN_FALSE;
-	}
 
 	if (return_value_used) {	
 		if (zend_hash_get_current_data(target_hash, (void **) &entry) == FAILURE) {
@@ -750,10 +746,6 @@ PHP_FUNCTION(next)
 		RETURN_FALSE;
 	}
 	zend_hash_move_forward(target_hash);
-	if (!target_hash->pInternalPointer) {
-		zend_hash_internal_pointer_end(target_hash);
-		RETURN_FALSE;
-	}
 
 	if (return_value_used) {
 		if (zend_hash_get_current_data(target_hash, (void **) &entry) == FAILURE) {

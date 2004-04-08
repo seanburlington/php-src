8ae1f4b2ee3f0ebe95bebf92c1d53c6aefe93ff0
cellog <cellog>
2004-04-08 15:03:21 +0000

    Dan wants tabs -> spaces

--- pear/go-pear-list.php
+++ pear/go-pear-list.php
@@ -3,24 +3,24 @@
  * that will be used to create the PEAR folder
  * in the windows snapshot.
  * See win32/build/mkdist.php for more details
- * $Id: go-pear-list.php,v 1.3 2004/04/08 14:59:31 cellog Exp $
+ * $Id: go-pear-list.php,v 1.4 2004/04/08 15:03:21 cellog Exp $
  */
 $packages  = array(
-"PEAR"					=>	"1.3.1",
-"Mail"					=>	"1.1.2",
-"Net_SMTP"				=>	"1.2.5",
-"Net_Socket"			=>	"1.0.1",
-"PHPUnit"				=>	"2.0.0beta1",
-"Console_Getopt"		=>	"1.2",
-"DB"					=>	"1.6.2",
-"HTTP"					=>	"1.2.3",
-"Archive_Tar"			=>	"1.1",
-"Pager"					=>	"1.0.8",
-"HTML_Template_IT"		=>	"1.1",
-"XML_Parser"			=>	"1.0.1",
-"XML_RPC"				=>	"1.1.0",
-"Net_UserAgent_Detect"	=>	"1.0",
-"PEAR_Frontend_Web"		=>	"0.3"
+"PEAR"                  =>    "1.3.1",
+"Mail"                  =>    "1.1.2",
+"Net_SMTP"              =>    "1.2.5",
+"Net_Socket"            =>    "1.0.1",
+"PHPUnit"               =>    "2.0.0beta1",
+"Console_Getopt"        =>    "1.2",
+"DB"                    =>    "1.6.2",
+"HTTP"                  =>    "1.2.3",
+"Archive_Tar"           =>    "1.1",
+"Pager"                 =>    "1.0.8",
+"HTML_Template_IT"      =>    "1.1",
+"XML_Parser"            =>    "1.0.1",
+"XML_RPC"               =>    "1.1.0",
+"Net_UserAgent_Detect"  =>    "1.0",
+"PEAR_Frontend_Web"     =>    "0.3"
 );
 
 ?>

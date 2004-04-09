6afde69340a74f598ed34abcd741028af136b28f
cellog <cellog>
2004-04-09 01:12:41 +0000

    as per PEAR-Core discussion, remove all but absolutely essential packages for the installer, and for PEAR_Frontend_Web

--- pear/go-pear-list.php
+++ pear/go-pear-list.php
@@ -3,24 +3,20 @@
  * that will be used to create the PEAR folder
  * in the windows snapshot.
  * See win32/build/mkdist.php for more details
- * $Id: go-pear-list.php,v 1.6 2004/04/08 23:52:36 cellog Exp $
+ * $Id: go-pear-list.php,v 1.7 2004/04/09 01:12:41 cellog Exp $
  */
 $packages  = array(
+// required packages for the installer
 "PEAR"                  =>    "1.3.1",
-"Mail"                  =>    "1.1.3",
-"Net_SMTP"              =>    "1.2.5",
-"Net_Socket"            =>    "1.0.1",
-"PHPUnit2"               =>    "2.0.0beta1",
+"XML_RPC"               =>    "1.1.0",
 "Console_Getopt"        =>    "1.2",
-"DB"                    =>    "1.6.2",
-"HTTP"                  =>    "1.2.3",
 "Archive_Tar"           =>    "1.1",
+
+// required packages for the web frontend
+"PEAR_Frontend_Web"     =>    "0.3"
 "Pager"                 =>    "1.0.8",
 "HTML_Template_IT"      =>    "1.1",
-"XML_Parser"            =>    "1.0.1",
-"XML_RPC"               =>    "1.1.0",
 "Net_UserAgent_Detect"  =>    "1.0",
-"PEAR_Frontend_Web"     =>    "0.3"
 );
 
 ?>

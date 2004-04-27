385e1573227a26ab937c03ae1e25f3bee33a5861
pollita <pollita>
2004-04-27 19:28:21 +0000

    MFH Don't strip off those []s, it'll only create problems in wrappers and elsewhere.

--- ext/standard/url.c
+++ ext/standard/url.c
@@ -15,7 +15,7 @@
    | Author: Jim Winstead <jimw@php.net>                                  |
    +----------------------------------------------------------------------+
  */
-/* $Id: url.c,v 1.58.2.13 2004/04/27 19:21:36 pollita Exp $ */
+/* $Id: url.c,v 1.58.2.14 2004/04/27 19:28:21 pollita Exp $ */
 
 #include <stdlib.h>
 #include <string.h>
@@ -225,11 +225,6 @@ PHPAPI php_url *php_url_parse(char *str)
 		p = e;
 	}
 
-	if (*s == '[' && *(p-1) == ']') {
-		s++;
-		p--;
-	}
-	
 	/* check if we have a valid host, if we don't reject the string as url */
 	if ((p-s) < 1) {
 		STR_FREE(ret->scheme);

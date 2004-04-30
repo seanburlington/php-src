0a70b32ed2a4cb195ca5fc5d267499e76a897b27
iliaa <iliaa>
2004-04-30 13:26:01 +0000

    Fixed bug #28228 (number_format() does not allow empty decimal separator).

--- ext/standard/math.c
+++ ext/standard/math.c
@@ -19,7 +19,7 @@
    +----------------------------------------------------------------------+
 */
 
-/* $Id: math.c,v 1.112 2004/03/10 15:59:50 abies Exp $ */
+/* $Id: math.c,v 1.113 2004/04/30 13:26:01 iliaa Exp $ */
 
 #include "php.h"
 #include "php_math.h"
@@ -1020,7 +1020,12 @@ PHPAPI char *_php_math_number_format(double d, int dec, char dec_point, char tho
 		is_negative = 1;
 		d = -d;
 	}
-	dec = MAX(0, dec);
+	if (!dec_point && dec > 0) {
+		d *= pow(10, dec);
+		dec = 0;
+	} else {
+		dec = MAX(0, dec);
+	}
 
 	PHP_ROUND_WITH_FUZZ(d, dec);
 
@@ -1140,6 +1145,8 @@ PHP_FUNCTION(number_format)
 		convert_to_string_ex(t_s);
 		if (Z_STRLEN_PP(d_p)==1) {
 			dec_point=Z_STRVAL_PP(d_p)[0];
+		} else if (Z_STRLEN_PP(d_p)==0) {
+			dec_point=0;
 		}
 		if (Z_STRLEN_PP(t_s)==1) {
 			thousand_sep=Z_STRVAL_PP(t_s)[0];

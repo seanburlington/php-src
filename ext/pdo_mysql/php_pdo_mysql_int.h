936e4ba193baaf642c90c2e11d17edad6b040a11
wez <wez>
2004-05-18 19:25:45 +0000

    Minor tweaks

--- ext/pdo_mysql/php_pdo_mysql_int.h
+++ ext/pdo_mysql/php_pdo_mysql_int.h
@@ -16,7 +16,7 @@
   +----------------------------------------------------------------------+
 */
 
-/* $Id: php_pdo_mysql_int.h,v 1.2 2004/05/18 19:06:47 wez Exp $ */
+/* $Id: php_pdo_mysql_int.h,v 1.3 2004/05/18 19:25:45 wez Exp $ */
 
 #ifndef PHP_PDO_MYSQL_INT_H
 #define PHP_PDO_MYSQL_INT_H
@@ -40,7 +40,7 @@ typedef struct {
 	MYSQL_RES		*result;
 	MYSQL_ROW		current_data;
 	long			*current_lengths;
-	unsigned int		last_err;
+	int		last_err;
 	pdo_mysql_column 	*cols;
 } pdo_mysql_stmt;
 
@@ -53,9 +53,9 @@ typedef struct {
 
 extern pdo_driver_t pdo_mysql_driver;
 
-int _mysql_error(char *what, int errno, const char *file, int line TSRMLS_DC);
+extern int _mysql_error(char *what, int errno, const char *file, int line TSRMLS_DC);
 #define pdo_mysql_error(w,s)	_mysql_error(w, s, __FILE__, __LINE__ TSRMLS_CC)
-int mysql_handle_error(pdo_dbh_t *dbh, pdo_mysql_db_handle *H, int errcode);
+extern int mysql_handle_error(pdo_dbh_t *dbh, pdo_mysql_db_handle *H, int errcode);
 
 extern struct pdo_stmt_methods mysql_stmt_methods;
 #endif

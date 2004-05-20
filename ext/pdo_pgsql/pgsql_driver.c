7c5708b41a54fa5c501a44bac3d8c49cbad1cb20
iliaa <iliaa>
2004-05-20 18:19:03 +0000

    Make sure that the error string is freed.

--- ext/pdo_pgsql/pgsql_driver.c
+++ ext/pdo_pgsql/pgsql_driver.c
@@ -16,7 +16,7 @@
   +----------------------------------------------------------------------+
 */
 
-/* $Id: pgsql_driver.c,v 1.4 2004/05/20 18:00:24 iliaa Exp $ */
+/* $Id: pgsql_driver.c,v 1.5 2004/05/20 18:19:03 iliaa Exp $ */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -85,6 +85,10 @@ static int pgsql_handle_closer(pdo_dbh_t *dbh TSRMLS_DC) /* {{{ */
 	if (H->server) {
 		PQfinish(H->server);
 		H->server = NULL;
+	}
+	if (H->einfo.errmsg) {
+		efree(H->einfo.errmsg);
+		H->einfo.errmsg = NULL;
 	}
 	return 0;
 }
@@ -174,6 +178,9 @@ static int pdo_pgsql_handle_factory(pdo_dbh_t *dbh, zval *driver_options TSRMLS_
 
 	H = pecalloc(1, sizeof(pdo_pgsql_db_handle), dbh->is_persistent);
 	dbh->driver_data = H;
+
+	H->einfo.errcode = 0;
+	H->einfo.errmsg = NULL;
 	
 	/* handle for the server */
 	host = vars[0].optval;

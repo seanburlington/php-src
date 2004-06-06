e15d6b6a58c7db628ce7f0dc6fa6af7769d5ed1b
pajoye <pajoye>
2004-06-06 22:49:10 +0000

    - Update for 5.0RC3

--- pear/go-pear-list.php
+++ pear/go-pear-list.php
@@ -3,17 +3,17 @@
  * that will be used to create the PEAR folder
  * in the windows snapshot.
  * See win32/build/mkdist.php for more details
- * $Id: go-pear-list.php,v 1.7 2004/04/09 01:12:41 cellog Exp $
+ * $Id: go-pear-list.php,v 1.8 2004/06/06 22:49:10 pajoye Exp $
  */
 $packages  = array(
 // required packages for the installer
 "PEAR"                  =>    "1.3.1",
 "XML_RPC"               =>    "1.1.0",
 "Console_Getopt"        =>    "1.2",
-"Archive_Tar"           =>    "1.1",
+"Archive_Tar"           =>    "1.2",
 
 // required packages for the web frontend
-"PEAR_Frontend_Web"     =>    "0.3"
+"PEAR_Frontend_Web"     =>    "0.4"
 "Pager"                 =>    "1.0.8",
 "HTML_Template_IT"      =>    "1.1",
 "Net_UserAgent_Detect"  =>    "1.0",

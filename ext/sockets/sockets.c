069d8e46f3dfaf823f8e790fcbefbe1afa5221d6
pollita <pollita>
2004-06-07 05:00:37 +0000

    MFB: Missing length limit parameter.

--- ext/sockets/sockets.c
+++ ext/sockets/sockets.c
@@ -19,7 +19,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: sockets.c,v 1.164 2004/05/26 06:53:04 derick Exp $ */
+/* $Id: sockets.c,v 1.165 2004/06/07 05:00:37 pollita Exp $ */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -322,7 +322,7 @@ static char *php_strerror(int error TSRMLS_DC)
 		buf = hstrerror(error);
 #else
 		{
-			spprintf(&(SOCKETS_G(strerror_buf)), "Host lookup error %d", error);
+			spprintf(&(SOCKETS_G(strerror_buf)), 0, "Host lookup error %d", error);
 			buf = SOCKETS_G(strerror_buf);
 		}
 #endif

515996b8e8bd30f7f032e03b7c50553d87b893b7
fmk <fmk>
2004-06-09 18:12:29 +0000

    MFH: Fix win32 build

--- ext/mssql/php_mssql.h
+++ ext/mssql/php_mssql.h
@@ -17,7 +17,7 @@
  */
 
 
-/* $Id: php_mssql.h,v 1.23.4.11 2004/06/04 01:02:21 pollita Exp $ */
+/* $Id: php_mssql.h,v 1.23.4.12 2004/06/09 18:12:29 fmk Exp $ */
 
 #ifndef PHP_MSSQL_H
 #define PHP_MSSQL_H
@@ -36,8 +36,8 @@
 #include "sqlfront.h"
 #include "sqldb.h"
 
-#if HAVE_FREETDS
 typedef short TDS_SHORT;
+#if HAVE_FREETDS
 #define SQLTEXT SYBTEXT
 #define SQLCHAR SYBCHAR
 #define SQLVARCHAR SYBVARCHAR

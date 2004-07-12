a0450d0f49197a6d8880cc7206d5e5090c6d111f
tony2001 <tony2001>
2004-07-12 07:42:41 +0000

    MFH: fix #28978 (multiple OCIBindByName on the same placeholder eats memory)

--- ext/oci8/oci8.c
+++ ext/oci8/oci8.c
@@ -21,7 +21,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: oci8.c,v 1.183.2.12 2004/02/05 08:30:17 tony2001 Exp $ */
+/* $Id: oci8.c,v 1.183.2.13 2004/07/12 07:42:41 tony2001 Exp $ */
 
 /* TODO list:
  *
@@ -641,7 +641,7 @@ PHP_MINFO_FUNCTION(oci)
 
 	php_info_print_table_start();
 	php_info_print_table_row(2, "OCI8 Support", "enabled");
-	php_info_print_table_row(2, "Revision", "$Revision: 1.183.2.12 $");
+	php_info_print_table_row(2, "Revision", "$Revision: 1.183.2.13 $");
 #ifndef PHP_WIN32
 	php_info_print_table_row(2, "Oracle Version", PHP_OCI8_VERSION );
 	php_info_print_table_row(2, "Compile-time ORACLE_HOME", PHP_OCI8_DIR );
@@ -2982,7 +2982,7 @@ break;
 	}
 
 	memset((void*)&bind,0,sizeof(oci_bind));
-	zend_hash_next_index_insert(statement->binds,&bind,sizeof(oci_bind),(void **)&bindp);
+	zend_hash_update(statement->binds, Z_STRVAL_PP(name), Z_STRLEN_PP(name) + 1, &bind, sizeof(oci_bind), (void **)&bindp);
 
 	bindp->descr = mydescr;
 	bindp->pStmt = mystmt;

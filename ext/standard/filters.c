d804c855080e2b4ca60e945f591ae4c9e11f5a34
moriyoshi <moriyoshi>
2004-07-20 19:35:38 +0000

    oops...

--- ext/standard/filters.c
+++ ext/standard/filters.c
@@ -17,7 +17,7 @@
    +----------------------------------------------------------------------+
 */
 
-/* $Id: filters.c,v 1.41 2004/07/20 18:16:36 moriyoshi Exp $ */
+/* $Id: filters.c,v 1.42 2004/07/20 19:35:38 moriyoshi Exp $ */
 
 #include "php.h"
 #include "php_globals.h"
@@ -176,7 +176,7 @@ typedef struct _php_strip_tags_filter {
 static int php_strip_tags_filter_ctor(php_strip_tags_filter *inst, const char *allowed_tags, int allowed_tags_len, int persistent)
 {
 	if (allowed_tags != NULL) {
-		if (NULL != (inst->allowed_tags = pemalloc(allowed_tags_len, persistent))) {
+		if (NULL == (inst->allowed_tags = pemalloc(allowed_tags_len, persistent))) {
 			return FAILURE;
 		}
 		memcpy((char *)inst->allowed_tags, allowed_tags, allowed_tags_len);

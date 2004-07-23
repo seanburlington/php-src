9c1861d4692e2d75ad40ccb5eea63ff0abe0e950
edink <edink>
2004-07-23 02:28:51 +0000

    MFH: Updated version number

--- ext/gd/gd.c
+++ ext/gd/gd.c
@@ -18,7 +18,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: gd.c,v 1.221.2.42 2004/05/20 23:38:05 iliaa Exp $ */
+/* $Id: gd.c,v 1.221.2.43 2004/07/23 02:28:51 edink Exp $ */
 
 /* gd 1.2 is copyright 1994, 1995, Quest Protein Database Center, 
    Cold Spring Harbor Labs. */
@@ -390,7 +390,7 @@ PHP_RSHUTDOWN_FUNCTION(gd)
 /* }}} */
 
 #if HAVE_GD_BUNDLED
-#define PHP_GD_VERSION_STRING "bundled (2.0.23 compatible)"
+#define PHP_GD_VERSION_STRING "bundled (2.0.28 compatible)"
 #elif HAVE_LIBGD20
 #define PHP_GD_VERSION_STRING "2.0 or higher"
 #elif HAVE_GDIMAGECOLORRESOLVE

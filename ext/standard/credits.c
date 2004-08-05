82b513e23c49906713b1301fc0f4b11a71f7de68
goba <goba>
2004-08-05 16:26:15 +0000

    update documentation credits, mentioning that more contributors are listed in the manual

--- ext/standard/credits.c
+++ ext/standard/credits.c
@@ -17,7 +17,7 @@
    +----------------------------------------------------------------------+
 */
 
-/* $Id: credits.c,v 1.32 2004/01/08 08:17:30 andi Exp $ */
+/* $Id: credits.c,v 1.33 2004/08/05 16:26:15 goba Exp $ */
 
 #include "php.h"
 #include "info.h"
@@ -85,9 +85,11 @@ PHPAPI void php_print_credits(int flag TSRMLS_DC)
 
 	if (flag & PHP_CREDITS_DOCS) {
 		php_info_print_table_start();
-		php_info_print_table_header(1, "PHP Documentation Team");
-		php_info_print_table_row(1, "Jouni Ahto, Alexander Aulbach, Stig Bakken, Rasmus Lerdorf, Egon Schmid, Lars Torben Wilson, Jim Winstead");
-		php_info_print_table_row(1, "Edited by:  Stig Bakken and Egon Schmid");
+		php_info_print_table_colspan_header(2, "PHP Documentation");
+		CREDIT_LINE("Authors", "Mehdi Achour, Friedhelm Betz, Antony Dovgal, Nuno Lopes, Philip Olson, Georg Richter, Damien Seguy, Jakub Vrana");
+		CREDIT_LINE("Editor", "Gabor Hojtsy");
+		CREDIT_LINE("User Note Maintainers", "Mehdi Achour, Friedhelm Betz, Vincent Gevers, Aidan Lister, Nuno Lopes, Tom Sommer");
+		CREDIT_LINE("Other Contributors", "Previously active authors, editors and other contributors are listed in the manual.");
 		php_info_print_table_end();
 	}
 

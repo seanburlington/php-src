61aaf82c1073687779c11369112059856f74d06d
fmk <fmk>
2004-08-21 03:09:45 +0000

    Fix win32 compilation. Missing uid_t and gid_t definitions.

--- main/SAPI.h
+++ main/SAPI.h
@@ -16,7 +16,7 @@
    +----------------------------------------------------------------------+
 */
 
-/* $Id: SAPI.h,v 1.109 2004/08/10 17:40:00 rasmus Exp $ */
+/* $Id: SAPI.h,v 1.110 2004/08/21 03:09:45 fmk Exp $ */
 
 #ifndef SAPI_H
 #define SAPI_H
@@ -24,6 +24,9 @@
 #include "zend.h"
 #include "zend_llist.h"
 #include "zend_operators.h"
+#ifdef PHP_WIN32
+#include "win95nt.h"
+#endif
 #include <sys/stat.h>
 
 #define SAPI_OPTION_NO_CHDIR 1

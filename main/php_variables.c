85b7200b9df8e99f64bf7a1006e65e1c662e0bf1
rasmus <rasmus>
2004-09-09 16:21:49 +0000

    Stop at the index on an unmatched [

--- main/php_variables.c
+++ main/php_variables.c
@@ -17,7 +17,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: php_variables.c,v 1.81 2004/07/11 21:14:50 sesser Exp $ */
+/* $Id: php_variables.c,v 1.81.2.1 2004/09/09 16:21:49 rasmus Exp $ */
 
 #include <stdio.h>
 #include "php.h"
@@ -133,7 +133,7 @@ PHPAPI void php_register_variable_ex(char *var, zval *val, pval *track_vars_arra
 				if (!ip) {
 					/* PHP variables cannot contain '[' in their names, so we replace the character with a '_' */
 					*(index_s - 1) = '_';
-					index_len = var_len = strlen(var);
+					index_len = var_len = strlen(index);
 					goto plain_var;
 					return;
 				}

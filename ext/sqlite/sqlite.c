c8456bcb90f53748608f8abd3237f772141abbbd
wez <wez>
2004-09-25 23:13:49 +0000

    Probable fix for Bug #30147

--- ext/sqlite/sqlite.c
+++ ext/sqlite/sqlite.c
@@ -17,7 +17,7 @@
    |          Marcus Boerger <helly@php.net>                              |
    +----------------------------------------------------------------------+
 
-   $Id: sqlite.c,v 1.148 2004/08/02 22:43:40 iliaa Exp $ 
+   $Id: sqlite.c,v 1.149 2004/09/25 23:13:49 wez Exp $ 
 */
 
 #ifdef HAVE_CONFIG_H
@@ -1071,7 +1071,7 @@ PHP_MINFO_FUNCTION(sqlite)
 {
 	php_info_print_table_start();
 	php_info_print_table_header(2, "SQLite support", "enabled");
-	php_info_print_table_row(2, "PECL Module version", PHP_SQLITE_MODULE_VERSION " $Id: sqlite.c,v 1.148 2004/08/02 22:43:40 iliaa Exp $");
+	php_info_print_table_row(2, "PECL Module version", PHP_SQLITE_MODULE_VERSION " $Id: sqlite.c,v 1.149 2004/09/25 23:13:49 wez Exp $");
 	php_info_print_table_row(2, "SQLite Library", sqlite_libversion());
 	php_info_print_table_row(2, "SQLite Encoding", sqlite_libencoding());
 	php_info_print_table_end();
@@ -1969,6 +1969,7 @@ PHP_FUNCTION(sqlite_fetch_object)
 	if (res->curr_row < res->nrows) {
 		php_sqlite_fetch_array(res, PHPSQLITE_ASSOC, decode_binary, 1, &dataset TSRMLS_CC);
 	} else {
+		php_std_error_handling();
 		RETURN_FALSE;
 	}
 

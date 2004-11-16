cfb0b2ffb6f709ac5c8fe4185973bcc262911dba
dmitry <dmitry>
2004-11-16 12:13:37 +0000

    Class mapping was implemented (Bug #29385 Soapserver always uses std class).

--- ext/soap/php_soap.h
+++ ext/soap/php_soap.h
@@ -17,7 +17,7 @@
   |          Dmitry Stogov <dmitry@zend.com>                             |
   +----------------------------------------------------------------------+
 */
-/* $Id: php_soap.h,v 1.33.2.1 2004/08/10 16:30:30 dmitry Exp $ */
+/* $Id: php_soap.h,v 1.33.2.2 2004/11/16 12:13:40 dmitry Exp $ */
 
 #ifndef PHP_SOAP_H
 #define PHP_SOAP_H
@@ -116,6 +116,7 @@ struct _soapService {
 	char      *actor;
 	char      *uri;
 	xmlCharEncodingHandlerPtr encoding;
+	HashTable *class_map;
 };
 
 #define SOAP_CLASS 1
@@ -160,6 +161,7 @@ ZEND_BEGIN_MODULE_GLOBALS(soap)
 	char*      cache_dir;
 	long       cache_ttl;
 	xmlCharEncodingHandlerPtr encoding;
+	HashTable *class_map;
 ZEND_END_MODULE_GLOBALS(soap)
 
 #ifdef PHP_WIN32

1315fcafc4e20e4b3c0bd47e71b851d0b23a9340
helly <helly>
2004-11-24 19:59:28 +0000

    - MFH Bugfix #30856

--- ext/reflection/php_reflection.c
+++ ext/reflection/php_reflection.c
@@ -19,7 +19,7 @@
    +----------------------------------------------------------------------+
 */
 
-/* $Id: php_reflection.c,v 1.111.2.12 2004/11/14 18:07:15 helly Exp $ */
+/* $Id: php_reflection.c,v 1.111.2.13 2004/11/24 19:59:28 helly Exp $ */
 #include "zend.h"
 #include "zend_API.h"
 #include "zend_exceptions.h"
@@ -2127,6 +2127,13 @@ ZEND_METHOD(reflection_class, getStaticProperties)
 	
 	METHOD_NOTSTATIC_NUMPARAMS(0);	
 	GET_REFLECTION_OBJECT_PTR(ce);
+
+	if (!ce->constants_updated) {
+		zend_hash_apply_with_argument(&ce->default_properties, (apply_func_arg_t) zval_update_constant, (void *) 1 TSRMLS_CC);
+		zend_hash_apply_with_argument(ce->static_members, (apply_func_arg_t) zval_update_constant, (void *) 1 TSRMLS_CC);
+		ce->constants_updated = 1;
+	}
+
 	array_init(return_value);
 	zend_hash_copy(Z_ARRVAL_P(return_value), ce->static_members, (copy_ctor_func_t) zval_add_ref, (void *) &tmp_copy, sizeof(zval *));
 }

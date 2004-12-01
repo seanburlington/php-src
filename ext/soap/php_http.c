19343c3fa36b9d26964802c9fad4093e68cd4d8c
dmitry <dmitry>
2004-12-01 16:59:23 +0000

    Fixed bug #30359 (SOAP client requests have no port in "Host" field ).

--- ext/soap/php_http.c
+++ ext/soap/php_http.c
@@ -17,7 +17,7 @@
   |          Dmitry Stogov <dmitry@zend.com>                             |
   +----------------------------------------------------------------------+
 */
-/* $Id: php_http.c,v 1.55.2.2 2004/11/04 15:29:54 dmitry Exp $ */
+/* $Id: php_http.c,v 1.55.2.3 2004/12/01 16:59:23 dmitry Exp $ */
 
 #include "php_soap.h"
 #include "ext/standard/base64.h"
@@ -380,6 +380,10 @@ try_again:
 		smart_str_append_const(&soap_headers, " HTTP/1.1\r\n"
 			"Host: ");
 		smart_str_appends(&soap_headers, phpurl->host);
+		if (phpurl->port != 80) {
+			smart_str_appendc(&soap_headers, ':');
+			smart_str_append_unsigned(&soap_headers, phpurl->port);
+		}
 		smart_str_append_const(&soap_headers, "\r\n"
 			"Connection: Keep-Alive\r\n"
 /*

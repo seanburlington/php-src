180874dfa86767c6e584d96860ed9b79e356e711
tony2001 <tony2001>
2004-12-09 17:16:33 +0000

    CS changes

--- ext/session/session.c
+++ ext/session/session.c
@@ -17,7 +17,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: session.c,v 1.391.2.5 2004/12/09 14:17:34 tony2001 Exp $ */
+/* $Id: session.c,v 1.391.2.6 2004/12/09 17:16:33 tony2001 Exp $ */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -1336,8 +1336,7 @@ PHP_FUNCTION(session_module_name)
 
 		if (PS(mod) && PS(mod)->s_name) {
 			RETVAL_STRING(safe_estrdup(PS(mod)->s_name), 0);
-		}
-		else {
+		} else {
 			RETVAL_EMPTY_STRING();
 		}
 		
@@ -1345,8 +1344,7 @@ PHP_FUNCTION(session_module_name)
 	} else {
 		if (PS(mod) && PS(mod)->s_name) {
 			RETURN_STRING(safe_estrdup(PS(mod)->s_name), 0);
-		}
-		else {
+		} else {
 			RETURN_EMPTY_STRING();
 		}
 	}

9d2302217de0afb7454900c2d18df5b61c0a015a
georg <georg>
2004-12-20 16:39:14 +0000

    prevent type conversion during mysql_stmt_execute for NULL variables

--- ext/mysqli/mysqli_api.c
+++ ext/mysqli/mysqli_api.c
@@ -15,7 +15,7 @@
   | Author: Georg Richter <georg@php.net>                                |
   +----------------------------------------------------------------------+
 
-  $Id: mysqli_api.c,v 1.94 2004/11/01 07:19:23 georg Exp $ 
+  $Id: mysqli_api.c,v 1.95 2004/12/20 16:39:14 georg Exp $ 
 */
 
 #ifdef HAVE_CONFIG_H
@@ -539,24 +539,24 @@ PHP_FUNCTION(mysqli_stmt_execute)
 	
 	for (i = 0; i < stmt->param.var_cnt; i++) {		
 		if (stmt->param.vars[i]) {
-			stmt->param.is_null[i] = (stmt->param.vars[i]->type == IS_NULL);
-
-			switch (stmt->stmt->params[i].buffer_type) {
-				case MYSQL_TYPE_VAR_STRING:
-					convert_to_string_ex(&stmt->param.vars[i]);
-					stmt->stmt->params[i].buffer = Z_STRVAL_PP(&stmt->param.vars[i]);
-					stmt->stmt->params[i].buffer_length = strlen(Z_STRVAL_PP(&stmt->param.vars[i]));
-					break;
-				case MYSQL_TYPE_DOUBLE:
-					convert_to_double_ex(&stmt->param.vars[i]);
-					stmt->stmt->params[i].buffer = (gptr)&Z_LVAL_PP(&stmt->param.vars[i]);
-					break;
-				case MYSQL_TYPE_LONG:
-					convert_to_long_ex(&stmt->param.vars[i]);
-					stmt->stmt->params[i].buffer = (gptr)&Z_LVAL_PP(&stmt->param.vars[i]);
-					break;
-				default:
-					break;
+			if ( !(stmt->param.is_null[i] = (stmt->param.vars[i]->type == IS_NULL)) ) {
+				switch (stmt->stmt->params[i].buffer_type) {
+					case MYSQL_TYPE_VAR_STRING:
+						convert_to_string_ex(&stmt->param.vars[i]);
+						stmt->stmt->params[i].buffer = Z_STRVAL_PP(&stmt->param.vars[i]);
+						stmt->stmt->params[i].buffer_length = strlen(Z_STRVAL_PP(&stmt->param.vars[i]));
+						break;
+					case MYSQL_TYPE_DOUBLE:
+						convert_to_double_ex(&stmt->param.vars[i]);
+						stmt->stmt->params[i].buffer = (gptr)&Z_LVAL_PP(&stmt->param.vars[i]);
+						break;
+					case MYSQL_TYPE_LONG:
+						convert_to_long_ex(&stmt->param.vars[i]);
+						stmt->stmt->params[i].buffer = (gptr)&Z_LVAL_PP(&stmt->param.vars[i]);
+						break;
+					default:
+						break;
+				}
 			}	
 		}
 	}

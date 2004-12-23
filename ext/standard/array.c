7996aff837a31b6282ee53ef2db170e592d624db
tony2001 <tony2001>
2004-12-23 16:35:48 +0000

    destroy return_value and fix leak in array_fill()

--- ext/standard/array.c
+++ ext/standard/array.c
@@ -21,7 +21,7 @@
    +----------------------------------------------------------------------+
 */
 
-/* $Id: array.c,v 1.287 2004/12/16 19:13:58 tony2001 Exp $ */
+/* $Id: array.c,v 1.288 2004/12/23 16:35:48 tony2001 Exp $ */
 
 #include "php.h"
 #include "php_ini.h"
@@ -1542,6 +1542,8 @@ PHP_FUNCTION(array_fill)
 	convert_to_long_ex(num);
 	i = Z_LVAL_PP(num) - 1;	
 	if (i < 0) {
+		zend_hash_destroy(Z_ARRVAL_P(return_value));
+		efree(Z_ARRVAL_P(return_value));
 		php_error_docref(NULL TSRMLS_CC, E_WARNING, "Number of elements must be positive");
 		RETURN_FALSE;
 	}

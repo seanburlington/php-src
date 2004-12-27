205926a0cab1c06b86b18e2926632202891d03b5
georg <georg>
2004-12-27 15:39:34 +0000

    fixed testcase 047 fixed windows compile error

--- ext/mysqli/mysqli_api.c
+++ ext/mysqli/mysqli_api.c
@@ -15,7 +15,7 @@
   | Author: Georg Richter <georg@php.net>                                |
   +----------------------------------------------------------------------+
 
-  $Id: mysqli_api.c,v 1.96 2004/12/27 11:48:57 georg Exp $ 
+  $Id: mysqli_api.c,v 1.97 2004/12/27 15:39:34 georg Exp $ 
 */
 
 #ifdef HAVE_CONFIG_H
@@ -982,6 +982,7 @@ PHP_FUNCTION(mysqli_info)
    Initialize mysqli and return a resource for use with mysql_real_connect */
 PHP_FUNCTION(mysqli_init)
 {
+	MYSQLI_RESOURCE *mysqli_resource;
 	MY_MYSQL *mysql = (MY_MYSQL *)calloc(1, sizeof(MY_MYSQL));
 
 	if (!(mysql->mysql = mysql_init(NULL))) {
@@ -989,7 +990,7 @@ PHP_FUNCTION(mysqli_init)
 		RETURN_FALSE;
 	}
 
-	MYSQLI_RESOURCE *mysqli_resource = (MYSQLI_RESOURCE *)ecalloc (1, sizeof(MYSQLI_RESOURCE));
+	mysqli_resource = (MYSQLI_RESOURCE *)ecalloc (1, sizeof(MYSQLI_RESOURCE));
 	mysqli_resource->ptr = (void *)mysql;
 	MYSQLI_RETURN_RESOURCE(mysqli_resource, mysqli_link_class_entry);	
 }

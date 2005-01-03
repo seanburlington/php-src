8a2b92b1a44b0dc26198b2ab064e5c9716530900
stas <stas>
2005-01-03 11:28:12 +0000

    merge patch from http://www.sqlite.org/cvstrac/chngview?cn=1953 bug led to PHP exiting on queries with unknown columns

--- ext/sqlite/libsqlite/src/auth.c
+++ ext/sqlite/libsqlite/src/auth.c
@@ -14,7 +14,7 @@
 ** systems that do not need this facility may omit it by recompiling
 ** the library with -DSQLITE_OMIT_AUTHORIZATION=1
 **
-** $Id: auth.c,v 1.3.6.2 2004/07/10 12:25:34 wez Exp $
+** $Id: auth.c,v 1.3.6.3 2005/01/03 11:28:12 stas Exp $
 */
 #include "sqliteInt.h"
 
@@ -111,6 +111,7 @@ void sqliteAuthRead(
   const char *zCol;     /* Name of the column of the table */
   int iSrc;             /* Index in pTabList->a[] of table being read */
   const char *zDBase;   /* Name of database being accessed */
+  TriggerStack *pStack; /* The stack of current triggers */
 
   if( db->xAuth==0 ) return;
   assert( pExpr->op==TK_COLUMN );
@@ -119,15 +120,14 @@ void sqliteAuthRead(
   }
   if( iSrc>=0 && iSrc<pTabList->nSrc ){
     pTab = pTabList->a[iSrc].pTab;
-  }else{
+  }else if( (pStack = pParse->trigStack)!=0 ){
     /* This must be an attempt to read the NEW or OLD pseudo-tables
     ** of a trigger.
     */
-    TriggerStack *pStack; /* The stack of current triggers */
-    pStack = pParse->trigStack;
-    assert( pStack!=0 );
     assert( pExpr->iTable==pStack->newIdx || pExpr->iTable==pStack->oldIdx );
     pTab = pStack->pTab;
+  }else{
+    return;
   }
   if( pTab==0 ) return;
   if( pExpr->iColumn>=0 ){

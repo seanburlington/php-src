d907e7c35035b46a16288d29f470af1702133da1
iliaa <iliaa>
2005-01-18 15:47:57 +0000

    Fixed bug #31479 (Fixed crash in chunk_split(), when chunklen > strlen).

--- ext/standard/string.c
+++ ext/standard/string.c
@@ -18,7 +18,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: string.c,v 1.427 2004/11/21 13:35:32 tony2001 Exp $ */
+/* $Id: string.c,v 1.428 2005/01/18 15:47:57 iliaa Exp $ */
 
 /* Synced with php 3.0 revision 1.193 1999-06-16 [ssb] */
 
@@ -1888,6 +1888,10 @@ PHP_FUNCTION(chunk_split)
 	if (chunklen <= 0) {
 		php_error_docref(NULL TSRMLS_CC, E_WARNING, "Chunk length should be greater than zero.");
 		RETURN_FALSE;
+	}
+
+	if (chunklen > Z_STRLEN_PP(p_str)) {
+		RETURN_STRINGL(Z_STRVAL_PP(p_str), Z_STRLEN_PP(p_str), 1);	
 	}
 
 	if (!Z_STRLEN_PP(p_str)) {

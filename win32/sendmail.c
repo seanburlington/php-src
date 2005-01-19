887456c2a511b59bf8c8dd45b60a09d75b873642
iliaa <iliaa>
2005-01-19 18:54:23 +0000

    Stricter cc header check.

--- win32/sendmail.c
+++ win32/sendmail.c
@@ -17,7 +17,7 @@
  *
  */
 
-/* $Id: sendmail.c,v 1.60 2005/01/19 17:38:20 sniper Exp $ */
+/* $Id: sendmail.c,v 1.61 2005/01/19 18:54:23 iliaa Exp $ */
 
 #include "php.h"				/*php specific */
 #include <stdio.h>
@@ -485,7 +485,7 @@ int SendText(char *RPath, char *Subject, char *mailTo, char *mailCc, char *mailB
 		efree(tempMailTo);
 	}
 	/* Send mail to all Cc rcpt's */
-	else if (headers && (pos1 = strstr(headers_lc, "cc:")) && ((pos1 == headers_lc) || iscntrl(*(pos1-1)))) {
+	else if (headers && (pos1 = strstr(headers_lc, "cc:")) && ((pos1 == headers_lc) || (*(pos1-1) == '\n'))) {
 		/* Real offset is memaddress from the original headers + difference of
 		 * string found in the lowercase headrs + 3 characters to jump over
 		 * the cc: */

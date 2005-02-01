329bcb30bbd734462c2fc4c6713ebce16d985dcf
iliaa <iliaa>
2005-02-01 23:28:30 +0000

    MFH: Fixed bug #31797 (exif_read_data() uses too low nesting limit).

--- ext/exif/exif.c
+++ ext/exif/exif.c
@@ -17,7 +17,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: exif.c,v 1.162.2.3 2005/01/21 23:59:55 andrei Exp $ */
+/* $Id: exif.c,v 1.162.2.4 2005/02/01 23:28:31 iliaa Exp $ */
 
 /*  ToDos
  *
@@ -93,7 +93,7 @@ typedef unsigned char uchar;
 
 #define EFREE_IF(ptr)	if (ptr) efree(ptr)
 
-#define MAX_IFD_NESTING_LEVEL 5
+#define MAX_IFD_NESTING_LEVEL 25
 
 static
 ZEND_BEGIN_ARG_INFO(exif_thumbnail_force_ref, 1)
@@ -112,7 +112,7 @@ function_entry exif_functions[] = {
 };
 /* }}} */
 
-#define EXIF_VERSION "1.4 $Id: exif.c,v 1.162.2.3 2005/01/21 23:59:55 andrei Exp $"
+#define EXIF_VERSION "1.4 $Id: exif.c,v 1.162.2.4 2005/02/01 23:28:31 iliaa Exp $"
 
 /* {{{ PHP_MINFO_FUNCTION
  */

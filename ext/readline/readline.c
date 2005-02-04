d51d7d7349144c52261c801b6bad02f7945e1cf8
iliaa <iliaa>
2005-02-04 00:21:58 +0000

    MFH: Proper fix for bug #31796 .

--- ext/readline/readline.c
+++ ext/readline/readline.c
@@ -16,7 +16,7 @@
    +----------------------------------------------------------------------+
 */
 
-/* $Id: readline.c,v 1.35.2.1 2005/02/03 22:46:24 iliaa Exp $ */
+/* $Id: readline.c,v 1.35.2.2 2005/02/04 00:21:58 iliaa Exp $ */
 
 /* {{{ includes & prototypes */
 
@@ -354,7 +354,7 @@ static char *_readline_command_generator(char *text,int state)
 		}
 	}
 
-	return strdup("");
+	return NULL;
 }
 
 static zval *_readline_string_zval(const char *str)
@@ -393,7 +393,13 @@ static char **_readline_completion_cb(char *text, int start, int end)
 
 	if (call_user_function(CG(function_table), NULL, params[0], &_readline_array, 3, params+1 TSRMLS_CC) == SUCCESS) {
 		if (Z_TYPE(_readline_array) == IS_ARRAY) {
-			matches = completion_matches(text,_readline_command_generator);
+			if (zend_hash_num_elements(Z_ARRVAL(_readline_array))) {
+				matches = completion_matches(text,_readline_command_generator);
+			} else {
+				matches = malloc(sizeof(char *) * 2);
+				matches[0] = strdup("");
+				matches[1] = '\0';
+			}
 		}
 	}
 	

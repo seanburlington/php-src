85a077cf5b789eb439041eaeb7f87d70359b4a8b
fmk <fmk>
2005-02-09 18:50:22 +0000

    MFH: Check for warnings and return the warning texts

--- ext/fbsql/php_fbsql.c
+++ ext/fbsql/php_fbsql.c
@@ -16,7 +16,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: php_fbsql.c,v 1.105.2.3 2005/01/27 20:21:32 fmk Exp $ */
+/* $Id: php_fbsql.c,v 1.105.2.4 2005/02/09 18:50:22 fmk Exp $ */
 
 /* TODO:
  *
@@ -1867,6 +1867,24 @@ int mdOk(PHPFBLink* link, FBCMetaData* md, char* sql)
 		fbcemdRelease(emd);
 		result = 0;
 	}
+	else if (fbcmdWarningsFound(md))
+	{
+		FBCErrorMetaData* emd = fbcdcErrorMetaData(c, md);
+		char*             emg = fbcemdAllErrorMessages(emd);
+		if (FB_SQL_G(generateWarnings))
+		{
+			if (emg)
+				php_error_docref(NULL TSRMLS_CC, E_WARNING, "Warning in statement: '%s' %s", sql, emg);
+			else
+				php_error_docref(NULL TSRMLS_CC, E_WARNING, "No message");
+		}
+		link->errorText = strdup(emg);
+		link->errorNo = fbcemdErrorCodeAtIndex(emd, 0);
+		free(emg);
+		fbcemdRelease(emd);
+		result = 1;
+	}
+	return result;
 	return result;
 }
 /* }}} */

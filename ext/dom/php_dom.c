ffa7412034153f2c86d6bb341c6ded22a579d796
rrichards <rrichards>
2005-02-09 11:46:43 +0000

    Fixed bug #31878 (Segmentation fault using clone keyword on nodes)

--- ext/dom/php_dom.c
+++ ext/dom/php_dom.c
@@ -18,7 +18,7 @@
    +----------------------------------------------------------------------+
 */
 
-/* $Id: php_dom.c,v 1.67 2005/01/20 11:47:05 rrichards Exp $ */
+/* $Id: php_dom.c,v 1.68 2005/02/09 11:46:43 rrichards Exp $ */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -933,7 +933,7 @@ void dom_objects_clone(void *object, void **object_clone TSRMLS_DC)
 					clone->document = intern->document;
 				}
 				php_libxml_increment_doc_ref((php_libxml_node_object *)clone, cloned_node->doc TSRMLS_CC);
-				php_libxml_increment_node_ptr((php_libxml_node_object *)clone, cloned_node, NULL TSRMLS_CC);
+				php_libxml_increment_node_ptr((php_libxml_node_object *)clone, cloned_node, (void *)clone TSRMLS_CC);
 			}
 
 		}

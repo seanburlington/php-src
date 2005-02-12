d9f0c03a708490a1928988eb02f600ab85012be9
sniper <sniper>
2005-02-12 18:14:03 +0000

    Remove unused set_h_errno

--- ext/sockets/sockets.c
+++ ext/sockets/sockets.c
@@ -19,7 +19,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: sockets.c,v 1.166 2004/09/17 12:44:56 wez Exp $ */
+/* $Id: sockets.c,v 1.167 2005/02/12 18:14:03 sniper Exp $ */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -50,7 +50,6 @@
 # include <sys/uio.h>
 # define IS_INVALID_SOCKET(a)	(a->bsd_socket < 0)
 # define set_errno(a) (errno = a)
-# define set_h_errno(a) (h_errno = a)
 #else /* windows */
 # include "php_sockets.h"
 # include "php_sockets_win.h"

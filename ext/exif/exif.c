37cdf8c760158fc3ca92c34e7a40961c2e3f8f60
iliaa <iliaa>
2005-02-15 21:56:17 +0000

    Fixed bug #31986 (Max nesting limit set too low).

--- ext/exif/exif.c
+++ ext/exif/exif.c
@@ -17,7 +17,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: exif.c,v 1.166 2005/02/01 23:27:33 iliaa Exp $ */
+/* $Id: exif.c,v 1.167 2005/02/15 21:56:17 iliaa Exp $ */
 
 /*  ToDos
  *
@@ -93,7 +93,7 @@ typedef unsigned char uchar;
 
 #define EFREE_IF(ptr)	if (ptr) efree(ptr)
 
-#define MAX_IFD_NESTING_LEVEL 25
+#define MAX_IFD_NESTING_LEVEL 100
 
 static
 ZEND_BEGIN_ARG_INFO(exif_thumbnail_force_ref, 1)
@@ -112,7 +112,7 @@ function_entry exif_functions[] = {
 };
 /* }}} */
 
-#define EXIF_VERSION "1.4 $Id: exif.c,v 1.166 2005/02/01 23:27:33 iliaa Exp $"
+#define EXIF_VERSION "1.4 $Id: exif.c,v 1.167 2005/02/15 21:56:17 iliaa Exp $"
 
 /* {{{ PHP_MINFO_FUNCTION
  */

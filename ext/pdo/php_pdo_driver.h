95ae5c294448f557c88b542dfcad844748f2ed1e
helly <helly>
2005-02-20 16:12:57 +0000

    - Add fetch flag PDO_FETCH_CLASSTYPE

--- ext/pdo/php_pdo_driver.h
+++ ext/pdo/php_pdo_driver.h
@@ -16,7 +16,7 @@
   +----------------------------------------------------------------------+
 */
 
-/* $Id: php_pdo_driver.h,v 1.50 2005/02/20 15:07:29 helly Exp $ */
+/* $Id: php_pdo_driver.h,v 1.51 2005/02/20 16:12:57 helly Exp $ */
 
 #ifndef PHP_PDO_DRIVER_H
 #define PHP_PDO_DRIVER_H
@@ -84,6 +84,7 @@ enum pdo_fetch_type {
 #define PDO_FETCH_FLAGS     0xFFFF0000  /* fetchAll() modes or'd to PDO_FETCH_XYZ */
 #define PDO_FETCH_GROUP     0x00010000  /* fetch into groups */
 #define PDO_FETCH_UNIQUE    0x00030000  /* fetch into groups assuming first col is unique */
+#define PDO_FETCH_CLASSTYPE 0x00040000  /* fetch class gets its class name from 1st column */
 
 /* fetch orientation for scrollable cursors */
 enum pdo_fetch_orientation {

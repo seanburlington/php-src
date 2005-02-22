fe29c5ed1553f2eca25fa7432e3e3df6175cfeea
helly <helly>
2005-02-22 11:37:53 +0000

    - Call dtor

--- ext/pdo/pdo_dbh.c
+++ ext/pdo/pdo_dbh.c
@@ -18,7 +18,7 @@
   +----------------------------------------------------------------------+
 */
 
-/* $Id: pdo_dbh.c,v 1.56 2005/02/13 03:26:43 wez Exp $ */
+/* $Id: pdo_dbh.c,v 1.57 2005/02/22 11:37:53 helly Exp $ */
 
 /* The PDO Database Handle Class */
 
@@ -1089,7 +1089,7 @@ zend_object_value pdo_dbh_new(zend_class_entry *ce TSRMLS_DC)
 	ALLOC_HASHTABLE(dbh->properties);
 	zend_hash_init(dbh->properties, 0, NULL, ZVAL_PTR_DTOR, 0);
 	
-	retval.handle = zend_objects_store_put(dbh, NULL, pdo_dbh_free_storage, NULL TSRMLS_CC);
+	retval.handle = zend_objects_store_put(dbh, zend_objects_destroy_object, pdo_dbh_free_storage, NULL TSRMLS_CC);
 	if(ce == pdo_dbh_ce) {
 		retval.handlers = &pdo_dbh_object_handlers;
 	}

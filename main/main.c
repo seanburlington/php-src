0f716ed3056415cafcc72a9743fd26cdfc35cbab
sniper <sniper>
2005-03-08 21:42:10 +0000

    This makes more sense. (Someone might actually use php_error_docref0() :)

--- main/main.c
+++ main/main.c
@@ -18,7 +18,7 @@
    +----------------------------------------------------------------------+
 */
 
-/* $Id: main.c,v 1.618 2005/02/27 12:50:22 sniper Exp $ */
+/* $Id: main.c,v 1.619 2005/03/08 21:42:10 sniper Exp $ */
 
 /* {{{ includes
  */
@@ -545,9 +545,9 @@ PHPAPI void php_verror(const char *docref, const char *params, int type, const c
 }
 /* }}} */
 
-/* {{{ php_error_docref */
+/* {{{ php_error_docref0 */
 /* See: CODING_STANDARDS for details. */
-PHPAPI void php_error_docref(const char *docref TSRMLS_DC, int type, const char *format, ...)
+PHPAPI void php_error_docref0(const char *docref TSRMLS_DC, int type, const char *format, ...)
 {
 	va_list args;
 	

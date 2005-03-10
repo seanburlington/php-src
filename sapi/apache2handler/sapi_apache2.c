54e664ab37d4719695e4d0d3759e807284152ad8
jorton <jorton>
2005-03-10 11:23:57 +0000

    MFH: - Fixed bug #31717: Reject PATH_INFO if "AcceptPathInfo off" is used.

--- sapi/apache2handler/sapi_apache2.c
+++ sapi/apache2handler/sapi_apache2.c
@@ -18,7 +18,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: sapi_apache2.c,v 1.40.2.6 2005/01/17 12:35:28 jorton Exp $ */
+/* $Id: sapi_apache2.c,v 1.40.2.7 2005/03/10 11:23:57 jorton Exp $ */
 
 #define ZEND_INCLUDE_FULL_WINDOWS_HEADERS
 
@@ -464,6 +464,16 @@ static int php_handler(request_rec *r)
 			} zend_end_try();
 			return DECLINED;
 		}
+	}
+
+	/* Give a 404 if PATH_INFO is used but is explicitly disabled in
+	 * the configuration; default behaviour is to accept. */ 
+	if (r->used_path_info == AP_REQ_REJECT_PATH_INFO
+		&& r->path_info && r->path_info[0]) {
+		zend_try {
+			zend_ini_deactivate(TSRMLS_C);
+		} zend_end_try();
+		return HTTP_NOT_FOUND;
 	}
 
 	/* handle situations where user turns the engine off */

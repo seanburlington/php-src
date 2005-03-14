128c89d8f86919b0d4c07db632802e495bb55e9a
cellog <cellog>
2005-03-14 23:57:07 +0000

    this needs updating

--- pear/go-pear-list.php
+++ pear/go-pear-list.php
@@ -3,19 +3,19 @@
  * that will be used to create the PEAR folder
  * in the windows snapshot.
  * See win32/build/mkdist.php for more details
- * $Id: go-pear-list.php,v 1.11 2005/02/26 01:27:49 cellog Exp $
+ * $Id: go-pear-list.php,v 1.12 2005/03/14 23:57:07 cellog Exp $
  */
 $packages  = array(
 // required packages for the installer
 "PEAR"                  =>    "1.3.5",
-"XML_RPC"               =>    "1.1.0",
+"XML_RPC"               =>    "1.2.2",
 "Console_Getopt"        =>    "1.2",
-"Archive_Tar"           =>    "1.2",
+"Archive_Tar"           =>    "1.3.0",
 
 // required packages for the web frontend
 "PEAR_Frontend_Web"     =>    "0.4",
 "HTML_Template_IT"      =>    "1.1",
-"Net_UserAgent_Detect"  =>    "1.0",
+"Net_UserAgent_Detect"  =>    "2.0.1",
 );
 
 ?>

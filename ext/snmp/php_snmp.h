16adcea6c1a34a0d177a7a00e4227d63819fcca9
harrie <harrie>
2005-03-16 14:29:40 +0000

    Adding SNMPv2 community based (i.e. protocol version) functions.

--- ext/snmp/php_snmp.h
+++ ext/snmp/php_snmp.h
@@ -20,7 +20,7 @@
   +----------------------------------------------------------------------+
 */
 
-/* $Id: php_snmp.h,v 1.23 2005/01/08 12:02:22 sniper Exp $ */
+/* $Id: php_snmp.h,v 1.24 2005/03/16 14:29:40 harrie Exp $ */
 
 #ifndef PHP_SNMP_H
 #define PHP_SNMP_H
@@ -51,6 +51,12 @@ PHP_FUNCTION(snmp_set_quick_print);
 PHP_FUNCTION(snmp_set_enum_print);
 PHP_FUNCTION(snmp_set_oid_numeric_print);
 PHP_FUNCTION(snmpset);
+
+PHP_FUNCTION(snmp2_get);
+PHP_FUNCTION(snmp2_getnext);
+PHP_FUNCTION(snmp2_walk);
+PHP_FUNCTION(snmp2_real_walk);
+PHP_FUNCTION(snmp2_set);
 
 PHP_FUNCTION(snmp3_get);
 PHP_FUNCTION(snmp3_getnext);

a911e6b4c1404882d70a56bfacb0befff1ac2478
iliaa <iliaa>
2005-03-17 17:16:43 +0000

    Fixed compiler warning on win32.

--- ext/ftp/ftp.c
+++ ext/ftp/ftp.c
@@ -17,7 +17,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: ftp.c,v 1.109 2005/03/10 23:37:45 iliaa Exp $ */
+/* $Id: ftp.c,v 1.110 2005/03/17 17:16:43 iliaa Exp $ */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -838,7 +838,9 @@ ftp_get(ftpbuf_t *ftp, php_stream *outstream, const char *path, ftptype_t type, 
 		}
 
 		if (type == FTPTYPE_ASCII) {
+#ifndef PHP_WIN32
 			char *s;
+#endif
 			char *ptr = data->buf;
 			char *e = ptr + rcvd;
 			/* logic depends on the OS EOL

72ab523574492b987849ed98412168f413c5e73b
cellog <cellog>
2005-03-18 01:55:10 +0000

    update to Archive_Tar 1.3.1

--- pear/go-pear-list.php
+++ pear/go-pear-list.php
@@ -3,14 +3,14 @@
  * that will be used to create the PEAR folder
  * in the windows snapshot.
  * See win32/build/mkdist.php for more details
- * $Id: go-pear-list.php,v 1.12 2005/03/14 23:57:07 cellog Exp $
+ * $Id: go-pear-list.php,v 1.13 2005/03/18 01:55:10 cellog Exp $
  */
 $packages  = array(
 // required packages for the installer
 "PEAR"                  =>    "1.3.5",
 "XML_RPC"               =>    "1.2.2",
 "Console_Getopt"        =>    "1.2",
-"Archive_Tar"           =>    "1.3.0",
+"Archive_Tar"           =>    "1.3.1",
 
 // required packages for the web frontend
 "PEAR_Frontend_Web"     =>    "0.4",

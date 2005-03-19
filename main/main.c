7cae3796cd308e7b2b76912366816ca9fe3b82f4
helly <helly>
2005-03-19 14:27:17 +0000

    - We don't need to block those here (works as it works with normal error   overloading)

--- main/main.c
+++ main/main.c
@@ -18,7 +18,7 @@
    +----------------------------------------------------------------------+
 */
 
-/* $Id: main.c,v 1.619 2005/03/08 21:42:10 sniper Exp $ */
+/* $Id: main.c,v 1.620 2005/03/19 14:27:17 helly Exp $ */
 
 /* {{{ includes
  */
@@ -652,10 +652,8 @@ static void php_error_cb(int type, const char *error_filename, const uint error_
 	/* according to error handling mode, suppress error, throw exception or show it */
 	if (PG(error_handling) != EH_NORMAL) {
 		switch (type) {
-			case E_ERROR:
 			case E_CORE_ERROR:
 			case E_COMPILE_ERROR:
-			case E_USER_ERROR:
 			case E_PARSE:
 				/* fatal errors are real errors and cannot be made exceptions */
 				break;

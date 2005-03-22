aea4de987e13b6c2fb69dfc5fdba16ba7787e21e
tony2001 <tony2001>
2005-03-22 15:09:18 +0000

    MFH: fix #28803 (enabled debug causes bailout errors with CLI on AIX because of fflush() called on already closed filedescriptor)

--- sapi/cli/php_cli.c
+++ sapi/cli/php_cli.c
@@ -19,7 +19,7 @@
    +----------------------------------------------------------------------+
 */
 
-/* $Id: php_cli.c,v 1.113.2.1 2004/11/15 14:08:33 tony2001 Exp $ */
+/* $Id: php_cli.c,v 1.113.2.2 2005/03/22 15:09:20 tony2001 Exp $ */
 
 #include "php.h"
 #include "php_globals.h"
@@ -220,7 +220,10 @@ static int sapi_cli_ub_write(const char *str, uint str_length TSRMLS_DC)
 
 static void sapi_cli_flush(void *server_context)
 {
-	if (fflush(stdout)==EOF) {
+	/* Ignore EBADF here, it's caused by the fact that STDIN/STDOUT/STDERR streams
+	 * are/could be closed before fflush() is called.
+	 */
+	if (fflush(stdout)==EOF && errno!=EBADF) {
 #ifndef PHP_CLI_WIN32_NO_CONSOLE
 		php_handle_aborted_connection();
 #endif

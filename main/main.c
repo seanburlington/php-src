b4e05d788adcd6d17b7bd3f3ccab6416f3bdf57d
helly <helly>
2005-03-23 07:57:17 +0000

    - MFH Notices shouldn't be treatedas errors and thus not being conidered to   be converted to exceptions.

--- main/main.c
+++ main/main.c
@@ -18,7 +18,7 @@
    +----------------------------------------------------------------------+
 */
 
-/* $Id: main.c,v 1.604.2.9 2005/03/19 14:28:04 helly Exp $ */
+/* $Id: main.c,v 1.604.2.10 2005/03/23 07:57:17 helly Exp $ */
 
 /* {{{ includes
  */
@@ -659,6 +659,10 @@ static void php_error_cb(int type, const char *error_filename, const uint error_
 			case E_COMPILE_ERROR:
 			case E_PARSE:
 				/* fatal errors are real errors and cannot be made exceptions */
+				break;
+			case E_NOTICE:
+			case E_USER_NOTICE:
+				/* notices are no errors and are not treated as such like E_WARNIGNS */
 				break;
 			default:
 				/* throw an exception if we are in EH_THROW mode

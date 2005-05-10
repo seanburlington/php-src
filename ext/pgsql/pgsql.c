d648fdac72727b4dc5de277bd474bb1099de8a63
tony2001 <tony2001>
2005-05-10 20:14:11 +0000

    MFH: fix #32904 (pg_get_notify() ignores result_type parameter)

--- ext/pgsql/pgsql.c
+++ ext/pgsql/pgsql.c
@@ -19,7 +19,7 @@
    +----------------------------------------------------------------------+
  */
  
-/* $Id: pgsql.c,v 1.244.2.38 2005/04/13 22:12:06 tony2001 Exp $ */
+/* $Id: pgsql.c,v 1.244.2.39 2005/05/10 20:14:11 tony2001 Exp $ */
 
 #include <stdlib.h>
 
@@ -3138,11 +3138,11 @@ PHP_FUNCTION(pg_get_notify)
 		RETURN_FALSE;
 	}
 	array_init(return_value);
-	if (result_type & (PGSQL_NUM|PGSQL_BOTH)) {
+	if (result_type == PGSQL_NUM || result_type == PGSQL_BOTH) {
 		add_index_string(return_value, 0, pgsql_notify->relname, 1);
 		add_index_long(return_value, 1, pgsql_notify->be_pid);
 	}
-	if (result_type & (PGSQL_ASSOC|PGSQL_BOTH)) {
+	if (result_type == PGSQL_ASSOC || result_type == PGSQL_BOTH) {
 		add_assoc_string(return_value, "message", pgsql_notify->relname, 1);
 		add_assoc_long(return_value, "pid", pgsql_notify->be_pid);
 	}

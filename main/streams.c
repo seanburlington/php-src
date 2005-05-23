bce7e8b10550e31c5feaf9bd4e670ca49698a302
tony2001 <tony2001>
2005-05-23 15:37:29 +0000

    revert by Wez's request

--- main/streams.c
+++ main/streams.c
@@ -20,7 +20,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: streams.c,v 1.125.2.97 2005/05/23 11:52:44 tony2001 Exp $ */
+/* $Id: streams.c,v 1.125.2.98 2005/05/23 15:37:29 tony2001 Exp $ */
 
 #define _GNU_SOURCE
 #include "php.h"
@@ -654,7 +654,7 @@ PHPAPI size_t _php_stream_read(php_stream *stream, char *buf, size_t size TSRMLS
 		}
 
 		/* just break anyway, to avoid greedy read */
-		if (stream->wrapper != NULL && stream->wrapper != &php_plain_files_wrapper) {
+		if (stream->wrapper != &php_plain_files_wrapper) {
 			break;
 		}
 	}

ab62dcf100800c93fee0d9e813d699bdbce7baa8
tony2001 <tony2001>
2005-05-23 11:52:12 +0000

    MFH: fix bug #32810 (fread after tmpfile() reads only 8192 bytes)

--- main/streams/streams.c
+++ main/streams/streams.c
@@ -19,7 +19,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: streams.c,v 1.61.2.11 2005/05/16 08:37:41 tony2001 Exp $ */
+/* $Id: streams.c,v 1.61.2.12 2005/05/23 11:52:12 tony2001 Exp $ */
 
 #define _GNU_SOURCE
 #include "php.h"
@@ -592,8 +592,9 @@ PHPAPI size_t _php_stream_read(php_stream *stream, char *buf, size_t size TSRMLS
 		}
 
 		/* just break anyway, to avoid greedy read */
-		if (stream->wrapper != &php_plain_files_wrapper)
+		if (stream->wrapper != NULL && stream->wrapper != &php_plain_files_wrapper) {
 			break;
+		}
 	}
 
 	if (didread > 0) {

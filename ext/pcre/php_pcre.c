31ef4ba4e0e80c82e23923fb7c9c6478c716e191
andrei <andrei>
2005-05-24 21:12:16 +0000

    MFH (pcre cache flush)

--- ext/pcre/php_pcre.c
+++ ext/pcre/php_pcre.c
@@ -16,7 +16,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: php_pcre.c,v 1.132.2.22 2005/01/21 10:34:46 derick Exp $ */
+/* $Id: php_pcre.c,v 1.132.2.23 2005/05/24 21:12:16 andrei Exp $ */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -152,13 +152,21 @@ PHPAPI pcre* pcre_get_compiled_regex(char *regex, pcre_extra **extra, int *preg_
 	   back the compiled pattern, otherwise go on and compile it. */
 	regex_len = strlen(regex);
 	if (zend_hash_find(&PCRE_G(pcre_cache), regex, regex_len+1, (void **)&pce) == SUCCESS) {
+		/*
+		 * We use a quick pcre_info() check to see whether cache is corrupted, and if it
+		 * is, we flush it and compile the pattern from scratch.
+		 */
+		if (pcre_info(pce->re, NULL, NULL) == PCRE_ERROR_BADMAGIC) {
+			zend_hash_clean(&PCRE_G(pcre_cache));
+		} else {
 #if HAVE_SETLOCALE
-		if (!strcmp(pce->locale, locale)) {
+			if (!strcmp(pce->locale, locale)) {
 #endif
-			*extra = pce->extra;
-			*preg_options = pce->preg_options;
-			return pce->re;
+				*extra = pce->extra;
+				*preg_options = pce->preg_options;
+				return pce->re;
 #if HAVE_SETLOCALE
+			}
 		}
 #endif
 	}

5d988c058c1da28f6ccfbe296b0c96d4832f8893
iliaa <iliaa>
2005-06-01 22:28:03 +0000

    Fixed bug #33210 (relax jpeg recursive loop protection).

--- ext/standard/image.c
+++ ext/standard/image.c
@@ -17,7 +17,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: image.c,v 1.112 2005/03/06 22:18:09 helly Exp $ */
+/* $Id: image.c,v 1.113 2005/06/01 22:28:03 iliaa Exp $ */
 
 #include "php.h"
 #include <stdio.h>
@@ -398,7 +398,7 @@ static unsigned int php_next_marker(php_stream * stream, int last_marker, int co
 				last_marker = M_PSEUDO; /* stop skipping non 0xff for M_COM */
 			}
 		}
-		if (++a > 10)
+		if (++a > 25)
 		{
 			/* who knows the maxim amount of 0xff? though 7 */
 			/* but found other implementations              */

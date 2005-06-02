ce23b87607d78da439fd205f15c7661cb133fd58
tony2001 <tony2001>
2005-06-02 21:04:12 +0000

    fix bug #33222 (segfault when CURL handle is closed in a callback). fix segfaults when CURL callback functions throw exception.

--- ext/curl/php_curl.h
+++ ext/curl/php_curl.h
@@ -17,7 +17,7 @@
    +----------------------------------------------------------------------+
 */
 
-/* $Id: php_curl.h,v 1.41 2004/03/12 18:37:55 sterling Exp $ */
+/* $Id: php_curl.h,v 1.42 2005/06/02 21:04:12 tony2001 Exp $ */
 
 #ifndef _PHP_CURL_H
 #define _PHP_CURL_H
@@ -121,6 +121,7 @@ typedef struct {
 	php_curl_handlers       *handlers;
 	long                     id;
 	unsigned int             uses;
+	zend_bool                in_callback;
 } php_curl;
 
 typedef struct {

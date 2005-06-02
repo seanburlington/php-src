443e83b3aaba7572df0e4d2d70b73bb04c0a7180
derick <derick>
2005-06-02 08:31:38 +0000

    - MFH: Fixed memory corruption in stristr().

--- ext/standard/string.c
+++ ext/standard/string.c
@@ -18,7 +18,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: string.c,v 1.333.2.50 2005/05/31 12:56:00 sniper Exp $ */
+/* $Id: string.c,v 1.333.2.51 2005/06/02 08:31:38 derick Exp $ */
 
 /* Synced with php 3.0 revision 1.193 1999-06-16 [ssb] */
 
@@ -1317,8 +1317,8 @@ PHP_FUNCTION(stristr)
 		if (!Z_STRLEN_PP(needle)) {
 			php_error_docref(NULL TSRMLS_CC, E_WARNING, "Empty delimiter.");
 			efree(haystack_orig);
-			zval_ptr_dtor(haystack);
-			zval_ptr_dtor(needle);
+//			zval_ptr_dtor(haystack);
+//			zval_ptr_dtor(needle);
 			RETURN_FALSE;
 		}
 
@@ -1339,8 +1339,8 @@ PHP_FUNCTION(stristr)
 		RETVAL_FALSE;
 	}
 
-	zval_ptr_dtor(haystack);
-	zval_ptr_dtor(needle);
+//	zval_ptr_dtor(haystack);
+//	zval_ptr_dtor(needle);
 	efree(haystack_orig);
 }
 /* }}} */

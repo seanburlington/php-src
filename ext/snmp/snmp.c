cc4305def6e511644ec1c1751773e192217f4fe7
hyanantha <hyanantha>
2005-06-28 13:43:29 +0000

    removing redundant NEW_LIBC checks

--- ext/snmp/snmp.c
+++ ext/snmp/snmp.c
@@ -20,7 +20,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: snmp.c,v 1.92.2.10 2005/05/10 12:20:03 sniper Exp $ */
+/* $Id: snmp.c,v 1.92.2.11 2005/06/28 13:43:29 hyanantha Exp $ */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -40,18 +40,12 @@
 #include "win32/time.h"
 #elif defined(NETWARE)
 #ifdef USE_WINSOCK
-/*#include <ws2nlm.h>*/
 #include <novsock2.h>
 #else
 #include <sys/socket.h>
 #endif
 #include <errno.h>
-/*#include <process.h>*/
-#ifdef NEW_LIBC
 #include <sys/timeval.h>
-#else
-#include "netware/time_nw.h"
-#endif
 #else
 #include <sys/socket.h>
 #include <netinet/in.h>

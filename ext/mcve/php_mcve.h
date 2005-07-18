0cd7d5fb1480b1ae7bca371ab8c832faa2cf8473
bradmssw <bradmssw>
2005-07-18 20:10:06 +0000

    sync missing functions from libmonetra-5

--- ext/mcve/php_mcve.h
+++ ext/mcve/php_mcve.h
@@ -16,7 +16,7 @@
    |          Chris Faulhaber <jedgar@fxp.org>                            |
    +----------------------------------------------------------------------+
 */
-/* $Id: php_mcve.h,v 1.15 2005/06/06 16:43:23 bradmssw Exp $ */
+/* $Id: php_mcve.h,v 1.16 2005/07/18 20:10:06 bradmssw Exp $ */
 
 #ifndef _PHP_MCVE_H
 #define _PHP_MCVE_H
@@ -51,6 +51,10 @@ PHP_FUNCTION(m_destroyconn);
 PHP_FUNCTION(m_setdropfile);
 PHP_FUNCTION(m_setip);
 PHP_FUNCTION(m_setssl);
+#if LIBMONETRA_VERSION >= 050000
+PHP_FUNCTION(m_setssl_cafile);
+PHP_FUNCTION(m_responsekeys);
+#endif
 PHP_FUNCTION(m_setssl_files);
 PHP_FUNCTION(m_setblocking);
 PHP_FUNCTION(m_settimeout);

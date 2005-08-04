a9073f58904f729de5785ef922bea97f616927f8
dmitry <dmitry>
2005-08-04 10:35:36 +0000

    Fixed bug #33989 (extract($GLOBALS,EXTR_REFS) crashes PHP)

--- ext/standard/array.c
+++ ext/standard/array.c
@@ -21,7 +21,7 @@
    +----------------------------------------------------------------------+
 */
 
-/* $Id: array.c,v 1.266.2.20 2005/07/04 10:08:21 dmitry Exp $ */
+/* $Id: array.c,v 1.266.2.21 2005/08/04 10:35:36 dmitry Exp $ */
 
 #include "php.h"
 #include "php_ini.h"
@@ -1393,11 +1393,11 @@ PHP_FUNCTION(extract)
 					zval **orig_var;
 
 					if (zend_hash_find(EG(active_symbol_table), final_name.c, final_name.len+1, (void **) &orig_var) == SUCCESS) {
-						zval_ptr_dtor(orig_var);
-
 						SEPARATE_ZVAL_TO_MAKE_IS_REF(entry);
 						zval_add_ref(entry);
 						
+						zval_ptr_dtor(orig_var);
+
 						*orig_var = *entry;
 					} else {
 						if ((*var_array)->refcount > 1) {

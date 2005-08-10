2b8d4493c62aede798cb102cb131b7e63cf18adc
tony2001 <tony2001>
2005-08-10 10:28:18 +0000

    fix build

--- ext/standard/array.c
+++ ext/standard/array.c
@@ -21,7 +21,7 @@
    +----------------------------------------------------------------------+
 */
 
-/* $Id: array.c,v 1.310 2005/08/10 08:23:39 dmitry Exp $ */
+/* $Id: array.c,v 1.311 2005/08/10 10:28:18 tony2001 Exp $ */
 
 #include "php.h"
 #include "php_ini.h"
@@ -3048,7 +3048,7 @@ static void php_array_intersect(INTERNAL_FUNCTION_PARAMETERS, int behavior, int 
 		HashTable *ht;
 		zval *tmp;
 
-		ALLOC_HASHTABLE_REL(ht);
+		ALLOC_HASHTABLE(ht);
 		zend_hash_init(ht, 0, NULL, ZVAL_PTR_DTOR, 0);
 		zend_hash_copy(ht, return_value->value.ht, (copy_ctor_func_t) zval_add_ref, (void *) &tmp, sizeof(zval *));
 		return_value->value.ht = ht;		
@@ -3433,7 +3433,7 @@ static void php_array_diff(INTERNAL_FUNCTION_PARAMETERS, int behavior, int data_
 		HashTable *ht;
 		zval *tmp;
 
-		ALLOC_HASHTABLE_REL(ht);
+		ALLOC_HASHTABLE(ht);
 		zend_hash_init(ht, 0, NULL, ZVAL_PTR_DTOR, 0);
 		zend_hash_copy(ht, return_value->value.ht, (copy_ctor_func_t) zval_add_ref, (void *) &tmp, sizeof(zval *));
 		return_value->value.ht = ht;		

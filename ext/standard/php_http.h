4dd68fc232c240884efff56687cffba6b573c164
iliaa <iliaa>
2005-08-31 01:19:38 +0000

    Added optional parameter to http_build_query() to allow specification of string separator.

--- ext/standard/php_http.h
+++ ext/standard/php_http.h
@@ -16,7 +16,7 @@
    +----------------------------------------------------------------------+
 */
 
-/* $Id: php_http.h,v 1.5 2005/08/03 14:08:10 sniper Exp $ */
+/* $Id: php_http.h,v 1.6 2005/08/31 01:19:38 iliaa Exp $ */
 
 #ifndef PHP_HTTP_H
 #define PHP_HTTP_H
@@ -28,7 +28,7 @@ PHPAPI int php_url_encode_hash_ex(HashTable *ht, smart_str *formstr,
 				const char *num_prefix, int num_prefix_len,
 				const char *key_prefix, int key_prefix_len,
 				const char *key_suffix, int key_suffix_len, 
-				zval *type TSRMLS_DC);
+				zval *type, char *arg_sep TSRMLS_DC);
 #define php_url_encode_hash(ht, formstr)	php_url_encode_hash_ex((ht), (formstr), NULL, 0, NULL, 0, NULL, 0, NULL TSRMLS_CC)
 
 PHP_FUNCTION(http_build_query);

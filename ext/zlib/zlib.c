62b09e3f310b9dbf1a1c105339b174e309903f6b
iliaa <iliaa>
2005-09-02 17:25:22 +0000

    MFh: Resolved bug #34240.

--- ext/zlib/zlib.c
+++ ext/zlib/zlib.c
@@ -19,7 +19,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: zlib.c,v 1.181.2.1 2005/08/21 16:04:38 iliaa Exp $ */
+/* $Id: zlib.c,v 1.181.2.2 2005/09/02 17:25:22 iliaa Exp $ */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -640,7 +640,7 @@ static int php_do_deflate(uint str_length, Bytef **p_buffer, uint *p_buffer_len,
 
 	if (do_end) {
 		err = deflate(&ZLIBG(stream), Z_FINISH);
-		buffer[outlen - ZLIBG(stream).avail_out] = '\0';
+		buffer[outlen + start_offset - ZLIBG(stream).avail_out] = '\0';
 	}
 
 	*p_buffer = buffer;

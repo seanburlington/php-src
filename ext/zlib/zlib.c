648b6ed466ef7b17cb8459f6991ed0a41decda3e
iliaa <iliaa>
2005-09-02 17:25:33 +0000

    MFH: Resolved bug #34240.

--- ext/zlib/zlib.c
+++ ext/zlib/zlib.c
@@ -18,7 +18,7 @@
    |          Jade Nicoletti <nicoletti@nns.ch>                           |
    +----------------------------------------------------------------------+
  */
-/* $Id: zlib.c,v 1.153.2.16.4.1 2005/08/21 16:05:35 iliaa Exp $ */
+/* $Id: zlib.c,v 1.153.2.16.4.2 2005/09/02 17:25:33 iliaa Exp $ */
 #define IS_EXT_MODULE
 
 #ifdef HAVE_CONFIG_H
@@ -717,7 +717,7 @@ static int php_do_deflate(uint str_length, Bytef **p_buffer, uint *p_buffer_len,
 
 	if (do_end) {
 		err = deflate(&ZLIBG(stream), Z_FINISH);
-		buffer[outlen - ZLIBG(stream).avail_out] = '\0';
+		buffer[outlen + start_offset - ZLIBG(stream).avail_out] = '\0';
 	}
 
 

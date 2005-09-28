ac30f0fe6f5fcdea6823b81ae35fbb53b8621756
iliaa <iliaa>
2005-09-28 22:31:26 +0000

    Fixed possible GLOBALS variable override when register_globals are ON. Fixed possible register_globals toggle via parse_str().

--- main/php_variables.c
+++ main/php_variables.c
@@ -17,7 +17,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: php_variables.c,v 1.108 2005/09/01 19:14:54 iliaa Exp $ */
+/* $Id: php_variables.c,v 1.109 2005/09/28 22:31:26 iliaa Exp $ */
 
 #include <stdio.h>
 #include "php.h"
@@ -118,6 +118,13 @@ PHPAPI void php_register_variable_ex(char *var, zval *val, zval *track_vars_arra
 		zval_dtor(val);
 		return;
 	}
+
+	/* GLOBALS hijack attempt, reject parameter */
+	if (symtable1 == EG(active_symbol_table) && !strcmp("GLOBALS", var)) {
+		zval_dtor(val);
+		return;
+	}
+
 	/* ensure that we don't have spaces or dots in the variable name (not binary safe) */
 	for (p=var; *p; p++) {
 		switch (*p) {

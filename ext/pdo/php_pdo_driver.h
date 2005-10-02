08eb2ecb35521c323fb8f111c1218e917b809b43
helly <helly>
2005-10-02 20:07:11 +0000

    - Add PDO::setAttribute(PDO::ATTR_STATEMENT_CLASS)

--- ext/pdo/php_pdo_driver.h
+++ ext/pdo/php_pdo_driver.h
@@ -16,7 +16,7 @@
   +----------------------------------------------------------------------+
 */
 
-/* $Id: php_pdo_driver.h,v 1.66.2.1 2005/09/24 14:18:01 edink Exp $ */
+/* $Id: php_pdo_driver.h,v 1.66.2.2 2005/10/02 20:07:11 helly Exp $ */
 
 #ifndef PHP_PDO_DRIVER_H
 #define PHP_PDO_DRIVER_H
@@ -475,6 +475,10 @@ struct _pdo_dbh_t {
 	HashTable *cls_methods[PDO_DBH_DRIVER_METHOD_KIND__MAX];
 
 	pdo_driver_t *driver;
+	
+	zend_class_entry *def_stmt_ce;
+	
+	zval *def_stmt_ctor_args;
 };
 
 /* describes a column */

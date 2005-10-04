17f55c0cefdc82a56460347d2e66686be87f4986
tony2001 <tony2001>
2005-10-04 18:15:23 +0000

    fix #34731, patch by novicky at aarongroup dot cz

--- ext/oci8/oci8.c
+++ ext/oci8/oci8.c
@@ -22,7 +22,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: oci8.c,v 1.257.2.8 2005/06/13 09:31:03 tony2001 Exp $ */
+/* $Id: oci8.c,v 1.257.2.9 2005/10/04 18:15:23 tony2001 Exp $ */
 
 /* TODO list:
  *
@@ -786,7 +786,7 @@ PHP_MINFO_FUNCTION(oci)
 
 	php_info_print_table_start();
 	php_info_print_table_row(2, "OCI8 Support", "enabled");
-	php_info_print_table_row(2, "Revision", "$Revision: 1.257.2.8 $");
+	php_info_print_table_row(2, "Revision", "$Revision: 1.257.2.9 $");
 
 	sprintf(buf, "%ld", num_persistent);
 	php_info_print_table_row(2, "Active Persistent Links", buf);
@@ -2879,7 +2879,6 @@ static oci_session *_oci_open_session(oci_server* server,char *username,char *pa
 		)
 	);
 
-	session->num = zend_list_insert(session, le_session);
  	session->is_open = 1;
 
 	mutex_lock(mx_lock);
@@ -2892,6 +2891,7 @@ static oci_session *_oci_open_session(oci_server* server,char *username,char *pa
 		}
 	mutex_unlock(mx_lock);
 
+	session->num = zend_list_insert(session, le_session);
 	oci_debug("_oci_open_session new sess=%d user=%s",session->num,username);
 
 	return session;

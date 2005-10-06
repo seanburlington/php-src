c9a6aee4429258d8f821042186b2a62f2492b1af
fmk <fmk>
2005-10-06 21:40:30 +0000

    Fix Win32 build by adding missing function declaration.

--- ext/standard/php_string.h
+++ ext/standard/php_string.h
@@ -17,7 +17,7 @@
    +----------------------------------------------------------------------+
 */
 
-/* $Id: php_string.h,v 1.90 2005/09/28 09:22:08 rolland Exp $ */
+/* $Id: php_string.h,v 1.91 2005/10/06 21:40:30 fmk Exp $ */
 
 /* Synced with php 3.0 revision 1.43 1999-06-16 [ssb] */
 
@@ -136,6 +136,7 @@ PHPAPI char *php_str_to_str_ex(char *haystack, int length, char *needle,
 PHPAPI char *php_str_to_str(char *haystack, int length, char *needle,
 		int needle_len, char *str, int str_len, int *_new_length);
 PHPAPI char *php_trim(char *c, int len, char *what, int what_len, zend_uchar str_type, zval *return_value, int mode TSRMLS_DC);
+PHPAPI int32_t php_u_strip_tags(UChar *rbuf, int32_t len, int *stateptr, UChar *allow, int32_t allow_len TSRMLS_DC);
 PHPAPI size_t php_strip_tags(char *rbuf, int len, int *state, char *allow, int allow_len);
 PHPAPI int php_char_to_str_ex(char *str, uint len, char from, char *to, int to_len, pval *result, int case_sensitivity, int *replace_count);
 PHPAPI int php_char_to_str(char *str, uint len, char from, char *to, int to_len, pval *result);

7ab8f9db008ba414bc0e444ab3edce47e49b7eb6
tony2001 <tony2001>
2005-10-12 13:33:33 +0000

    MF51: fix TS build

--- ext/mysqli/mysqli.c
+++ ext/mysqli/mysqli.c
@@ -15,7 +15,7 @@
   | Author: Georg Richter <georg@php.net>                                |
   +----------------------------------------------------------------------+
 
-  $Id: mysqli.c,v 1.75 2005/10/10 20:14:31 tony2001 Exp $ 
+  $Id: mysqli.c,v 1.76 2005/10/12 13:33:33 tony2001 Exp $ 
 */
 
 #ifdef HAVE_CONFIG_H
@@ -663,7 +663,7 @@ ZEND_FUNCTION(mysqli_stmt_construct)
 	switch (ZEND_NUM_ARGS())
 	{
 		case 1:  /* mysql_stmt_init */
-	        if (zend_parse_parameters(1, "O", &mysql_link, mysqli_link_class_entry)==FAILURE) {
+	        if (zend_parse_parameters(1 TSRMLS_CC, "O", &mysql_link, mysqli_link_class_entry)==FAILURE) {
 				return;
 			}
 			MYSQLI_FETCH_RESOURCE(mysql, MY_MYSQL *, &mysql_link, "mysqli_link");
@@ -673,7 +673,7 @@ ZEND_FUNCTION(mysqli_stmt_construct)
 			stmt->stmt = mysql_stmt_init(mysql->mysql);
 		break;
 		case 2:
-	        if (zend_parse_parameters(2, "Os", &mysql_link, mysqli_link_class_entry, &statement, stmt_len)==FAILURE) {
+	        if (zend_parse_parameters(2 TSRMLS_CC, "Os", &mysql_link, mysqli_link_class_entry, &statement, stmt_len)==FAILURE) {
 				return;
 			}
 			MYSQLI_FETCH_RESOURCE(mysql, MY_MYSQL *, &mysql_link, "mysqli_link");
@@ -717,12 +717,12 @@ ZEND_FUNCTION(mysqli_result_construct)
 
 	switch (ZEND_NUM_ARGS()) {
 		case 1:
-	        if (zend_parse_parameters(1, "O", &mysql_link, mysqli_link_class_entry)==FAILURE) {
+	        if (zend_parse_parameters(1 TSRMLS_CC, "O", &mysql_link, mysqli_link_class_entry)==FAILURE) {
 				return;
 			}
 		break;
 		case 2:
-	        if (zend_parse_parameters(2, "Ol", &mysql_link, mysqli_link_class_entry, &resmode)==FAILURE) {
+	        if (zend_parse_parameters(2 TSRMLS_CC, "Ol", &mysql_link, mysqli_link_class_entry, &resmode)==FAILURE) {
 				return;
 			}
 		break;

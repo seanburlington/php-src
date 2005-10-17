cd8c4888881b7227251c267f5fb23895bc5227f3
iliaa <iliaa>
2005-10-17 02:43:20 +0000

    MFH: Further URL validations in safe_mode/open_basedir configs.

--- ext/curl/interface.c
+++ ext/curl/interface.c
@@ -16,7 +16,7 @@
    +----------------------------------------------------------------------+
 */
 
-/* $Id: interface.c,v 1.62.2.2 2005/10/06 20:47:41 iliaa Exp $ */
+/* $Id: interface.c,v 1.62.2.3 2005/10/17 02:43:20 iliaa Exp $ */
 
 #define ZEND_INCLUDE_FULL_WINDOWS_HEADERS
 
@@ -75,7 +75,7 @@ static void _php_curl_close(zend_rsrc_list_entry *rsrc TSRMLS_DC);
 			RETURN_FALSE; 																		\
 		} 																						\
 																								\
-		if (php_check_open_basedir(tmp_url->path TSRMLS_CC) || 									\
+		if (tmp_url->query || php_check_open_basedir(tmp_url->path TSRMLS_CC) || 									\
 			(PG(safe_mode) && !php_checkuid(tmp_url->path, "rb+", CHECKUID_CHECK_MODE_PARAM))	\
 		) { 																					\
 			php_url_free(tmp_url); 																\

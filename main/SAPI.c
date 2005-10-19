e7b33657d6f21c59912673bbf6e4321989cc31c0
iliaa <iliaa>
2005-10-19 20:30:38 +0000

    Fixed bug #29983 (PHP does not explicitly set mime type & charset).

--- main/SAPI.c
+++ main/SAPI.c
@@ -18,7 +18,7 @@
    +----------------------------------------------------------------------+
 */
 
-/* $Id: SAPI.c,v 1.203 2005/08/11 23:36:02 andrei Exp $ */
+/* $Id: SAPI.c,v 1.204 2005/10/19 20:30:38 iliaa Exp $ */
 
 #include <ctype.h>
 #include <sys/stat.h>
@@ -794,6 +794,12 @@ SAPI_API int sapi_send_headers(TSRMLS_D)
 	/* Success-oriented.  We set headers_sent to 1 here to avoid an infinite loop
 	 * in case of an error situation.
 	 */
+	if (SG(sapi_headers).send_default_content_type && sapi_module.send_headers) {
+		sapi_header_struct default_header;
+		sapi_get_default_content_type_header(&default_header TSRMLS_CC);
+		sapi_add_header_ex(default_header.header, default_header.header_len, 0, 0 TSRMLS_CC);
+	}
+
 	SG(headers_sent) = 1;
 
 	if (sapi_module.send_headers) {

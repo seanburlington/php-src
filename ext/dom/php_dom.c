66db882d1606cfbd1ba676d9c1d88f1f7fddbc7d
rasmus <rasmus>
2005-11-05 22:25:12 +0000

    This SimpleXML requirement is no longer needed and by removing it we remove a circular dep where it was impossible to build simplexml shared and not load it.

--- ext/dom/php_dom.c
+++ ext/dom/php_dom.c
@@ -18,7 +18,7 @@
    +----------------------------------------------------------------------+
 */
 
-/* $Id: php_dom.c,v 1.73.2.2 2005/09/19 18:47:56 dmitry Exp $ */
+/* $Id: php_dom.c,v 1.73.2.3 2005/11/05 22:25:12 rasmus Exp $ */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -364,7 +364,6 @@ void *php_dom_export_node(zval *object TSRMLS_DC)
    Get a simplexml_element object from dom to allow for processing */
 PHP_FUNCTION(dom_import_simplexml)
 {
-#ifdef HAVE_SIMPLEXML
 	zval *rv = NULL;
 	zval *node;
 	xmlNodePtr nodep = NULL;
@@ -384,10 +383,6 @@ PHP_FUNCTION(dom_import_simplexml)
 		php_error_docref(NULL TSRMLS_CC, E_WARNING, "Invalid Nodetype to import");
 		RETURN_NULL();
 	}
-#else
-	php_error_docref(NULL TSRMLS_CC, E_WARNING, "SimpleXML support is not enabled");
-	return;
-#endif
 }
 /* }}} */
 
@@ -442,9 +437,6 @@ static zend_object_handlers* dom_get_obj_handlers(TSRMLS_D) {
 static zend_module_dep dom_deps[] = {
 	ZEND_MOD_REQUIRED("libxml")
 	ZEND_MOD_CONFLICTS("domxml")
-#ifdef HAVE_SIMPLEXML
-	ZEND_MOD_REQUIRED("simplexml")
-#endif
 	{NULL, NULL, NULL}
 };
 

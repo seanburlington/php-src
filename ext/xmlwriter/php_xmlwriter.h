cf1556103352c9f705914c90c78d4a1016ccd329
pajoye <pajoye>
2005-11-15 15:36:29 +0000

    - switch to bsd license - prepare 2.0.1

--- ext/xmlwriter/php_xmlwriter.h
+++ ext/xmlwriter/php_xmlwriter.h
@@ -2,21 +2,22 @@
   +----------------------------------------------------------------------+
   | PHP Version 5                                                        |
   +----------------------------------------------------------------------+
-  | Copyright (c) 1997-2004 The PHP Group                                |
+  | Copyright (c) 1997-2005 Rob Richards, Pierre-Alain Joye              |
   +----------------------------------------------------------------------+
-  | This source file is subject to version 3.0 of the PHP license,       |
-  | that is bundled with this package in the file LICENSE, and is        |
-  | available through the world-wide-web at the following url:           |
-  | http://www.php.net/license/3_0.txt.                                  |
-  | If you did not receive a copy of the PHP license and are unable to   |
-  | obtain it through the world-wide-web, please send a note to          |
-  | license@php.net so we can mail you a copy immediately.               |
+  | This source file is subject to the New BSD license, That is bundled  |
+  | with this package in the file LICENSE, and is available through      |
+  | the world-wide-web at                                                |
+  | http://www.opensource.org/licenses/bsd-license.php                   |
+  | If you did not receive a copy of the new BSDlicense and are unable   |
+  | to obtain it through the world-wide-web, please send a note to       |
+  | pecl-dev@lists.php.net so we can mail you a copy immediately.        |
   +----------------------------------------------------------------------+
   | Author: Rob Richards <rrichards@php.net>                             |
+  |         Pierre-A. Joye <pajoye@php.net>                              |
   +----------------------------------------------------------------------+
 */
 
-/* $Id: php_xmlwriter.h,v 1.8 2005/06/23 11:28:25 pajoye Exp $ */
+/* $Id: php_xmlwriter.h,v 1.9 2005/11/15 15:36:29 pajoye Exp $ */
 
 #ifndef PHP_XMLWRITER_H
 #define PHP_XMLWRITER_H

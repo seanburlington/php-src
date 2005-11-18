d2269b1db272a8c42cac723adc0db84c5eb1290e
fmk <fmk>
2005-11-18 19:14:31 +0000

    MFH: Fix #33153 Crash in mssql_next_result().

--- ext/mssql/php_mssql.c
+++ ext/mssql/php_mssql.c
@@ -16,7 +16,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: php_mssql.c,v 1.152.2.4 2005/11/14 23:14:25 fmk Exp $ */
+/* $Id: php_mssql.c,v 1.152.2.5 2005/11/18 19:14:32 fmk Exp $ */
 
 #ifdef COMPILE_DL_MSSQL
 #define HAVE_MSSQL 1
@@ -1865,6 +1865,11 @@ PHP_FUNCTION(mssql_next_result)
 
 	mssql_ptr = result->mssql_ptr;
 	retvalue = dbresults(mssql_ptr->link);
+
+	while (dbnumcols(mssql_ptr->link) <= 0 && retvalue == SUCCEED) {
+		retvalue = dbresults(mssql_ptr->link);
+	}
+
 	if (retvalue == FAIL) {
 		RETURN_FALSE;
 	}

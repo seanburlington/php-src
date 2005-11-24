555eb2d1cd35c3405cc1a3f5c7b82d36a302b307
mike <mike>
2005-11-24 14:28:33 +0000

    - fix tiger algo

--- ext/hash/php_hash_tiger.h
+++ ext/hash/php_hash_tiger.h
@@ -16,7 +16,7 @@
    +----------------------------------------------------------------------+
 */
 
-/* $Id: php_hash_tiger.h,v 1.1 2005/11/24 08:59:54 mike Exp $ */
+/* $Id: php_hash_tiger.h,v 1.2 2005/11/24 14:28:33 mike Exp $ */
 
 #ifndef PHP_HASH_TIGER_H
 #define PHP_HASH_TIGER_H
@@ -47,7 +47,7 @@ typedef struct {
 
 PHP_HASH_API void PHP_3TIGERInit(PHP_TIGER_CTX *context);
 PHP_HASH_API void PHP_4TIGERInit(PHP_TIGER_CTX *context);
-PHP_HASH_API void PHP_TIGERUpdate(PHP_TIGER_CTX *context, unsigned char *input, size_t len);
+PHP_HASH_API void PHP_TIGERUpdate(PHP_TIGER_CTX *context, const unsigned char *input, size_t len);
 PHP_HASH_API void PHP_TIGER128Final(unsigned char digest[16], PHP_TIGER_CTX *context);
 PHP_HASH_API void PHP_TIGER160Final(unsigned char digest[20], PHP_TIGER_CTX *context);
 PHP_HASH_API void PHP_TIGER192Final(unsigned char digest[24], PHP_TIGER_CTX *context);

0c18e75bde2f4135d4c9a89c5697be8813f59a5d
iliaa <iliaa>
2005-11-24 23:24:53 +0000

    Fixed copy & paste error

--- ext/fileinfo/fileinfo.c
+++ ext/fileinfo/fileinfo.c
@@ -16,7 +16,7 @@
   +----------------------------------------------------------------------+
 */
 
-/* $Id: fileinfo.c,v 1.11 2005/11/24 23:12:38 iliaa Exp $ */
+/* $Id: fileinfo.c,v 1.12 2005/11/24 23:24:53 iliaa Exp $ */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -103,7 +103,7 @@ PHP_FILEINFO_API zend_object_value finfo_objects_new(zend_class_entry *class_typ
 	intern = ecalloc(1, sizeof(struct finfo_object));
 	intern->zo.ce = class_type;
 	intern->zo.properties = NULL;
-	intern->std.guards = NULL;
+	intern->zo.guards = NULL;
 	intern->ptr = NULL;
 
 	retval.handle = zend_objects_store_put(intern, finfo_objects_dtor, NULL, NULL TSRMLS_CC);

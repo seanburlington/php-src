79dd409000cc063f9662d95c0516e319eb285b5a
tony2001 <tony2001>
2005-12-01 13:46:05 +0000

    fix potential problem in php_oci_bind_post_exec() when OCIDateToText() fails

--- ext/oci8/oci8_statement.c
+++ ext/oci8/oci8_statement.c
@@ -25,7 +25,7 @@
    +----------------------------------------------------------------------+
 */
 
-/* $Id: oci8_statement.c,v 1.7 2005/11/22 16:30:36 tony2001 Exp $ */
+/* $Id: oci8_statement.c,v 1.8 2005/12/01 13:46:05 tony2001 Exp $ */
 
 
 #ifdef HAVE_CONFIG_H
@@ -697,7 +697,9 @@ int php_oci_bind_post_exec(void *data TSRMLS_DC)
 							php_oci_error(connection->err, connection->errcode TSRMLS_CC);
 							ZVAL_NULL(*entry);
 						}
-						ZVAL_STRINGL(*entry, buff, buff_len, 1);
+						else {
+							ZVAL_STRINGL(*entry, buff, buff_len, 1);
+						}
 						zend_hash_move_forward(hash);
 					}
 					else {
@@ -706,7 +708,9 @@ int php_oci_bind_post_exec(void *data TSRMLS_DC)
 							php_oci_error(connection->err, connection->errcode TSRMLS_CC);
 							add_next_index_null(bind->zval);
 						}
-						add_next_index_stringl(bind->zval, buff, buff_len, 1);
+						else {
+							add_next_index_stringl(bind->zval, buff, buff_len, 1);
+						}
 					}
 				}
 				break;

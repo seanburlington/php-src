a9348a145f495c0ce1dc62c9f4c9a058e15e4f06
cellog <cellog>
2005-12-04 22:50:53 +0000

    update header, use PHP 3.01 license

--- ext/phar/phar.c
+++ ext/phar/phar.c
@@ -1,22 +1,22 @@
 /*
   +----------------------------------------------------------------------+
-  | PHP Version 5                                                        |
+  | phar php single-file executable PHP extension                        |
   +----------------------------------------------------------------------+
-  | Copyright (c) 1997-2005 The PHP Group                                |
+  | Copyright (c) 2005 The PHP Group                                     |
   +----------------------------------------------------------------------+
-  | This source file is subject to version 3.0 of the PHP license,       |
+  | This source file is subject to version 3.01 of the PHP license,      |
   | that is bundled with this package in the file LICENSE, and is        |
   | available through the world-wide-web at the following url:           |
-  | http://www.php.net/license/3_0.txt.                                  |
+  | http://www.php.net/license/3_01.txt.                                 |
   | If you did not receive a copy of the PHP license and are unable to   |
   | obtain it through the world-wide-web, please send a note to          |
   | license@php.net so we can mail you a copy immediately.               |
   +----------------------------------------------------------------------+
-  | Author:                                                              |
+  | Author: Gregory Beaver <cellog@php.net>                              |
   +----------------------------------------------------------------------+
 */
 
-/* $Id: phar.c,v 1.1 2005/12/04 20:35:37 cellog Exp $ */
+/* $Id: phar.c,v 1.2 2005/12/04 22:50:53 cellog Exp $ */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"

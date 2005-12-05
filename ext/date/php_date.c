bee459f0132f9f8c5a39ddbdd7bfab991c7f45c5
iliaa <iliaa>
2005-12-05 17:27:01 +0000

    Fixed bug #35558 (mktime() interpreting 3 digit years incorrectly).

--- ext/date/php_date.c
+++ ext/date/php_date.c
@@ -16,7 +16,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: php_date.c,v 1.43.2.29 2005/12/01 16:24:43 iliaa Exp $ */
+/* $Id: php_date.c,v 1.43.2.30 2005/12/05 17:27:02 iliaa Exp $ */
 
 #include "php.h"
 #include "php_streams.h"
@@ -860,7 +860,7 @@ PHPAPI void php_mktime(INTERNAL_FUNCTION_PARAMETERS, int gmt)
 		case 6:
 			if (yea >= 0 && yea < 70) {
 				yea += 2000;
-			} else if (yea >= 70 && yea <= 100) {
+			} else if (yea >= 70 && yea <= 110) {
 				yea += 1900;
 			}
 			now->y = yea;

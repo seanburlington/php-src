a0e770af780ec3a513e923acb5dd00894c5eeda1
sniper <sniper>
2005-12-11 22:38:24 +0000

    - Added missing fold tags

--- ext/ldap/ldap.c
+++ ext/ldap/ldap.c
@@ -22,7 +22,7 @@
    +----------------------------------------------------------------------+
  */
  
-/* $Id: ldap.c,v 1.163 2005/12/06 02:24:38 sniper Exp $ */
+/* $Id: ldap.c,v 1.164 2005/12/11 22:38:24 sniper Exp $ */
 #define IS_EXT_MODULE
 
 #ifdef HAVE_CONFIG_H
@@ -314,7 +314,7 @@ PHP_MINFO_FUNCTION(ldap)
 
 	php_info_print_table_start();
 	php_info_print_table_row(2, "LDAP Support", "enabled");
-	php_info_print_table_row(2, "RCS Version", "$Id: ldap.c,v 1.163 2005/12/06 02:24:38 sniper Exp $");
+	php_info_print_table_row(2, "RCS Version", "$Id: ldap.c,v 1.164 2005/12/11 22:38:24 sniper Exp $");
 
 	if (LDAPG(max_links) == -1) {
 		snprintf(tmp, 31, "%ld/unlimited", LDAPG(num_links));
@@ -525,8 +525,9 @@ static php_ldap_bictx *_php_sasl_setdefs(LDAP *ld, char *sasl_mech, char *sasl_r
 
 	return ctx;
 }
+/* }}} */
 
-/* {{{ _php_sasl_setdefs
+/* {{{ _php_sasl_freedefs
  */
 static void _php_sasl_freedefs(php_ldap_bictx *ctx)
 {
@@ -537,6 +538,7 @@ static void _php_sasl_freedefs(php_ldap_bictx *ctx)
 	if (ctx->authzid) ber_memfree(ctx->authzid);
 	ber_memfree(ctx);
 }
+/* }}} */
 
 /* {{{ _php_sasl_interact
    Internal interact function for SASL */
@@ -569,6 +571,7 @@ static int _php_sasl_interact(LDAP *ld, unsigned flags, void *defaults, void *in
 	}
 	return LDAP_SUCCESS;
 }
+/* }}} */
 
 /* {{{ proto bool ldap_sasl_bind(resource link [, string binddn, string password, string sasl_mech, string sasl_realm, string sasl_authz_id, string props])
    Bind to LDAP directory using SASL */

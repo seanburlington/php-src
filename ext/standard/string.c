a21a500282cb1707f21ef35cbf0358b6e4b3c15c
tony2001 <tony2001>
2005-12-14 00:55:18 +0000

    MFH: fix spelling and #35665

--- ext/standard/string.c
+++ ext/standard/string.c
@@ -18,7 +18,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: string.c,v 1.420.2.13 2005/09/28 22:35:43 iliaa Exp $ */
+/* $Id: string.c,v 1.420.2.14 2005/12/14 00:55:18 tony2001 Exp $ */
 
 /* Synced with php 3.0 revision 1.193 1999-06-16 [ssb] */
 
@@ -4171,7 +4171,7 @@ PHP_FUNCTION(str_repeat)
 	/* Initialize the result string */	
 	result_len = Z_STRLEN_PP(input_str) * Z_LVAL_PP(mult);
 	if (result_len < 1 || result_len > 2147483647) {
-		php_error_docref(NULL TSRMLS_CC, E_WARNING, "You may not create strings longer then 2147483647 bytes");
+		php_error_docref(NULL TSRMLS_CC, E_WARNING, "You may not create strings longer than 2147483647 bytes");
 		RETURN_FALSE;
 	}
 	result = (char *)emalloc(result_len + 1);

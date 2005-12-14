fffa5606e61e622cdb9f2a7d8d638c3fbeffdd5d
helly <helly>
2005-12-14 09:55:04 +0000

    - Fix issue spotted by Wez using ms vc 2005

--- ext/spl/spl_directory.c
+++ ext/spl/spl_directory.c
@@ -16,7 +16,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: spl_directory.c,v 1.60 2005/12/11 16:43:00 helly Exp $ */
+/* $Id: spl_directory.c,v 1.61 2005/12/14 09:55:04 helly Exp $ */
 
 #ifdef HAVE_CONFIG_H
 # include "config.h"
@@ -303,7 +303,7 @@ static spl_filesystem_object * spl_filesystem_object_create_info(spl_filesystem_
 
 	php_set_error_handling(EH_THROW, U_CLASS_ENTRY(spl_ce_RuntimeException) TSRMLS_CC);
 
-	return_value->value.obj = spl_filesystem_object_new_ex(ce ? ce : intern->info_class, &intern TSRMLS_CC);
+	return_value->value.obj = spl_filesystem_object_new_ex(ce ? ce : source->info_class, &intern TSRMLS_CC);
 	Z_TYPE_P(return_value) = IS_OBJECT;
 
 	spl_filesystem_info_set_filename(intern, file_path, file_path_len, use_copy TSRMLS_CC);

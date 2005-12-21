c72f2c282a0b395f4b4ce67a0f3395c65725fd47
fmk <fmk>
2005-12-21 22:43:04 +0000

    Fix #35730 Use correct character encoding with FreeTDS

--- ext/mssql/php_mssql.h
+++ ext/mssql/php_mssql.h
@@ -17,7 +17,7 @@
  */
 
 
-/* $Id: php_mssql.h,v 1.42.2.1 2005/12/06 01:25:16 sniper Exp $ */
+/* $Id: php_mssql.h,v 1.42.2.2 2005/12/21 22:43:06 fmk Exp $ */
 
 #ifndef PHP_MSSQL_H
 #define PHP_MSSQL_H
@@ -158,6 +158,9 @@ ZEND_BEGIN_MODULE_GLOBALS(mssql)
 	zend_bool allow_persistent;
 	char *appname;
 	char *server_message;
+#ifdef HAVE_FREETDS
+	char *charset;
+#endif
 	long min_error_severity, min_message_severity;
 	long cfg_min_error_severity, cfg_min_message_severity;
 	long connect_timeout, timeout;

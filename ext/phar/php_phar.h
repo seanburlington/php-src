781c94488c4c520542b323e61ae2cd249e217acc
helly <helly>
2006-01-04 15:27:17 +0000

    - Clearify names - Fix several memleaks - Cache files

--- ext/phar/php_phar.h
+++ ext/phar/php_phar.h
@@ -12,11 +12,12 @@
   | obtain it through the world-wide-web, please send a note to          |
   | license@php.net so we can mail you a copy immediately.               |
   +----------------------------------------------------------------------+
-  | Author: Gregory Beaver <cellog@php.net>                              |
+  | Authors: Gregory Beaver <cellog@php.net>                             |
+  |          Marcus Boerger <helly@php.net>                              |
   +----------------------------------------------------------------------+
 */
 
-/* $Id: php_phar.h,v 1.5 2005/12/09 23:59:59 cellog Exp $ */
+/* $Id: php_phar.h,v 1.6 2006/01/04 15:27:17 helly Exp $ */
 
 #ifndef PHP_PHAR_H
 #define PHP_PHAR_H
@@ -54,15 +55,18 @@ typedef struct _phar_manifest_entry {
 	php_uint32	offset_within_phar;
 	php_uint32	compressed_filesize;
 	zend_bool	crc_checked;
+	char        *filedata;
 } phar_manifest_entry;
 
 typedef struct _phar_file_data {
-	char		*file;
-	HashTable	*manifest;
+	char		*filename;
+	int			filename_len;
 	char		*alias;
-	int		alias_len;
+	int			alias_len;
 	size_t		internal_file_start;
 	zend_bool	is_compressed;
+	HashTable	*manifest;
+	php_stream	*fp;
 } phar_file_data;
 
 ZEND_BEGIN_MODULE_GLOBALS(phar)

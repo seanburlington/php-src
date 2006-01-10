ee0f5dde519b434cfceae422e130305cbcbe957f
tony2001 <tony2001>
2006-01-10 08:32:49 +0000

    silence compile warning in non-ZTS mode

--- ext/oci8/oci8.c
+++ ext/oci8/oci8.c
@@ -26,7 +26,7 @@
    +----------------------------------------------------------------------+
 */
 
-/* $Id: oci8.c,v 1.289 2006/01/10 08:29:19 tony2001 Exp $ */
+/* $Id: oci8.c,v 1.290 2006/01/10 08:32:49 tony2001 Exp $ */
 /* TODO
  *
  * file://localhost/www/docs/oci10/ociaahan.htm#423823 - implement lob_empty() with OCI_ATTR_LOBEMPTY
@@ -88,7 +88,9 @@ static void php_oci_descriptor_list_dtor (zend_rsrc_list_entry * TSRMLS_DC);
 static void php_oci_collection_list_dtor (zend_rsrc_list_entry * TSRMLS_DC);
 
 static int php_oci_persistent_helper(zend_rsrc_list_entry *le TSRMLS_DC);
+#ifdef ZTS
 static int php_oci_regular_helper(zend_rsrc_list_entry *le TSRMLS_DC);
+#endif
 static int php_oci_connection_ping(php_oci_connection * TSRMLS_DC);
 static int php_oci_connection_status(php_oci_connection * TSRMLS_DC);
 static int php_oci_connection_close(php_oci_connection * TSRMLS_DC);
@@ -637,7 +639,7 @@ PHP_MINFO_FUNCTION(oci)
 
 	php_info_print_table_start();
 	php_info_print_table_row(2, "OCI8 Support", "enabled");
-	php_info_print_table_row(2, "Revision", "$Revision: 1.289 $");
+	php_info_print_table_row(2, "Revision", "$Revision: 1.290 $");
 
 	sprintf(buf, "%ld", OCI_G(num_persistent));
 	php_info_print_table_row(2, "Active Persistent Connections", buf);
@@ -1728,6 +1730,7 @@ static int php_oci_persistent_helper(zend_rsrc_list_entry *le TSRMLS_DC)
 	return 0;
 } /* }}} */
 
+#ifdef ZTS
 /* {{{ php_oci_regular_helper() 
  Helper function to close non-persistent connections at the end of request in ZTS mode */
 static int php_oci_regular_helper(zend_rsrc_list_entry *le TSRMLS_DC)
@@ -1742,5 +1745,6 @@ static int php_oci_regular_helper(zend_rsrc_list_entry *le TSRMLS_DC)
 	}
 	return 0;
 } /* }}} */
+#endif
 
 #endif /* HAVE_OCI8 */

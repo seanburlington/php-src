c28bf90e948368975f405eb4eb6837cbd06c8761
tony2001 <tony2001>
2006-01-16 19:47:07 +0000

    fix bug #35999 (recursive mkdir() does not work with relative path like "foo/bar")

--- main/streams/plain_wrapper.c
+++ main/streams/plain_wrapper.c
@@ -16,7 +16,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: plain_wrapper.c,v 1.56 2006/01/01 13:09:57 sniper Exp $ */
+/* $Id: plain_wrapper.c,v 1.57 2006/01/16 19:47:07 tony2001 Exp $ */
 
 #include "php.h"
 #include "php_globals.h"
@@ -1095,12 +1095,17 @@ static int php_plain_files_mkdir(php_stream_wrapper *wrapper, char *dir, int mod
 			offset = p - buf + 1;
 		}
 
-		/* find a top level directory we need to create */
-		while ((p = strrchr(buf + offset, DEFAULT_SLASH))) {
-			*p = '\0';
-			if (VCWD_STAT(buf, &sb) == 0) {
-				*p = DEFAULT_SLASH;
-				break;
+		if (p && dir_len == 1) {
+			/* buf == "DEFAULT_SLASH" */	
+		}
+		else {
+			/* find a top level directory we need to create */
+			while ( (p = strrchr(buf + offset, DEFAULT_SLASH)) || (p = strrchr(buf, DEFAULT_SLASH)) ) {
+				*p = '\0';
+				if (VCWD_STAT(buf, &sb) == 0) {
+					*p = DEFAULT_SLASH;
+					break;
+				}
 			}
 		}
 

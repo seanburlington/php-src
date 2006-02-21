432ba2dc3670de370025e15336a4eccbfeb4e104
iliaa <iliaa>
2006-02-21 15:35:01 +0000

    MFH: Fixed bug #36458 (sleep() accepts negative values).

--- ext/standard/basic_functions.c
+++ ext/standard/basic_functions.c
@@ -17,7 +17,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: basic_functions.c,v 1.543.2.51.2.6 2006/01/01 13:46:57 sniper Exp $ */
+/* $Id: basic_functions.c,v 1.543.2.51.2.7 2006/02/21 15:35:01 iliaa Exp $ */
 
 #include "php.h"
 #include "php_streams.h"
@@ -1662,14 +1662,16 @@ PHP_FUNCTION(flush)
    Delay for a given number of seconds */
 PHP_FUNCTION(sleep)
 {
-	pval **num;
-
-	if (ZEND_NUM_ARGS() != 1 || zend_get_parameters_ex(1, &num) == FAILURE) {
-		WRONG_PARAM_COUNT;
+	long num;
+  
+	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "l", &num) == FAILURE) {
+		RETURN_FALSE;
 	}
-
-	convert_to_long_ex(num);
-	php_sleep(Z_LVAL_PP(num));
+	if (num < 0) {
+		php_error_docref(NULL TSRMLS_CC, E_WARNING, "Number of seconds must be greater than or equal to 0");
+		RETURN_FALSE;
+	}
+	php_sleep(num);
 }
 /* }}} */
 
@@ -1678,13 +1680,16 @@ PHP_FUNCTION(sleep)
 PHP_FUNCTION(usleep)
 {
 #if HAVE_USLEEP
-	pval **num;
-
-	if (ZEND_NUM_ARGS() != 1 || zend_get_parameters_ex(1, &num) == FAILURE) {
-		WRONG_PARAM_COUNT;
+	long num;
+  
+	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "l", &num) == FAILURE) {
+		RETURN_FALSE;
 	}
-	convert_to_long_ex(num);
-	usleep(Z_LVAL_PP(num));
+	if (num < 0) {
+		php_error_docref(NULL TSRMLS_CC, E_WARNING, "Number of microseconds must be greater than or equal to 0");
+		RETURN_FALSE;
+	}
+	usleep(num);
 #endif
 }
 /* }}} */

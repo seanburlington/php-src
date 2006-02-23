e3023a03b2364517c642817c57ceab2745c3ee97
iliaa <iliaa>
2006-02-23 18:28:07 +0000

    Fixed bug #35900 (stream_select() should warning when tv_sec is negative).

--- ext/standard/streamsfuncs.c
+++ ext/standard/streamsfuncs.c
@@ -17,7 +17,7 @@
   +----------------------------------------------------------------------+
 */
 
-/* $Id: streamsfuncs.c,v 1.58.2.2 2006/02/01 10:31:57 tony2001 Exp $ */
+/* $Id: streamsfuncs.c,v 1.58.2.3 2006/02/23 18:28:07 iliaa Exp $ */
 
 #include "php.h"
 #include "php_globals.h"
@@ -751,6 +751,14 @@ PHP_FUNCTION(stream_select)
 	/* If seconds is not set to null, build the timeval, else we wait indefinitely */
 	if (sec != NULL) {
 		convert_to_long(sec);
+
+		if (sec < 0) {
+			php_error_docref(NULL TSRMLS_CC, E_WARNING, "The seconds parameter must be greater then 0.");
+			RETURN_FALSE;
+		} else if (usec < 0) {
+			php_error_docref(NULL TSRMLS_CC, E_WARNING, "The microseconds parameter must be greater then 0.");
+			RETURN_FALSE;
+		}
 
 		/* Solaris + BSD do not like microsecond values which are >= 1 sec */
 		if (usec > 999999) {

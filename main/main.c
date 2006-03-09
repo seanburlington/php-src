269aa06c83c41620e3f1cb472400b0f51314c461
pollita <pollita>
2006-03-09 20:32:22 +0000

    MFH: Move temporary local storage of primary file's realpath up one block to ensure it stays on the stack.

--- main/main.c
+++ main/main.c
@@ -18,7 +18,7 @@
    +----------------------------------------------------------------------+
 */
 
-/* $Id: main.c,v 1.640.2.16 2006/02/26 10:49:51 helly Exp $ */
+/* $Id: main.c,v 1.640.2.17 2006/03/09 20:32:22 pollita Exp $ */
 
 /* {{{ includes
  */
@@ -1662,6 +1662,8 @@ PHPAPI int php_execute_script(zend_file_handle *primary_file TSRMLS_DC)
 #endif
 
 	zend_try {
+		char realfile[MAXPATHLEN];
+
 #ifdef PHP_WIN32
 		UpdateIniFromRegistry(primary_file->filename TSRMLS_CC);
 #endif
@@ -1680,7 +1682,6 @@ PHPAPI int php_execute_script(zend_file_handle *primary_file TSRMLS_DC)
 		}
 
 		if (primary_file->filename) {			
-			char realfile[MAXPATHLEN];
 			int realfile_len;
 			int dummy = 1;
 			if (VCWD_REALPATH(primary_file->filename, realfile)) {

e0bc7b8381e77def792dcb723159a8f1df460e64
tony2001 <tony2001>
2006-03-17 10:17:36 +0000

    MFB: fix #36743 (In a class extending XMLReader array properties are not writable)

--- ext/xmlreader/php_xmlreader.c
+++ ext/xmlreader/php_xmlreader.c
@@ -16,7 +16,7 @@
   +----------------------------------------------------------------------+
 */
 
-/* $Id: php_xmlreader.c,v 1.27 2006/02/06 11:28:20 tony2001 Exp $ */
+/* $Id: php_xmlreader.c,v 1.28 2006/03/17 10:17:36 tony2001 Exp $ */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -106,6 +106,40 @@ static int xmlreader_property_reader(xmlreader_object *obj, xmlreader_prop_handl
 	}
 
 	return SUCCESS;
+}
+/* }}} */
+
+/* {{{ xmlreader_get_property_ptr_ptr */
+zval **xmlreader_get_property_ptr_ptr(zval *object, zval *member TSRMLS_DC)
+{
+	xmlreader_object *obj;
+	zval tmp_member;
+	zval **retval = NULL;
+	xmlreader_prop_handler *hnd;
+	zend_object_handlers *std_hnd;
+	int ret = FAILURE;
+
+ 	if (member->type != IS_STRING) {
+		tmp_member = *member;
+		zval_copy_ctor(&tmp_member);
+		convert_to_string(&tmp_member);
+		member = &tmp_member;
+	}
+
+	obj = (xmlreader_object *)zend_objects_get_address(object TSRMLS_CC);
+
+	if (obj->prop_handler != NULL) {
+		ret = zend_hash_find(obj->prop_handler, Z_STRVAL_P(member), Z_STRLEN_P(member)+1, (void **) &hnd);
+	}
+	if (ret == FAILURE) {
+		std_hnd = zend_get_std_object_handlers();
+		retval = std_hnd->get_property_ptr_ptr(object, member TSRMLS_CC);
+	}
+
+	if (member == &tmp_member) {
+		zval_dtor(member);
+	}
+	return retval;
 }
 /* }}} */
 
@@ -1121,7 +1155,7 @@ PHP_MINIT_FUNCTION(xmlreader)
 	memcpy(&xmlreader_object_handlers, zend_get_std_object_handlers(), sizeof(zend_object_handlers));
 	xmlreader_object_handlers.read_property = xmlreader_read_property;
 	xmlreader_object_handlers.write_property = xmlreader_write_property;
-	xmlreader_object_handlers.get_property_ptr_ptr = NULL;
+	xmlreader_object_handlers.get_property_ptr_ptr = xmlreader_get_property_ptr_ptr;
 
 	INIT_CLASS_ENTRY(ce, "XMLReader", xmlreader_functions);
 	ce.create_object = xmlreader_objects_new;

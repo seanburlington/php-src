69e72d10de97d14009e3ac2e0b65d6f85400e7ae
andrei <andrei>
2006-03-24 00:25:49 +0000

    UTODO note

--- main/SAPI.c
+++ main/SAPI.c
@@ -18,7 +18,7 @@
    +----------------------------------------------------------------------+
 */
 
-/* $Id: SAPI.c,v 1.212 2006/03/08 14:41:45 iliaa Exp $ */
+/* $Id: SAPI.c,v 1.213 2006/03/24 00:25:49 andrei Exp $ */
 
 #include <ctype.h>
 #include <sys/stat.h>
@@ -234,6 +234,10 @@ SAPI_API void sapi_update_default_charset(TSRMLS_D)
 		UErrorCode status = U_ZERO_ERROR;
 
 		canonical_name = ucnv_getName(ZEND_U_CONVERTER(UG(output_encoding_conv)), &status);
+		/*
+		 * UTODO handle NULL return which signifies that MIME standard has no
+		 * name for this converter
+		 */
 		SG(default_charset) = (char *)ucnv_getStandardName(canonical_name, "MIME", &status);
 	} else {
 		SG(default_charset) = zend_ini_string("default_charset", sizeof("default_charset"), 0);

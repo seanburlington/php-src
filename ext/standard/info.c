0293578c64f078d724a54bd8093924d24bec4caa
tony2001 <tony2001>
2006-03-31 11:11:12 +0000

    fix compile failure with old GCC (see bug #36931)

--- ext/standard/info.c
+++ ext/standard/info.c
@@ -18,7 +18,7 @@
    +----------------------------------------------------------------------+
 */
 
-/* $Id: info.c,v 1.249.2.9 2006/03/30 19:58:18 tony2001 Exp $ */
+/* $Id: info.c,v 1.249.2.10 2006/03/31 11:11:12 tony2001 Exp $ */
 
 #include "php.h"
 #include "php_ini.h"
@@ -61,10 +61,12 @@ PHPAPI extern char *php_ini_scanned_files;
 	
 static int php_info_write_wrapper(const char *str, uint str_length)
 {
+	int new_len, written;
+	char *elem_esc;
+
 	TSRMLS_FETCH();
 
-	int new_len, written;
-	char *elem_esc = php_escape_html_entities((char *)str, str_length, &new_len, 0, ENT_QUOTES, NULL TSRMLS_CC);
+	elem_esc = php_escape_html_entities((char *)str, str_length, &new_len, 0, ENT_QUOTES, NULL TSRMLS_CC);
 
 	written = php_body_write(elem_esc, new_len TSRMLS_CC);
 

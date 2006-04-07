882975f1185beb526d33bb197be2bb1600f84f97
pajoye <pajoye>
2006-04-07 13:33:55 +0000

    - fix leak when an error occured in socket_read, free tmpbuf

--- ext/sockets/sockets.c
+++ ext/sockets/sockets.c
@@ -19,7 +19,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: sockets.c,v 1.171.2.7 2006/01/01 12:50:13 sniper Exp $ */
+/* $Id: sockets.c,v 1.171.2.8 2006/04/07 13:33:55 pajoye Exp $ */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -871,6 +871,7 @@ PHP_FUNCTION(socket_read)
 			php_sock->error = errno;
 			SOCKETS_G(last_error) = errno;
 		} else {
+			efree(tmpbuf);
 			PHP_SOCKET_ERROR(php_sock, "unable to read from socket", errno);
 		}
 

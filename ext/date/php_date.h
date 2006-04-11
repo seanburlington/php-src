ee5c4eba61937772ba52b0379637c089eb567eb0
derick <derick>
2006-04-11 18:03:45 +0000

    - MFH: Fixed bug #37017 (strtotime fails before 13:00:00 with some time zones   identifiers). (Derick) - MFH: Fixed bug #36988 (mktime freezes on long numbers). (Derick) - MFH: Implemented better error and warning handling that is also used for the   date_parse() function. - MFH: Fixed problems with "T" in front of a time string was seen as a time   zone string. - MFH: Fixed a problem were 5 and 6 character timezone abbreviations where not   correctly parsed.
    Experimental support (All MFH):
    - Added the date_parse() function that returns a parsed date/time string
      including warnings and errors.
    - Added the timezone_name_from_abbr() function that exposes the guessing
      mechanism that tries to find a timezone identifier from a timezone
      abbreviation and GMT offset.

--- ext/date/php_date.h
+++ ext/date/php_date.h
@@ -16,7 +16,7 @@
    +----------------------------------------------------------------------+
 */
 
-/* $Id: php_date.h,v 1.17.2.10 2006/01/28 06:18:01 fmk Exp $ */
+/* $Id: php_date.h,v 1.17.2.11 2006/04/11 18:03:46 derick Exp $ */
 
 #ifndef PHP_DATE_H
 #define PHP_DATE_H
@@ -49,6 +49,7 @@ PHP_FUNCTION(getdate);
 #ifdef EXPERIMENTAL_DATE_SUPPORT
 /* Advanced Interface */
 PHP_FUNCTION(date_create);
+PHP_FUNCTION(date_parse);
 PHP_FUNCTION(date_format);
 PHP_FUNCTION(date_modify);
 PHP_FUNCTION(date_timezone_get);
@@ -61,6 +62,7 @@ PHP_FUNCTION(date_isodate_set);
 
 PHP_FUNCTION(timezone_open);
 PHP_FUNCTION(timezone_name_get);
+PHP_FUNCTION(timezone_name_from_abbr);
 PHP_FUNCTION(timezone_offset_get);
 PHP_FUNCTION(timezone_transistions_get);
 PHP_FUNCTION(timezone_identifiers_list);

d9df4e813ab089423f1aef77f78d7044ccfd25b6
iliaa <iliaa>
2006-04-17 23:29:46 +0000

    MFB51: Fixed Bug #36872 (session_destroy() fails after call to session_regenerate_id(true)).

--- ext/session/mod_files.c
+++ ext/session/mod_files.c
@@ -16,7 +16,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: mod_files.c,v 1.102 2006/01/01 13:09:53 sniper Exp $ */
+/* $Id: mod_files.c,v 1.103 2006/04/17 23:29:46 iliaa Exp $ */
 
 #include "php.h"
 
@@ -397,10 +397,12 @@ PS_DESTROY_FUNC(files)
 	if (!ps_files_path_create(buf, sizeof(buf), data, key))
 		return FAILURE;
 	
-	ps_files_close(data);
+	if (data->fd != -1) {
+		ps_files_close(data);
 	
-	if (VCWD_UNLINK(buf) == -1) {
-		return FAILURE;
+		if (VCWD_UNLINK(buf) == -1) {
+			return FAILURE;
+		}
 	}
 
 	return SUCCESS;

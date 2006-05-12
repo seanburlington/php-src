39bbed7862043ff58c24f3ad56db071eaca5194f
tony2001 <tony2001>
2006-05-12 09:42:56 +0000

    MFH: fix #37416 (iterator_to_array() hides exceptions thrown in rewind() method)

--- ext/spl/spl_iterators.c
+++ ext/spl/spl_iterators.c
@@ -16,7 +16,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: spl_iterators.c,v 1.73.2.30.2.3 2006/05/10 16:50:57 andrei Exp $ */
+/* $Id: spl_iterators.c,v 1.73.2.30.2.4 2006/05/12 09:42:56 tony2001 Exp $ */
 
 #ifdef HAVE_CONFIG_H
 # include "config.h"
@@ -2395,11 +2395,20 @@ PHP_FUNCTION(iterator_to_array)
 	if (iter->funcs->rewind) {
 		iter->funcs->rewind(iter TSRMLS_CC);
 	}
+	if (EG(exception)) {
+		return;
+	}
 	while (iter->funcs->valid(iter TSRMLS_CC) == SUCCESS) {
 		iter->funcs->get_current_data(iter, &data TSRMLS_CC);
+		if (EG(exception)) {
+			return;
+		}
 		(*data)->refcount++;
 		if (iter->funcs->get_current_key) {
 			key_type = iter->funcs->get_current_key(iter, &str_key, &str_key_len, &int_key TSRMLS_CC);
+			if (EG(exception)) {
+				return;
+			}
 			switch(key_type) {
 				case HASH_KEY_IS_STRING:
 					add_assoc_zval_ex(return_value, str_key, str_key_len, *data);
@@ -2413,8 +2422,14 @@ PHP_FUNCTION(iterator_to_array)
 			add_next_index_zval(return_value, *data);
 		}
 		iter->funcs->move_forward(iter TSRMLS_CC);
+		if (EG(exception)) {
+			return;
+		}
 	}
 	iter->funcs->dtor(iter TSRMLS_CC);
+	if (EG(exception)) {
+		return;
+	}
 }
 /* }}} */
 
@@ -2435,11 +2450,23 @@ PHP_FUNCTION(iterator_count)
 	if (iter->funcs->rewind) {
 		iter->funcs->rewind(iter TSRMLS_CC);
 	}
+	if (EG(exception)) {
+		return;
+	}
 	while (iter->funcs->valid(iter TSRMLS_CC) == SUCCESS) {
+		if (EG(exception)) {
+			return;
+		}
 		count++;
 		iter->funcs->move_forward(iter TSRMLS_CC);
+		if (EG(exception)) {
+			return;
+		}
 	}
 	iter->funcs->dtor(iter TSRMLS_CC);
+	if (EG(exception)) {
+		return;
+	}
 	
 	RETURN_LONG(count);
 }

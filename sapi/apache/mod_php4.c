d6cfe79c09e0e903869376609d5fa396d853a075
rasmus <rasmus>
2006-05-13 21:42:14 +0000

    ap_get_client_block can return -1, so don't use an unsigned int here

--- sapi/apache/mod_php4.c
+++ sapi/apache/mod_php4.c
@@ -17,7 +17,7 @@
    | PHP 4.0 patches by Zeev Suraski <zeev@zend.com>                      |
    +----------------------------------------------------------------------+
  */
-/* $Id: mod_php4.c,v 1.146.2.15.2.2 2006/04/02 17:59:33 iliaa Exp $ */
+/* $Id: mod_php4.c,v 1.146.2.15.2.3 2006/05/13 21:42:14 rasmus Exp $ */
 
 #include "php_apache_http.h"
 #include "http_conf_globals.h"
@@ -126,7 +126,7 @@ static void sapi_apache_flush(void *server_context)
  */
 static int sapi_apache_read_post(char *buffer, uint count_bytes TSRMLS_DC)
 {
-	uint total_read_bytes=0, read_bytes;
+	int total_read_bytes=0, read_bytes;
 	request_rec *r = (request_rec *) SG(server_context);
 	void (*handler)(int);
 

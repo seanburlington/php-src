1440b72aae21b12962952943e990d5402f13736d
helly <helly>
2006-05-18 22:07:31 +0000

    - Fix bug #37510 session_regenerate_id changes session_id() even on failure

--- ext/session/session.c
+++ ext/session/session.c
@@ -17,7 +17,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: session.c,v 1.433 2006/04/10 15:06:51 sniper Exp $ */
+/* $Id: session.c,v 1.434 2006/05/18 22:07:31 helly Exp $ */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -1364,6 +1364,11 @@ PHP_FUNCTION(session_regenerate_id)
 		WRONG_PARAM_COUNT;
 	}
 
+	if (SG(headers_sent)) {
+		php_error_docref(NULL TSRMLS_CC, E_WARNING, "Cannot regenerate session id - headers already sent");
+		RETURN_FALSE;
+	}
+
 	if (PS(session_status) == php_session_active) {
 		if (PS(id)) {
 			if (del_ses && PS(mod)->s_destroy(&PS(mod_data), PS(id) TSRMLS_CC) == FAILURE) {
@@ -1720,6 +1725,6 @@ PHP_MINFO_FUNCTION(session)
  * tab-width: 4
  * c-basic-offset: 4
  * End:
- * vim600: sw=4 ts=4 fdm=marker
+ * vim600: noet sw=4 ts=4 fdm=marker
  * vim<600: sw=4 ts=4
  */

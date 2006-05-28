c56125a84754840be7c5f7ecbae88053a879e38f
mike <mike>
2006-05-28 20:31:15 +0000

    - call ap_set_content_type() only once, else each time we call it,   configured output filters for that content type will be added

--- sapi/apache2handler/php_apache.h
+++ sapi/apache2handler/php_apache.h
@@ -16,7 +16,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: php_apache.h,v 1.9 2006/01/01 13:09:57 sniper Exp $ */
+/* $Id: php_apache.h,v 1.10 2006/05/28 20:31:15 mike Exp $ */
 
 #ifndef PHP_APACHE_H
 #define PHP_APACHE_H
@@ -44,6 +44,8 @@ typedef struct php_struct {
 #endif
 	/* Whether or not we've processed PHP in the output filters yet. */
 	int request_processed;
+	/* final content type */
+	char *content_type;
 } php_struct;
 
 void *merge_php_config(apr_pool_t *p, void *base_conf, void *new_conf);

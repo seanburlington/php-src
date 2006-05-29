d8dca7e086a2d61a1d3764b704fb16a33b01303a
tony2001 <tony2001>
2006-05-29 21:31:49 +0000

    MFH: no need to check it for not-NULL

--- ext/pcre/php_pcre.c
+++ ext/pcre/php_pcre.c
@@ -16,7 +16,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: php_pcre.c,v 1.168.2.9.2.3 2006/05/29 20:26:32 tony2001 Exp $ */
+/* $Id: php_pcre.c,v 1.168.2.9.2.4 2006/05/29 21:31:49 tony2001 Exp $ */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -1115,10 +1115,8 @@ PHPAPI char *php_pcre_replace(char *regex,   int regex_len,
 			}
 		} else {
 			pcre_handle_exec_error(count TSRMLS_CC);
-			if (result) {
-				efree(result);
-				result = NULL;
-			}
+			efree(result);
+			result = NULL;
 			break;
 		}
 			

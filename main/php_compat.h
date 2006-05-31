843dd0af865e147e6600a4b2108cb552d18df1f4
andrei <andrei>
2006-05-31 17:19:05 +0000

    MFB

--- main/php_compat.h
+++ main/php_compat.h
@@ -16,7 +16,7 @@
   +----------------------------------------------------------------------+
 */
 
-/* $Id: php_compat.h,v 1.28 2006/04/10 11:54:54 sniper Exp $ */
+/* $Id: php_compat.h,v 1.29 2006/05/31 17:19:05 andrei Exp $ */
 
 #ifndef PHP_COMPAT_H
 #define PHP_COMPAT_H
@@ -29,6 +29,7 @@
 
 #if defined(HAVE_BUNDLED_PCRE) || !defined(PHP_VERSION)
 #define pcre_compile 			php_pcre_compile
+#define pcre_compile2 			php_pcre_compile2
 #define pcre_copy_substring		php_pcre_copy_substring
 #define pcre_exec				php_pcre_exec
 #define pcre_get_substring		php_pcre_substring

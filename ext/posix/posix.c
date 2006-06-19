73136707d1d7df9b6d90d5ed577273fa24a5a076
iliaa <iliaa>
2006-06-19 23:52:39 +0000

    Added missing ifdef block.
    # Thanks Mike

--- ext/posix/posix.c
+++ ext/posix/posix.c
@@ -16,7 +16,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: posix.c,v 1.70.2.3.2.2 2006/06/19 02:19:13 iliaa Exp $ */
+/* $Id: posix.c,v 1.70.2.3.2.3 2006/06/19 23:52:39 iliaa Exp $ */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -147,7 +147,7 @@ zend_function_entry posix_functions[] = {
 static PHP_MINFO_FUNCTION(posix)
 {
 	php_info_print_table_start();
-	php_info_print_table_row(2, "Revision", "$Revision: 1.70.2.3.2.2 $");
+	php_info_print_table_row(2, "Revision", "$Revision: 1.70.2.3.2.3 $");
 	php_info_print_table_end();
 }
 /* }}} */
@@ -1059,6 +1059,7 @@ PHP_FUNCTION(posix_strerror)
 
 #endif
 
+#ifdef HAVE_INITGROUPS
 /* {{{ proto bool initgroups(string name, int base_group_id)
    Calculate the group access list for the user specified in name. */
 PHP_FUNCTION(posix_initgroups)
@@ -1074,6 +1075,7 @@ PHP_FUNCTION(posix_initgroups)
 	RETURN_BOOL(!initgroups((const char *)name, basegid));
 }
 /* }}} */
+#endif
 
 /*
  * Local variables:

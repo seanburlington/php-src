3026f145ef6b749735fe9d4c498bfdebca986c6c
tony2001 <tony2001>
2006-06-28 15:03:16 +0000

    fix #37931 (possible crash after database restart when using persistent connections)

--- ext/oci8/oci8.c
+++ ext/oci8/oci8.c
@@ -26,7 +26,7 @@
    +----------------------------------------------------------------------+
 */
 
-/* $Id: oci8.c,v 1.305 2006/06/23 08:25:58 tony2001 Exp $ */
+/* $Id: oci8.c,v 1.306 2006/06/28 15:03:16 tony2001 Exp $ */
 /* TODO
  *
  * file://localhost/www/docs/oci10/ociaahan.htm#423823 - implement lob_empty() with OCI_ATTR_LOBEMPTY
@@ -667,7 +667,7 @@ PHP_MINFO_FUNCTION(oci)
 	php_info_print_table_start();
 	php_info_print_table_row(2, "OCI8 Support", "enabled");
 	php_info_print_table_row(2, "Version", "1.2.1");
-	php_info_print_table_row(2, "Revision", "$Revision: 1.305 $");
+	php_info_print_table_row(2, "Revision", "$Revision: 1.306 $");
 
 	sprintf(buf, "%ld", OCI_G(num_persistent));
 	php_info_print_table_row(2, "Active Persistent Connections", buf);
@@ -1089,8 +1089,8 @@ php_oci_connection *php_oci_do_connect_ex(char *username, int username_len, char
 						}
 					}
 					/* server died */
+					connection->is_open = 0;
 					zend_hash_del(&EG(persistent_list), hashed_details.c, hashed_details.len+1);
-					php_oci_connection_close(connection TSRMLS_CC);
 					connection = NULL;
 					goto open;
 				}

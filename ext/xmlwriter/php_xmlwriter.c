8befaa15467a97308af5cf5a2847090daac36df7
tony2001 <tony2001>
2006-07-11 16:32:47 +0000

    fix typo add test with error messages

--- ext/xmlwriter/php_xmlwriter.c
+++ ext/xmlwriter/php_xmlwriter.c
@@ -17,7 +17,7 @@
   +----------------------------------------------------------------------+
 */
 
-/* $Id: php_xmlwriter.c,v 1.35 2006/06/05 12:20:08 helly Exp $ */
+/* $Id: php_xmlwriter.c,v 1.36 2006/07/11 16:32:47 tony2001 Exp $ */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -116,7 +116,7 @@ PHP_XMLWRITER_API zend_object_value xmlwriter_object_new(zend_class_entry *class
 
 #define XMLW_NAME_CHK(__err) \
 	if (xmlValidateName((xmlChar *) name, 0) != 0) {	\
-		php_error_docref(NULL TSRMLS_CC, E_WARNING, #__err);	\
+		php_error_docref(NULL TSRMLS_CC, E_WARNING, __err);	\
 		RETURN_FALSE;	\
 	}	\
 

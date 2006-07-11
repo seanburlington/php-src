6edb7c29a7f73f1a6c1c40329ce8b8819eb33733
tony2001 <tony2001>
2006-07-11 12:37:26 +0000

    MFB: eliminate compile warnings

--- ext/date/lib/parse_tz.c
+++ ext/date/lib/parse_tz.c
@@ -16,7 +16,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: parse_tz.c,v 1.27 2006/05/14 17:30:26 derick Exp $ */
+/* $Id: parse_tz.c,v 1.28 2006/07/11 12:37:26 tony2001 Exp $ */
 
 #include "timelib.h"
 
@@ -240,8 +240,8 @@ const timelib_tzdb_index_entry *timelib_timezone_builtin_identifiers_list(int *c
 
 int timelib_timezone_id_is_valid(char *timezone, const timelib_tzdb *tzdb)
 {
-	unsigned char *tzf;
-	return (seek_to_tz_position((unsigned char**) &tzf, timezone, tzdb));
+	const unsigned char *tzf;
+	return (seek_to_tz_position(&tzf, timezone, tzdb));
 }
 
 timelib_tzinfo *timelib_parse_tzfile(char *timezone, const timelib_tzdb *tzdb)
@@ -249,7 +249,7 @@ timelib_tzinfo *timelib_parse_tzfile(char *timezone, const timelib_tzdb *tzdb)
 	const unsigned char *tzf;
 	timelib_tzinfo *tmp;
 
-	if (seek_to_tz_position((unsigned char**) &tzf, timezone, tzdb)) {
+	if (seek_to_tz_position(&tzf, timezone, tzdb)) {
 		tmp = timelib_tzinfo_ctor(timezone);
 
 		read_header((char**) &tzf, tmp);

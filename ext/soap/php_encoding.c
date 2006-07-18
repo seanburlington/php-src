0be941b508765dfaadb9cb4f5904d31ab77895c3
dmitry <dmitry>
2006-07-18 09:24:54 +0000

    Fixed compilation error

--- ext/soap/php_encoding.c
+++ ext/soap/php_encoding.c
@@ -17,7 +17,7 @@
   |          Dmitry Stogov <dmitry@zend.com>                             |
   +----------------------------------------------------------------------+
 */
-/* $Id: php_encoding.c,v 1.135 2006/07/13 16:47:25 dmitry Exp $ */
+/* $Id: php_encoding.c,v 1.136 2006/07/18 09:24:54 dmitry Exp $ */
 
 #include <time.h>
 
@@ -353,11 +353,13 @@ xmlNodePtr master_to_xml(encodePtr encode, zval *data, int style, xmlNodePtr par
 					zend_uchar utype = zend_hash_get_current_key_ex(SOAP_GLOBAL(class_map), &type_name, &type_len, &idx, 0, &pos);
 				    
 				    if (utype == HASH_KEY_IS_STRING || utype == HASH_KEY_IS_UNICODE) {
+				    	encodePtr enc;
+
 						if (utype == HASH_KEY_IS_UNICODE) {
 							type_name.s = soap_unicode_to_string(type_name.u, type_len TSRMLS_CC);
 						}
 					    /* TODO: namespace isn't stored */
-				    	encodePtr enc = get_encoder(SOAP_GLOBAL(sdl), SOAP_GLOBAL(sdl)->target_ns, type_name.s);
+				    	enc = get_encoder(SOAP_GLOBAL(sdl), SOAP_GLOBAL(sdl)->target_ns, type_name.s);
 				    	if (enc) {
 				    		encode = enc;
 						} else if (SOAP_GLOBAL(sdl)) {

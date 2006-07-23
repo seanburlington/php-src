1989caf7f2bcd2a16623fdd2ceae6798a529322d
helly <helly>
2006-07-23 05:48:08 +0000

    -fix a bunch of warnings

--- ext/pdo_pgsql/pdo_pgsql.c
+++ ext/pdo_pgsql/pdo_pgsql.c
@@ -16,7 +16,7 @@
   +----------------------------------------------------------------------+
 */
 
-/* $Id: pdo_pgsql.c,v 1.13 2006/03/14 11:04:13 edink Exp $ */
+/* $Id: pdo_pgsql.c,v 1.14 2006/07/23 05:48:08 helly Exp $ */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -31,6 +31,11 @@
 #include "php_pdo_pgsql_int.h"
 
 #ifdef HAVE_PG_CONFIG_H
+#undef PACKAGE_BUGREPORT
+#undef PACKAGE_NAME
+#undef PACKAGE_STRING
+#undef PACKAGE_TARNAME
+#undef PACKAGE_VERSION
 #include <pg_config.h>
 #endif
 
@@ -123,7 +128,7 @@ PHP_MINFO_FUNCTION(pdo_pgsql)
 	php_info_print_table_row(2, "PostgreSQL(libpq) Version", PG_VERSION);
 #endif	
 	php_info_print_table_row(2, "Module version", pdo_pgsql_module_entry.version);
-	php_info_print_table_row(2, "Revision", " $Id: pdo_pgsql.c,v 1.13 2006/03/14 11:04:13 edink Exp $ ");
+	php_info_print_table_row(2, "Revision", " $Id: pdo_pgsql.c,v 1.14 2006/07/23 05:48:08 helly Exp $ ");
 
 	php_info_print_table_end();
 }

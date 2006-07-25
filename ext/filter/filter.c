60f61924dba76d8f755a9c1e0d54043ab0858991
tony2001 <tony2001>
2006-07-25 10:36:11 +0000

    check g_hash for NULL before resetting it

--- ext/filter/filter.c
+++ ext/filter/filter.c
@@ -18,7 +18,7 @@
   +----------------------------------------------------------------------+
 */
 
-/* $Id: filter.c,v 1.59 2006/07/24 03:59:03 iliaa Exp $ */
+/* $Id: filter.c,v 1.60 2006/07/25 10:36:11 tony2001 Exp $ */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -274,7 +274,7 @@ PHP_MINFO_FUNCTION(filter)
 {
 	php_info_print_table_start();
 	php_info_print_table_row( 2, "Input Validation and Filtering", "enabled" );
-	php_info_print_table_row( 2, "Revision", "$Revision: 1.59 $");
+	php_info_print_table_row( 2, "Revision", "$Revision: 1.60 $");
 	php_info_print_table_end();
 
 	DISPLAY_INI_ENTRIES();
@@ -701,7 +701,9 @@ PHP_FUNCTION(input_get_args)
 		RETURN_FALSE;
 	} else {
 		g_hash = HASH_OF(array_ptr);
-		zend_hash_internal_pointer_reset_ex(g_hash, &pos);
+		if (g_hash) {
+			zend_hash_internal_pointer_reset(g_hash);
+		}
 		array_init(return_value);
 	}
 

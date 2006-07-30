8e8c93c455b3cc7f35c3657f81db1aca28ae7589
tony2001 <tony2001>
2006-07-30 11:34:02 +0000

    MFH: fix compile warnings (#38257)

--- ext/xmlrpc/libxmlrpc/xml_element.c
+++ ext/xmlrpc/libxmlrpc/xml_element.c
@@ -31,7 +31,7 @@
 */
 
 
-static const char rcsid[] = "#(@) $Id: xml_element.c,v 1.6 2004/06/01 20:16:06 iliaa Exp $";
+static const char rcsid[] = "#(@) $Id: xml_element.c,v 1.9.4.1 2006/07/30 11:34:02 tony2001 Exp $";
 
 
 
@@ -44,6 +44,18 @@ static const char rcsid[] = "#(@) $Id: xml_element.c,v 1.6 2004/06/01 20:16:06 i
  *   06/2000
  * HISTORY
  *   $Log: xml_element.c,v $
+ *   Revision 1.9.4.1  2006/07/30 11:34:02  tony2001
+ *   MFH: fix compile warnings (#38257)
+ *
+ *   Revision 1.9  2005/04/22 11:06:53  jorton
+ *   Fixed bug #32797 (invalid C code in xmlrpc extension).
+ *
+ *   Revision 1.8  2005/03/28 00:07:24  edink
+ *   Reshufle includes to make it compile on windows
+ *
+ *   Revision 1.7  2005/03/26 03:13:58  sniper
+ *   - Made it possible to build ext/xmlrpc with libxml2
+ *
  *   Revision 1.6  2004/06/01 20:16:06  iliaa
  *   Fixed bug #28597 (xmlrpc_encode_request() incorrectly encodes chars in
  *   200-210 range).
@@ -101,6 +113,7 @@ static const char rcsid[] = "#(@) $Id: xml_element.c,v 1.6 2004/06/01 20:16:06 i
  *   there must be some.
  ******/
  
+#include "ext/xml/expat_compat.h"
 #ifdef _WIN32
 #include "xmlrpc_win32.h"
 #endif
@@ -110,10 +123,9 @@ static const char rcsid[] = "#(@) $Id: xml_element.c,v 1.6 2004/06/01 20:16:06 i
 
 #include "xml_element.h"
 #include "queue.h"
-#include "expat.h"
 #include "encodings.h"
 
-#define my_free(thing)  if(thing) {free(thing); thing = 0;}
+#define my_free(thing)  if(thing) {free(thing); thing = NULL;}
 
 #define XML_DECL_START                 "<?xml"
 #define XML_DECL_START_LEN             sizeof(XML_DECL_START) - 1
@@ -189,7 +201,10 @@ void xml_elem_free_non_recurse(xml_element* root) {
 
       Q_Destroy(&root->children);
       Q_Destroy(&root->attrs);
-      my_free((char*)root->name);
+      if(root->name) {
+          free((char *)root->name);
+          root->name = NULL;
+      }
       simplestring_free(&root->text);
       my_free(root);
    }
@@ -572,7 +587,7 @@ typedef struct _xml_elem_data {
 
 
 /* expat start of element handler */
-static void startElement(void *userData, const char *name, const char **attrs)
+static void _xmlrpc_startElement(void *userData, const char *name, const char **attrs)
 {
    xml_element *c;
    xml_elem_data* mydata = (xml_elem_data*)userData;
@@ -600,7 +615,7 @@ static void startElement(void *userData, const char *name, const char **attrs)
 }
 
 /* expat end of element handler */
-static void endElement(void *userData, const char *name)
+static void _xmlrpc_endElement(void *userData, const char *name)
 {
    xml_elem_data* mydata = (xml_elem_data*)userData;
 
@@ -612,7 +627,7 @@ static void endElement(void *userData, const char *name)
 }
 
 /* expat char data handler */
-static void charHandler(void *userData,
+static void _xmlrpc_charHandler(void *userData,
                         const char *s,
                         int len)
 {
@@ -685,8 +700,8 @@ xml_element* xml_elem_parse_buf(const char* in_buf, int len, XML_ELEM_INPUT_OPTI
       mydata.input_options = options;
       mydata.needs_enc_conversion = options->encoding && strcmp(options->encoding, encoding_utf_8);
 
-      XML_SetElementHandler(parser, startElement, endElement);
-      XML_SetCharacterDataHandler(parser, charHandler);
+      XML_SetElementHandler(parser, (XML_StartElementHandler)_xmlrpc_startElement, (XML_EndElementHandler)_xmlrpc_endElement);
+      XML_SetCharacterDataHandler(parser, (XML_CharacterDataHandler)_xmlrpc_charHandler);
 
       /* pass the xml_elem_data struct along */
       XML_SetUserData(parser, (void*)&mydata);

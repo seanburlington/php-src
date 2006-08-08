2821452d7fc2c66fbcbe4c88f60ed8f25a8fb2d6
iliaa <iliaa>
2006-08-08 14:57:33 +0000

    MFB: Fixed bug #38377 (session_destroy() gives warning after session_regenerate_id()).

--- ext/session/mod_files.c
+++ ext/session/mod_files.c
@@ -16,7 +16,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: mod_files.c,v 1.104 2006/07/27 15:36:43 iliaa Exp $ */
+/* $Id: mod_files.c,v 1.105 2006/08/08 14:57:33 iliaa Exp $ */
 
 #include "php.h"
 
@@ -402,7 +402,12 @@ PS_DESTROY_FUNC(files)
 		ps_files_close(data);
 	
 		if (VCWD_UNLINK(buf) == -1) {
-			return FAILURE;
+			/* This is a little safety check for instances when we are dealing with a regenerated session
+			 * that was not yet written to disk
+			 */
+			if (!VCWD_ACCESS(buf, F_OK)) {
+				return FAILURE;
+			}
 		}
 	}
 

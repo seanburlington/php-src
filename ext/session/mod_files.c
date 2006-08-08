5693c95d788ec7f5fd3ed0997dc32ed965e7a620
iliaa <iliaa>
2006-08-08 14:54:49 +0000

    Fixed bug #38377 (session_destroy() gives warning after session_regenerate_id()).

--- ext/session/mod_files.c
+++ ext/session/mod_files.c
@@ -16,7 +16,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: mod_files.c,v 1.100.2.3.2.1 2006/07/27 15:33:16 iliaa Exp $ */
+/* $Id: mod_files.c,v 1.100.2.3.2.2 2006/08/08 14:54:49 iliaa Exp $ */
 
 #include "php.h"
 
@@ -402,7 +402,12 @@ PS_DESTROY_FUNC(files)
 		ps_files_close(data);
 	
 		if (VCWD_UNLINK(buf) == -1) {
-			return FAILURE;
+			/* This is a little safety check for instances when we are dealing with a regenerated session
+			 * that was not yet written to disk
+			 */
+			if (!VCWD_ACCESS(buf, F_OK)) {
+				return FAILURE;
+			}
 		}
 	}
 

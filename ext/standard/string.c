6bcb8dc73c7d6c77d25df53de286e8e2061a9355
fmk <fmk>
2006-08-09 18:15:06 +0000

    Fix ZTS builds

--- ext/standard/string.c
+++ ext/standard/string.c
@@ -18,7 +18,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: string.c,v 1.569 2006/08/09 17:40:21 andrei Exp $ */
+/* $Id: string.c,v 1.570 2006/08/09 18:15:06 fmk Exp $ */
 
 /* Synced with php 3.0 revision 1.193 1999-06-16 [ssb] */
 
@@ -4367,7 +4367,7 @@ PHP_FUNCTION(addcslashes)
 	char *str, *what;
 	int str_len, what_len;
 
-	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_DC, "SS", &str, &str_len,
+	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "SS", &str, &str_len,
 							  &what, &what_len) == FAILURE) {
 		return;
 	}
@@ -4423,7 +4423,7 @@ PHP_FUNCTION(stripcslashes)
 	char *str;
 	int str_len;
 
-	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_DC, "S", &str, &str_len) == FAILURE) {
+	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "S", &str, &str_len) == FAILURE) {
 		return;
 	}
 

9ebbbd51cc0a6b4690ede785a7939594f2efa088
tony2001 <tony2001>
2006-08-09 10:16:50 +0000

    MFH: fix #8112 (Persistent connections misbehave when Apache process times out)

--- ext/oci8/oci8.c
+++ ext/oci8/oci8.c
@@ -26,7 +26,7 @@
    +----------------------------------------------------------------------+
 */
 
-/* $Id: oci8.c,v 1.269.2.16.2.13 2006/08/06 14:09:58 tony2001 Exp $ */
+/* $Id: oci8.c,v 1.269.2.16.2.14 2006/08/09 10:16:50 tony2001 Exp $ */
 /* TODO
  *
  * file://localhost/www/docs/oci10/ociaahan.htm#423823 - implement lob_empty() with OCI_ATTR_LOBEMPTY
@@ -653,7 +653,7 @@ PHP_MINFO_FUNCTION(oci)
 	php_info_print_table_start();
 	php_info_print_table_row(2, "OCI8 Support", "enabled");
 	php_info_print_table_row(2, "Version", "1.2.1");
-	php_info_print_table_row(2, "Revision", "$Revision: 1.269.2.16.2.13 $");
+	php_info_print_table_row(2, "Revision", "$Revision: 1.269.2.16.2.14 $");
 
 	sprintf(buf, "%ld", OCI_G(num_persistent));
 	php_info_print_table_row(2, "Active Persistent Connections", buf);
@@ -1745,6 +1745,17 @@ static int php_oci_persistent_helper(zend_rsrc_list_entry *le TSRMLS_DC)
 		connection = (php_oci_connection *)le->ptr;
 
 		if (connection->used_this_request) {
+			if ((PG(connection_status) & PHP_CONNECTION_TIMEOUT)) {
+				/* call OCIBreak() on timeout to avoid appearance of deadlocked persistent connections */
+				connection->errcode = PHP_OCI_CALL(OCIBreak, (connection->svc, connection->err));
+				if (connection->errcode != OCI_SUCCESS) {
+					php_oci_error(connection->err, connection->errcode TSRMLS_CC);
+					PHP_OCI_HANDLE_ERROR(connection, connection->errcode);
+					/* OCIBreak() failed. delete the connection */
+					return 1;
+				}
+			}
+
 			if (connection->descriptors) {
 				zend_hash_destroy(connection->descriptors);
 				efree(connection->descriptors);

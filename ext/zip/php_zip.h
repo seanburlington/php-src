d8573fc9f517f25d772cc839178e6c9c615140f1
pajoye <pajoye>
2006-08-13 20:16:48 +0000

    - add overwrite mode to ZipArchive::open, always starts a new archive - fix build with php6 - remove safemode when built against php6

--- ext/zip/php_zip.h
+++ ext/zip/php_zip.h
@@ -16,7 +16,7 @@
   +----------------------------------------------------------------------+
 */
 
-/* $Id: php_zip.h,v 1.10 2006/07/24 16:58:58 pajoye Exp $ */
+/* $Id: php_zip.h,v 1.11 2006/08/13 20:16:48 pajoye Exp $ */
 
 #ifndef PHP_ZIP_H
 #define PHP_ZIP_H
@@ -37,7 +37,7 @@ extern zend_module_entry zip_module_entry;
 #include "lib/zip.h"
 
 #ifndef ZEND_ENGINE_2_1
-# if (PHP_MAJOR_VERSION == 5 && PHP_MINOR_VERSION > 0)
+# if (PHP_MAJOR_VERSION == 5 && PHP_MINOR_VERSION > 0) || PHP_MAJOR_VERSION == 6
 #  define ZEND_ENGINE_2_1
 # endif
 #endif

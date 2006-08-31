c884d7effa4a31d36b1003ba2fcc08556f9253cf
tony2001 <tony2001>
2006-08-31 14:21:21 +0000

    more z/Z fixes

--- ext/standard/string.c
+++ ext/standard/string.c
@@ -18,7 +18,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: string.c,v 1.445.2.14.2.18 2006/08/29 19:50:04 iliaa Exp $ */
+/* $Id: string.c,v 1.445.2.14.2.19 2006/08/31 14:21:21 tony2001 Exp $ */
 
 /* Synced with php 3.0 revision 1.193 1999-06-16 [ssb] */
 
@@ -1741,22 +1741,22 @@ PHP_FUNCTION(stripos)
    Finds position of last occurrence of a string within another string */
 PHP_FUNCTION(strrpos)
 {
-	zval *zneedle;
+	zval **zneedle;
 	char *needle, *haystack;
 	int needle_len, haystack_len;
 	long offset = 0;
 	char *p, *e, ord_needle[2];
 
-	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "sz|l", &haystack, &haystack_len, &zneedle, &offset) == FAILURE) {
+	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "sZ|l", &haystack, &haystack_len, &zneedle, &offset) == FAILURE) {
 		RETURN_FALSE;
 	}
 
-	if (Z_TYPE_P(zneedle) == IS_STRING) {
-		needle = Z_STRVAL_P(zneedle);
-		needle_len = Z_STRLEN_P(zneedle);
+	if (Z_TYPE_PP(zneedle) == IS_STRING) {
+		needle = Z_STRVAL_PP(zneedle);
+		needle_len = Z_STRLEN_PP(zneedle);
 	} else {
-		convert_to_long(zneedle);
-		ord_needle[0] = (char)(Z_LVAL_P(zneedle) & 0xFF);
+		convert_to_long_ex(zneedle);
+		ord_needle[0] = (char)(Z_LVAL_PP(zneedle) & 0xFF);
 		ord_needle[1] = '\0';
 		needle = ord_needle;
 		needle_len = 1;
@@ -1806,23 +1806,23 @@ PHP_FUNCTION(strrpos)
    Finds position of last occurrence of a string within another string */
 PHP_FUNCTION(strripos)
 {
-	zval *zneedle;
+	zval **zneedle;
 	char *needle, *haystack;
 	int needle_len, haystack_len;
 	long offset = 0;
 	char *p, *e, ord_needle[2];
 	char *needle_dup, *haystack_dup;
 
-	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "sz|l", &haystack, &haystack_len, &zneedle, &offset) == FAILURE) {
+	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "sZ|l", &haystack, &haystack_len, &zneedle, &offset) == FAILURE) {
 		RETURN_FALSE;
 	}
 
-	if (Z_TYPE_P(zneedle) == IS_STRING) {
-		needle = Z_STRVAL_P(zneedle);
-		needle_len = Z_STRLEN_P(zneedle);
+	if (Z_TYPE_PP(zneedle) == IS_STRING) {
+		needle = Z_STRVAL_PP(zneedle);
+		needle_len = Z_STRLEN_PP(zneedle);
 	} else {
-		convert_to_long(zneedle);
-		ord_needle[0] = (char)(Z_LVAL_P(zneedle) & 0xFF);
+		convert_to_long_ex(zneedle);
+		ord_needle[0] = (char)(Z_LVAL_PP(zneedle) & 0xFF);
 		ord_needle[1] = '\0';
 		needle = ord_needle;
 		needle_len = 1;

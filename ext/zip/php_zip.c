4a7b23ba8b4b3ea0f55b51fbff4eb532d67156c2
pajoye <pajoye>
2006-09-06 17:38:36 +0000

    - bump version, 5.2.0 will have zip-2.0.0-stable

--- ext/zip/php_zip.c
+++ ext/zip/php_zip.c
@@ -16,7 +16,7 @@
   +----------------------------------------------------------------------+
 */
 
-/* $Id: php_zip.c,v 1.1.2.11 2006/09/06 17:24:41 pajoye Exp $ */
+/* $Id: php_zip.c,v 1.1.2.12 2006/09/06 17:38:36 pajoye Exp $ */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -1983,8 +1983,8 @@ PHP_MINFO_FUNCTION(zip)
 	php_info_print_table_start();
 
 	php_info_print_table_row(2, "Zip", "enabled");
-	php_info_print_table_row(2, "Extension Version","$Id: php_zip.c,v 1.1.2.11 2006/09/06 17:24:41 pajoye Exp $");
-	php_info_print_table_row(2, "Zip version", "1.7.1");
+	php_info_print_table_row(2, "Extension Version","$Id: php_zip.c,v 1.1.2.12 2006/09/06 17:38:36 pajoye Exp $");
+	php_info_print_table_row(2, "Zip version", "2.0.0");
 	php_info_print_table_row(2, "Libzip version", "0.7.1");
 
 	php_info_print_table_end();

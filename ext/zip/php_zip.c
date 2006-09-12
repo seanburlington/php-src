c36596d08805d167e23062c8f82aec7cd8e17e8a
pajoye <pajoye>
2006-09-12 12:04:03 +0000

    - MFB:PECL Bug #8676, addFile was not updated and still used VCWD_REALPATH,   it now uses expand_filepath

--- ext/zip/php_zip.c
+++ ext/zip/php_zip.c
@@ -16,7 +16,7 @@
   +----------------------------------------------------------------------+
 */
 
-/* $Id: php_zip.c,v 1.13 2006/09/06 19:50:22 bjori Exp $ */
+/* $Id: php_zip.c,v 1.14 2006/09/12 12:04:03 pajoye Exp $ */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -971,10 +971,10 @@ ZIPARCHIVE_METHOD(addFile)
 		entry_name_len = filename_len;
 	}
 
-	if (!VCWD_REALPATH(filename, resolved_path)) {
+	if(!expand_filepath(filename, resolved_path TSRMLS_CC)) {
 		RETURN_FALSE;
 	}
-	
+
 	zs = zip_source_file(intern, resolved_path, 0, 0);
 	if (!zs) {
 		RETURN_FALSE;
@@ -1958,7 +1958,7 @@ PHP_MINFO_FUNCTION(zip)
 	php_info_print_table_start();
 
 	php_info_print_table_row(2, "Zip", "enabled");
-	php_info_print_table_row(2, "Extension Version","$Id: php_zip.c,v 1.13 2006/09/06 19:50:22 bjori Exp $");
+	php_info_print_table_row(2, "Extension Version","$Id: php_zip.c,v 1.14 2006/09/12 12:04:03 pajoye Exp $");
 	php_info_print_table_row(2, "Zip version", "2.0.0");
 	php_info_print_table_row(2, "Libzip version", "0.7.1");
 

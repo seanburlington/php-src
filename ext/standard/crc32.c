bd66bdbd8dd91f5a379e8c40eec6b9ef24f44898
pollita <pollita>
2006-09-25 01:33:57 +0000

    Flag a couple more unicode ready functions

--- ext/standard/crc32.c
+++ ext/standard/crc32.c
@@ -16,13 +16,13 @@
    +----------------------------------------------------------------------+
 */
 
-/* $Id: crc32.c,v 1.19 2006/02/09 15:48:18 pajoye Exp $ */
+/* $Id: crc32.c,v 1.20 2006/09/25 01:33:57 pollita Exp $ */
 
 #include "php.h"
 #include "basic_functions.h"
 #include "crc32.h"
 
-/* {{{ proto string crc32(string str)
+/* {{{ proto string crc32(string str) U
    Calculate the crc32 polynomial of a string */
 PHP_NAMED_FUNCTION(php_if_crc32)
 {
@@ -31,7 +31,7 @@ PHP_NAMED_FUNCTION(php_if_crc32)
 	php_uint32 crcinit = 0;
 	register php_uint32 crc;
 
-	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "s", &p, &nr) == FAILURE) {
+	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "S", &p, &nr) == FAILURE) {
 		return;
 	}
 	crc = crcinit^0xFFFFFFFF;

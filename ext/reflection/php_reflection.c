d210da241b7068eb9570fdb4b07f66cd33ef7301
tony2001 <tony2001>
2006-10-02 12:15:47 +0000

    fix #39001 (ReflectionProperty returns incorrect declaring class for protected properties)

--- ext/reflection/php_reflection.c
+++ ext/reflection/php_reflection.c
@@ -20,7 +20,7 @@
    +----------------------------------------------------------------------+
 */
 
-/* $Id: php_reflection.c,v 1.256 2006/09/26 07:55:54 dmitry Exp $ */
+/* $Id: php_reflection.c,v 1.257 2006/10/02 12:15:47 tony2001 Exp $ */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -4080,12 +4080,19 @@ ZEND_METHOD(reflection_property, getDeclaringClass)
 	property_reference *ref;
 	zend_class_entry *tmp_ce, *ce;
 	zend_property_info *tmp_info;
+	zstr prop_name, class_name;
+	int prop_name_len;
 
 	METHOD_NOTSTATIC_NUMPARAMS(reflection_property_ptr, 0);
 	GET_REFLECTION_OBJECT_PTR(ref);
 
+	if (zend_u_unmangle_property_name(UG(unicode)?IS_UNICODE:IS_STRING, ref->prop->name, ref->prop->name_length, &class_name, &prop_name) != SUCCESS) {
+		RETURN_FALSE;
+	}
+
+	prop_name_len = USTR_LEN(prop_name);
 	ce = tmp_ce = ref->ce;
-	while (tmp_ce && zend_u_hash_find(&tmp_ce->properties_info, UG(unicode)?IS_UNICODE:IS_STRING, ref->prop->name, ref->prop->name_length + 1, (void **) &tmp_info) == SUCCESS) {
+	while (tmp_ce && zend_u_hash_find(&tmp_ce->properties_info, UG(unicode)?IS_UNICODE:IS_STRING, prop_name, prop_name_len + 1, (void **) &tmp_info) == SUCCESS) {
 		ce = tmp_ce;
 		tmp_ce = tmp_ce->parent;
 	}
@@ -4896,7 +4903,7 @@ PHP_MINFO_FUNCTION(reflection) /* {{{ */
 	php_info_print_table_start();
 	php_info_print_table_header(2, "Reflection", "enabled");
 
-	php_info_print_table_row(2, "Version", "$Id: php_reflection.c,v 1.256 2006/09/26 07:55:54 dmitry Exp $");
+	php_info_print_table_row(2, "Version", "$Id: php_reflection.c,v 1.257 2006/10/02 12:15:47 tony2001 Exp $");
 
 	php_info_print_table_end();
 } /* }}} */

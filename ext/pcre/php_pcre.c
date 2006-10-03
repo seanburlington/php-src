47abc557a05f273e4b649c082492bd19b8101512
tony2001 <tony2001>
2006-10-03 20:42:57 +0000

    don't forget to increase refcount when adding entry to the return_value

--- ext/pcre/php_pcre.c
+++ ext/pcre/php_pcre.c
@@ -16,7 +16,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: php_pcre.c,v 1.203 2006/09/19 20:41:56 andrei Exp $ */
+/* $Id: php_pcre.c,v 1.204 2006/10/03 20:42:57 tony2001 Exp $ */
 
 /*  TODO
  *  php_pcre_replace_impl():
@@ -1868,6 +1868,7 @@ PHPAPI void  php_pcre_grep_impl(pcre_cache_entry *pce, zval *input, zval *return
 		/* If the entry fits our requirements */
 		if ((count > 0 && !invert) ||
 			(count == PCRE_ERROR_NOMATCH && invert)) {
+			(*entry)->refcount++;
 
 			/* Add to return array */
 			switch (zend_hash_get_current_key_ex(Z_ARRVAL_P(input), &string_key, &string_key_len, &num_key, 0, NULL))

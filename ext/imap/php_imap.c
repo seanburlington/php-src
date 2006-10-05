be984a7c21a7cfd5fe817ca4fd5500b27dcf4e4c
tony2001 <tony2001>
2006-10-05 14:28:33 +0000

    MFB: fix build with new c-client

--- ext/imap/php_imap.c
+++ ext/imap/php_imap.c
@@ -26,7 +26,7 @@
    | PHP 4.0 updates:  Zeev Suraski <zeev@zend.com>                       |
    +----------------------------------------------------------------------+
  */
-/* $Id: php_imap.c,v 1.224 2006/09/24 18:06:53 iliaa Exp $ */
+/* $Id: php_imap.c,v 1.225 2006/10/05 14:28:33 tony2001 Exp $ */
 
 #define IMAP41
 
@@ -2177,7 +2177,12 @@ PHP_FUNCTION(imap_utf8)
 	dest.size = 0;
 
 	cpytxt(&src, Z_STRVAL_PP(str), Z_STRLEN_PP(str));
+
+#ifndef HAVE_NEW_MIME2TEXT
 	utf8_mime2text(&src, &dest);
+#else
+	utf8_mime2text(&src, &dest, U8T_CANONICAL);
+#endif
 	RETVAL_STRINGL(dest.data, dest.size, 1);
 	if (dest.data) {
 		free(dest.data);

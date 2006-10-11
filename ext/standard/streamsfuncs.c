72317bbbc214e722ff7c67948a03bb2a9892b604
pollita <pollita>
2006-10-11 23:20:54 +0000

    Bugfix#38649 Optional param not initialized

--- ext/standard/streamsfuncs.c
+++ ext/standard/streamsfuncs.c
@@ -17,7 +17,7 @@
   +----------------------------------------------------------------------+
 */
 
-/* $Id: streamsfuncs.c,v 1.95 2006/10/09 02:48:06 pollita Exp $ */
+/* $Id: streamsfuncs.c,v 1.96 2006/10/11 23:20:54 pollita Exp $ */
 
 #include "php.h"
 #include "php_globals.h"
@@ -317,7 +317,7 @@ PHP_FUNCTION(stream_socket_sendto)
 	php_stream *stream;
 	zval *zstream;
 	long flags = 0;
-	char *data, *target_addr;
+	char *data, *target_addr = NULL;
 	int datalen, target_addr_len = 0;
 	php_sockaddr_storage sa;
 	socklen_t sl = 0;

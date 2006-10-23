6ff65b46e520ef893cab2e9c11e1874ea47e1cc2
dmitry <dmitry>
2006-10-23 06:49:49 +0000

    Fixed decoding list of arrays (related to bug #39121)

--- ext/soap/php_encoding.c
+++ ext/soap/php_encoding.c
@@ -17,7 +17,7 @@
   |          Dmitry Stogov <dmitry@zend.com>                             |
   +----------------------------------------------------------------------+
 */
-/* $Id: php_encoding.c,v 1.144 2006/10/03 07:00:57 dmitry Exp $ */
+/* $Id: php_encoding.c,v 1.145 2006/10/23 06:49:49 dmitry Exp $ */
 
 #include <time.h>
 
@@ -1346,7 +1346,16 @@ static zval *to_zval_object_ex(encodeTypePtr type, xmlNodePtr data, zend_class_e
 
 				prop = get_zval_property(ret, (char*)trav->name TSRMLS_CC);
 				if (!prop) {
-					set_zval_property(ret, (char*)trav->name, tmpVal TSRMLS_CC);
+					if (!trav->next || !get_node(trav->next, (char*)trav->name)) {
+						set_zval_property(ret, (char*)trav->name, tmpVal TSRMLS_CC);
+					} else {
+						zval *arr;
+
+						MAKE_STD_ZVAL(arr);
+						array_init(arr);
+						add_next_index_zval(arr, tmpVal);
+						set_zval_property(ret, (char*)trav->name, arr TSRMLS_CC);
+					}
 				} else {
 					/* Property already exist - make array */
 					if (Z_TYPE_P(prop) != IS_ARRAY) {

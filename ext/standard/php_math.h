121d3bd9a2c3084366bde22ed2d0e1c0dc4860fe
iliaa <iliaa>
2006-11-09 20:01:41 +0000

    Added missing PHPAPI declaration

--- ext/standard/php_math.h
+++ ext/standard/php_math.h
@@ -2,7 +2,7 @@
    +----------------------------------------------------------------------+
    | PHP Version 5                                                        |
    +----------------------------------------------------------------------+
-   | Copyright (c) 1997-2007 The PHP Group                                |
+   | Copyright (c) 1997-2006 The PHP Group                                |
    +----------------------------------------------------------------------+
    | This source file is subject to version 3.01 of the PHP license,      |
    | that is bundled with this package in the file LICENSE, and is        |
@@ -17,12 +17,13 @@
    +----------------------------------------------------------------------+
 */
 
-/* $Id: php_math.h,v 1.28.2.3 2007/01/01 09:40:29 sebastian Exp $ */
+/* $Id: php_math.h,v 1.28.2.2.2.1 2006/11/09 20:01:41 iliaa Exp $ */
 
 #ifndef PHP_MATH_H
 #define PHP_MATH_H
 
 PHPAPI char *_php_math_number_format(double, int, char , char);
+PHPAPI char * _php_math_longtobase(zval *arg, int base);
 
 PHP_FUNCTION(sin);
 PHP_FUNCTION(cos);

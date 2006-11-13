47b288522440112d3f678ba1f09c0173da87bcbd
tony2001 <tony2001>
2006-11-13 19:32:58 +0000

    MFH: improve the fix and the test

--- ext/filter/filter.c
+++ ext/filter/filter.c
@@ -19,7 +19,7 @@
   +----------------------------------------------------------------------+
 */
 
-/* $Id: filter.c,v 1.52.2.27 2006/11/13 15:00:05 tony2001 Exp $ */
+/* $Id: filter.c,v 1.52.2.28 2006/11/13 19:32:58 tony2001 Exp $ */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -275,7 +275,7 @@ PHP_MINFO_FUNCTION(filter)
 {
 	php_info_print_table_start();
 	php_info_print_table_header( 2, "Input Validation and Filtering", "enabled" );
-	php_info_print_table_row( 2, "Revision", "$Revision: 1.52.2.27 $");
+	php_info_print_table_row( 2, "Revision", "$Revision: 1.52.2.28 $");
 	php_info_print_table_end();
 
 	DISPLAY_INI_ENTRIES();
@@ -607,14 +607,16 @@ static void php_filter_array_handler(zval *input, zval **op, zval *return_value 
 	zval **tmp, **arg_elm;
 
 	if (!op) {
-		SEPARATE_ZVAL(&input);
+		zval_dtor(return_value);
 		*return_value = *input;
 		zval_copy_ctor(return_value);
+		INIT_PZVAL(return_value);
 		php_filter_call(&return_value, FILTER_DEFAULT, NULL, 0, FILTER_REQUIRE_ARRAY TSRMLS_CC);
 	} else if (Z_TYPE_PP(op) == IS_LONG) {
-		SEPARATE_ZVAL(&input);
+		zval_dtor(return_value);
 		*return_value = *input;
 		zval_copy_ctor(return_value);
+		INIT_PZVAL(return_value);
 		php_filter_call(&return_value, Z_LVAL_PP(op), NULL, 0, FILTER_REQUIRE_ARRAY TSRMLS_CC);
 	} else if (Z_TYPE_PP(op) == IS_ARRAY) {
 		array_init(return_value);

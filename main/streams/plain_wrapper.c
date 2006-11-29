204b6d6dceacf164f10dc463eca4185aabdb85ea
tony2001 <tony2001>
2006-11-29 11:12:59 +0000

    MFH: fix #39673 (file_get_contents causes bus error on certain offsets)

--- main/streams/plain_wrapper.c
+++ main/streams/plain_wrapper.c
@@ -16,7 +16,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: plain_wrapper.c,v 1.52.2.6.2.12 2006/11/16 16:06:51 bjori Exp $ */
+/* $Id: plain_wrapper.c,v 1.52.2.6.2.13 2006/11/29 11:12:59 tony2001 Exp $ */
 
 #include "php.h"
 #include "php_globals.h"
@@ -627,8 +627,15 @@ static int php_stdiop_set_option(php_stream *stream, int option, int value, void
 
 					case PHP_STREAM_MMAP_MAP_RANGE:
 						do_fstat(data, 1);
+						if (range->length == 0 && range->offset > 0 && range->offset < data->sb.st_size) {
+							range->length = data->sb.st_size - range->offset;
+						}
 						if (range->length == 0 || range->length > data->sb.st_size) {
 							range->length = data->sb.st_size;
+						}
+						if (range->offset >= data->sb.st_size) {
+							range->offset = data->sb.st_size;
+							range->length = 0;
 						}
 						switch (range->mode) {
 							case PHP_STREAM_MAP_MODE_READONLY:

fd009e756b2cebc678e37272d42b572173ecac44
tony2001 <tony2001>
2006-12-06 16:28:36 +0000

    MFH

--- ext/standard/formatted_print.c
+++ ext/standard/formatted_print.c
@@ -16,7 +16,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: formatted_print.c,v 1.82.2.1.2.3 2006/12/06 14:47:36 tony2001 Exp $ */
+/* $Id: formatted_print.c,v 1.82.2.1.2.4 2006/12/06 16:28:36 tony2001 Exp $ */
 
 #include <math.h>				/* modf() */
 #include "php.h"
@@ -223,13 +223,13 @@ php_sprintf_appenddouble(char **buffer, int *pos,
 	}
 
 	switch (fmt) {			
+		case 'F':
+			fmt = 'f';
+			/* break is missing */
 		case 'e':
 			if (precision) {
 				precision--;
 			}
-		case 'F':
-			fmt = 'f';
-			/* break is missing */
 		case 'E':
 		case 'f':
 			s = ap_php_conv_fp(fmt, number, 0, precision,

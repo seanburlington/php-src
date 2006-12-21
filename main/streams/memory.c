9df04b3d4aef63d80b7696ba8c56a6f510b093da
tony2001 <tony2001>
2006-12-21 01:18:14 +0000

    apply correct fix (aka "oops!")

--- main/streams/memory.c
+++ main/streams/memory.c
@@ -16,7 +16,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: memory.c,v 1.27 2006/12/21 00:46:51 tony2001 Exp $ */
+/* $Id: memory.c,v 1.28 2006/12/21 01:18:14 tony2001 Exp $ */
 
 #define _GNU_SOURCE
 #include "php.h"
@@ -673,7 +673,6 @@ static php_stream * php_stream_url_wrap_rfc2397(php_stream_wrapper *wrapper, cha
 	if ((stream = php_stream_temp_create_rel(0, ~0u)) != NULL) {
 		/* store data */
 		php_stream_temp_write(stream, comma, ilen TSRMLS_CC);
-		efree(comma);
 		php_stream_temp_seek(stream, 0, SEEK_SET, &newoffs TSRMLS_CC);
 		/* set special stream stuff (enforce exact mode) */
 		vlen = strlen(mode);

e19353152e9808fb0a014b13240558cfe9028cbe
tony2001 <tony2001>
2006-12-25 15:28:57 +0000

    MFH

--- ext/imap/php_imap.c
+++ ext/imap/php_imap.c
@@ -26,7 +26,7 @@
    | PHP 4.0 updates:  Zeev Suraski <zeev@zend.com>                       |
    +----------------------------------------------------------------------+
  */
-/* $Id: php_imap.c,v 1.208.2.7.2.13 2006/12/25 14:23:18 iliaa Exp $ */
+/* $Id: php_imap.c,v 1.208.2.7.2.14 2006/12/25 15:28:57 tony2001 Exp $ */
 
 #define IMAP41
 
@@ -62,7 +62,7 @@ MAILSTREAM DEFAULTPROTO;
 #define CRLF_LEN sizeof("\015\012") - 1
 #define PHP_EXPUNGE 32768
 #define PHP_IMAP_ADDRESS_SIZE_BUF 10
-#ifdef SENDBUFLEN
+#ifndef SENDBUFLEN
 #define SENDBUFLEN 16385
 #endif
 

499b118bb0a19f51aa11c966038262d5895f8efa
tony2001 <tony2001>
2006-12-25 22:27:12 +0000

    more safe_emalloc()

--- ext/xsl/xsltprocessor.c
+++ ext/xsl/xsltprocessor.c
@@ -17,7 +17,7 @@
    +----------------------------------------------------------------------+
 */
 
-/* $Id: xsltprocessor.c,v 1.53 2006/12/05 12:05:03 rrichards Exp $ */
+/* $Id: xsltprocessor.c,v 1.54 2006/12/25 22:27:12 tony2001 Exp $ */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -88,7 +88,7 @@ static char **php_xsl_xslt_make_params(HashTable *parht, int xpath_params TSRMLS
 	zstr string_key = NULL_ZSTR;
 
 	parsize = (2 * zend_hash_num_elements(parht) + 1) * sizeof(char *);
-	params = (char **)emalloc(parsize);
+	params = (char **)safe_emalloc((2 * zend_hash_num_elements(parht) + 1), sizeof(char *), 0);
 	memset((char *)params, 0, parsize);
 
 	for (zend_hash_internal_pointer_reset(parht);

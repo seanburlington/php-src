42846137a29785ac3d1dd2bef3adec91a5066c06
tony2001 <tony2001>
2006-12-28 15:26:20 +0000

    it's ptr, not ptrptr

--- ext/fdf/fdf.c
+++ ext/fdf/fdf.c
@@ -17,7 +17,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: fdf.c,v 1.97 2006/07/23 23:55:26 sniper Exp $ */
+/* $Id: fdf.c,v 1.98 2006/12/28 15:26:20 tony2001 Exp $ */
 
 /* FdfTk lib 2.0 is a Complete C/C++ FDF Toolkit available from
    http://beta1.adobe.com/ada/acrosdk/forms.html. */
@@ -1818,10 +1818,10 @@ PHP_FUNCTION(fdf_enum_values) {
 	if (!zend_is_callable(callback, 0, &name)) {
 		convert_to_string(&name);
 		php_error_docref1(NULL TSRMLS_CC, Z_STRVAL(name), E_WARNING, "Second argument is expected to be a valid callback");
-		zval_ptr_dtor(&name);
+		zval_dtor(&name);
 		RETURN_FALSE;
 	}
-	zval_ptr_dtor(&name);
+	zval_dtor(&name);
 	FDF_G(enum_callback) = callback;
 	FDF_G(enum_fdf) = fdf;
 

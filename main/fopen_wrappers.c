ccf5c551bd92bc54c016b357e9b9c7e21ff9194f
bjori <bjori>
2007-01-12 14:34:46 +0000

    MFB: Fix build (wharmby at uk dot ibm dot com)

--- main/fopen_wrappers.c
+++ main/fopen_wrappers.c
@@ -17,7 +17,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: fopen_wrappers.c,v 1.189 2007/01/12 09:10:05 tony2001 Exp $ */
+/* $Id: fopen_wrappers.c,v 1.190 2007/01/12 14:34:46 bjori Exp $ */
 
 /* {{{ includes
  */
@@ -280,6 +280,7 @@ PHPAPI int php_fopen_primary_script(zend_file_handle *file_handle TSRMLS_DC)
 			char user[32];			/* to try open the directory */
 			struct passwd *pw;
 #if defined(ZTS) && defined(HAVE_GETPWNAM_R) && defined(_SC_GETPW_R_SIZE_MAX)
+			struct passwd pwstruc;
 			long pwbuflen = sysconf(_SC_GETPW_R_SIZE_MAX);
 			char *pwbuf;
 

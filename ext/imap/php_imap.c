0388ee59aeb5ed1b6e8460643e92adea4e848407
tony2001 <tony2001>
2007-01-29 10:08:16 +0000

    backported fix for ext/imap compilation failure with recent c-client versions (fixes #39401)

--- ext/imap/php_imap.c
+++ ext/imap/php_imap.c
@@ -26,7 +26,7 @@
    | PHP 4.0 updates:  Zeev Suraski <zeev@zend.com>                       |
    +----------------------------------------------------------------------+
  */
-/* $Id: php_imap.c,v 1.142.2.44.2.10 2007/01/01 09:46:43 sebastian Exp $ */
+/* $Id: php_imap.c,v 1.142.2.44.2.11 2007/01/29 10:08:17 tony2001 Exp $ */
 
 #define IMAP41
 
@@ -74,7 +74,11 @@ static int _php_imap_address_size(ADDRESS *addresslist);
 void rfc822_date(char *date);
 char *cpystr(const char *str);
 char *cpytxt(SIZEDTEXT *dst, char *text, unsigned long size);
+#ifndef HAVE_NEW_MIME2TEXT
 long utf8_mime2text(SIZEDTEXT *src, SIZEDTEXT *dst);
+#else
+long utf8_mime2text (SIZEDTEXT *src, SIZEDTEXT *dst, long flags);
+#endif
 unsigned long find_rightmost_bit(unsigned long *valptr);
 void fs_give(void **block);
 void *fs_get(size_t size);
@@ -2064,7 +2068,11 @@ PHP_FUNCTION(imap_utf8)
 	dest.size = 0;
 
 	cpytxt(&src, Z_STRVAL_PP(str), Z_STRLEN_PP(str));
+#ifndef HAVE_NEW_MIME2TEXT
 	utf8_mime2text(&src, &dest);
+#else
+	utf8_mime2text(&src, &dest, U8T_CANONICAL);
+#endif
 	RETURN_STRINGL(dest.data, strlen(dest.data), 1);
 }
 /* }}} */

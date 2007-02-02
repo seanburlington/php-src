44ca04c8abb11fdaca5067583a095b201db486a4
mike <mike>
2007-02-02 17:11:10 +0000

    - add note about the past for the future, see http://bugs.php.net/?id=40310&edit=1

--- ext/zlib/zlib.c
+++ ext/zlib/zlib.c
@@ -20,7 +20,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: zlib.c,v 1.213 2007/01/01 09:29:34 sebastian Exp $ */
+/* $Id: zlib.c,v 1.214 2007/02/02 17:11:10 mike Exp $ */
 
 #ifdef HAVE_CONFIG_H
 #	include "config.h"
@@ -218,6 +218,8 @@ int php_zlib_output_handler(void **handler_context, php_output_context *output_c
 					deflateEnd(&ctx->Z);
 					return FAILURE;
 			}
+			/* "Vary: Accept-Encoding" header sent along uncompressed content breaks caching in MSIE,
+			   so let's just send it with successfully compressed content, see http://bugs.php.net/40325 */
 			sapi_add_header_ex(ZEND_STRL("Vary: Accept-Encoding"), 1, 1 TSRMLS_CC);
 			php_output_handler_hook(PHP_OUTPUT_HANDLER_HOOK_IMMUTABLE, NULL TSRMLS_CC);
 		}

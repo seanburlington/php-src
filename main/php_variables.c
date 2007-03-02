fb9a12861f3eca486712fd61f328751c2b434ba1
stas <stas>
2007-03-02 22:03:05 +0000

    fix variable name

--- main/php_variables.c
+++ main/php_variables.c
@@ -17,7 +17,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: php_variables.c,v 1.104.2.10.2.5 2007/03/02 21:58:05 stas Exp $ */
+/* $Id: php_variables.c,v 1.104.2.10.2.6 2007/03/02 22:03:05 stas Exp $ */
 
 #include <stdio.h>
 #include "php.h"
@@ -124,7 +124,7 @@ PHPAPI void php_register_variable_ex(char *var, zval *val, zval *track_vars_arra
 			char *index_s;
 			int new_idx_len = 0;
 
-			if(++nest_level > PG(max_nesting_level)) {
+			if(++nest_level > PG(max_input_nesting_level)) {
 				/* too many levels of nesting */
 				php_error_docref(NULL TSRMLS_CC, E_ERROR, "Input variable nesting level more than allowed %d (change max_input_nesting_level in php.ini to increase the limit)", PG(max_input_nesting_level));
 			}

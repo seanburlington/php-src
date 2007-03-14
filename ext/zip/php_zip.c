178a0c9b631c351b299dc52bc5c354dd3c4826f3
pajoye <pajoye>
2007-03-14 11:25:20 +0000

    - MFB: free buffer when zip_entry_read returns an empty entry

--- ext/zip/php_zip.c
+++ ext/zip/php_zip.c
@@ -16,7 +16,7 @@
   +----------------------------------------------------------------------+
 */
 
-/* $Id: php_zip.c,v 1.39 2007/03/14 11:22:13 pajoye Exp $ */
+/* $Id: php_zip.c,v 1.40 2007/03/14 11:25:20 pajoye Exp $ */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -788,6 +788,7 @@ static PHP_FUNCTION(zip_entry_read)
 			buffer[n] = 0;
 			RETURN_STRINGL(buffer, n, 0);
 		} else {
+			efree(buffer);
 			RETURN_EMPTY_STRING()
 		}
 	} else {
@@ -2101,7 +2102,7 @@ static PHP_MINFO_FUNCTION(zip)
 	php_info_print_table_start();
 
 	php_info_print_table_row(2, "Zip", "enabled");
-	php_info_print_table_row(2, "Extension Version","$Id: php_zip.c,v 1.39 2007/03/14 11:22:13 pajoye Exp $");
+	php_info_print_table_row(2, "Extension Version","$Id: php_zip.c,v 1.40 2007/03/14 11:25:20 pajoye Exp $");
 	php_info_print_table_row(2, "Zip version", "2.0.0");
 	php_info_print_table_row(2, "Libzip version", "0.7.1");
 

96f73a28f6fe56345b3ec5fed05ba3eb5c1103f6
helly <helly>
2007-03-24 16:28:45 +0000

    - Drop double variable declaration

--- ext/spl/spl_array.c
+++ ext/spl/spl_array.c
@@ -16,7 +16,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: spl_array.c,v 1.122 2007/03/20 20:21:39 helly Exp $ */
+/* $Id: spl_array.c,v 1.123 2007/03/24 16:28:45 helly Exp $ */
 
 #ifdef HAVE_CONFIG_H
 # include "config.h"
@@ -497,7 +497,6 @@ static int spl_array_has_dimension_ex(int check_inherited, zval *object, zval *o
 			index = Z_LVAL_P(offset);
 		}
 		if (check_empty) {
-			zval **tmp;
 			HashTable *ht = spl_array_get_hash_table(intern, 0 TSRMLS_CC);
 			if (zend_hash_index_find(ht, index, (void **)&tmp) != FAILURE && zend_is_true(*tmp)) {
 				return 1;

f92bec85ab37469051581f8451d19649534e028e
tony2001 <tony2001>
2007-04-05 09:32:58 +0000

    fix build without FreeType

--- ext/gd/gd.c
+++ ext/gd/gd.c
@@ -18,7 +18,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: gd.c,v 1.368 2007/04/04 01:54:34 pajoye Exp $ */
+/* $Id: gd.c,v 1.369 2007/04/05 09:32:58 tony2001 Exp $ */
 
 /* gd 1.2 is copyright 1994, 1995, Quest Protein Database Center,
    Cold Spring Harbor Labs. */
@@ -1084,7 +1084,7 @@ zend_module_entry gd_module_entry = {
 	NULL,
 #endif
 	NULL,
-#if HAVE_GD_STRINGFT && (HAVE_GD_FONTCACHESHUTDOWN || HAVE_GD_FREEFONTCACHE)
+#if HAVE_GD_STRINGFT && (HAVE_LIBFREETYPE && (HAVE_GD_FONTCACHESHUTDOWN || HAVE_GD_FREEFONTCACHE))
 	PHP_RSHUTDOWN(gd),
 #else
 	NULL,
@@ -1134,7 +1134,7 @@ PHP_MSHUTDOWN_FUNCTION(gd)
 #if HAVE_LIBT1
 	T1_CloseLib();
 #endif
-#if HAVE_GD_FONTMUTEX
+#if HAVE_GD_FONTMUTEX && HAVE_LIBFREETYPE
 	gdFontCacheMutexShutdown();
 #endif
 	return SUCCESS;
@@ -1149,7 +1149,7 @@ PHP_MINIT_FUNCTION(gd)
 {
 	le_gd = zend_register_list_destructors_ex(php_free_gd_image, NULL, "gd", module_number);
 	le_gd_font = zend_register_list_destructors_ex(php_free_gd_font, NULL, "gd font", module_number);
-#if HAVE_GD_FONTMUTEX
+#if HAVE_GD_FONTMUTEX && HAVE_LIBFREETYPE
 	gdFontCacheMutexSetup();
 #endif
 #if HAVE_LIBT1
@@ -1234,7 +1234,7 @@ PHP_MINIT_FUNCTION(gd)
 
 /* {{{ PHP_RSHUTDOWN_FUNCTION
  */
-#if HAVE_GD_STRINGFT && (HAVE_GD_FONTCACHESHUTDOWN || HAVE_GD_FREEFONTCACHE)
+#if HAVE_GD_STRINGFT && (HAVE_LIBFREETYPE && (HAVE_GD_FONTCACHESHUTDOWN || HAVE_GD_FREEFONTCACHE))
 PHP_RSHUTDOWN_FUNCTION(gd)
 {
 #if HAVE_GD_FONTCACHESHUTDOWN

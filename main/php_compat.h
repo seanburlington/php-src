160aa6c541f766b6948ab70e333eda49156efd46
nlopess <nlopess>
2007-05-07 15:43:21 +0000

    update pcre symbols list

--- main/php_compat.h
+++ main/php_compat.h
@@ -16,7 +16,7 @@
   +----------------------------------------------------------------------+
 */
 
-/* $Id: php_compat.h,v 1.32 2007/02/25 18:48:19 nlopess Exp $ */
+/* $Id: php_compat.h,v 1.33 2007/05/07 15:43:21 nlopess Exp $ */
 
 #ifndef PHP_COMPAT_H
 #define PHP_COMPAT_H
@@ -56,8 +56,7 @@
 #define _pcre_xclass	 		php__pcre_xclass
 #define pcre_callout			php_pcre_callout
 #define _pcre_OP_lengths		php__pcre_OP_lengths
-/* this one doesn't work because pcre.h isn't included from the pcre_chartables.c file
-#define _pcre_default_tables		php__pcre_default_tables */
+#define _pcre_default_tables		php__pcre_default_tables
 #define pcre_get_stringtable_entries	php_pcre_get_stringtable_entries
 #define _pcre_is_newline		php__pcre_is_newline
 #define pcre_stack_free			php_pcre_stack_free

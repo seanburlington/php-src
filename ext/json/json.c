f32a46a443d10afccf5264a37c3fe14fcc08d5db
iliaa <iliaa>
2007-06-13 17:10:06 +0000

    MFB: Fixed bug #41673 (json_encode breaks large numbers in arrays).

--- ext/json/json.c
+++ ext/json/json.c
@@ -16,7 +16,7 @@
   +----------------------------------------------------------------------+
 */
 
-/* $Id: json.c,v 1.26 2007/06/04 23:52:34 iliaa Exp $ */
+/* $Id: json.c,v 1.27 2007/06/13 17:10:06 iliaa Exp $ */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -363,15 +363,9 @@ static void json_encode_r(smart_str *buf, zval *val TSRMLS_DC) /* {{{ */
                 double dbl = Z_DVAL_P(val);
 
                 if (!zend_isinf(dbl) && !zend_isnan(dbl)) {
-		    len = spprintf(&d, 0, "%.*g", (int) EG(precision), dbl);
-                    if (d) {
-                        if (dbl > LONG_MAX && !memchr(d, '.', len)) {
-                            smart_str_append_unsigned(buf, (unsigned long)Z_DVAL_P(val));
-                        } else {
-                            smart_str_appendl(buf, d, len);
-                        }
+			len = spprintf(&d, 0, "%.*g", (int) EG(precision), dbl);
+			smart_str_appendl(buf, d, len);
                         efree(d);
-                    }
                 } else {
                     zend_error(E_WARNING, "[json] (json_encode_r) double %.9g does not conform to the JSON spec, encoded as 0.", dbl);
                     smart_str_appendc(buf, '0');

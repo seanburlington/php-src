b8f4d766806435b8158ccb88d08242998fa7a1cd
pajoye <pajoye>
2007-06-17 10:39:18 +0000

    - MFB: #41321, read error downgraded to E_NOTICE

--- ext/standard/image.c
+++ ext/standard/image.c
@@ -17,7 +17,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: image.c,v 1.129 2007/02/24 16:25:55 helly Exp $ */
+/* $Id: image.c,v 1.130 2007/06/17 10:39:18 pajoye Exp $ */
 
 #include "php.h"
 #include <stdio.h>
@@ -1215,7 +1215,7 @@ PHPAPI int php_getimagetype(php_stream * stream, char *filetype TSRMLS_DC)
 
 	if ( !filetype) filetype = tmp;
 	if((php_stream_read(stream, filetype, 3)) != 3) {
-		php_error_docref(NULL TSRMLS_CC, E_WARNING, "Read error!");
+		php_error_docref(NULL TSRMLS_CC, E_NOTICE, "Read error!");
 		return IMAGE_FILETYPE_UNKNOWN;
 	}
 

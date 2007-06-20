4fc10f555a6a4ffd599fa133e47ea94ebab36459
pajoye <pajoye>
2007-06-20 05:27:38 +0000

    - MFB:  Fixed a memory leak inside load_all_certs_file()

--- ext/openssl/openssl.c
+++ ext/openssl/openssl.c
@@ -20,7 +20,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: openssl.c,v 1.140 2007/05/28 23:33:13 iliaa Exp $ */
+/* $Id: openssl.c,v 1.141 2007/06/20 05:27:38 pajoye Exp $ */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -1095,17 +1095,20 @@ static STACK_OF(X509) * load_all_certs_from_file(char *certfile)
 	}
 
 	if (php_check_open_basedir(certfile TSRMLS_CC)) {
+		sk_X509_free(stack);
 		goto end;
 	}
 
 	if(!(in=BIO_new_file(certfile, "r"))) {
 		php_error_docref(NULL TSRMLS_CC, E_WARNING, "error opening the file, %s", certfile);
+		sk_X509_free(stack);
 		goto end;
 	}
 
 	/* This loads from a file, a stack of x509/crl/pkey sets */
 	if(!(sk=PEM_X509_INFO_read_bio(in, NULL, NULL, NULL))) {
 		php_error_docref(NULL TSRMLS_CC, E_WARNING, "error reading the file, %s", certfile);
+		sk_X509_free(stack);
 		goto end;
 	}
 

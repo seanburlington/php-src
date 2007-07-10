d6b4021453cc5c9026e8c68a1c5b105d23880ac7
stas <stas>
2007-07-10 17:58:19 +0000

    suppress warnings to avoid potential loop

--- main/main.c
+++ main/main.c
@@ -18,7 +18,7 @@
    +----------------------------------------------------------------------+
 */
 
-/* $Id: main.c,v 1.640.2.23.2.40 2007/07/10 17:42:29 stas Exp $ */
+/* $Id: main.c,v 1.640.2.23.2.41 2007/07/10 17:58:19 stas Exp $ */
 
 /* {{{ includes
  */
@@ -356,11 +356,11 @@ PHPAPI void php_log_err(char *log_message TSRMLS_DC)
 			return;
 		}
 #endif
-		if (PG(safe_mode) && (!php_checkuid(PG(error_log), NULL, CHECKUID_CHECK_FILE_AND_DIR))) {
+		if (PG(safe_mode) && (!php_checkuid(PG(error_log), NULL, CHECKUID_CHECK_FILE_AND_DIR|CHECKUID_NO_ERRORS))) {
 			return FAILURE;
 		}
 
-		if (PG(open_basedir) && php_check_open_basedir(PG(error_log) TSRMLS_CC)) {
+		if (PG(open_basedir) && php_check_open_basedir_ex(PG(error_log), 0 TSRMLS_CC)) {
 			return FAILURE;
 		}
 		

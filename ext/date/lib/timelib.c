3320cb6640ef45393ac954e6becaf5134fd5c5f6
derick <derick>
2007-07-13 17:49:18 +0000

    - Let's always use the macro as llabs() requires c99.

--- ext/date/lib/timelib.c
+++ ext/date/lib/timelib.c
@@ -16,7 +16,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: timelib.c,v 1.15 2007/07/13 12:48:47 rrichards Exp $ */
+/* $Id: timelib.c,v 1.16 2007/07/13 17:49:18 derick Exp $ */
 
 #include "timelib.h"
 #include <ctype.h>
@@ -28,11 +28,7 @@
 		m = NULL;	\
 	}			\
 
-#if defined(_MSC_VER)
-	#define TIMELIB_LLABS(y) y < 0 ? (y * -1) : y
-#else
-	#define TIMELIB_LLABS(y) llabs(y)
-#endif
+#define TIMELIB_LLABS(y) (y < 0 ? (y * -1) : y)
 
 timelib_time* timelib_time_ctor()
 {

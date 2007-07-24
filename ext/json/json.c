b6d4ccf7dc7a90cde17e80f33a0d96a076f9b517
bjori <bjori>
2007-07-24 22:54:26 +0000

    Fixed bug#42090 (json_decode causes segmentation fault)

--- ext/json/json.c
+++ ext/json/json.c
@@ -16,7 +16,7 @@
   +----------------------------------------------------------------------+
 */
 
-/* $Id: json.c,v 1.27 2007/06/13 17:10:06 iliaa Exp $ */
+/* $Id: json.c,v 1.28 2007/07/24 22:54:26 bjori Exp $ */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -485,7 +485,7 @@ PHP_FUNCTION(json_decode)
 				RETURN_DOUBLE(d);
 			}
 		}
-		if (*str.s == '"' || str.s[str_len] == '"') {
+		if (str_len > 1 && *str.s == '"' && str.s[str_len-1] == '"') {
 			RETURN_STRINGL(str.s+1, str_len-2, 1);
 		} else {
 			RETURN_STRINGL(str.s, str_len, 1);
@@ -514,7 +514,7 @@ PHP_FUNCTION(json_decode)
 				RETURN_DOUBLE(d);
 			}
 		}
-		if (*str.u == 0x22 /*'"'*/ || str.u[str_len] == 0x22 /*'"'*/) {
+		if (str_len > 1 && *str.u == 0x22 /*'"'*/ && str.u[str_len-1] == 0x22 /*'"'*/) {
 			RETURN_UNICODEL(str.u+1, str_len-2, 1);
 		} else {
 			RETURN_UNICODEL(str.u, str_len, 1);

f1e5db1c330117cf3e0c98bae4708f58e67c418e
sixd <sixd>
2007-08-02 19:04:21 +0000

    Bug #42173 (INTERVAL and TIMESTAMP type fixes)

--- ext/oci8/oci8_statement.c
+++ ext/oci8/oci8_statement.c
@@ -25,7 +25,7 @@
    +----------------------------------------------------------------------+
 */
 
-/* $Id: oci8_statement.c,v 1.49 2007/07/31 19:19:39 tony2001 Exp $ */
+/* $Id: oci8_statement.c,v 1.50 2007/08/02 19:04:21 sixd Exp $ */
 
 
 #ifdef HAVE_CONFIG_H
@@ -642,6 +642,15 @@ int php_oci_statement_execute(php_oci_statement *statement, ub4 mode TSRMLS_DC)
 #endif
 #ifdef SQLT_TIMESTAMP_TZ
 						|| (outcol->data_type == SQLT_TIMESTAMP_TZ)
+#endif
+#ifdef SQLT_TIMESTAMP_LTZ
+						|| (outcol->data_type == SQLT_TIMESTAMP_LTZ)
+#endif
+#ifdef SQLT_INTERVAL_YM
+						|| (outcol->data_type == SQLT_INTERVAL_YM)
+#endif
+#ifdef SQLT_INTERVAL_DS
+						|| (outcol->data_type == SQLT_INTERVAL_DS)
 #endif
 						) {
 						outcol->storage_size4 = 512; /* XXX this should fit "most" NLS date-formats and Numbers */

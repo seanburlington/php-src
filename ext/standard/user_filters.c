9fc5a7425eafa1c05af248e470f9da433b27392c
pollita <pollita>
2007-08-04 07:51:15 +0000

    Bugfix#36492 Userfilters can leak buckets

--- ext/standard/user_filters.c
+++ ext/standard/user_filters.c
@@ -18,7 +18,7 @@
    +----------------------------------------------------------------------+
 */
 
-/* $Id: user_filters.c,v 1.48 2007/07/14 08:38:20 tony2001 Exp $ */
+/* $Id: user_filters.c,v 1.49 2007/08/04 07:51:15 pollita Exp $ */
 
 #include "php.h"
 #include "php_globals.h"
@@ -232,8 +232,21 @@ php_stream_filter_status_t userfilter_filter( php_stream *stream, php_stream_fil
 		php_error_docref(NULL TSRMLS_CC, E_WARNING, "failed to call filter function");
 	}
 
-	if (retval)
+	if (retval) {
 		zval_ptr_dtor(&retval);
+	}
+
+	if (buckets_in->head) {
+		php_stream_bucket *bucket = buckets_in->head;
+
+		php_error_docref(NULL TSRMLS_CC, E_WARNING, "Unprocessed filter buckets remaining on input brigade");
+		while ((bucket = buckets_in->head)) {
+			/* Remove unconsumed buckets from the brigade */
+			php_stream_bucket_unlink(bucket TSRMLS_CC);
+			php_stream_bucket_delref(bucket TSRMLS_CC);
+		}
+	}
+
 	zval_ptr_dtor(&zclosing);
 	zval_ptr_dtor(&zconsumed);
 	zval_ptr_dtor(&zout);

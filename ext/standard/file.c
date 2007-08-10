bfb0d4e151d0523c19097b0493484198a5aec7fe
tony2001 <tony2001>
2007-08-10 08:58:12 +0000

    MFB: Fixed bug #42243 (copy() does not ouput an error when the first arg is a dir)

--- ext/standard/file.c
+++ ext/standard/file.c
@@ -21,7 +21,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: file.c,v 1.495 2007/07/14 08:38:19 tony2001 Exp $ */
+/* $Id: file.c,v 1.496 2007/08/10 08:58:12 tony2001 Exp $ */
 
 /* Synced with php 3.0 revision 1.218 1999-06-16 [ssb] */
 
@@ -1902,6 +1902,10 @@ PHPAPI int php_copy_file_ex(char *src, char *dest, int src_chk TSRMLS_DC)
 	}
 	if (php_stream_stat_path_ex(dest, PHP_STREAM_URL_STAT_QUIET, &dest_s, NULL) != 0) {
 		goto safe_to_copy;
+	}
+	if (S_ISDIR(src_s.sb.st_mode)) {
+		php_error_docref(NULL TSRMLS_CC, E_WARNING, "The first argument to copy() function cannot be a directory");
+		return FAILURE;
 	}
 	if (!src_s.sb.st_ino || !dest_s.sb.st_ino) {
 		goto no_stat;

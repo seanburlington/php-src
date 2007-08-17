96290137328f887ffd32500346777d19fd661349
jani <jani>
2007-08-17 10:42:12 +0000

    - Fixed bug #41904 (proc_open with empty env array)

--- ext/standard/proc_open.c
+++ ext/standard/proc_open.c
@@ -15,7 +15,7 @@
    | Author: Wez Furlong <wez@thebrainroom.com>                           |
    +----------------------------------------------------------------------+
  */
-/* $Id: proc_open.c,v 1.57 2007/05/28 23:00:25 iliaa Exp $ */
+/* $Id: proc_open.c,v 1.58 2007/08/17 10:42:12 jani Exp $ */
 
 #if 0 && (defined(__linux__) || defined(sun) || defined(__IRIX__))
 # define _BSD_SOURCE 		/* linux wants this when XOPEN mode is on */
@@ -95,6 +95,10 @@ static php_process_env_t _php_array_to_envp(zval *environment, int is_persistent
 	cnt = zend_hash_num_elements(Z_ARRVAL_P(environment));
 	
 	if (cnt < 1) {
+#ifndef PHP_WIN32
+		env.envarray = (char **) pecalloc(1, sizeof(char *), is_persistent);
+#endif
+		env.envp = (char *) pecalloc(4, 1, is_persistent);
 		return env;
 	}
 

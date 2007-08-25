554a94d2c525bc9a55c8c5a338afd405ccf36751
cellog <cellog>
2007-08-25 01:33:46 +0000

    add missing read-only check for Phar::setAlias

--- ext/phar/phar_object.c
+++ ext/phar/phar_object.c
@@ -17,7 +17,7 @@
   +----------------------------------------------------------------------+
 */
 
-/* $Id: phar_object.c,v 1.61 2007/08/25 01:32:34 cellog Exp $ */
+/* $Id: phar_object.c,v 1.60.2.1 2007/08/25 01:33:46 cellog Exp $ */
 
 #include "phar_internal.h"
 
@@ -286,51 +286,6 @@ PHP_METHOD(Phar, count)
 	RETURN_LONG(zend_hash_num_elements(&phar_obj->arc.archive->manifest));
 }
 /* }}} */
-
-/* {{{ proto bool Phar::delete(string file)
- * Delete a file from within the Phar
- */
-PHP_METHOD(Phar, delete)
-{
-	char *fname;
-	int fname_len;
-	char *error;
-	phar_entry_info *entry;
-	PHAR_ARCHIVE_OBJECT();
-
-	if (PHAR_G(readonly)) {
-		zend_throw_exception_ex(spl_ce_UnexpectedValueException, 0 TSRMLS_CC,
-			"Cannot write out phar archive, phar is read-only");
-	}
-
-	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "s", &fname, &fname_len) == FAILURE) {
-		RETURN_FALSE;
-	}
-
-	if (zend_hash_exists(&phar_obj->arc.archive->manifest, fname, (uint) fname_len)) {
-		if (SUCCESS == zend_hash_find(&phar_obj->arc.archive->manifest, fname, (uint) fname_len, (void**)&entry)) {
-			if (entry->is_deleted) {
-				/* entry is deleted, but has not been flushed to disk yet */
-				RETURN_TRUE;
-			} else {
-				entry->is_deleted = 1;
-				entry->is_modified = 1;
-				phar_obj->arc.archive->is_modified = 1;
-			}
-		}
-	} else {
-		zend_throw_exception_ex(spl_ce_BadMethodCallException, 0 TSRMLS_CC, "Entry %s does not exist and cannot be deleted", fname);
-		RETURN_FALSE;
-	}
-
-	phar_flush(phar_obj->arc.archive, NULL, 0, &error TSRMLS_CC);
-	if (error) {
-		zend_throw_exception_ex(phar_ce_PharException, 0 TSRMLS_CC, error);
-		efree(error);
-	}
-		
-	RETURN_TRUE;
-}
 
 /* {{{ proto int Phar::getAlias()
  * Returns the alias for the PHAR or NULL
@@ -1501,7 +1456,6 @@ zend_function_entry php_archive_methods[] = {
 	PHP_ME(Phar, compressAllFilesGZ,    NULL,                      ZEND_ACC_PUBLIC)
 	PHP_ME(Phar, compressAllFilesBZIP2, NULL,                      ZEND_ACC_PUBLIC)
 	PHP_ME(Phar, count,                 NULL,                      ZEND_ACC_PUBLIC)
-	PHP_ME(Phar, delete,                arginfo_phar_mapPhar,      ZEND_ACC_PUBLIC)
 	PHP_ME(Phar, delMetadata,           NULL,                      ZEND_ACC_PUBLIC)
 	PHP_ME(Phar, getAlias,              NULL,                      ZEND_ACC_PUBLIC)
 	PHP_ME(Phar, getMetadata,           NULL,                      ZEND_ACC_PUBLIC)

abd3a94edb7510cb8341eda60c8fd1689d243af4
cellog <cellog>
2007-08-25 01:32:34 +0000

    move to 1.3.0 development add Phar::delete()

--- ext/phar/phar_internal.h
+++ ext/phar/phar_internal.h
@@ -17,7 +17,7 @@
   +----------------------------------------------------------------------+
 */
 
-/* $Id: phar_internal.h,v 1.36 2007/08/17 04:47:49 cellog Exp $ */
+/* $Id: phar_internal.h,v 1.37 2007/08/25 01:32:34 cellog Exp $ */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -62,7 +62,7 @@
 #define E_RECOVERABLE_ERROR E_ERROR
 #endif
 
-#define PHAR_EXT_VERSION_STR      "1.2.1"
+#define PHAR_EXT_VERSION_STR      "1.3.0"
 #define PHAR_API_VERSION_STR      "1.1.0"
 /* x.y.z maps to 0xyz0 */
 #define PHAR_API_VERSION          0x1100

fc23badc1d53eaa03f1ec3b75a616ee84b35440f
rrichards <rrichards>
2007-08-30 16:31:57 +0000

    fix bug #42462 (Segmentation when trying to set an attribute in DOMElement)

--- ext/dom/php_dom.c
+++ ext/dom/php_dom.c
@@ -18,7 +18,7 @@
    +----------------------------------------------------------------------+
 */
 
-/* $Id: php_dom.c,v 1.104 2007/08/06 16:21:44 rrichards Exp $ */
+/* $Id: php_dom.c,v 1.105 2007/08/30 16:31:57 rrichards Exp $ */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -909,7 +909,6 @@ void node_list_unlink(xmlNodePtr node TSRMLS_DC)
 		} else {
 			if (node->type == XML_ENTITY_REF_NODE)
 				break;
-
 			node_list_unlink(node->children TSRMLS_CC);
 
 			switch (node->type) {
@@ -918,6 +917,7 @@ void node_list_unlink(xmlNodePtr node TSRMLS_DC)
 				case XML_DOCUMENT_TYPE_NODE:
 				case XML_ENTITY_DECL:
 				case XML_ATTRIBUTE_NODE:
+				case XML_TEXT_NODE:
 					break;
 				default:
 					node_list_unlink((xmlNodePtr) node->properties TSRMLS_CC);

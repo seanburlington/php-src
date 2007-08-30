02bc78c7d4b52d23901bf13ec3c7e20e5d0c0664
cellog <cellog>
2007-08-30 03:30:23 +0000

    oops, still 1.2.1 (bad merge)

--- ext/phar/phar_internal.h
+++ ext/phar/phar_internal.h
@@ -17,7 +17,7 @@
   +----------------------------------------------------------------------+
 */
 
-/* $Id: phar_internal.h,v 1.36.2.1 2007/08/30 03:20:06 cellog Exp $ */
+/* $Id: phar_internal.h,v 1.36.2.2 2007/08/30 03:30:23 cellog Exp $ */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -62,7 +62,7 @@
 #define E_RECOVERABLE_ERROR E_ERROR
 #endif
 
-#define PHAR_EXT_VERSION_STR      "1.3.0"
+#define PHAR_EXT_VERSION_STR      "1.2.1"
 #define PHAR_API_VERSION_STR      "1.1.0"
 /* x.y.z maps to 0xyz0 */
 #define PHAR_API_VERSION          0x1100

238b6fc259b0304d76e0dff4a43c35aa2f2a297f
jani <jani>
2007-08-31 07:43:28 +0000

    ws

--- main/SAPI.h
+++ main/SAPI.h
@@ -16,7 +16,7 @@
    +----------------------------------------------------------------------+
 */
 
-/* $Id: SAPI.h,v 1.119 2007/01/01 09:29:35 sebastian Exp $ */
+/* $Id: SAPI.h,v 1.120 2007/08/31 07:43:28 jani Exp $ */
 
 #ifndef SAPI_H
 #define SAPI_H
@@ -107,9 +107,9 @@ typedef struct {
 	char *current_user;
 	int current_user_length;
 
-    /* this is necessary for CLI module */
-    int argc;
-    char **argv;
+	/* this is necessary for CLI module */
+	int argc;
+	char **argv;
 	int proto_num;
 } sapi_request_info;
 

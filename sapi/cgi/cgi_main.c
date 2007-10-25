5e3b96443bf7b4d528023c40292fdc4d64306601
jani <jani>
2007-10-25 10:01:25 +0000

    ws

--- sapi/cgi/cgi_main.c
+++ sapi/cgi/cgi_main.c
@@ -21,7 +21,7 @@
    +----------------------------------------------------------------------+
 */
 
-/* $Id: cgi_main.c,v 1.341 2007/10/25 05:39:24 dmitry Exp $ */
+/* $Id: cgi_main.c,v 1.342 2007/10/25 10:01:25 jani Exp $ */
 
 #include "php.h"
 #include "php_globals.h"
@@ -769,7 +769,7 @@ static void php_cgi_usage(char *argv0)
 				"  -v               Version number\n"
 				"  -w               Display source with stripped comments and whitespace.\n"
 				"  -z <file>        Load Zend extension <file>.\n"
-			   "  -T <count>       Measure execution time of script repeated <count> times.\n",
+				"  -T <count>       Measure execution time of script repeated <count> times.\n",
 				prog, prog);
 }
 /* }}} */
@@ -1549,7 +1549,7 @@ consult the installation file that came with this distribution, or visit \n\
 		while ((c = php_getopt(argc, argv, OPTIONS, &php_optarg, &php_optind, 1, 2)) != -1) {
 			switch (c) {
 				case 'T':
-			        benchmark = 1;
+					benchmark = 1;
 					repeats = atoi(php_optarg);
 					gettimeofday(&start, NULL);
 					break;

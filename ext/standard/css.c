7d56540fe8f554a76085ab947e5b34f21a580a93
jani <jani>
2007-11-05 12:39:44 +0000

    MFH

--- ext/standard/css.c
+++ ext/standard/css.c
@@ -2,7 +2,7 @@
    +----------------------------------------------------------------------+
    | PHP Version 5                                                        |
    +----------------------------------------------------------------------+
-   | Copyright (c) 1997-2009 The PHP Group                                |
+   | Copyright (c) 1997-2007 The PHP Group                                |
    +----------------------------------------------------------------------+
    | This source file is subject to version 3.01 of the PHP license,      |
    | that is bundled with this package in the file LICENSE, and is        |
@@ -16,15 +16,12 @@
    +----------------------------------------------------------------------+
 */
 
-/* $Id: css.c,v 1.13.2.1.2.3 2008/12/31 11:17:44 sebastian Exp $ */
+/* $Id: css.c,v 1.13.2.1.2.1.2.1 2007/11/05 12:40:40 jani Exp $ */
 
 #include "php.h"
 #include "info.h"
 
-
-/* {{{ php_info_print_css
- */
-PHPAPI void php_info_print_css(TSRMLS_D)
+PHPAPI void php_info_print_css(TSRMLS_D) /* {{{ */
 {
 	PUTS("body {background-color: #ffffff; color: #000000;}\n");
 	PUTS("body, td, th, h1, h2 {font-family: sans-serif;}\n");

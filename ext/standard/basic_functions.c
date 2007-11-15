e48e75a44e36ecfe2428ce619cfba3761a796dc6
bjori <bjori>
2007-11-15 13:13:21 +0000

    MFH: Allow numeric options & tests

--- ext/standard/basic_functions.c
+++ ext/standard/basic_functions.c
@@ -17,7 +17,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: basic_functions.c,v 1.725.2.31.2.64.2.11 2007/11/14 14:42:25 bjori Exp $ */
+/* $Id: basic_functions.c,v 1.725.2.31.2.64.2.12 2007/11/15 13:13:21 bjori Exp $ */
 
 #include "php.h"
 #include "php_streams.h"
@@ -4506,7 +4506,8 @@ static int parse_opts(char * opts, opt_struct ** result)
 	int i, count = 0;
 
 	for (i = 0; i < strlen(opts); i++) {
-		if ((opts[i] >= 65 && opts[i] <= 90) ||
+		if ((opts[i] >= 48 && opts[i] <= 57) ||
+			(opts[i] >= 65 && opts[i] <= 90) ||
 			(opts[i] >= 97 && opts[i] <= 122)
 		) {
 			count++;
@@ -4516,8 +4517,9 @@ static int parse_opts(char * opts, opt_struct ** result)
 	paras = safe_emalloc(sizeof(opt_struct), count, 0);
 	memset(paras, 0, sizeof(opt_struct) * count);
 	*result = paras;
-	while (	(*opts >= 65 && *opts <= 90) ||
-			(*opts >= 97 && *opts <= 122)
+	while ( (*opts >= 48 && *opts <= 57) || /* 0 - 9 */
+			(*opts >= 65 && *opts <= 90) || /* A - Z */
+			(*opts >= 97 && *opts <= 122)   /* a - z */
 	) {
 		paras->opt_char = *opts;
 		paras->need_param = (*(++opts) == ':') ? 1 : 0;

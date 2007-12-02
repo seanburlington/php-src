c01fae07db548036c50afa3fb58e110cbe959724
iliaa <iliaa>
2007-12-02 20:59:08 +0000

    MFB: Fixed bug #43457 (Prepared statement with incorrect parms doens't throw exception with pdo_pgsql driver)

--- ext/pdo_pgsql/pgsql_statement.c
+++ ext/pdo_pgsql/pgsql_statement.c
@@ -16,7 +16,7 @@
   +----------------------------------------------------------------------+
 */
 
-/* $Id: pgsql_statement.c,v 1.44 2007/05/28 23:33:13 iliaa Exp $ */
+/* $Id: pgsql_statement.c,v 1.45 2007/12/02 20:59:09 iliaa Exp $ */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -175,7 +175,7 @@ static int pgsql_stmt_param_hook(pdo_stmt_t *stmt, struct pdo_bound_param_data *
 								param->name, param->namelen + 1, (void**)&nameptr)) {
 							param->paramno = atoi(nameptr + 1) - 1;
 						} else {
-							pdo_pgsql_error_stmt(stmt, PGRES_FATAL_ERROR, "HY093");
+							pdo_raise_impl_error(stmt->dbh, stmt, "HY093", param->name TSRMLS_CC);
 							return 0;
 						}
 					}

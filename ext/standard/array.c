a48ed976f5ed4f1af6a93fc57cf8c349634f810f
iliaa <iliaa>
2007-12-03 14:11:08 +0000

    Fixed bug #43482 (array_pad() does not warn on very small pad numbers).

--- ext/standard/array.c
+++ ext/standard/array.c
@@ -21,7 +21,7 @@
    +----------------------------------------------------------------------+
 */
 
-/* $Id: array.c,v 1.308.2.21.2.43 2007/11/25 15:56:47 iliaa Exp $ */
+/* $Id: array.c,v 1.308.2.21.2.44 2007/12/03 14:11:08 iliaa Exp $ */
 
 #include "php.h"
 #include "php_ini.h"
@@ -2655,6 +2655,11 @@ PHP_FUNCTION(array_pad)
 	/* Do some initial calculations */
 	input_size = zend_hash_num_elements(Z_ARRVAL_PP(input));
 	pad_size_abs = abs(Z_LVAL_PP(pad_size));
+	if (pad_size_abs < 0) {
+		php_error_docref(NULL TSRMLS_CC, E_WARNING, "You may only pad up to 1048576 elements at a time");
+		zval_dtor(return_value);
+		RETURN_FALSE;
+	}
 	do_pad = (input_size >= pad_size_abs) ? 0 : 1;
 	
 	/* Copy the original array */

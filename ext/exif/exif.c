3f5e815ef84f4f4f8ec3e835830323e963f0d406
iliaa <iliaa>
2007-12-16 17:15:05 +0000

    MFB: Fixed bug #43606 (define missing depencies of the exif extension)

--- ext/exif/exif.c
+++ ext/exif/exif.c
@@ -17,7 +17,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: exif.c,v 1.196 2007/09/27 18:28:39 dmitry Exp $ */
+/* $Id: exif.c,v 1.197 2007/12/16 17:15:05 iliaa Exp $ */
 
 /*  ToDos
  *
@@ -142,7 +142,7 @@ const zend_function_entry exif_functions[] = {
 };
 /* }}} */
 
-#define EXIF_VERSION "1.4 $Id: exif.c,v 1.196 2007/09/27 18:28:39 dmitry Exp $"
+#define EXIF_VERSION "1.4 $Id: exif.c,v 1.197 2007/12/16 17:15:05 iliaa Exp $"
 
 /* {{{ PHP_MINFO_FUNCTION
  */
@@ -241,12 +241,21 @@ PHP_MSHUTDOWN_FUNCTION(exif)
 }
 /* }}} */
 
+/* {{{ exif dependencies */
+static const zend_module_dep exif_module_deps[] = {
+	ZEND_MOD_REQUIRED("standard")
+	#if EXIF_USE_MBSTRING
+	ZEND_MOD_REQUIRED("mbstring")
+	#endif
+	{NULL, NULL, NULL}
+};
+/* }}} */
+
 /* {{{ exif_module_entry
  */
 zend_module_entry exif_module_entry = {
-#if ZEND_MODULE_API_NO >= 20010901
-	STANDARD_MODULE_HEADER,
-#endif
+	STANDARD_MODULE_HEADER_EX, NULL,
+	exif_module_deps,
 	"exif",
 	exif_functions,
 	PHP_MINIT(exif), 

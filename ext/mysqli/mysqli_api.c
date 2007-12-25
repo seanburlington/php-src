dcabd7bcf3bcc12ebb83f35bbfde5052dc689444
hholzgra <hholzgra>
2007-12-25 18:54:29 +0000

    MFH: Fix for bug #42548 "PROCEDURE xxx can't return a result set"

--- ext/mysqli/mysqli_api.c
+++ ext/mysqli/mysqli_api.c
@@ -17,7 +17,7 @@
   |          Ulf Wendel <uw@php.net>                                     |
   +----------------------------------------------------------------------+
 
-  $Id: mysqli_api.c,v 1.118.2.22.2.16.2.8 2007/11/09 10:56:28 andrey Exp $ 
+  $Id: mysqli_api.c,v 1.118.2.22.2.16.2.9 2007/12/25 18:55:40 hholzgra Exp $ 
 */
 
 #ifdef HAVE_CONFIG_H
@@ -1621,6 +1621,8 @@ PHP_FUNCTION(mysqli_real_connect)
 
 	MYSQLI_FETCH_RESOURCE(mysql, MY_MYSQL *, &mysql_link, "mysqli_link", MYSQLI_STATUS_INITIALIZED);
 
+	/* set some required options */
+	flags |= CLIENT_MULTI_RESULTS; /* needed for mysql_multi_query() */
 	/* remove some insecure options */
 	flags &= ~CLIENT_MULTI_STATEMENTS;   /* don't allow multi_queries via connect parameter */
 	if (PG(open_basedir) && PG(open_basedir)[0] != '\0') {

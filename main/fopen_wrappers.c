a8d02a9666d6e848b04a57a0acc3adcb3ecdd7d0
bjori <bjori>
2007-12-27 02:10:26 +0000

    MFB5.3: Fixed bug#43105 (PHP seems to fail to close open files.)

--- main/fopen_wrappers.c
+++ main/fopen_wrappers.c
@@ -17,7 +17,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: fopen_wrappers.c,v 1.175.2.3.2.16 2007/10/09 10:06:34 scottmac Exp $ */
+/* $Id: fopen_wrappers.c,v 1.175.2.3.2.17 2007/12/27 02:10:26 bjori Exp $ */
 
 /* {{{ includes
  */
@@ -620,6 +620,7 @@ PHPAPI char *expand_filepath(const char *filepath, char *real_path TSRMLS_DC)
 				 * relatively referenced file is accessible */
 				copy_len = strlen(filepath) > MAXPATHLEN - 1 ? MAXPATHLEN - 1 : strlen(filepath);
 				real_path = estrndup(filepath, copy_len);
+				close(fdtest);
 				return real_path;
 			} else {
 				cwd[0] = '\0';

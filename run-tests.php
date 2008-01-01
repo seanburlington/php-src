efc37db0c20b65bf00e904118ac4570162057daa
sebastian <sebastian>
2008-01-01 06:15:48 +0000

    MFB: Leftover: Bump copyright year.

--- run-tests.php
+++ run-tests.php
@@ -4,7 +4,7 @@
    +----------------------------------------------------------------------+
    | PHP Version 5                                                        |
    +----------------------------------------------------------------------+
-   | Copyright (c) 1997-2007 The PHP Group                                |
+   | Copyright (c) 1997-2008 The PHP Group                                |
    +----------------------------------------------------------------------+
    | This source file is subject to version 3.01 of the PHP license,      |
    | that is bundled with this package in the file LICENSE, and is        |
@@ -24,7 +24,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: run-tests.php,v 1.226.2.37.2.40 2007/12/25 12:06:22 dmitry Exp $ */
+/* $Id: run-tests.php,v 1.226.2.37.2.41 2008/01/01 06:15:48 sebastian Exp $ */
 
 /* Sanity check to ensure that pcre extension needed by this script is available.
  * In the event it is not, print a nice error message indicating that this script will
@@ -403,7 +403,7 @@ if (isset($argc) && $argc > 1) {
 					$html_output = is_resource($html_file);
 					break;
 				case '--version':
-					echo '$Revision: 1.226.2.37.2.40 $'."\n";
+					echo '$Revision: 1.226.2.37.2.41 $'."\n";
 					exit(1);
 
 				case 'u':

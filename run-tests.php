84c26b4b4602e39405ad8f4447701621020dd5d9
cellog <cellog>
2008-01-05 21:39:47 +0000

    remove potential preg warnings if --EXPECTHEADERS-- is combined with --EXPECTF-- or --EXPECTRE--

--- run-tests.php
+++ run-tests.php
@@ -24,7 +24,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: run-tests.php,v 1.226.2.37.2.42 2008/01/05 21:05:14 cellog Exp $ */
+/* $Id: run-tests.php,v 1.226.2.37.2.43 2008/01/05 21:39:47 cellog Exp $ */
 
 /* Sanity check to ensure that pcre extension needed by this script is available.
  * In the event it is not, print a nice error message indicating that this script will
@@ -403,7 +403,7 @@ if (isset($argc) && $argc > 1) {
 					$html_output = is_resource($html_file);
 					break;
 				case '--version':
-					echo '$Revision: 1.226.2.37.2.42 $'."\n";
+					echo '$Revision: 1.226.2.37.2.43 $'."\n";
 					exit(1);
 
 				case 'u':
@@ -1579,7 +1579,7 @@ COMMAND $cmd
 		$wanted = $wanted_headers . "\n--HEADERS--\n" . $wanted;
 		$output = $output_headers . "\n--HEADERS--\n" . $output;
 		if (isset($wanted_re)) {
-			$wanted_re = $wanted_headers . "\n--HEADERS--\n" . $wanted_re;
+			$wanted_re = preg_quote($wanted_headers . "\n--HEADERS--\n", '/') . $wanted_re;
 		}
 	}
 

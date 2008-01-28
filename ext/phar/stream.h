11992b8736617cd1ff59c1b99e41ce6378d61d84
cellog <cellog>
2008-01-28 08:52:06 +0000

    remove ext/zip dependency entirely, write better native zip support re-organize, create util.c, move entry_info/archive_data/entry_data access methods to this file refactor entry->fp, now this is abstracted with phar_get_efp() and phar_seek_efp(), fixes all weird dependency issues permanently solve the "millions of file pointers" issue for read access.  All compressed files are read into a single temporary stream, and their constraints are controlled by the entry->fp abstraction
    Improvements in this zip implementation over ext/zip:
     * full read/write support for bzip2 compressed files
     * much more efficient access for accessing only a few files within large zip files, as crc/header validation is
       done just-in-time
     * full stream support for opendir/rename/rmdir/mkdir as well as all of the other stream funcs
     * full support for setting file perms via Phar::chmod(), stored as zip-standard extra field
     * no problem with large zips and many open file pointers
    
    # TODO: add big-endian system support for tar/zip file format headers, otherwise the implementation is complete
    # TODO: test on windows and fix any windows-specific issues
    # TODO: verify zips created work with unzip/winzip/windows explorer and so on

--- ext/phar/stream.h
+++ ext/phar/stream.h
@@ -17,10 +17,10 @@
   +----------------------------------------------------------------------+
 */
 
-/* $Id: stream.h,v 1.2 2008/01/10 15:12:59 cellog Exp $ */
+/* $Id: stream.h,v 1.3 2008/01/28 08:52:06 cellog Exp $ */
 
 BEGIN_EXTERN_C()
-int phar_postprocess_file(php_stream_wrapper *wrapper, int options, phar_entry_data *idata, php_uint32 crc32 TSRMLS_DC);
+int phar_postprocess_file(php_stream_wrapper *wrapper, int options, phar_entry_data *idata, php_uint32 crc32, char **error TSRMLS_DC);
 
 php_url* phar_open_url(php_stream_wrapper *wrapper, char *filename, char *mode, int options TSRMLS_DC);
 void phar_entry_remove(phar_entry_data *idata, char **error TSRMLS_DC);

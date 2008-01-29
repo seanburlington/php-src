f3c0adbe98c35b45980ab953cdbeaf565273a87e
andrey <andrey>
2008-01-29 12:00:52 +0000

    Moved initialization into a separate function

--- ext/mysqlnd/mysqlnd_statistics.c
+++ ext/mysqlnd/mysqlnd_statistics.c
@@ -18,7 +18,7 @@
   +----------------------------------------------------------------------+
 */
 
-/* $Id: mysqlnd_statistics.c,v 1.3 2007/12/31 07:12:12 sebastian Exp $ */
+/* $Id: mysqlnd_statistics.c,v 1.4 2008/01/29 12:00:52 andrey Exp $ */
 #include "php.h"
 #include "mysqlnd.h"
 #include "mysqlnd_priv.h"
@@ -141,6 +141,32 @@ PHPAPI void _mysqlnd_get_client_stats(zval *return_value TSRMLS_DC ZEND_FILE_LIN
 	}
 	mysqlnd_fill_stats_hash(stats_ptr, return_value TSRMLS_CC ZEND_FILE_LINE_CC);
 	DBG_VOID_RETURN;
+}
+/* }}} */
+
+
+/* {{{ mysqlnd_stats_init */
+void
+mysqlnd_stats_init(MYSQLND_STATS ** stats)
+{
+	*stats = calloc(1, sizeof(MYSQLND_STATS));
+#ifdef ZTS
+	(*stats)->LOCK_access = tsrm_mutex_alloc();
+#endif
+
+}
+/* }}} */
+
+
+/* {{{ mysqlnd_stats_end */
+void
+mysqlnd_stats_end(MYSQLND_STATS * stats)
+{
+#ifdef ZTS
+	tsrm_mutex_free(stats->LOCK_access);
+#endif
+	/* mnd_free will reference LOCK_access and crash...*/
+	free(stats);
 }
 /* }}} */
 
--- ext/mysqlnd/mysqlnd_statistics.h
+++ ext/mysqlnd/mysqlnd_statistics.h
@@ -18,7 +18,7 @@
   +----------------------------------------------------------------------+
 */
 
-/* $Id: mysqlnd_statistics.h,v 1.6 2008/01/02 21:16:46 andrey Exp $ */
+/* $Id: mysqlnd_statistics.h,v 1.7 2008/01/29 12:00:52 andrey Exp $ */
 
 #ifndef MYSQLND_STATISTICS_H
 #define MYSQLND_STATISTICS_H
@@ -195,6 +195,10 @@ extern const MYSQLND_STRING mysqlnd_stats_values_names[];
 
 void mysqlnd_fill_stats_hash(const MYSQLND_STATS * const stats, zval *return_value
 							 TSRMLS_DC ZEND_FILE_LINE_DC);
+
+
+void mysqlnd_stats_init(MYSQLND_STATS ** stats);
+void mysqlnd_stats_end(MYSQLND_STATS * stats);
 
 #endif	/* MYSQLND_STATISTICS_H */
 

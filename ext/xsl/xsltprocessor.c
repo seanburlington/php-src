c06e6bd70e7898aebbbceb14261f2039f7870323
sebastian <sebastian>
2008-01-29 21:21:04 +0000

    - Fix arginfo and prototype.

--- ext/xsl/xsltprocessor.c
+++ ext/xsl/xsltprocessor.c
@@ -17,7 +17,7 @@
    +----------------------------------------------------------------------+
 */
 
-/* $Id: xsltprocessor.c,v 1.62 2008/01/29 07:51:40 sebastian Exp $ */
+/* $Id: xsltprocessor.c,v 1.63 2008/01/29 21:21:04 sebastian Exp $ */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -74,6 +74,7 @@ ZEND_END_ARG_INFO();
 
 static
 ZEND_BEGIN_ARG_INFO_EX(arginfo_xsl_xsltprocessor_register_php_functions, 0, 0, 0)
+	ZEND_ARG_INFO(0, restrict)
 ZEND_END_ARG_INFO();
 
 static
@@ -831,7 +832,7 @@ PHP_FUNCTION(xsl_xsltprocessor_remove_parameter)
 }
 /* }}} end xsl_xsltprocessor_remove_parameter */
 
-/* {{{ proto void xsl_xsltprocessor_register_php_functions() U
+/* {{{ proto void xsl_xsltprocessor_register_php_functions([mixed $restrict]); U
 */
 PHP_FUNCTION(xsl_xsltprocessor_register_php_functions)
 {

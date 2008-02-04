a81c3d869b0a5a0698d1e644863743e0fabca91f
helly <helly>
2008-02-04 16:47:06 +0000

    - Fix ArrayObject serializing - Fix test expectations

--- ext/spl/spl_array.c
+++ ext/spl/spl_array.c
@@ -16,7 +16,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: spl_array.c,v 1.136 2007/12/31 07:12:14 sebastian Exp $ */
+/* $Id: spl_array.c,v 1.137 2008/02/04 16:47:06 helly Exp $ */
 
 #ifdef HAVE_CONFIG_H
 # include "config.h"
@@ -1483,16 +1483,14 @@ SPL_METHOD(Array, unserialize)
 {
 	spl_array_object *intern = (spl_array_object*)zend_object_store_get_object(getThis() TSRMLS_CC);
 	
-	zstr buf;
-	unsigned int buf_len;
-	UChar *p, *s;
-	zend_uchar buf_type; 
-		
+	char *buf;
+	int buf_len;
+	const unsigned char *p, *s;
 	php_unserialize_data_t var_hash;
 	zval *pentry, *pmembers, *pflags = NULL;
 	long flags;
 	
-	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "t", &buf, &buf_len, &buf_type) == FAILURE) {
+	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "s", &buf, &buf_len) == FAILURE) {
 		return;
 	}
 
@@ -1501,8 +1499,7 @@ SPL_METHOD(Array, unserialize)
 		return;
 	}
 
-	s = p = (buf_type == IS_UNICODE ? buf.u : (UChar *)buf.s);
-
+	s = p = (const unsigned char*)buf;
 	PHP_VAR_UNSERIALIZE_INIT(var_hash);
 
 	if (*p!= 'x' || *++p != ':') {
@@ -1569,7 +1566,7 @@ SPL_METHOD(Array, unserialize)
 
 outexcept:
 	PHP_VAR_UNSERIALIZE_DESTROY(var_hash);
-	zend_throw_exception_ex(spl_ce_UnexpectedValueException, 0 TSRMLS_CC, "Error at offset %ld of %d bytes", (long)((char*)p - (long)(buf_type == IS_UNICODE ? buf.u : (UChar *)buf.s)), buf_len);
+	zend_throw_exception_ex(spl_ce_UnexpectedValueException, 0 TSRMLS_CC, "Error at offset %ld of %d bytes", (long)((char*)p - (long)buf), buf_len);
 	return;
 
 } /* }}} */

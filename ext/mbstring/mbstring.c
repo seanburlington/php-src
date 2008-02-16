2370c47fb38080c12ab9a1ad5342cd532a435645
hirokawa <hirokawa>
2008-02-16 08:49:40 +0000

    MFB

--- ext/mbstring/mbstring.c
+++ ext/mbstring/mbstring.c
@@ -17,7 +17,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: mbstring.c,v 1.142.2.47.2.21 2007/04/04 15:28:18 masugata Exp $ */
+/* $Id: mbstring.c,v 1.142.2.47.2.22 2008/02/16 08:50:07 hirokawa Exp $ */
 
 /*
  * PHP4 Multibyte String module "mbstring"
@@ -971,6 +971,8 @@ PHP_RINIT_FUNCTION(mbstring)
 	const struct mb_overload_def *p;
 
 	MBSTRG(current_language) = MBSTRG(language);
+	php_mb_nls_get_default_detect_order_list(MBSTRG(language), 
+        &MBSTRG(default_detect_order_list), &MBSTRG(default_detect_order_list_size));
 
 	if (MBSTRG(internal_encoding) == mbfl_no_encoding_invalid) {
 		char *default_enc = NULL;

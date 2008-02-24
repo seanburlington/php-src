2525435d187a5cc6f3009bf42d56d4092b98b4af
iliaa <iliaa>
2008-02-24 16:09:13 +0000

    MFB: Fixed bug #44200 (A crash in PDO when no bound targets exists and yet bound parameters are present)

--- ext/pdo/pdo_stmt.c
+++ ext/pdo/pdo_stmt.c
@@ -18,7 +18,7 @@
   +----------------------------------------------------------------------+
 */
 
-/* $Id: pdo_stmt.c,v 1.186 2008/02/20 03:12:23 iliaa Exp $ */
+/* $Id: pdo_stmt.c,v 1.187 2008/02/24 16:09:13 iliaa Exp $ */
 
 /* The PDO Statement Handle Class */
 
@@ -141,6 +141,10 @@ static int dispatch_param_event(pdo_stmt_t *stmt, enum pdo_param_event event_typ
 	}
 
 	ht = stmt->bound_params;
+
+	if (ht && !stmt->bound_param_map) {
+		return 0;
+	}
 
 iterate:
 	if (ht) {

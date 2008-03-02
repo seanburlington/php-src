30626ad22dfb49b1c47cd080f516273a6738cc61
iliaa <iliaa>
2008-03-02 15:35:05 +0000

    Fixed bug #44306 (Better detection of MIPS processors on Windows)

--- ext/standard/info.c
+++ ext/standard/info.c
@@ -18,7 +18,7 @@
    +----------------------------------------------------------------------+
 */
 
-/* $Id: info.c,v 1.249.2.10.2.14.2.2 2007/12/31 07:17:15 sebastian Exp $ */
+/* $Id: info.c,v 1.249.2.10.2.14.2.3 2008/03/02 15:35:05 iliaa Exp $ */
 
 #include "php.h"
 #include "php_ini.h"
@@ -277,7 +277,7 @@ PHPAPI char *php_get_uname(char mode)
 				php_uname = tmp_uname;
 				break;
 			case PROCESSOR_ARCHITECTURE_MIPS :
-				php_uname = "MIPS R4000";
+				snprintf(tmp_uname, sizeof(tmp_uname), "MIPS R%d000", SysInfo.wProcessorLevel);
 				php_uname = tmp_uname;
 				break;
 			case PROCESSOR_ARCHITECTURE_ALPHA :

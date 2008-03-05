a2e5448652aca4cd3db821ac7ffcb8398e1ca701
iliaa <iliaa>
2008-03-05 23:52:45 +0000

    Revert patch for bug #44325

--- ext/mssql/php_mssql.c
+++ ext/mssql/php_mssql.c
@@ -16,7 +16,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: php_mssql.c,v 1.152.2.13.2.4.2.5 2008/03/04 19:38:38 iliaa Exp $ */
+/* $Id: php_mssql.c,v 1.152.2.13.2.4.2.6 2008/03/05 23:52:45 iliaa Exp $ */
 
 #ifdef COMPILE_DL_MSSQL
 #define HAVE_MSSQL 1
@@ -2067,19 +2067,14 @@ PHP_FUNCTION(mssql_bind)
 
 	/* modify datalen and maxlen according to dbrpcparam documentation */
 	if ( (type==SQLVARCHAR) || (type==SQLCHAR) || (type==SQLTEXT) )	{	/* variable-length type */
-		if (is_null || Z_TYPE_PP(var) == IS_NULL) {
+		if (is_null) {
 			maxlen=0;
 			datalen=0;
-		} else {
+		}
+		else {
 			convert_to_string_ex(var);
-			datalen = Z_STRLEN_PP(var);
-			value = (LPBYTE)Z_STRVAL_PP(var);
-			if (!datalen) {
-				datalen = 1;
-				if (maxlen == -1) {
-					maxlen = 1;
-				}
-			}
+			datalen=Z_STRLEN_PP(var);
+			value=(LPBYTE)Z_STRVAL_PP(var);
 		}
 	}
 	else	{	/* fixed-length type */

f5dbfab80f0f00b0994cee2d6d957d418cb4117b
pajoye <pajoye>
2008-03-05 21:20:14 +0000

    - MFB: #42505, new sendmail default path breaks on Novell (Guenter Knauf)

--- main/main.c
+++ main/main.c
@@ -18,7 +18,7 @@
    +----------------------------------------------------------------------+
 */
 
-/* $Id: main.c,v 1.761 2008/03/05 13:35:02 dmitry Exp $ */
+/* $Id: main.c,v 1.762 2008/03/05 21:20:14 pajoye Exp $ */
 
 /* {{{ includes
  */
@@ -460,9 +460,8 @@ static PHP_INI_MH(OnChangeMailForceExtra)
  * PHP_INCLUDE_PATH
  */
 
-#if defined(PHP_PROG_SENDMAIL) && !defined(NETWARE)
-#	define DEFAULT_SENDMAIL_PATH PHP_PROG_SENDMAIL " -t -i "
-#elif defined(PHP_WIN32)
+/* Windows and Netware use the internal mail */
+#if defined(PHP_WIN32) || defined(NETWARE)
 #	define DEFAULT_SENDMAIL_PATH NULL
 #else
 #	define DEFAULT_SENDMAIL_PATH "/usr/sbin/sendmail -t -i" 

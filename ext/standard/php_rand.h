fcf824f0d4043a22396543c4a71c04975840c5e7
stas <stas>
2008-03-07 03:34:47 +0000

    improve seed calculation # better solution would be to port mt's multi-value seed procedure

--- ext/standard/php_rand.h
+++ ext/standard/php_rand.h
@@ -20,7 +20,7 @@
    | Based on code from: Shawn Cokus <Cokus@math.washington.edu>          |
    +----------------------------------------------------------------------+
  */
-/* $Id: php_rand.h,v 1.28.2.1.2.2 2007/12/31 07:20:13 sebastian Exp $ */
+/* $Id: php_rand.h,v 1.28.2.1.2.3 2008/03/07 03:34:47 stas Exp $ */
 
 #ifndef PHP_RAND_H
 #define	PHP_RAND_H
@@ -47,9 +47,9 @@
 #define PHP_MT_RAND_MAX ((long) (0x7FFFFFFF)) /* (1<<31) - 1 */ 
 
 #ifdef PHP_WIN32
-#define GENERATE_SEED() ((long) (time(0) * GetCurrentProcessId() * 1000000 * php_combined_lcg(TSRMLS_C)))
+#define GENERATE_SEED() (((long) (time(0) * GetCurrentProcessId())) ^ ((long) (1000000.0 * php_combined_lcg(TSRMLS_C))))
 #else
-#define GENERATE_SEED() ((long) (time(0) * getpid() * 1000000 * php_combined_lcg(TSRMLS_C)))
+#define GENERATE_SEED() (((long) (time(0) * getpid())) ^ ((long) (1000000.0 * php_combined_lcg(TSRMLS_C)))))
 #endif
 
 PHPAPI void php_srand(long seed TSRMLS_DC);

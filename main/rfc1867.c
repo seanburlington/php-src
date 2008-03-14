81494c600fe348abb8b703bf91a5b9ac5212f658
tony2001 <tony2001>
2008-03-14 13:10:22 +0000

    check if return value of write() is -1 and abort upload in this case setting the correct error status

--- main/rfc1867.c
+++ main/rfc1867.c
@@ -17,7 +17,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: rfc1867.c,v 1.192 2007/12/31 07:12:18 sebastian Exp $ */
+/* $Id: rfc1867.c,v 1.193 2008/03/14 13:10:22 tony2001 Exp $ */
 
 /*
  *  This product includes software developed by the Apache Group
@@ -1263,7 +1263,13 @@ var_done:
 				} else if (blen > 0) {
 					wlen = fwrite(buff, 1, blen, fp);
 
-					if (wlen < blen) {
+					if (wlen == -1) {
+						/* write failed */
+#if DEBUG_FILE_UPLOAD
+						sapi_module.sapi_error(E_NOTICE, "write() failed - %s", strerror(errno));
+#endif
+						cancel_upload = UPLOAD_ERROR_F;
+					} else if (wlen < blen) {
 #if DEBUG_FILE_UPLOAD
 						sapi_module.sapi_error(E_NOTICE, "Only %d bytes were written, expected to write %d", wlen, blen);
 #endif
@@ -1712,7 +1718,13 @@ static SAPI_POST_HANDLER_FUNC(rfc1867_post_handler_legacy)
 				} else if (blen > 0) {
 					wlen = write(fd, buff, blen);
 			
-					if (wlen < blen) {
+					if (wlen == -1) {
+						/* write failed */
+#if DEBUG_FILE_UPLOAD
+						sapi_module.sapi_error(E_NOTICE, "write() failed - %s", strerror(errno));
+#endif
+						cancel_upload = UPLOAD_ERROR_F;
+					} else if (wlen < blen) {
 #if DEBUG_FILE_UPLOAD
 						sapi_module.sapi_error(E_NOTICE, "Only %d bytes were written, expected to write %d", wlen, blen);
 #endif

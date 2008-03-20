ec28ed9ad8631a6418b844079d7279cf5db266f3
iliaa <iliaa>
2008-03-20 23:26:01 +0000

    MFB: Refine fix for multibyte char hanling inside command names and args

--- ext/standard/exec.c
+++ ext/standard/exec.c
@@ -16,7 +16,7 @@
    |         Ilia Alshanetsky <iliaa@php.net>                             |
    +----------------------------------------------------------------------+
  */
-/* $Id: exec.c,v 1.128 2008/03/18 00:32:58 felipe Exp $ */
+/* $Id: exec.c,v 1.129 2008/03/20 23:26:01 iliaa Exp $ */
 
 #include <stdio.h>
 #include "php.h"
@@ -259,8 +259,13 @@ PHPAPI char *php_escape_shell_cmd(char *str)
 	cmd = safe_emalloc(2, l, 1);
 
 	for (x = 0, y = 0; x < l; x++) {
+		int mb_len = php_mblen(str + x, (l - x));
+
 		/* skip non-valid multibyte characters */
-		if (php_mblen(str + x, (l - x)) < 0) {
+		if (mb_len < 0) {
+			continue;
+		} else if (mb_len > 1) {
+			x += mb_len - 1;
 			continue;
 		}
 
@@ -341,6 +346,16 @@ PHPAPI char *php_escape_shell_arg(char *str)
 #endif
 
 	for (x = 0; x < l; x++) {
+		int mb_len = php_mblen(str + x, (l - x));
+
+		/* skip non-valid multibyte characters */
+		if (mb_len < 0) {
+			continue;
+		} else if (mb_len > 1) {
+			x += mb_len - 1;
+			continue;
+		}
+
 		switch (str[x]) {
 #ifdef PHP_WIN32
 		case '"':

73af59ae14f81cb003f301ceabbe467887943001
rrichards <rrichards>
2008-03-20 16:48:09 +0000

    MFH: fix bug #44478 (Inconsistent behaviour when assigning new nodes) add test

--- ext/simplexml/simplexml.c
+++ ext/simplexml/simplexml.c
@@ -18,7 +18,7 @@
   +----------------------------------------------------------------------+
 */
 
-/* $Id: simplexml.c,v 1.151.2.22.2.35.2.11 2008/03/18 14:10:45 felipe Exp $ */
+/* $Id: simplexml.c,v 1.151.2.22.2.35.2.12 2008/03/20 16:48:09 rrichards Exp $ */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -431,19 +431,12 @@ static void change_node_zval(xmlNodePtr node, zval *value TSRMLS_DC)
 			convert_to_string(value);
 			/* break missing intentionally */
 		case IS_STRING:
-			if (node->type == XML_ATTRIBUTE_NODE) {
-				buffer = xmlEncodeEntitiesReentrant(node->doc, (xmlChar *)Z_STRVAL_P(value));
-				buffer_len = xmlStrlen(buffer);
-			} else {
-				buffer = (xmlChar *)Z_STRVAL_P(value);
-				buffer_len = Z_STRLEN_P(value);
-			}
+			buffer = xmlEncodeEntitiesReentrant(node->doc, (xmlChar *)Z_STRVAL_P(value));
+			buffer_len = xmlStrlen(buffer);
 			/* check for NULL buffer in case of memory error in xmlEncodeEntitiesReentrant */
 			if (buffer) {
 				xmlNodeSetContentLen(node, buffer, buffer_len);
-				if (node->type == XML_ATTRIBUTE_NODE) {
-					xmlFree(buffer);
-				}
+				xmlFree(buffer);
 			}
 			if (value == &value_copy) {
 				zval_dtor(value);
@@ -2467,7 +2460,7 @@ PHP_MINFO_FUNCTION(simplexml)
 {
 	php_info_print_table_start();
 	php_info_print_table_header(2, "Simplexml support", "enabled");
-	php_info_print_table_row(2, "Revision", "$Revision: 1.151.2.22.2.35.2.11 $");
+	php_info_print_table_row(2, "Revision", "$Revision: 1.151.2.22.2.35.2.12 $");
 	php_info_print_table_row(2, "Schema support",
 #ifdef LIBXML_SCHEMAS_ENABLED
 		"enabled");

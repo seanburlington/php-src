eef20107302c2303c959ca80ef25950dffcaa495
helly <helly>
2008-03-24 12:53:27 +0000

    - Add documentation

--- ext/phar/phar.c
+++ ext/phar/phar.c
@@ -17,7 +17,7 @@
   +----------------------------------------------------------------------+
 */
 
-/* $Id: phar.c,v 1.324 2008/03/24 03:05:23 cellog Exp $ */
+/* $Id: phar.c,v 1.325 2008/03/24 12:53:27 helly Exp $ */
 
 #define PHAR_MAIN 1
 #include "phar_internal.h"
@@ -1386,8 +1386,15 @@ int phar_detect_phar_fname_ext(const char *filename, int check_length, const cha
 	*ext_str = NULL;
 	for (i = 0; i < sizeof(ext_info) / sizeof(ext_info[0]); ++i) {
 		pos = strstr(filename, ext_info[i].ext);
-		/* Marcus, I don't understand the next line, can you add explanatory comments? XXOO Greg */
-		if (pos && (!ext_info[i].check || pos[ext_info[i].len] != '\0') && (!*ext_str || pos <= *ext_str)) {
+		/* If pos is not NULL then we found the extension. But we have to check
+		 * to more things. First some extensions cannot be the end of the string
+		 * (e.g. .php). Second when we already found an extension at an earlier
+		 * position, we ignore the new one. In case we found one at the same
+		 * place, we probably found a longer one. As the list is ordered that
+		 * way we do not need to check for a greater length.
+		 */
+		if (pos && (/*1*/ !ext_info[i].check || pos[ext_info[i].len] != '\0')
+		        && (/*2*/ !*ext_str || pos <= *ext_str)) {
 			*ext_str = pos;
 			*ext_len = ext_info[i].len;
 		}
@@ -2828,7 +2835,7 @@ PHP_MINFO_FUNCTION(phar) /* {{{ */
 	php_info_print_table_header(2, "Phar: PHP Archive support", "enabled");
 	php_info_print_table_row(2, "Phar EXT version", PHP_PHAR_VERSION);
 	php_info_print_table_row(2, "Phar API version", PHP_PHAR_API_VERSION);
-	php_info_print_table_row(2, "CVS revision", "$Revision: 1.324 $");
+	php_info_print_table_row(2, "CVS revision", "$Revision: 1.325 $");
 	php_info_print_table_row(2, "Phar-based phar archives", "enabled");
 	php_info_print_table_row(2, "Tar-based phar archives", "enabled");
 	php_info_print_table_row(2, "ZIP-based phar archives", "enabled");

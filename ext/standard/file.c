11a0e013f4a7af83b7ba25cffd72eafb0ea0ed23
iliaa <iliaa>
2008-03-31 22:44:38 +0000

    Revert unintended bits from "Added a warning message" commit

--- ext/standard/file.c
+++ ext/standard/file.c
@@ -21,7 +21,7 @@
    +----------------------------------------------------------------------+
 */
 
-/* $Id: file.c,v 1.516 2008/03/30 15:31:33 felipe Exp $ */
+/* $Id: file.c,v 1.517 2008/03/31 22:44:38 iliaa Exp $ */
 
 /* Synced with php 3.0 revision 1.218 1999-06-16 [ssb] */
 
@@ -622,8 +622,8 @@ PHP_FUNCTION(file_put_contents)
 	long flags = ((argc < 3) && UG(unicode)) ? PHP_FILE_TEXT : 0;
 	zval *zcontext = NULL;
 	php_stream_context *context = NULL;
+	char mode[3] = { 'w', 0, 0 };
 	php_stream *srcstream = NULL;
-	char mode[3] = "wb";
 
 	if (zend_parse_parameters(argc TSRMLS_CC, "Zz/|lr!", &ppfilename, &data, &flags, &zcontext) == FAILURE) {
 		return;
@@ -657,10 +657,6 @@ PHP_FUNCTION(file_put_contents)
 		php_stream_close(stream);
 		php_error_docref(NULL TSRMLS_CC, E_WARNING, "Exclusive locks are not supported for this stream");
 		RETURN_FALSE;
-	}
-
-	if (mode[0] = 'c') {
-		php_stream_truncate_set_size(stream, 0);
 	}
 
 	switch (Z_TYPE_P(data)) {

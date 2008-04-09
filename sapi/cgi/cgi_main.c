9b67c976352a3fb8bccd164594b5a8a01d37f150
dmitry <dmitry>
2008-04-09 09:17:01 +0000

    Fixed bug #44673 (With CGI argv/argc starts from arguments, not from script)

--- sapi/cgi/cgi_main.c
+++ sapi/cgi/cgi_main.c
@@ -21,7 +21,7 @@
    +----------------------------------------------------------------------+
 */
 
-/* $Id: cgi_main.c,v 1.353 2008/03/26 14:46:17 scottmac Exp $ */
+/* $Id: cgi_main.c,v 1.354 2008/04/09 09:17:01 dmitry Exp $ */
 
 #include "php.h"
 #include "php_globals.h"
@@ -1805,7 +1805,7 @@ consult the installation file that came with this distribution, or visit \n\
 				} else if (argc > php_optind) {
 					/* file is on command line, but not in -f opt */
 					STR_FREE(SG(request_info).path_translated);
-					SG(request_info).path_translated = estrdup(argv[php_optind++]);
+					SG(request_info).path_translated = estrdup(argv[php_optind]);
 					/* arguments after the file are considered script args */
 					SG(request_info).argc = argc - php_optind;
 					SG(request_info).argv = &argv[php_optind];

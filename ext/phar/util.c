a702eefd4be8b512a51a7cdccd14f435b0a9d665
cellog <cellog>
2008-05-12 00:43:10 +0000

    increase code coverage in util.c

--- ext/phar/util.c
+++ ext/phar/util.c
@@ -18,7 +18,7 @@
   +----------------------------------------------------------------------+
 */
 
-/* $Id: util.c,v 1.53 2008/05/11 23:03:55 cellog Exp $ */
+/* $Id: util.c,v 1.54 2008/05/12 00:43:10 cellog Exp $ */
 
 #include "phar_internal.h"
 #if !defined(PHP_VERSION_ID) || PHP_VERSION_ID < 50300
@@ -212,7 +212,7 @@ char *phar_find_in_include_path(char *filename, int filename_len, phar_archive_d
 {
 #if PHP_VERSION_ID >= 50300
 	char *path, *fname, *arch, *entry, *ret, *test;
-	int arch_len, entry_len;
+	int arch_len, entry_len, fname_len;
 
 	if (pphar) {
 		*pphar = NULL;
@@ -222,7 +222,9 @@ char *phar_find_in_include_path(char *filename, int filename_len, phar_archive_d
 		return phar_save_resolve_path(filename, filename_len TSRMLS_CC);
 	}
 	fname = zend_get_executed_filename(TSRMLS_C);
-	if (SUCCESS != phar_split_fname(fname, strlen(fname), &arch, &arch_len, &entry, &entry_len, 1, 0 TSRMLS_CC)) {
+	fname_len = strlen(fname);
+
+	if (fname_len < 7 || memcmp(fname, "phar://", 7) || SUCCESS != phar_split_fname(fname, strlen(fname), &arch, &arch_len, &entry, &entry_len, 1, 0 TSRMLS_CC)) {
 		return phar_save_resolve_path(filename, filename_len TSRMLS_CC);
 	}
 	efree(entry);

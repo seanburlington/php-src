878778f1ccf5e51c368bf726c14fbaabcc7ec73d
jani <jani>
2008-06-02 11:23:16 +0000

    - Revert pointless patch: a) the version is already there, see line above, b) one branch commits are idiotic to begin with, we have three active branches! This only leads to out-of-sync situations ála PDO..

--- ext/ldap/ldap.c
+++ ext/ldap/ldap.c
@@ -23,7 +23,7 @@
    +----------------------------------------------------------------------+
  */
  
-/* $Id: ldap.c,v 1.161.2.3.2.11.2.12 2008/06/02 10:25:54 pajoye Exp $ */
+/* $Id: ldap.c,v 1.161.2.3.2.11.2.13 2008/06/02 11:23:16 jani Exp $ */
 #define IS_EXT_MODULE
 
 #ifdef HAVE_CONFIG_H
@@ -49,9 +49,7 @@
 #if HAVE_NSLDAP
 #include <winsock2.h>
 #endif
-#ifndef strdup
 #define strdup _strdup
-#endif
 #undef WINDOWS
 #undef strcasecmp
 #undef strncasecmp
@@ -239,7 +237,7 @@ PHP_MINFO_FUNCTION(ldap)
 
 	php_info_print_table_start();
 	php_info_print_table_row(2, "LDAP Support", "enabled");
-	php_info_print_table_row(2, "RCS Version", "$Id: ldap.c,v 1.161.2.3.2.11.2.12 2008/06/02 10:25:54 pajoye Exp $");
+	php_info_print_table_row(2, "RCS Version", "$Id: ldap.c,v 1.161.2.3.2.11.2.13 2008/06/02 11:23:16 jani Exp $");
 
 	if (LDAPG(max_links) == -1) {
 		snprintf(tmp, 31, "%ld/unlimited", LDAPG(num_links));
@@ -256,14 +254,10 @@ PHP_MINFO_FUNCTION(ldap)
 #ifdef LDAP_VENDOR_NAME
 	php_info_print_table_row(2, "Vendor Name", LDAP_VENDOR_NAME);
 #endif
-	
+
 #ifdef LDAP_VENDOR_VERSION
 	snprintf(tmp, 31, "%d", LDAP_VENDOR_VERSION);
 	php_info_print_table_row(2, "Vendor Version", tmp);
-
-	snprintf(tmp, 31, "%d.%d.%d", LDAP_VENDOR_VERSION_MAJOR, 
-				LDAP_VENDOR_VERSION_MINOR, LDAP_VENDOR_VERSION_PATCH);
-	php_info_print_table_row(2, "Vendor Version String", tmp);
 #endif
 
 #if HAVE_NSLDAP

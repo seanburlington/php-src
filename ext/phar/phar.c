95dc9dbce260d107f119d836e08f77575e549b6a
sfox <sfox>
2008-06-23 19:18:01 +0000

    - One solution for the openssl issues - config.m4 needs bringing into line with config.w32, there may be broken behaviour under *nix until this is done

--- ext/phar/phar.c
+++ ext/phar/phar.c
@@ -17,7 +17,7 @@
   +----------------------------------------------------------------------+
 */
 
-/* $Id: phar.c,v 1.370.2.28 2008/06/22 00:50:32 cellog Exp $ */
+/* $Id: phar.c,v 1.370.2.29 2008/06/23 19:18:01 sfox Exp $ */
 
 #define PHAR_MAIN 1
 #include "phar_internal.h"
@@ -3408,7 +3408,7 @@ PHP_MINFO_FUNCTION(phar) /* {{{ */
 	php_info_print_table_header(2, "Phar: PHP Archive support", "enabled");
 	php_info_print_table_row(2, "Phar EXT version", PHP_PHAR_VERSION);
 	php_info_print_table_row(2, "Phar API version", PHP_PHAR_API_VERSION);
-	php_info_print_table_row(2, "CVS revision", "$Revision: 1.370.2.28 $");
+	php_info_print_table_row(2, "CVS revision", "$Revision: 1.370.2.29 $");
 	php_info_print_table_row(2, "Phar-based phar archives", "enabled");
 	php_info_print_table_row(2, "Tar-based phar archives", "enabled");
 	php_info_print_table_row(2, "ZIP-based phar archives", "enabled");
@@ -3422,6 +3422,15 @@ PHP_MINFO_FUNCTION(phar) /* {{{ */
 	} else {
 		php_info_print_table_row(2, "bzip2 compression", "disabled (install pecl/bz2)");
 	}
+#ifdef PHAR_HAVE_OPENSSL
+	php_info_print_table_row(2, "Native OpenSSL support", "enabled");
+#else
+	if (zend_hash_exists(&module_registry, "openssl", sizeof("openssl"))) {
+		php_info_print_table_row(2, "OpenSSL support", "enabled");
+	} else {
+		php_info_print_table_row(2, "OpenSSL support", "disabled (install ext/openssl)");
+	}
+#endif
 	php_info_print_table_end();
 
 	php_info_print_box_start(0);

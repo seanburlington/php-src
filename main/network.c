d9bc05961529eff094932d294be4356d4d68ffd7
jani <jani>
2008-07-11 13:11:49 +0000

    MFH: silence warning

--- main/network.c
+++ main/network.c
@@ -17,7 +17,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: network.c,v 1.118.2.2.2.6.2.6 2008/03/11 10:27:10 tony2001 Exp $ */
+/* $Id: network.c,v 1.118.2.2.2.6.2.7 2008/07/11 13:11:49 jani Exp $ */
 
 /*#define DEBUG_MAIN_NETWORK 1*/
 
@@ -167,7 +167,9 @@ static int php_network_getaddresses(const char *host, int socktype, struct socka
 	struct sockaddr **sap;
 	int n;
 #if HAVE_GETADDRINFO
+# if HAVE_IPV6
 	static int ipv6_borked = -1; /* the way this is used *is* thread safe */
+# endif
 	struct addrinfo hints, *res, *sai;
 #else
 	struct hostent *host_info;

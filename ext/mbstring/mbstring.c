733cbbaae29bb88506217bb9477ed2df29dc34d5
moriyoshi <moriyoshi>
2008-07-25 14:04:54 +0000

    - MFH: Fix warnings

--- ext/mbstring/mbstring.c
+++ ext/mbstring/mbstring.c
@@ -17,7 +17,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: mbstring.c,v 1.224.2.22.2.25.2.24 2008/07/25 13:57:23 moriyoshi Exp $ */
+/* $Id: mbstring.c,v 1.224.2.22.2.25.2.25 2008/07/25 14:04:54 moriyoshi Exp $ */
 
 /*
  * PHP 4 Multibyte String module "mbstring"
@@ -2425,7 +2425,7 @@ PHP_FUNCTION(mb_strripos)
 		RETURN_FALSE;
 	}
 
-	if (offset > haystack.len) {
+	if ((unsigned int)offset > haystack.len) {
 		RETURN_FALSE;
 	}
 
@@ -2833,11 +2833,11 @@ PHP_FUNCTION(mb_strcut)
 		}
 	}
 
-	if (from > string.len) {
+	if ((unsigned int)from > string.len) {
 		RETURN_FALSE;
 	}
 
-	if (((unsigned) from + (unsigned) len) > string.len) {
+	if (((unsigned int)from + (unsigned int)len) > string.len) {
 		len = string.len - from;
 	}
 
@@ -3219,7 +3219,7 @@ PHP_FUNCTION(mb_detect_encoding)
 	}
 
 	if (ZEND_NUM_ARGS() < 3) {
-		strict = MBSTRG(strict_detection);
+		strict = (zend_bool)MBSTRG(strict_detection);
 	}
 
 	if (size > 0 && list != NULL) {

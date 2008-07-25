4c1f1b28f56b954d6de53e3e71f75c2e60f46584
tony2001 <tony2001>
2008-07-25 09:55:51 +0000

    fix typo

--- run-tests.php
+++ run-tests.php
@@ -24,7 +24,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: run-tests.php,v 1.370 2008/07/25 09:32:06 moriyoshi Exp $ */
+/* $Id: run-tests.php,v 1.371 2008/07/25 09:55:51 tony2001 Exp $ */
 
 /* Sanity check to ensure that pcre extension needed by this script is available.
  * In the event it is not, print a nice error message indicating that this script will
@@ -426,7 +426,7 @@ if ($compression) {
 	$output_file = 'compress.zlib://' . $output_file . '.gz';
 }
 
-$jnust_save_results = false;
+$just_save_results = false;
 $leak_check = false;
 $html_output = false;
 $html_file = null;
@@ -608,7 +608,7 @@ if (isset($argc) && $argc > 1) {
 					$html_output = is_resource($html_file);
 					break;
 				case '--version':
-					echo '$Revision: 1.370 $' . "\n";
+					echo '$Revision: 1.371 $' . "\n";
 					exit(1);
 
 				default:

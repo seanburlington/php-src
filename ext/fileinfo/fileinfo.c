66a26b9ef904b4171995039c22b83f3f96fcdf42
felipe <felipe>
2008-07-25 11:54:06 +0000

    - Fix build

--- ext/fileinfo/fileinfo.c
+++ ext/fileinfo/fileinfo.c
@@ -16,7 +16,7 @@
   +----------------------------------------------------------------------+
 */
 
-/* $Id: fileinfo.c,v 1.21 2008/07/25 09:16:09 jani Exp $ */
+/* $Id: fileinfo.c,v 1.22 2008/07/25 11:54:06 felipe Exp $ */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -267,7 +267,7 @@ PHP_FUNCTION(finfo_open)
 		}
 		file = resolved_path;
 
-		if ((PG(safe_mode) && (!php_checkuid(file, NULL, CHECKUID_CHECK_FILE_AND_DIR))) || php_check_open_basedir(file TSRMLS_CC)) {
+		if (php_check_open_basedir(file TSRMLS_CC)) {
 			RETURN_FALSE;
 		}
 	}

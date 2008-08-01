36c29ffb29966eb47da5ae7cbb36ffb51e73e8d1
pajoye <pajoye>
2008-08-01 15:07:59 +0000

    - MFH: support for Oracle 11g (either 8 or 11g can be built but not both

--- ext/oci8/php_oci8.h
+++ ext/oci8/php_oci8.h
@@ -25,7 +25,7 @@
    +----------------------------------------------------------------------+
 */
 
-/* $Id: php_oci8.h,v 1.36.2.2.2.1.2.9 2008/07/27 17:50:40 sixd Exp $ */
+/* $Id: php_oci8.h,v 1.36.2.2.2.1.2.10 2008/08/01 15:07:59 pajoye Exp $ */
 
 #if HAVE_OCI8
 # ifndef PHP_OCI8_H
@@ -50,6 +50,7 @@
 
 extern zend_module_entry oci8_module_entry;
 #define phpext_oci8_ptr &oci8_module_entry
+#define phpext_oci8_11g_ptr &oci8_module_entry
 
 
 PHP_MINIT_FUNCTION(oci);

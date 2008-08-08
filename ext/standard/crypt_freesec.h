169f39dac32befa8c61f9aa0f8432756fa84f8ca
bjori <bjori>
2008-08-08 08:19:46 +0000

    Fix build on FreeBSD4.11 (bug#45757)

--- ext/standard/crypt_freesec.h
+++ ext/standard/crypt_freesec.h
@@ -1,10 +1,10 @@
-/* $Id: crypt_freesec.h,v 1.1 2008/07/28 11:34:53 pajoye Exp $ */
+/* $Id: crypt_freesec.h,v 1.2 2008/08/08 08:19:46 bjori Exp $ */
 #ifndef _CRYPT_FREESEC_H
 #define _CRYPT_FREESEC_H
 
 #if PHP_WIN32
 # include "win32/php_stdint.h"
-#else
+#elif HAVE_STDINT_H
 # include <stdint.h>
 #endif
 

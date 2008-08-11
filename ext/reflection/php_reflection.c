05495093740593aba40cfff439f4a0ee6846b46d
felipe <felipe>
2008-08-11 12:45:37 +0000

    - Fix build (ZTS)

--- ext/reflection/php_reflection.c
+++ ext/reflection/php_reflection.c
@@ -20,7 +20,7 @@
    +----------------------------------------------------------------------+
 */
 
-/* $Id: php_reflection.c,v 1.311 2008/08/11 12:36:00 helly Exp $ */
+/* $Id: php_reflection.c,v 1.312 2008/08/11 12:45:37 felipe Exp $ */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -3369,7 +3369,7 @@ ZEND_METHOD(reflection_class, getMethods)
 
 	array_init(return_value);
 	zend_hash_apply_with_arguments(&ce->function_table TSRMLS_CC, (apply_func_args_t) _addmethod_va, 4, &ce, return_value, filter, intern->obj);
-	if (intern->obj && instanceof_function(ce, zend_ce_closure)) {
+	if (intern->obj && instanceof_function(ce, zend_ce_closure TSRMLS_CC)) {
 		zend_function *closure = zend_get_closure_invoke_method(intern->obj TSRMLS_CC);
 		if (closure) {
 			_addmethod(closure, ce, return_value, filter, intern->obj TSRMLS_CC);
@@ -5377,7 +5377,7 @@ PHP_MINFO_FUNCTION(reflection) /* {{{ */
 	php_info_print_table_start();
 	php_info_print_table_header(2, "Reflection", "enabled");
 
-	php_info_print_table_row(2, "Version", "$Id: php_reflection.c,v 1.311 2008/08/11 12:36:00 helly Exp $");
+	php_info_print_table_row(2, "Version", "$Id: php_reflection.c,v 1.312 2008/08/11 12:45:37 felipe Exp $");
 
 	php_info_print_table_end();
 } /* }}} */

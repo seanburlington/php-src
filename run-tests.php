2d884b2412f13daf2f1d4ee36b07f72b0fdfaff9
jani <jani>
2008-08-14 20:53:00 +0000

    - Nuke annoying errors already silenced elsewhere too

--- run-tests.php
+++ run-tests.php
@@ -24,7 +24,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: run-tests.php,v 1.226.2.37.2.46 2008/07/23 13:43:05 jani Exp $ */
+/* $Id: run-tests.php,v 1.226.2.37.2.47 2008/08/14 20:53:00 jani Exp $ */
 
 /* Sanity check to ensure that pcre extension needed by this script is available.
  * In the event it is not, print a nice error message indicating that this script will
@@ -224,7 +224,7 @@ More .INIs  : " . (function_exists(\'php_ini_scanned_files\') ? str_replace("\n"
 		$php_cgi_info = '';
 	}
 	@unlink($info_file);
-	define('TESTED_PHP_VERSION', `$php -r "echo PHP_VERSION;"`);
+	define('TESTED_PHP_VERSION', `$php -n -r "echo PHP_VERSION;"`);
 
 	// load list of enabled extensions
 	save_text($info_file, '<?php echo join(",",get_loaded_extensions()); ?>');
@@ -428,7 +428,7 @@ if (isset($argc) && $argc > 1) {
 					$html_output = is_resource($html_file);
 					break;
 				case '--version':
-					echo '$Revision: 1.226.2.37.2.46 $'."\n";
+					echo '$Revision: 1.226.2.37.2.47 $'."\n";
 					exit(1);
 
 				case 'u':
@@ -786,7 +786,7 @@ if ($just_save_results || !getenv('NO_INTERACTION')) {
 		}	
 		
 		$failed_tests_data .= $sep . "PHPINFO" . $sep;
-		$failed_tests_data .= shell_exec($php.' -dhtml_errors=0 -i');
+		$failed_tests_data .= shell_exec($php . ' -ddisplay_errors=stderr -dhtml_errors=0 -i 2> /dev/null');
 		
 		if ($just_save_results || !mail_qa_team($failed_tests_data, $compression, $status)) {
 			file_put_contents($output_file, $failed_tests_data);
@@ -1387,7 +1387,7 @@ TEST $file
 			return 'BORKED';
 		}
 		save_text($tmp_post, $request);
-		$cmd = "$php$pass_options$ini_settings -f \"$test_file\" 2>&1 < $tmp_post";
+		$cmd = "$php $pass_options $ini_settings -f \"$test_file\" 2>&1 < $tmp_post";
 	} elseif (array_key_exists('POST', $section_text) && !empty($section_text['POST'])) {
 
 		$post = trim($section_text['POST']);
@@ -1407,7 +1407,7 @@ TEST $file
 		$env['CONTENT_TYPE']   = 'application/x-www-form-urlencoded';
 		$env['CONTENT_LENGTH'] = $content_length;
 
-		$cmd = "$php$pass_options$ini_settings -f \"$test_file\" 2>&1 < $tmp_post";
+		$cmd = "$php $pass_options $ini_settings -f \"$test_file\" 2>&1 < $tmp_post";
 
 	} else {
 
@@ -1415,7 +1415,7 @@ TEST $file
 		$env['CONTENT_TYPE']   = '';
 		$env['CONTENT_LENGTH'] = '';
 
-		$cmd = "$php$pass_options$ini_settings -f \"$test_file\" $args 2>&1";
+		$cmd = "$php $pass_options $ini_settings -f \"$test_file\" $args 2>&1";
 	}
 
 	if ($leak_check) {

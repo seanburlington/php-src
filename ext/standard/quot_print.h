3e484937156e75a263d4e31a909e545a1801b0fd
tony2001 <tony2001>
2008-08-14 10:11:26 +0000

    [DOC] add quoted_printable_encode()

--- ext/standard/quot_print.h
+++ ext/standard/quot_print.h
@@ -16,13 +16,15 @@
    +----------------------------------------------------------------------+
 */
 
-/* $Id: quot_print.h,v 1.16 2007/12/31 07:12:16 sebastian Exp $ */
+/* $Id: quot_print.h,v 1.17 2008/08/14 10:11:26 tony2001 Exp $ */
 
 #ifndef QUOT_PRINT_H
 #define QUOT_PRINT_H
 
 PHPAPI unsigned char *php_quot_print_decode(const unsigned char *str, size_t length, size_t *ret_length, int replace_us_by_ws);
+PHPAPI unsigned char *php_quot_print_encode(const unsigned char *str, size_t length, size_t *ret_length);
 
 PHP_FUNCTION(quoted_printable_decode);
+PHP_FUNCTION(quoted_printable_encode);
 
 #endif /* QUOT_PRINT_H */

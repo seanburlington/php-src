f86252e97f8ff46a9024df72c4963bd32e234068
tony2001 <tony2001>
2008-08-18 07:46:31 +0000

    reverted because of test failures

--- main/output.c
+++ main/output.c
@@ -18,7 +18,7 @@
    +----------------------------------------------------------------------+
 */
 
-/* $Id: output.c,v 1.167.2.3.2.4.2.8 2008/08/18 04:08:17 lbarnaud Exp $ */
+/* $Id: output.c,v 1.167.2.3.2.4.2.9 2008/08/18 07:46:31 tony2001 Exp $ */
 
 #include "php.h"
 #include "ext/standard/head.h"
@@ -336,13 +336,7 @@ PHPAPI void php_end_ob_buffer(zend_bool send_buffer, zend_bool just_flush TSRMLS
 PHPAPI void php_end_ob_buffers(zend_bool send_buffer TSRMLS_DC)
 {
 	while (OG(ob_nesting_level)!=0) {
-		/* in case of unclean_shutdown, do not output the buffer if it is not 
-		 * meant to be until end of script or ob_end_*() call */
-		if (CG(unclean_shutdown) && !OG(active_ob_buffer).chunk_size) {
-			php_end_ob_buffer(0, 0 TSRMLS_CC);
-		} else {
-			php_end_ob_buffer(send_buffer, 0 TSRMLS_CC);
-		}
+		php_end_ob_buffer(send_buffer, 0 TSRMLS_CC);
 	}
 }
 /* }}} */

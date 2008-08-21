ecd155f30d0f43c22a3f1f397ab43c1fa206a3dc
helly <helly>
2008-08-21 16:55:31 +0000

    - Fix 64bit issue

--- ext/standard/basic_functions.c
+++ ext/standard/basic_functions.c
@@ -17,7 +17,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: basic_functions.c,v 1.925 2008/08/21 08:37:01 tony2001 Exp $ */
+/* $Id: basic_functions.c,v 1.926 2008/08/21 16:55:31 helly Exp $ */
 
 #include "php.h"
 #include "php_streams.h"
@@ -4929,7 +4929,7 @@ PHP_FUNCTION(get_current_user)
  */
 static int add_config_entry_cb(zval *entry TSRMLS_DC, int num_args, va_list args, zend_hash_key *hash_key)
 {
-	zval *retval = (zval *)va_arg(args, int);
+	zval *retval = (zval *)va_arg(args, zval*);
 	zval *tmp;
 
 	if (Z_TYPE_P(entry) == IS_STRING) {

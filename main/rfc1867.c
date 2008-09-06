db2a3e8e6cba389525883aa7d39b0be93ec00d33
lbarnaud <lbarnaud>
2008-09-06 17:17:44 +0000

    Fixed #43540 (rfc1867 handler newlength problem)

--- main/rfc1867.c
+++ main/rfc1867.c
@@ -17,7 +17,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: rfc1867.c,v 1.195 2008/09/06 08:27:38 lbarnaud Exp $ */
+/* $Id: rfc1867.c,v 1.196 2008/09/06 17:17:44 lbarnaud Exp $ */
 
 /*
  *  This product includes software developed by the Apache Group
@@ -1177,7 +1177,7 @@ static SAPI_POST_HANDLER_FUNC(rfc1867_post_handler_unicode)
 				/* if (sapi_module.input_filter(PARSE_POST, param, &u_val, u_val_len, &new_val_len TSRMLS_CC)) { */
 					if (php_rfc1867_callback != NULL) {
 						multipart_event_formdata event_formdata;
-						size_t newlength = 0;
+						size_t newlength = (size_t) u_val_len;
 
 						event_formdata.post_bytes_processed = SG(read_post_bytes);
 						event_formdata.name = ZSTR(param);

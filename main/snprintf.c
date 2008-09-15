15203bf2b67a284519b8f4017fe07f7aa87a6e51
dmitry <dmitry>
2008-09-15 11:47:27 +0000

    Fixed bug #43053 (Regression: some numbers shown in scientific notation). (int-e at gmx dot de) some 64bit test files may need to be fixed

--- main/snprintf.c
+++ main/snprintf.c
@@ -16,7 +16,7 @@
   +----------------------------------------------------------------------+
 */
 
-/* $Id: snprintf.c,v 1.63 2008/02/07 18:40:29 helly Exp $ */
+/* $Id: snprintf.c,v 1.64 2008/09/15 11:47:27 dmitry Exp $ */
 
 
 #include "php.h"
@@ -152,9 +152,7 @@ PHPAPI char *php_gcvt(double value, int ndigit, char dec_point, char exponent, c
 		*dst++ = '-';
 	}
 
-	for (i = 0; i < ndigit && digits[i] != '\0'; i++);
-
-	if ((decpt >= 0 && decpt - i > 4) || (decpt < 0 && decpt < -3)) { /* use E-style */
+	if ((decpt >= 0 && decpt > ndigit) || decpt < -3) { /* use E-style */
 		/* exponential format (e.g. 1.2345e+13) */
 		if (--decpt < 0) {
 			sign = 1;

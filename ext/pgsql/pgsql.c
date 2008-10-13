f35fbaeb9d7688ee0c351f49de9e9e824bac9426
felipe <felipe>
2008-10-13 13:41:16 +0000

    - Fixed bug #37100 (data is returned truncated with BINARY CURSOR)   Patch by Tony

--- ext/pgsql/pgsql.c
+++ ext/pgsql/pgsql.c
@@ -20,7 +20,7 @@
    +----------------------------------------------------------------------+
  */
  
-/* $Id: pgsql.c,v 1.387 2008/10/08 23:04:58 iliaa Exp $ */
+/* $Id: pgsql.c,v 1.388 2008/10/13 13:41:16 felipe Exp $ */
 
 #include <stdlib.h>
 
@@ -2472,10 +2472,9 @@ PHP_FUNCTION(pg_fetch_result)
 	if (PQgetisnull(pgsql_result, pgsql_row, field_offset)) {
 		Z_TYPE_P(return_value) = IS_NULL;
 	} else {
-		Z_STRVAL_P(return_value) = PQgetvalue(pgsql_result, pgsql_row, field_offset);
-		Z_STRLEN_P(return_value) = (Z_STRVAL_P(return_value) ? strlen(Z_STRVAL_P(return_value)) : 0);
-		Z_STRVAL_P(return_value) = safe_estrndup(Z_STRVAL_P(return_value),Z_STRLEN_P(return_value));
-		Z_TYPE_P(return_value) = IS_STRING;
+		char *value = PQgetvalue(pgsql_result, pgsql_row, field_offset);
+		int value_len = PQgetlength(pgsql_result, pgsql_row, field_offset);
+		ZVAL_STRINGL(return_value, value, value_len, 1);
 	}
 }
 /* }}} */

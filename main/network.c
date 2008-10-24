f1607726c68ce3865304baf226c39e741217c4b6
felipe <felipe>
2008-10-24 10:44:19 +0000

    - Fixed bug #46082 (stream_set_blocking() can cause a crash in some circumstances)

--- main/network.c
+++ main/network.c
@@ -17,7 +17,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: network.c,v 1.135 2008/08/21 23:37:40 pajoye Exp $ */
+/* $Id: network.c,v 1.136 2008/10/24 10:44:19 felipe Exp $ */
 
 /*#define DEBUG_MAIN_NETWORK 1*/
 
@@ -1057,7 +1057,11 @@ PHPAPI int php_set_sock_blocking(int socketd, int block TSRMLS_DC)
 	/* with ioctlsocket, a non-zero sets nonblocking, a zero sets blocking */
 	flags = !block;
 	if (ioctlsocket(socketd, FIONBIO, &flags) == SOCKET_ERROR) {
-		php_error_docref(NULL TSRMLS_CC, E_WARNING, "%s", WSAGetLastError());
+		char *error_string;
+		
+		error_string = php_socket_strerror(WSAGetLastError(), NULL, 0);
+		php_error_docref(NULL TSRMLS_CC, E_WARNING, "%s", error_string);
+		efree(error_string);
 		ret = FAILURE;
 	}
 #else

9066797281c8ef41554807ccdf72ede6897e7fa1
felipe <felipe>
2008-10-30 14:34:25 +0000

    - Fix the fix - Improved test

--- ext/standard/streamsfuncs.c
+++ ext/standard/streamsfuncs.c
@@ -17,7 +17,7 @@
   +----------------------------------------------------------------------+
 */
 
-/* $Id: streamsfuncs.c,v 1.58.2.6.2.15.2.25 2008/10/30 10:09:20 felipe Exp $ */
+/* $Id: streamsfuncs.c,v 1.58.2.6.2.15.2.26 2008/10/30 14:34:25 felipe Exp $ */
 
 #include "php.h"
 #include "php_globals.h"
@@ -411,7 +411,7 @@ PHP_FUNCTION(stream_get_contents)
 
 	php_stream_from_zval(stream, &zsrc);
 
-	if (pos >= 0 && php_stream_seek(stream, pos, SEEK_SET) < 0) {
+	if ((pos > 0 || (pos == 0 && ZEND_NUM_ARGS() > 2)) && php_stream_seek(stream, pos, SEEK_SET) < 0) {
 		php_error_docref(NULL TSRMLS_CC, E_WARNING, "Failed to seek to position %ld in the stream", pos);
 		RETURN_FALSE;
 	}

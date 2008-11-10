4d7b3696e7dd1d7b11e0683d0d2a1b9a133813c2
lbarnaud <lbarnaud>
2008-11-10 05:57:18 +0000

    MFH: Added the oldset parameter to pcntl_sigprocmask(). Already documented.

--- ext/pcntl/php_signal.h
+++ ext/pcntl/php_signal.h
@@ -16,11 +16,18 @@
    +----------------------------------------------------------------------+
 */
 
-/* $Id: php_signal.h,v 1.9.2.1.2.1.2.1 2007/12/31 07:17:11 sebastian Exp $ */
+/* $Id: php_signal.h,v 1.9.2.1.2.1.2.2 2008/11/10 05:57:18 lbarnaud Exp $ */
 
 #include <signal.h>
 #ifndef PHP_SIGNAL_H
 #define PHP_SIGNAL_H
+
+#ifndef NSIG
+# define NSIG 32
+#endif
+#ifndef SIGRTMAX
+# define SIGRTMAX 64
+#endif
 
 typedef void Sigfunc(int);
 Sigfunc *php_signal(int signo, Sigfunc *func, int restart);

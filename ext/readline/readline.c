050571396224d4f36d21e7bfd8553628b9064c44
felipe <felipe>
2008-11-20 22:25:37 +0000

    - MFH: Fixed memory leaks in readline_completion_function()

--- ext/readline/readline.c
+++ ext/readline/readline.c
@@ -16,7 +16,7 @@
    +----------------------------------------------------------------------+
 */
 
-/* $Id: readline.c,v 1.42.2.3.2.3.2.10 2008/11/17 11:27:58 felipe Exp $ */
+/* $Id: readline.c,v 1.42.2.3.2.3.2.11 2008/11/20 22:25:37 felipe Exp $ */
 
 /* {{{ includes & prototypes */
 
@@ -169,8 +169,10 @@ PHP_MINIT_FUNCTION(readline)
 
 PHP_RSHUTDOWN_FUNCTION(readline)
 {
-	if (_readline_completion) 
+	if (_readline_completion) {
+		zval_dtor(_readline_completion);
 		FREE_ZVAL(_readline_completion);
+	}
 #if HAVE_RL_CALLBACK_READ_CHAR
 	if (_prepped_callback) {
 		rl_callback_handler_remove();
@@ -491,8 +493,10 @@ PHP_FUNCTION(readline_completion_function)
 		efree(name);
 		RETURN_FALSE;
 	}
+	efree(name);
 
 	if (_readline_completion) {
+		zval_dtor(_readline_completion);
 		FREE_ZVAL(_readline_completion);
 	}
 

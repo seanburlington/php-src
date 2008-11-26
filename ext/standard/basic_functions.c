720023021eb74a3607a3a54e8624c2242354abc9
lbarnaud <lbarnaud>
2008-11-26 04:08:07 +0000

    Missed file from commit for #44181 & #44182

--- ext/standard/basic_functions.c
+++ ext/standard/basic_functions.c
@@ -17,7 +17,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: basic_functions.c,v 1.940 2008/11/24 19:24:09 stas Exp $ */
+/* $Id: basic_functions.c,v 1.941 2008/11/26 04:08:07 lbarnaud Exp $ */
 
 #include "php.h"
 #include "php_streams.h"
@@ -328,7 +328,7 @@ ZEND_BEGIN_ARG_INFO_EX(arginfo_array_search, 0, 0, 2)
 ZEND_END_ARG_INFO()
 
 ZEND_BEGIN_ARG_INFO_EX(arginfo_extract, 0, 0, 1)
-	ZEND_ARG_INFO(0, arg) /* ARRAY_INFO(0, arg, 0) */
+	ZEND_ARG_INFO(ZEND_SEND_PREFER_REF, arg) /* ARRAY_INFO(0, arg, 0) */
 	ZEND_ARG_INFO(0, extract_type)
 	ZEND_ARG_INFO(0, prefix)
 ZEND_END_ARG_INFO()

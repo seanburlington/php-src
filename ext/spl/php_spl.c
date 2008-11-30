f80aa90af713d43f790f4e44e3fd2e065bf0b1ae
bjori <bjori>
2008-11-30 00:12:29 +0000

    MFH: Fix proto

--- ext/spl/php_spl.c
+++ ext/spl/php_spl.c
@@ -16,7 +16,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: php_spl.c,v 1.52.2.28.2.17.2.31 2008/11/27 19:01:23 dmitry Exp $ */
+/* $Id: php_spl.c,v 1.52.2.28.2.17.2.32 2008/11/30 00:12:29 bjori Exp $ */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -85,7 +85,7 @@ static zend_class_entry * spl_find_ce_by_name(char *name, int len, zend_bool aut
 	return *ce;
 }
 
-/* {{{ proto array class_parents(object instance)
+/* {{{ proto array class_parents(object instance [, boolean autoload = true])
  Return an array containing the names of all parent classes */
 PHP_FUNCTION(class_parents)
 {

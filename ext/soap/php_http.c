85af11df8a43d3ecb37aa44e772a69dd0b142626
dmitry <dmitry>
2008-12-01 09:50:17 +0000

    Fixed bug #43069 (SoapClient causes 505 HTTP Version not supported error message)

--- ext/soap/php_http.c
+++ ext/soap/php_http.c
@@ -17,7 +17,7 @@
   |          Dmitry Stogov <dmitry@zend.com>                             |
   +----------------------------------------------------------------------+
 */
-/* $Id: php_http.c,v 1.115 2008/11/28 14:21:08 dmitry Exp $ */
+/* $Id: php_http.c,v 1.116 2008/12/01 09:50:17 dmitry Exp $ */
 
 #include "php_soap.h"
 #include "ext/standard/base64.h"
@@ -374,6 +374,16 @@ try_again:
 			client->url = NULL;
 		}
 		client->url = phpurl;
+
+		if (client->stream_context && 
+		    php_stream_context_get_option(client->stream_context, "http", "protocol_version", &tmp) == SUCCESS &&
+		    Z_TYPE_PP(tmp) == IS_DOUBLE &&
+		    Z_DVAL_PP(tmp) == 1.0) {
+			http_1_1 = 0;
+		} else {
+			http_1_1 = 1;
+		}
+		
 		smart_str_append_const(&soap_headers, "POST ");
 		if (use_proxy && !use_ssl) {
 			smart_str_appends(&soap_headers, phpurl->scheme);
@@ -395,19 +405,24 @@ try_again:
 			smart_str_appendc(&soap_headers, '#');
 			smart_str_appends(&soap_headers, phpurl->fragment);
 		}
-		smart_str_append_const(&soap_headers, " HTTP/1.1\r\n"
-			"Host: ");
+		if (http_1_1) {
+			smart_str_append_const(&soap_headers, " HTTP/1.1\r\n");
+		} else {
+			smart_str_append_const(&soap_headers, " HTTP/1.0\r\n");
+		}
+		smart_str_append_const(&soap_headers, "Host: ");
 		smart_str_appends(&soap_headers, phpurl->host);
 		if (phpurl->port != (use_ssl?443:80)) {
 			smart_str_appendc(&soap_headers, ':');
 			smart_str_append_unsigned(&soap_headers, phpurl->port);
 		}
-		smart_str_append_const(&soap_headers, "\r\n"
-			"Connection: Keep-Alive\r\n");
-/*
-			"Connection: close\r\n"
-			"Accept: text/html; text/xml; text/plain\r\n"
-*/
+		if (http_1_1) {
+			smart_str_append_const(&soap_headers, "\r\n"
+				"Connection: Keep-Alive\r\n");
+		} else {
+			smart_str_append_const(&soap_headers, "\r\n"
+				"Connection: close\r\n");
+		}
 		if (client->user_agent) {
 			if (client->user_agent[0] != 0) {
 				smart_str_append_const(&soap_headers, "User-Agent: ");

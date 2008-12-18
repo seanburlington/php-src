615963ef2c88ce430754c593867a54b7df91fec4
iliaa <iliaa>
2008-12-18 20:02:19 +0000

    Fixed bug #46887 (Invalid calls to php_error_docref())
    # Patch by oeriksson at mandriva dot com and crrodriguez at opensuse dot org

--- ext/xmlwriter/php_xmlwriter.c
+++ ext/xmlwriter/php_xmlwriter.c
@@ -17,7 +17,7 @@
   +----------------------------------------------------------------------+
 */
 
-/* $Id: php_xmlwriter.c,v 1.20.2.12.2.15.2.9 2008/11/17 11:28:00 felipe Exp $ */
+/* $Id: php_xmlwriter.c,v 1.20.2.12.2.15.2.10 2008/12/18 20:02:19 iliaa Exp $ */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -168,7 +168,7 @@ static zend_object_value xmlwriter_object_new(zend_class_entry *class_type TSRML
 
 #define XMLW_NAME_CHK(__err) \
 	if (xmlValidateName((xmlChar *) name, 0) != 0) {	\
-		php_error_docref(NULL TSRMLS_CC, E_WARNING, __err);	\
+		php_error_docref(NULL TSRMLS_CC, E_WARNING, "%s", __err);	\
 		RETURN_FALSE;	\
 	}	\
 	

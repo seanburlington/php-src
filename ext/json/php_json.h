e53774b8f2c212bb2e9cba3b56b2c58c2a3c9787
scottmac <scottmac>
2008-12-19 02:00:57 +0000

    MFH Add json_last_error() for getting a bit of information about what failed during a decode, also fixes a segfault when we have [1} [DOC]

--- ext/json/php_json.h
+++ ext/json/php_json.h
@@ -16,7 +16,7 @@
   +----------------------------------------------------------------------+
 */
 
-/* $Id: php_json.h,v 1.8.2.2.2.3 2008/07/22 14:57:46 jani Exp $ */
+/* $Id: php_json.h,v 1.8.2.2.2.4 2008/12/19 02:00:57 scottmac Exp $ */
 
 #ifndef PHP_JSON_H
 #define PHP_JSON_H
@@ -28,6 +28,16 @@ extern zend_module_entry json_module_entry;
 
 #ifdef ZTS
 #include "TSRM.h"
+#endif
+
+ZEND_BEGIN_MODULE_GLOBALS(json)
+	int error_code;
+ZEND_END_MODULE_GLOBALS(json)
+
+#ifdef ZTS
+# define JSON_G(v) TSRMG(json_globals_id, zend_json_globals *, v)
+#else
+# define JSON_G(v) (json_globals.v)
 #endif
 
 #endif  /* PHP_JSON_H */

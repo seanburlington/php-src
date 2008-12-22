e44dafa0a9d52aa86aa6b6d0763cce4d558acfe0
scottmac <scottmac>
2008-12-22 13:22:03 +0000

    MFH SQLite 3.6.7 plus a vc6 backport for LL

--- ext/sqlite3/libsqlite/sqlite3ext.h
+++ ext/sqlite3/libsqlite/sqlite3ext.h
@@ -15,7 +15,7 @@
 ** as extensions by SQLite should #include this file instead of 
 ** sqlite3.h.
 **
-** @(#) $Id: sqlite3ext.h,v 1.1.2.8 2008/11/27 08:46:13 scottmac Exp $
+** @(#) $Id: sqlite3ext.h,v 1.1.2.9 2008/12/22 13:22:25 scottmac Exp $
 */
 #ifndef _SQLITE3EXT_H_
 #define _SQLITE3EXT_H_

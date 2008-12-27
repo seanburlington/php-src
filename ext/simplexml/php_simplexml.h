0e7b0a32f2299b0c61c5658180a07aba9124c234
rrichards <rrichards>
2008-12-27 01:07:18 +0000

    force export on win32 to workaround win32 conig issues

--- ext/simplexml/php_simplexml.h
+++ ext/simplexml/php_simplexml.h
@@ -16,7 +16,7 @@
   +----------------------------------------------------------------------+
 */
 
-/* $Id: php_simplexml.h,v 1.31 2008/12/26 20:27:14 rrichards Exp $ */
+/* $Id: php_simplexml.h,v 1.32 2008/12/27 01:07:18 rrichards Exp $ */
 
 #ifndef PHP_SIMPLEXML_H
 #define PHP_SIMPLEXML_H
@@ -77,17 +77,7 @@ typedef struct {
 #define SIMPLEXML_G(v) (simplexml_globals.v)
 #endif
 
-#ifdef PHP_WIN32
-#ifdef SIMPLEXML_EXPORTS
-#define PHP_SXE_API __declspec(dllexport)
-#else
-#define PHP_SXE_API __declspec(dllimport)
-#endif /* SIMPLEXML_EXPORTS */
-#elif defined(__GNUC__) && __GNUC__ >= 4
-#define PHP_SXE_API __attribute__ ((visibility("default")))
-#else
-#define PHP_SXE_API
-#endif /* PHP_WIN32 */
+#define PHP_SXE_API ZEND_DLEXPORT
 
 PHP_SXE_API zend_class_entry *sxe_get_element_class_entry(TSRMLS_D);
 

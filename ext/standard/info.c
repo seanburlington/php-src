8cab8d0b48e79388a21a123f3a21f832db1fcf5b
helly <helly>
2008-12-31 13:48:59 +0000

    - Fix info output

--- ext/standard/info.c
+++ ext/standard/info.c
@@ -18,7 +18,7 @@
    +----------------------------------------------------------------------+
 */
 
-/* $Id: info.c,v 1.300 2008/12/31 12:52:28 helly Exp $ */
+/* $Id: info.c,v 1.301 2008/12/31 13:48:59 helly Exp $ */
 
 #include "php.h"
 #include "php_ini.h"
@@ -867,7 +867,7 @@ PHPAPI void php_print_info(int flag TSRMLS_DC)
 
 		SECTION("Additional Modules");
 		php_info_print_table_start();
-		php_info_print_table_header(2, "Module Name");
+		php_info_print_table_header(1, "Module Name");
 		zend_hash_apply(&sorted_registry, (apply_func_t) _display_module_info_def TSRMLS_CC);
 		php_info_print_table_end();
 

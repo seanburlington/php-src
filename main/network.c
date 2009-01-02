40f732058699e0ba0c9aa7d8018f3c2c5ab7d953
felipe <felipe>
2009-01-02 21:43:07 +0000

    - MFH: Errr, rule nº 1: be portable :)

--- main/network.c
+++ main/network.c
@@ -17,7 +17,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: network.c,v 1.118.2.2.2.14 2009/01/02 21:29:04 felipe Exp $ */
+/* $Id: network.c,v 1.118.2.2.2.15 2009/01/02 21:43:07 felipe Exp $ */
 
 /*#define DEBUG_MAIN_NETWORK 1*/
 
@@ -1141,7 +1141,9 @@ PHPAPI int php_poll2(php_pollfd *ufds, unsigned int nfds, int timeout)
 		tv.tv_sec = timeout / 1000;
 		tv.tv_usec = (timeout - (tv.tv_sec * 1000)) * 1000;
 	}
+#ifndef PHP_WIN32
 	errno = 0;
+#endif
 	n = select(max_fd + 1, &rset, &wset, &eset, timeout >= 0 ? &tv : NULL);
 
 	if (n >= 0) {

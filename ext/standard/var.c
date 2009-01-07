d18756751f39b89201746dc8ba1e01611c1625ee
derick <derick>
2009-01-07 14:36:49 +0000

    - MFH: Fixed bug #47027 (var_export doesn't show numeric indices on ArrayObject).

--- ext/standard/var.c
+++ ext/standard/var.c
@@ -18,7 +18,7 @@
    +----------------------------------------------------------------------+
 */
 
-/* $Id: var.c,v 1.203.2.7.2.24 2008/12/31 11:17:46 sebastian Exp $ */
+/* $Id: var.c,v 1.203.2.7.2.25 2009/01/07 14:36:49 derick Exp $ */
 
 
 
@@ -378,13 +378,15 @@ static int php_object_element_export(zval **zv, int num_args, va_list args, zend
 
 	level = va_arg(args, int);
 
+	php_printf("%*c", level + 1, ' ');
 	if (hash_key->nKeyLength != 0) {
-		php_printf("%*c", level + 1, ' ');
-		zend_unmangle_property_name(hash_key->arKey, hash_key->nKeyLength-1, &class_name, &prop_name);
+		zend_unmangle_property_name(hash_key->arKey, hash_key->nKeyLength - 1, &class_name, &prop_name);
 		php_printf(" '%s' => ", prop_name);
-		php_var_export(zv, level + 2 TSRMLS_CC);
-		PUTS (",\n");
+	} else {
+		php_printf(" %ld => ", hash_key->h);
 	}
+	php_var_export(zv, level + 2 TSRMLS_CC);
+	PUTS (",\n");
 	return 0;
 }
 

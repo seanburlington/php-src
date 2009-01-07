c0e688c8e5063a09936fc81d5d2896f8133fde50
felipe <felipe>
2009-01-07 20:21:46 +0000

    MFH: - Added check for EWOULDBLOCK - Using EWOULDBLOCK instead of EAGAIN by portability issues (Related to #46917)

--- main/php_network.h
+++ main/php_network.h
@@ -16,7 +16,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: php_network.h,v 1.56.2.1.2.1.2.4 2008/12/31 11:15:47 sebastian Exp $ */
+/* $Id: php_network.h,v 1.56.2.1.2.1.2.5 2009/01/07 20:21:46 felipe Exp $ */
 
 #ifndef _PHP_NETWORK_H
 #define _PHP_NETWORK_H
@@ -39,6 +39,10 @@
 #	define fsync _commit
 #	define ftruncate(a, b) chsize(a, b)
 #endif /* defined(PHP_WIN32) */
+
+#ifndef EWOULDBLOCK
+# define EWOULDBLOCK EAGAIN
+#endif
 
 #ifdef PHP_WIN32
 #define php_socket_errno() WSAGetLastError()

8d362b9b956c90eed87755dd6db33505f3fa4576
lbarnaud <lbarnaud>
2009-01-08 18:40:27 +0000

    MFH: Fix memleak when a user filter appends a bucket and returns != PSFS_PASS_ON Improved tests

--- ext/standard/user_filters.c
+++ ext/standard/user_filters.c
@@ -18,7 +18,7 @@
    +----------------------------------------------------------------------+
 */
 
-/* $Id: user_filters.c,v 1.31.2.4.2.9.2.10 2008/12/31 11:15:46 sebastian Exp $ */
+/* $Id: user_filters.c,v 1.31.2.4.2.9.2.11 2009/01/08 18:40:27 lbarnaud Exp $ */
 
 #include "php.h"
 #include "php_globals.h"
@@ -243,6 +243,14 @@ php_stream_filter_status_t userfilter_filter(
 			/* Remove unconsumed buckets from the brigade */
 			php_stream_bucket_unlink(bucket TSRMLS_CC);
 			php_stream_bucket_delref(bucket TSRMLS_CC);
+		}
+	}
+	if (ret != PSFS_PASS_ON) {
+		php_stream_bucket *bucket = buckets_out->head;
+		while (bucket != NULL) {
+			php_stream_bucket_unlink(bucket TSRMLS_CC);
+			php_stream_bucket_delref(bucket TSRMLS_CC);
+			bucket = buckets_out->head;
 		}
 	}
 

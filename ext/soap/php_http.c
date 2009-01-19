2493f835c8173f90943f594e3b7ec488fe19c599
iliaa <iliaa>
2009-01-19 21:58:02 +0000

    MFB: Fixed bug #47131 (SOAP Extension ignores "user_agent" ini setting).

--- ext/soap/php_http.c
+++ ext/soap/php_http.c
@@ -17,7 +17,7 @@
   |          Dmitry Stogov <dmitry@zend.com>                             |
   +----------------------------------------------------------------------+
 */
-/* $Id: php_http.c,v 1.77.2.11.2.12.2.9 2008/12/31 11:15:43 sebastian Exp $ */
+/* $Id: php_http.c,v 1.77.2.11.2.12.2.10 2009/01/19 21:58:02 iliaa Exp $ */
 
 #include "php_soap.h"
 #include "ext/standard/base64.h"
@@ -436,7 +436,11 @@ try_again:
 				smart_str_appendl(&soap_headers, Z_STRVAL_PP(tmp), Z_STRLEN_PP(tmp));
 				smart_str_append_const(&soap_headers, "\r\n");
 			}
-		} else{
+		} else if (FG(user_agent)) {
+			smart_str_append_const(&soap_headers, "User-Agent: ");
+			smart_str_appends(&soap_headers, FG(user_agent));
+			smart_str_append_const(&soap_headers, "\r\n");
+		} else {
 			smart_str_append_const(&soap_headers, "User-Agent: PHP-SOAP/"PHP_VERSION"\r\n");
 		}
 

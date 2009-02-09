b423aa7e5bd67cd15a5f2c51a70323184a793163
iliaa <iliaa>
2009-02-09 15:34:01 +0000

    Initialize optional var

--- ext/mbstring/mbstring.c
+++ ext/mbstring/mbstring.c
@@ -17,7 +17,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: mbstring.c,v 1.224.2.22.2.25.2.45 2009/01/29 22:59:57 scottmac Exp $ */
+/* $Id: mbstring.c,v 1.224.2.22.2.25.2.46 2009/02/09 15:34:01 iliaa Exp $ */
 
 /*
  * PHP 4 Multibyte String module "mbstring"
@@ -2034,7 +2034,7 @@ PHP_FUNCTION(mb_preferred_mime_name)
    Parses GET/POST/COOKIE data and sets global variables */
 PHP_FUNCTION(mb_parse_str)
 {
-	zval *track_vars_array;
+	zval *track_vars_array = NULL;
 	char *encstr = NULL;
 	int encstr_len;
 	php_mb_encoding_handler_info_t info;

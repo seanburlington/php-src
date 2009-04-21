b77eb93c2cdee5b1555678622bc9d7eefdf1a419
iliaa <iliaa>
2009-04-21 12:26:33 +0000

    MFB: Adjusted prototype

--- ext/ldap/ldap.c
+++ ext/ldap/ldap.c
@@ -23,7 +23,7 @@
    +----------------------------------------------------------------------+
  */
  
-/* $Id: ldap.c,v 1.196 2009/03/29 18:44:45 venaas Exp $ */
+/* $Id: ldap.c,v 1.197 2009/04/21 12:26:33 iliaa Exp $ */
 #define IS_EXT_MODULE
 
 #ifdef HAVE_CONFIG_H
@@ -235,7 +235,7 @@ PHP_MINFO_FUNCTION(ldap)
 
 	php_info_print_table_start();
 	php_info_print_table_row(2, "LDAP Support", "enabled");
-	php_info_print_table_row(2, "RCS Version", "$Id: ldap.c,v 1.196 2009/03/29 18:44:45 venaas Exp $");
+	php_info_print_table_row(2, "RCS Version", "$Id: ldap.c,v 1.197 2009/04/21 12:26:33 iliaa Exp $");
 
 	if (LDAPG(max_links) == -1) {
 		snprintf(tmp, 31, "%ld/unlimited", LDAPG(num_links));
@@ -286,7 +286,7 @@ PHP_MINFO_FUNCTION(ldap)
 }
 /* }}} */
 
-/* {{{ proto resource ldap_connect([string host [, int port]])
+/* {{{ proto resource ldap_connect([string host [, int port [, string wallet [, string wallet_passwd [, int authmode]]]]])
    Connect to an LDAP server */
 PHP_FUNCTION(ldap_connect)
 {

39d08f3f84cdcc75be8474ae25162a48e20ce918
pajoye <pajoye>
2009-04-23 22:25:13 +0000

    - MFH: #47940,  leaks in imap_body()

--- ext/imap/php_imap.c
+++ ext/imap/php_imap.c
@@ -26,7 +26,7 @@
    | PHP 4.0 updates:  Zeev Suraski <zeev@zend.com>                       |
    +----------------------------------------------------------------------+
  */
-/* $Id: php_imap.c,v 1.208.2.7.2.26.2.31 2009/01/08 16:13:51 iliaa Exp $ */
+/* $Id: php_imap.c,v 1.208.2.7.2.26.2.32 2009/04/23 22:25:13 pajoye Exp $ */
 
 #define IMAP41
 
@@ -1575,6 +1575,8 @@ PHP_FUNCTION(imap_body)
 	long msgno, flags = 0;
 	pils *imap_le_struct; 
 	int msgindex, argc = ZEND_NUM_ARGS();
+	char *body;
+	unsigned long body_len = 0;
 
 	if (zend_parse_parameters(argc TSRMLS_CC, "rl|l", &streamind, &msgno, &flags) == FAILURE) {
 		return;
@@ -1600,7 +1602,13 @@ PHP_FUNCTION(imap_body)
 		RETURN_FALSE;
 	}
 
-	RETVAL_STRING(mail_fetchtext_full(imap_le_struct->imap_stream, msgno, NIL, (argc == 3 ? flags : NIL)), 1);
+	body = mail_fetchtext_full (imap_le_struct->imap_stream, msgno, &body_len, (argc == 3 ? flags : NIL));
+	if (body_len == 0) {
+		RETVAL_EMPTY_STRING();
+	} else {
+		RETVAL_STRINGL(body, body_len, 1);
+	}
+	free(body);
 }
 /* }}} */
 

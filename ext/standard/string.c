b75629528add427282f4f4b1d457e212571df8d9
lbarnaud <lbarnaud>
2009-04-24 21:23:47 +0000

    strip_tags() fixes: - MFH5.3 (Fix bug when < is used within attribute.) - Fix handling of case when searching for allowed tags in unicode variant - tests for both unicode and binary variants

--- ext/standard/string.c
+++ ext/standard/string.c
@@ -18,7 +18,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: string.c,v 1.697 2009/04/01 17:04:16 mattwil Exp $ */
+/* $Id: string.c,v 1.698 2009/04/24 21:23:47 lbarnaud Exp $ */
 
 /* Synced with php 3.0 revision 1.193 1999-06-16 [ssb] */
 
@@ -6253,7 +6253,8 @@ int php_u_tag_find(UChar *tag, int len, UChar *set, int set_len)
 
 	while (!done) {
 		U16_NEXT(tag, idx, len, ch);
-		switch (u_tolower(ch)) {
+		ch = u_tolower(ch);
+		switch (ch) {
 		case '<':
 			*(n++) = ch;
 			break;
@@ -6403,6 +6404,9 @@ PHPAPI int php_u_strip_tags(UChar *rbuf, int len, int *stateptr, UChar *allow, i
 			break;
 
 		case 0x3C: /* '<' */
+			if (in_q) {
+				break;
+			}
 			U16_GET(buf, 0, idx, len, next);
 			if (u_isWhitespace(next) == TRUE) {
 				goto reg_u_char;

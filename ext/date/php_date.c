cfc4c9be7748bf64a96637253aa71dbe7d847160
pajoye <pajoye>
2009-05-04 17:11:22 +0000

    - cleaner fix

--- ext/date/php_date.c
+++ ext/date/php_date.c
@@ -16,7 +16,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: php_date.c,v 1.43.2.45.2.51.2.72 2009/05/04 16:28:50 pajoye Exp $ */
+/* $Id: php_date.c,v 1.43.2.45.2.51.2.73 2009/05/04 17:11:22 pajoye Exp $ */
 
 #include "php.h"
 #include "php_streams.h"
@@ -33,7 +33,6 @@
 
 #ifdef PHP_WIN32
 # include "win32/php_stdint.h"
-static inline int64_t llabs( int64_t i ) { return i >= 0? i: -i; }
 #endif
 
 /* {{{ arginfo */

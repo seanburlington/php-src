446ad4540f1d7bc0adaeb3aeb6d2448e04e37676
iliaa <iliaa>
2009-05-14 12:36:19 +0000

    MFB: Fixed\ compiler\ warnings

--- ext/xmlrpc/xmlrpc-epi-php.c
+++ ext/xmlrpc/xmlrpc-epi-php.c
@@ -51,7 +51,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: xmlrpc-epi-php.c,v 1.39.2.5.2.15 2009/05/13 14:25:58 iliaa Exp $ */
+/* $Id: xmlrpc-epi-php.c,v 1.39.2.5.2.16 2009/05/14 12:36:19 iliaa Exp $ */
 
 /**********************************************************************
 * BUGS:                                                               *
@@ -67,6 +67,7 @@
 
 #include "php.h"
 #include "ext/standard/info.h"
+#include "ext/date/php_date.h"
 #include "php_ini.h"
 #include "php_xmlrpc.h"
 #include "xmlrpc.h"
@@ -1300,7 +1301,7 @@ int set_zval_xmlrpc_type(zval* value, XMLRPC_VALUE_TYPE newtype)
 			if(newtype == xmlrpc_datetime) {
 				XMLRPC_VALUE v = XMLRPC_CreateValueDateTime_ISO8601(NULL, value->value.str.val);
 				if(v) {
-					time_t timestamp = (time_t) php_parse_date(XMLRPC_GetValueDateTime_ISO8601(v), NULL);
+					time_t timestamp = (time_t) php_parse_date((char *)XMLRPC_GetValueDateTime_ISO8601(v), NULL);
 					if(timestamp) {
 						zval* ztimestamp;
 

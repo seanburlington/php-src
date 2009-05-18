fda0c01b094be70b32acdad598b89323644b3567
jani <jani>
2009-05-18 17:23:42 +0000

    MFH: Sync WS / CS changes where applicable

--- ext/session/php_session.h
+++ ext/session/php_session.h
@@ -16,7 +16,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: php_session.h,v 1.101.2.2.2.8 2009/04/26 01:20:56 jani Exp $ */
+/* $Id: php_session.h,v 1.101.2.2.2.9 2009/05/18 17:23:42 jani Exp $ */
 
 #ifndef PHP_SESSION_H
 #define PHP_SESSION_H
@@ -202,18 +202,18 @@ PHPAPI const ps_serializer *_php_find_ps_serializer(char *name TSRMLS_DC);
 	zval **struc;
 
 #define PS_ENCODE_LOOP(code) do {									\
-		HashTable *_ht = Z_ARRVAL_P(PS(http_session_vars)); \
-		int key_type;						\
+		HashTable *_ht = Z_ARRVAL_P(PS(http_session_vars));			\
+		int key_type;												\
 																	\
-		for (zend_hash_internal_pointer_reset(_ht);			\
+		for (zend_hash_internal_pointer_reset(_ht);					\
 				(key_type = zend_hash_get_current_key_ex(_ht, &key, &key_length, &num_key, 0, NULL)) != HASH_KEY_NON_EXISTANT; \
-				zend_hash_move_forward(_ht)) {				\
-			if (key_type == HASH_KEY_IS_LONG) {                                             \
-				php_error_docref(NULL TSRMLS_CC, E_NOTICE, "Skipping numeric key %ld", num_key); \
-				continue;                                                               \
-			}										\
-			key_length--;										\
-			if (php_get_session_var(key, key_length, &struc TSRMLS_CC) == SUCCESS) { \
+					zend_hash_move_forward(_ht)) {					\
+			if (key_type == HASH_KEY_IS_LONG) {						\
+				php_error_docref(NULL TSRMLS_CC, E_NOTICE, "Skipping numeric key %ld", num_key);	\
+				continue;											\
+			}														\
+			key_length--;											\
+			if (php_get_session_var(key, key_length, &struc TSRMLS_CC) == SUCCESS) {	\
 				code;		 										\
 			} 														\
 		}															\

ee3c3f7d05575a6e1d58d5e47aa66e3c44efb7f3
jani <jani>
2009-06-04 11:53:38 +0000

    MFH:- Fixed bug #48419 (non-portable STDIN fileno in cgi_main.c) # This doesn't really fix it since it's some weird Windows thing but this # does make this code slightly more portable :)

--- sapi/cgi/cgi_main.c
+++ sapi/cgi/cgi_main.c
@@ -21,7 +21,7 @@
    +----------------------------------------------------------------------+
 */
 
-/* $Id: cgi_main.c,v 1.267.2.15.2.69 2009/01/19 18:17:59 dsp Exp $ */
+/* $Id: cgi_main.c,v 1.267.2.15.2.70 2009/06/04 11:53:38 jani Exp $ */
 
 #include "php.h"
 #include "php_globals.h"
@@ -461,6 +461,9 @@ static int sapi_cgi_send_headers(sapi_headers_struct *sapi_headers TSRMLS_DC)
 	return SAPI_HEADER_SENT_SUCCESSFULLY;
 }
 
+#ifndef STDIN_FILENO
+# define STDIN_FILENO 0
+#endif
 
 static int sapi_cgi_read_post(char *buffer, uint count_bytes TSRMLS_DC)
 {
@@ -473,10 +476,10 @@ static int sapi_cgi_read_post(char *buffer, uint count_bytes TSRMLS_DC)
 			fcgi_request *request = (fcgi_request*) SG(server_context);
 			tmp_read_bytes = fcgi_read(request, buffer + read_bytes, count_bytes - read_bytes);
 		} else {
-			tmp_read_bytes = read(0, buffer + read_bytes, count_bytes - read_bytes);
+			tmp_read_bytes = read(STDIN_FILENO, buffer + read_bytes, count_bytes - read_bytes);
 		}
 #else
-		tmp_read_bytes = read(0, buffer + read_bytes, count_bytes - read_bytes);
+		tmp_read_bytes = read(STDIN_FILENO, buffer + read_bytes, count_bytes - read_bytes);
 #endif
 
 		if (tmp_read_bytes <= 0) {

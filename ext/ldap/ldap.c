a6a2d5bdfc40378bd4afaf9160d59d4f26d71c1f
patrickallaert <patrickallaert>
2009-06-07 21:27:42 +0000

    MFH: Removed credit

--- ext/ldap/ldap.c
+++ ext/ldap/ldap.c
@@ -19,12 +19,11 @@
    |          Jani Taskinen  <sniper@iki.fi>                              |
    |          Stig Venaas    <venaas@uninett.no>                          |
    |          Doug Goldstein <cardoe@cardoe.com>                          |
-   |          Patrick Allaert <patrickallaert@php.net>                    |
    | PHP 4.0 updates:  Zeev Suraski <zeev@zend.com>                       |
    +----------------------------------------------------------------------+
  */
  
-/* $Id: ldap.c,v 1.161.2.3.2.15 2009/06/07 13:14:30 patrickallaert Exp $ */
+/* $Id: ldap.c,v 1.161.2.3.2.16 2009/06/07 21:27:42 patrickallaert Exp $ */
 #define IS_EXT_MODULE
 
 #ifdef HAVE_CONFIG_H
@@ -323,7 +322,7 @@ PHP_MINFO_FUNCTION(ldap)
 
 	php_info_print_table_start();
 	php_info_print_table_row(2, "LDAP Support", "enabled");
-	php_info_print_table_row(2, "RCS Version", "$Id: ldap.c,v 1.161.2.3.2.15 2009/06/07 13:14:30 patrickallaert Exp $");
+	php_info_print_table_row(2, "RCS Version", "$Id: ldap.c,v 1.161.2.3.2.16 2009/06/07 21:27:42 patrickallaert Exp $");
 
 	if (LDAPG(max_links) == -1) {
 		snprintf(tmp, 31, "%ld/unlimited", LDAPG(num_links));

55b339fc60343010b269868952d934456a2b1f3f
patrickallaert <patrickallaert>
2009-06-07 13:06:03 +0000

    Fixed bug #48469 (ldap_get_entries() memory leaks on empty search results)

--- ext/ldap/ldap.c
+++ ext/ldap/ldap.c
@@ -19,11 +19,12 @@
    |          Jani Taskinen  <sniper@iki.fi>                              |
    |          Stig Venaas    <venaas@uninett.no>                          |
    |          Doug Goldstein <cardoe@cardoe.com>                          |
+   |          Patrick Allaert <patrickallaert@php.net>                    |
    | PHP 4.0 updates:  Zeev Suraski <zeev@zend.com>                       |
    +----------------------------------------------------------------------+
  */
  
-/* $Id: ldap.c,v 1.198 2009/04/21 18:07:42 bjori Exp $ */
+/* $Id: ldap.c,v 1.199 2009/06/07 13:06:03 patrickallaert Exp $ */
 #define IS_EXT_MODULE
 
 #ifdef HAVE_CONFIG_H
@@ -235,7 +236,7 @@ PHP_MINFO_FUNCTION(ldap)
 
 	php_info_print_table_start();
 	php_info_print_table_row(2, "LDAP Support", "enabled");
-	php_info_print_table_row(2, "RCS Version", "$Id: ldap.c,v 1.198 2009/04/21 18:07:42 bjori Exp $");
+	php_info_print_table_row(2, "RCS Version", "$Id: ldap.c,v 1.199 2009/06/07 13:06:03 patrickallaert Exp $");
 
 	if (LDAPG(max_links) == -1) {
 		snprintf(tmp, 31, "%ld/unlimited", LDAPG(num_links));
@@ -972,9 +973,6 @@ PHP_FUNCTION(ldap_get_entries)
 	ldap = ld->link;
 	num_entries = ldap_count_entries(ldap, ldap_result);
 
-	array_init(return_value);
-	add_assoc_long(return_value, "count", num_entries);
-
 	if (num_entries == 0) {
 		RETURN_NULL();
 	}
@@ -984,6 +982,9 @@ PHP_FUNCTION(ldap_get_entries)
 	if (ldap_result_entry == NULL) {
 		RETURN_FALSE;
 	}
+
+	array_init(return_value);
+	add_assoc_long(return_value, "count", num_entries);
 
 	while (ldap_result_entry != NULL) {
 

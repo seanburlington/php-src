585d3b8a54f6ba988857ef55e4fdbb2a5c9b9da9
felipe <felipe>
2009-06-10 19:00:54 +0000

    - Fix the fix (Bug #46973 - IPv6 address filter rejects valid address)

--- ext/filter/logical_filters.c
+++ ext/filter/logical_filters.c
@@ -17,7 +17,7 @@
   +----------------------------------------------------------------------+
 */
 
-/* $Id: logical_filters.c,v 1.38 2009/03/31 11:20:31 mattwil Exp $ */
+/* $Id: logical_filters.c,v 1.39 2009/06/10 19:00:54 felipe Exp $ */
 
 #include "php_filter.h"
 #include "filter_private.h"
@@ -574,7 +574,7 @@ static int _php_filter_validate_ipv6(char *str, int str_len TSRMLS_DC) /* {{{ */
 			}
 			if (*str == ':') {
 				if (compressed || --blocks == 0) {
-					return ipv4 != NULL;
+					return 0;
 				}			
 				if (++str == end || (ipv4 && ipv4 == str)) {
 					return 1;

a7b7b7bc4f97b040d077dadc38b75b6e67440eeb
andrey <andrey>
2009-06-11 08:51:20 +0000

    Protect the code when variable is NULL, which should never happen, but anyway, you know about "never happens"

--- ext/mysqlnd/mysqlnd_block_alloc.c
+++ ext/mysqlnd/mysqlnd_block_alloc.c
@@ -18,7 +18,7 @@
   +----------------------------------------------------------------------+
 */
 
-/* $Id: mysqlnd_block_alloc.c,v 1.8 2008/12/31 11:12:33 sebastian Exp $ */
+/* $Id: mysqlnd_block_alloc.c,v 1.9 2009/06/11 08:51:20 andrey Exp $ */
 
 #include "php.h"
 #include "mysqlnd.h"
@@ -183,10 +183,12 @@ void
 mysqlnd_mempool_destroy(MYSQLND_MEMORY_POOL * pool TSRMLS_DC)
 {
 	DBG_ENTER("mysqlnd_mempool_destroy");
-	/* mnd_free will reference LOCK_access and might crash, depending on the caller...*/
-	mysqlnd_mempool_free_contents(pool TSRMLS_CC);
-	mnd_free(pool->arena);
-	mnd_free(pool);
+	if (pool) {
+		/* mnd_free will reference LOCK_access and might crash, depending on the caller...*/
+		mysqlnd_mempool_free_contents(pool TSRMLS_CC);
+		mnd_free(pool->arena);
+		mnd_free(pool);
+	}
 	DBG_VOID_RETURN;
 }
 /* }}} */
